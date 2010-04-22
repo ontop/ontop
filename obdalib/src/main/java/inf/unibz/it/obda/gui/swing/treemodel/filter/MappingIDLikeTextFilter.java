bf30c27e2bce365cc25522d7bcc2a7f9931db5bf
mruzzi <mruzzi@example.org>
2010-04-22 15:27:44 +0000

    Added the first version of mapping filters

---
+++ obdalib/src/main/java/inf/unibz/it/obda/gui/swing/treemodel/filter/MappingIDLikeTextFilter.java
@@ -0,0 +1,59 @@
+package inf.unibz.it.obda.gui.swing.treemodel.filter;
+
+import java.net.URI;
+
+import inf.unibz.it.dl.domain.NamedConcept;
+import inf.unibz.it.obda.domain.OBDAMappingAxiom;
+import inf.unibz.it.obda.rdbmsgav.domain.RDBMSOBDAMappingAxiom;
+import inf.unibz.it.obda.rdbmsgav.domain.RDBMSSQLQuery;
+import inf.unibz.it.ucq.domain.ConceptQueryAtom;
+import inf.unibz.it.ucq.domain.ConjunctiveQuery;
+import inf.unibz.it.ucq.domain.VariableTerm;
+import inf.unibz.it.ucq.parser.exception.QueryParseException;
+
+/**
+ * A filter that receives a String str during construction and returns
+ * a match if the input mapping contains str in the id field.
+ * 
+ * @author mariano
+ *
+ */
+public class MappingIDLikeTextFilter implements TreeModelFilter<OBDAMappingAxiom> {
+
+	String str = "";
+	
+	public MappingIDLikeTextFilter(String str) {
+		this.str = str;
+	}
+	
+	/* (non-Javadoc)
+	 * @see inf.unibz.it.obda.gui.swing.treemodel.filter.TreeModelFilter#match(java.lang.Object)
+	 */
+	@Override
+	public boolean match(OBDAMappingAxiom object) {
+		if (object.getId().indexOf(str) != -1) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static void main(String args[]) {
+		try {
+			RDBMSOBDAMappingAxiom m = new RDBMSOBDAMappingAxiom("test id");
+			ConjunctiveQuery t = new ConjunctiveQuery();
+			t.addQueryAtom(new ConceptQueryAtom(new NamedConcept(URI.create("test")), new VariableTerm("x")));
+			m.setTargetQuery(t);
+			RDBMSSQLQuery q = new RDBMSSQLQuery();
+			q.setInputQuery("SELECT ..", null);
+			m.setSourceQuery(q);
+			
+			MappingIDLikeTextFilter filter = new MappingIDLikeTextFilter("id2");
+			System.out.println(filter.match(m));
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ obdalib/src/main/java/inf/unibz/it/obda/gui/swing/treemodel/filter/TreeModelFilter.java
@@ -0,0 +1,24 @@
+package inf.unibz.it.obda.gui.swing.treemodel.filter;
+
+/**
+ * An abstract filter that can be used by TreeModels to discriminate certain
+ * elements stored in the controller associated to the tree model.
+ * 
+ * @param <T>
+ *            T is the class name of the kind of object that is taken as input
+ *            by an implementation of TreeModelFilter.
+ */
+public interface TreeModelFilter<T extends Object> {
+
+	/**
+	 * A matching function that returns true if the object complies to the
+	 * internal logic of the Filter. Instances of TreeModelFilter should define
+	 * their own logic.
+	 * 
+	 * @param object
+	 *            The object that needs to be matches
+	 * @return true if the object matches the internal logic, false otherwise
+	 */
+	public boolean match(T object);
+
+}
