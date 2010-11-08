ba76bb319f078176c719c1c12443f8dd18c6ae51
Josef Hardi <josef.hardi@gmail.com>
2010-11-08 09:06:52 +0000

    Adds method saveDocumentToXMLFile(Document, File).

--- obdalib/obdalib-core/src/main/java/inf/unibz/it/utils/xml/XMLUtils.java
+++ obdalib/obdalib-core/src/main/java/inf/unibz/it/utils/xml/XMLUtils.java
@@ -5,15 +5,16 @@
  * The OBDA-API is licensed under the terms of the Lesser General Public
  * License v.3 (see OBDAAPI_LICENSE.txt for details). The components of this
  * work include:
- * 
- * a) The OBDA-API developed by the author and licensed under the LGPL; and, 
- * b) third-party components licensed under terms that may be different from 
- *   those of the LGPL.  Information about such licenses can be found in the 
+ *
+ * a) The OBDA-API developed by the author and licensed under the LGPL; and,
+ * b) third-party components licensed under terms that may be different from
+ *   those of the LGPL.  Information about such licenses can be found in the
  *   file named OBDAAPI_3DPARTY-LICENSES.txt.
  */
 package inf.unibz.it.utils.xml;
 
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -25,13 +26,18 @@ import com.sun.org.apache.xml.internal.serialize.OutputFormat;
 import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 
 public class XMLUtils {
-	
+
 	private XMLUtils() {
-		
+
 	}
-	
+
 	public static void saveDocumentToXMLFile(Document doc, String filename) throws FileNotFoundException, IOException {
-		FileOutputStream fos = new FileOutputStream(filename);
+		File file = new File(filename);
+		saveDocumentToXMLFile(doc, file);
+	}
+
+	public static void saveDocumentToXMLFile(Document doc, File file) throws FileNotFoundException, IOException {
+		FileOutputStream fos = new FileOutputStream(file);
 		// XERCES 1 or 2 additionnal classes.
 		OutputFormat of = new OutputFormat("XML","ISO-8859-1",true);
 		of.setIndent(2);
@@ -44,6 +50,6 @@ public class XMLUtils {
 		serializer.serialize( docroot );
 		fos.close();
 	}
-	
-	
+
+
 }
