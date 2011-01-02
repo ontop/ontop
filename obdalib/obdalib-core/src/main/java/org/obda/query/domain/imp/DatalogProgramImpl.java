0e5f2c6f38dcc95cee4fae90e315af41569dd67f
Mariano Rodriguez-Muro <rodriguez@inf.unibz.it>
2011-01-02 13:14:46 +0000

    Added indexing to the DatalogProgram implementation and interfaces.

--- obdalib/obdalib-core/src/main/java/org/obda/query/domain/imp/DatalogProgramImpl.java
+++ obdalib/obdalib-core/src/main/java/org/obda/query/domain/imp/DatalogProgramImpl.java
@@ -11,76 +11,83 @@ import org.obda.query.domain.CQIE;
 import org.obda.query.domain.DatalogProgram;
 import org.obda.query.domain.Predicate;
 
-public class DatalogProgramImpl implements DatalogProgram{
+public class DatalogProgramImpl implements DatalogProgram {
 
-	private List<CQIE> rules = null;
-	
-	private Map<Predicate, List<CQIE>> predicateIndex = null;
-	
-	public DatalogProgramImpl(){
+	private List<CQIE>					rules			= null;
+
+	private Map<Predicate, List<CQIE>>	predicateIndex	= null;
+
+	public DatalogProgramImpl() {
 		rules = new Vector<CQIE>();
 		predicateIndex = new HashMap<Predicate, List<CQIE>>();
 	}
-	
-	public void appendRule(CQIE rule){
+
+	public void appendRule(CQIE rule) {
+		if (rule == null) {
+			throw new IllegalArgumentException("DatalogProgram: Recieved a null rule.");
+		}
 		rules.add(rule);
-		
-		Predicate predicate = rule.getHead().getPredicate();
-		List<CQIE> indexedRules = this.getRules(predicate);
-		indexedRules.add(rule);
+
+		Atom head = rule.getHead();
+		if (head != null) {
+			Predicate predicate = rule.getHead().getPredicate();
+			List<CQIE> indexedRules = this.getRules(predicate);
+			indexedRules.add(rule);
+		}
 	}
-	
-	public void appendRule(List<CQIE> rules){
-		for (CQIE rule: rules) {
+
+	public void appendRule(List<CQIE> rules) {
+		for (CQIE rule : rules) {
 			appendRule(rule);
 		}
-		 
+
 	}
-	
-	public void removeRule(CQIE rule){
+
+	public void removeRule(CQIE rule) {
 		rules.remove(rule);
-		
+
 		Predicate predicate = rule.getHead().getPredicate();
 		List<CQIE> indexedRules = this.getRules(predicate);
 		indexedRules.remove(rule);
 	}
-	
-	public void removeRules(List<CQIE> rules){
-		for (CQIE rule: rules) {
+
+	public void removeRules(List<CQIE> rules) {
+		for (CQIE rule : rules) {
 			removeRule(rule);
 		}
 	}
-	public boolean isUCQ(){
-		
-		if(rules.size() >1){
+
+	public boolean isUCQ() {
+
+		if (rules.size() > 1) {
 			boolean isucq = true;
 			CQIE rule0 = rules.get(0);
 			Atom head0 = rule0.getHead();
-			for(int i=1;i<rules.size() && isucq;i++){
-				
+			for (int i = 1; i < rules.size() && isucq; i++) {
+
 				CQIE ruleI = rules.get(i);
 				Atom headI = ruleI.getHead();
-				if(head0.getArity() != headI.getArity() || !(head0.getPredicate().equals(headI.getPredicate()))){
+				if (head0.getArity() != headI.getArity() || !(head0.getPredicate().equals(headI.getPredicate()))) {
 					isucq = false;
 				}
 			}
 			return isucq;
-		}else if(rules.size() ==1){
-			return true; 
-		}else{
+		} else if (rules.size() == 1) {
+			return true;
+		} else {
 			return false;
 		}
-//	  returns true if the head of all the rules has the same predicate and 
-//	  same arity
+		// returns true if the head of all the rules has the same predicate and
+		// same arity
 	}
-	
+
 	public List<CQIE> getRules() {
 		return rules;
 	}
-	
+
 	public String toString() {
 		StringBuffer bf = new StringBuffer();
-		for (CQIE rule: this.rules) {
+		for (CQIE rule : this.rules) {
 			bf.append(rule.toString());
 			bf.append("\n");
 		}
@@ -90,8 +97,7 @@ public class DatalogProgramImpl implements DatalogProgram{
 	@Override
 	public List<CQIE> getRules(Predicate headPredicate) {
 		List<CQIE> rules = this.predicateIndex.get(headPredicate);
-		if (rules == null) 
-		{
+		if (rules == null) {
 			rules = new LinkedList<CQIE>();
 			predicateIndex.put(headPredicate, rules);
 		}
