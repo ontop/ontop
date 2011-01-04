1e33c21417c405075eff1723619b07a9f794fe3d
Josef Hardi <josef.hardi@gmail.com>
2011-01-04 09:36:19 +0000

    Add a new method interface toString() and some javadoc descriptions.

--- obdalib/obdalib-core/src/main/java/org/obda/query/domain/Term.java
+++ obdalib/obdalib-core/src/main/java/org/obda/query/domain/Term.java
@@ -2,6 +2,24 @@ package org.obda.query.domain;
 
 public interface Term {
 
+	/**
+	 * Duplicate the object by performing a deep cloning.
+	 *
+	 * @return the copy of the object.
+	 */
 	public Term copy();
+
+	/**
+	 * Get the name of the term object.
+	 *
+	 * @return a string name.
+	 */
 	public String getName();
+
+	/**
+	 * Get the string representation of the term object.
+	 *
+	 * @return a string text.
+	 */
+	public String toString();
 }
