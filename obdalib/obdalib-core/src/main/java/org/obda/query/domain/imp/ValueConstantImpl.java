1b8aa29516e86ea1b6335c0e227c3cfae82e3ba6
Josef Hardi <josef.hardi@gmail.com>
2011-01-04 13:41:20 +0000

    Remove the parameter identifier from the object constructor.

--- obdalib/obdalib-core/src/main/java/org/obda/query/domain/imp/ValueConstantImpl.java
+++ obdalib/obdalib-core/src/main/java/org/obda/query/domain/imp/ValueConstantImpl.java
@@ -4,46 +4,57 @@ import org.obda.query.domain.ValueConstant;
 
 import com.sun.msv.datatype.xsd.XSDatatype;
 
-public class ValueConstantImpl implements ValueConstant{
+public class ValueConstantImpl implements ValueConstant {
 
 	private String value = null;
-	private int identifier = Integer.MIN_VALUE;
 	private XSDatatype type = null;
+	private int identifier = -1;
 
-	protected ValueConstantImpl(String value, int identifier, XSDatatype type){
+	/**
+	 * The default constructor.
+	 *
+	 * @param value the constant value.
+	 * @param type the constant type.
+	 */
+	protected ValueConstantImpl(String value, XSDatatype type){
 		this.value = value;
-		this.identifier = identifier;
 		this.type = type;
+		this.identifier = value.hashCode();
 	}
 
-	 @Override
+	@Override
 	public boolean equals(Object obj){
 
-		 if(obj == null || !(obj instanceof ValueConstantImpl)){
+		 if(obj == null || !(obj instanceof ValueConstantImpl))
 			 return false;
-		 }
 
-		 return this.hash() == ((ValueConstantImpl)obj).hash();
+		 ValueConstantImpl value2 = (ValueConstantImpl) obj;
+		 return this.identifier == value2.identifier;
 	 }
 
-
-	 public long hash(){
+	@Override
+	public int hashCode(){
 		return identifier;
-	 }
-
-	public ValueConstant copy() {
-		return new ValueConstantImpl(new String(value),identifier,type);
 	}
 
+	@Override
+	public ValueConstant copy() {
+		return new ValueConstantImpl(new String(value), this.type);
+	}
+
+	@Override
 	public String getName() {
 		return value;
 	}
 
+	@Override
 	public XSDatatype getType() {
 		return type;
 	}
-	
+
+	@Override
 	public String toString() {
+		// TODO add the data type support
 		return "\"" + getName().toString() + "\"";
 	}
 }
