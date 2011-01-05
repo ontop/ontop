89097ec9ffbdb5d9b06c223a2a5d9a7ab2bcb620
Josef Hardi <josef.hardi@gmail.com>
2011-01-05 16:17:15 +0000

    The test case includes different versions of OBDA input file. The current API support for version 2 and version 3 of the file.
    Version 1:
    - Original file from the initial project.
    - No prefix definition.
    - No DOCTYPE definition.
    - Using old head class: inf.unibz.it.obda.api.domain.ucq.ConjunctiveQuery
    
    Version 2:
    - With prefix definition.
    - Using new head class: org.obda.query.domain.imp.CQIEImpl
    - No DOCTYPE definition.
    
    Version 3:
    - With prefix definition.
    - With DOCTYPE definition.
    - The CQ string contains head definition, i.e., "&obdap;p(*) :- ..."

--- obdalib/obdalib-owlapi/src/test/java/inf/unibz/it/obda/owlapi/OWLAPIDataManagerTest.java
+++ obdalib/obdalib-owlapi/src/test/java/inf/unibz/it/obda/owlapi/OWLAPIDataManagerTest.java
@@ -18,7 +18,35 @@ import org.semanticweb.owl.model.OWLOntologyManager;
 
 public class OWLAPIDataManagerTest extends TestCase {
 
-	private final String[][] ORACLE = {
+	/** The expected output for old OBDA file (v1) */
+	private final String[][] ORACLE_FAIL = {
+		// The answer for the target queries.
+		{"nullq(id, name, ssn, lastname, dateofbirth) :- " +
+			"nullInvestor(nullgetPersonObj(id)), " +
+			"nullfirstName(nullgetPersonObj(id), name), " +
+			"nulllastName(nullgetPersonObj(id), lastname), " +
+			"nulldateOfBirth(nullgetPersonObj(id), dateofbirth), " +
+			"nullssn(nullgetPersonObj(id), ssn)",
+		 "nullq(id, addressid) :- " +
+		 	"nullhasAddress(nullgetPersonObj(id), " +
+		 	"nullgetAddressObj(addressid))",
+		 "nullq(id, addressid) :- " +
+		 	"nullhasAddress(nullgetCompanyObj(id), " +
+		 	"nullgetAddressObj(addressid))",
+		 "nullq(id, city) :- " +
+		 	"nullhasBase(nullgetCompanyObj(id), " +
+		 	"nullbaseCity(city))"
+		},
+		// The answer for the source queries.
+		{"select id, name, lastname, dateofbirth, ssn from client",
+		 "select id, name, lastname, addressid from client",
+		 "select id, addressid from company",
+		 "select id, baseCity from company"
+		}
+	};
+
+	/** The expected output for newer OBDA file (v2 and v3) */
+	private final String[][] ORACLE_SUCCESS = {
 		// The answer for the target queries.
 		{"http://obda.org/mapping/predicates/q(id, name, ssn, lastname, dateofbirth) :- " +
 			"http://www.owl-ontologies.com/ontology.owl#Investor(http://www.owl-ontologies.com/ontology.owl#getPersonObj(id)), " +
@@ -32,38 +60,25 @@ public class OWLAPIDataManagerTest extends TestCase {
 		 "http://obda.org/mapping/predicates/q(id, addressid) :- " +
 		 	"http://www.owl-ontologies.com/ontology.owl#hasAddress(http://www.owl-ontologies.com/ontology.owl#getCompanyObj(id), " +
 		 	"http://www.owl-ontologies.com/ontology.owl#getAddressObj(addressid))",
-		 "http://obda.org/mapping/predicates/q(id, addressid) :- " +
-		 	"http://www.owl-ontologies.com/ontology.owl#hasAddress(http://www.owl-ontologies.com/ontology.owl#getCompanyObj(id), " +
-		 	"http://www.owl-ontologies.com/ontology.owl#getAddressObj(addressid))"
+		 "http://obda.org/mapping/predicates/q(id, city) :- " +
+		 	"http://www.owl-ontologies.com/ontology-base.owl#hasBase(http://www.owl-ontologies.com/ontology.owl#getCompanyObj(id), " +
+		 	"http://www.owl-ontologies.com/ontology-base.owl#baseCity(city))"
 		},
 		// The answer for the source queries.
 		{"select id, name, lastname, dateofbirth, ssn from client",
 		 "select id, name, lastname, addressid from client",
 		 "select id, addressid from company",
-		 "select id, addressid from company"
+		 "select id, baseCity from company"
 		}
 	};
 
 	private OWLAPIController controller;
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-
-		String owlfile = "src/test/resources/input.owl";
-
-		// Load the OWL file.
-        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        OWLOntology ontology =
-        	manager.loadOntologyFromPhysicalURI((new File(owlfile)).toURI());
-
-        // Load the OBDA data.
-        controller = new OWLAPIController(manager, ontology);
-        controller.loadData(new File(owlfile).toURI());
-	}
-
 	@Test
-	public void testMappings() {
+	public void testMappingOnVersionOne() throws Exception {
+
+		loadObdaFile("input-v1.owl");
+
 		MappingController mapControl = controller.getMappingController();
 		Set<URI> uris = mapControl.getMappings().keySet();
 		for (URI uri : uris) {
@@ -74,17 +89,87 @@ public class OWLAPIDataManagerTest extends TestCase {
 				Query targetQuery = axiom.getTargetQuery();
 				String sTargetQuery = targetQuery.toString();
 				assertEquals("Target query is not the same!",
-						ORACLE[0][i], sTargetQuery);
+						ORACLE_FAIL[0][i], sTargetQuery);
 
 				// The source queries
 				Query sourceQuery = axiom.getSourceQuery();
 				String sSourceQuery = sourceQuery.toString();
 				assertEquals("Source query is not the same!",
-						ORACLE[1][i], sSourceQuery);
+						ORACLE_FAIL[1][i], sSourceQuery);
 
 				i++; // increment
 			}
 		}
 	}
 
+	@Test
+	public void testMappingOnVersionTwo() throws Exception {
+
+		loadObdaFile("input-v2.owl");
+
+		MappingController mapControl = controller.getMappingController();
+		Set<URI> uris = mapControl.getMappings().keySet();
+		for (URI uri : uris) {
+			ArrayList<OBDAMappingAxiom> axioms = mapControl.getMappings().get(uri);
+			int i = 0;
+			for (OBDAMappingAxiom axiom : axioms) {
+				// The target queries
+				Query targetQuery = axiom.getTargetQuery();
+				String sTargetQuery = targetQuery.toString();
+				assertEquals("Target query is not the same!",
+						ORACLE_SUCCESS[0][i], sTargetQuery);
+
+				// The source queries
+				Query sourceQuery = axiom.getSourceQuery();
+				String sSourceQuery = sourceQuery.toString();
+				assertEquals("Source query is not the same!",
+						ORACLE_SUCCESS[1][i], sSourceQuery);
+
+				i++; // increment
+			}
+		}
+	}
+
+	@Test
+	public void testMappingOnVersionThree() throws Exception {
+
+		loadObdaFile("input-v3.owl");
+
+		MappingController mapControl = controller.getMappingController();
+		Set<URI> uris = mapControl.getMappings().keySet();
+		for (URI uri : uris) {
+			ArrayList<OBDAMappingAxiom> axioms = mapControl.getMappings().get(uri);
+			int i = 0;
+			for (OBDAMappingAxiom axiom : axioms) {
+				// The target queries
+				Query targetQuery = axiom.getTargetQuery();
+				String sTargetQuery = targetQuery.toString();
+				assertEquals("Target query is not the same!",
+						ORACLE_SUCCESS[0][i], sTargetQuery);
+
+				// The source queries
+				Query sourceQuery = axiom.getSourceQuery();
+				String sSourceQuery = sourceQuery.toString();
+				assertEquals("Source query is not the same!",
+						ORACLE_SUCCESS[1][i], sSourceQuery);
+
+				i++; // increment
+			}
+		}
+	}
+
+	private void loadObdaFile(String filename) throws Exception {
+		super.setUp();
+
+		String owlfile = "src/test/resources/" + filename;
+
+		// Load the OWL file.
+        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+        OWLOntology ontology =
+        	manager.loadOntologyFromPhysicalURI((new File(owlfile)).toURI());
+
+        // Load the OBDA data.
+        controller = new OWLAPIController(manager, ontology);
+        controller.loadData(new File(owlfile).toURI());
+	}
 }
