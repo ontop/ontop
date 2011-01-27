f273376cfb4a5101d60ee82b4777dfbdcf38e44c
Josef Hardi <josef.hardi@gmail.com>
2011-01-27 15:42:16 +0000

    Oops forgot to add this! This is part of the reasoner preference changes.

---
+++ obdalib/reformulation-plugin/src/main/java/org/obda/reformulation/protege4/ProtegeReformulationPlatformPreferences.java
@@ -0,0 +1,12 @@
+package org.obda.reformulation.protege4;
+
+import org.obda.owlrefplatform.core.ReformulationPlatformPreferences;
+import org.protege.editor.core.Disposable;
+
+public class ProtegeReformulationPlatformPreferences extends
+		ReformulationPlatformPreferences implements Disposable {
+	
+	public void dispose() {
+		// Do nothing.
+	}
+}
