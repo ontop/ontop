d7d5e5014344598e29a658208e2356d286e7a5c4
Josef Hardi <josef.hardi@gmail.com>
2011-02-11 13:57:39 +0000

    Commit the reformulation-plugin package.

---
+++ obdalib/obdalib-protege4/src/main/java/org/obda/reformulation/protege4/ProtegeOBDAOWLReformulationPlatformFactory.java
@@ -0,0 +1,8 @@
+package org.obda.reformulation.protege4;
+
+import org.obda.owlrefplatform.core.OBDAOWLReformulationPlatformFactoryImpl;
+import org.protege.editor.owl.model.inference.ProtegeOWLReasonerFactory;
+
+public class ProtegeOBDAOWLReformulationPlatformFactory extends OBDAOWLReformulationPlatformFactoryImpl implements ProtegeOWLReasonerFactory{
+
+}
---
+++ obdalib/obdalib-protege4/src/main/java/org/obda/reformulation/protege4/configpanel/OBDAOWLReformulationPlatformConfigPanel.java
@@ -0,0 +1,28 @@
+package org.obda.reformulation.protege4.configpanel;
+
+import org.obda.owlrefplatform.core.ReformulationPlatformPreferences;
+import org.protege.editor.owl.ui.preferences.OWLPreferencesPanel;
+
+public class OBDAOWLReformulationPlatformConfigPanel extends OWLPreferencesPanel {
+
+	private ReformulationPlatformPreferences preference = null;
+	private ConfigPanel configPanel = null;
+	
+	@Override
+	public void applyChanges() {
+		// Do nothing.
+	}
+
+	@Override
+	public void initialise() throws Exception {
+		preference = (ReformulationPlatformPreferences)
+			getEditorKit().get(ReformulationPlatformPreferences.class.getName());
+		configPanel = new ConfigPanel(preference);
+		this.add(configPanel);
+	}
+
+	@Override
+	public void dispose() throws Exception {
+		// Do nothing.
+	}
+}
