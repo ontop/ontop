72502bd8c96e0e011da685d6c31b0f810bbc6854
Manfred Gestgrasser <Manfred.Gerstgrasser@stud-inf.unibz.it>
2011-04-05 08:07:56 +0000

    modified GUI of the the data source selection panel

--- obdalib/obdalib-protege4/src/main/java/inf/unibz/it/obda/protege4/gui/view/DatasourcesManagerViewComponent.java
+++ obdalib/obdalib-protege4/src/main/java/inf/unibz/it/obda/protege4/gui/view/DatasourcesManagerViewComponent.java
@@ -1,11 +1,16 @@
 package inf.unibz.it.obda.protege4.gui.view;
 
 import inf.unibz.it.obda.api.controller.APIController;
-import inf.unibz.it.obda.gui.swing.datasource.panels.DatasourceManagerPanel;
+import inf.unibz.it.obda.gui.swing.datasource.panels.DataSourceSelectionPanel;
 import inf.unibz.it.obda.gui.swing.datasource.panels.DatasourceParameterEditorPanel;
 import inf.unibz.it.obda.protege4.core.OBDAPluginController;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JPanel;
 
 import org.apache.log4j.Logger;
 import org.protege.editor.owl.ui.view.AbstractOWLViewComponent;
@@ -25,9 +30,37 @@ public class DatasourcesManagerViewComponent extends AbstractOWLViewComponent {
     protected void initialiseOWLView() throws Exception {
     	OBDAPluginController apic = getOWLEditorKit().get(APIController.class.getName());
     	
-        setLayout(new BorderLayout());
-        add(new DatasourceManagerPanel(apic), BorderLayout.CENTER);
-        add(new DatasourceParameterEditorPanel(apic), BorderLayout.EAST);
+        setLayout(new GridBagLayout());
+        GridBagConstraints gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 0.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        DataSourceSelectionPanel selectionpanel = new DataSourceSelectionPanel(apic);
+        add(selectionpanel, gridBagConstraints);
+        
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weighty = 0.0;
+        gridBagConstraints.weightx = 1.0;
+        DatasourceParameterEditorPanel editor = new DatasourceParameterEditorPanel(apic);
+        add(editor,gridBagConstraints);
+        selectionpanel.getDataSourceSelector().addDatasourceListListener(editor);
+        
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weighty = 1.0;
+        gridBagConstraints.weightx = 1.0;
+        JPanel newPanel = new JPanel();
+        newPanel.setBorder(javax.swing.BorderFactory.createTitledBorder("User Settings"));
+        newPanel.setMaximumSize(new Dimension(80, 50));
+        add(newPanel,gridBagConstraints);
         
         log.info("Datasource browser initialized");
     }
