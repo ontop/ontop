f6fea8d64e4d14bef17fef5abe0ef6ea01f3f928
Sergejs Pugac <pugacs@gmail.com>
2011-06-03 11:08:18 +0000

    Added tests for SemanticReduction

--- obdalib/reformulation-core/src/test/java/it/unibz/krdb/obda/reformulation/tests/SemanticReductionTest.java
+++ obdalib/reformulation-core/src/test/java/it/unibz/krdb/obda/reformulation/tests/SemanticReductionTest.java
@@ -7,22 +7,40 @@ import it.unibz.krdb.obda.owlrefplatform.core.abox.SDAG;
 import it.unibz.krdb.obda.owlrefplatform.core.abox.SemanticReduction;
 import it.unibz.krdb.obda.owlrefplatform.core.abox.TDAG;
 import it.unibz.krdb.obda.owlrefplatform.core.ontology.Assertion;
+import junit.framework.TestCase;
+import org.semanticweb.owl.model.OWLOntologyCreationException;
 
 import java.util.List;
-
-import junit.framework.TestCase;
-
-import org.semanticweb.owl.model.OWLOntologyCreationException;
 
 public class SemanticReductionTest extends TestCase {
     SemanticIndexHelper helper = new SemanticIndexHelper();
 
-    public void test_1_0_0() throws OWLOntologyCreationException {
-        DAG dag = helper.load_dag("test_1_0_0");
+
+    public void test_2_0_0() throws OWLOntologyCreationException {
+        DAG dag = helper.load_dag("test_2_0_0");
         TDAG tdag = new TDAG(dag);
         SDAG sdag = new SDAG(tdag);
         SemanticReduction reduction = new SemanticReduction(dag, tdag, sdag);
         List<Assertion> rv = reduction.reduce();
         assertEquals(0, rv.size());
     }
+
+    public void test_2_0_1() throws OWLOntologyCreationException {
+        DAG dag = helper.load_dag("test_2_0_1");
+        TDAG tdag = new TDAG(dag);
+        SDAG sdag = new SDAG(tdag);
+        SemanticReduction reduction = new SemanticReduction(dag, tdag, sdag);
+        List<Assertion> rv = reduction.reduce();
+        assertEquals(0, rv.size());
+    }
+
+    public void test_2_1_0() throws OWLOntologyCreationException {
+        DAG dag = helper.load_dag("test_2_1_0");
+        TDAG tdag = new TDAG(dag);
+        SDAG sdag = new SDAG(tdag);
+        SemanticReduction reduction = new SemanticReduction(dag, tdag, sdag);
+        List<Assertion> rv = reduction.reduce();
+        assertEquals(1, rv.size());
+    }
+
 }
