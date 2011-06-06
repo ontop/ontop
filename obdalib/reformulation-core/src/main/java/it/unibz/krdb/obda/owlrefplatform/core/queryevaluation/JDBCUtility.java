7b40ba2682467e2b248892d58e41dc7962a22418
Mariano Rodriguez-Muro <rodriguez@inf.unibz.it>
2011-06-06 09:34:05 +0000

    We now allow undefined drivers and throw warnings if they are not oracle, postgresl, h2 or mysql

--- obdalib/reformulation-core/src/main/java/it/unibz/krdb/obda/owlrefplatform/core/queryevaluation/JDBCUtility.java
+++ obdalib/reformulation-core/src/main/java/it/unibz/krdb/obda/owlrefplatform/core/queryevaluation/JDBCUtility.java
@@ -2,6 +2,11 @@ package it.unibz.krdb.obda.owlrefplatform.core.queryevaluation;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
 /**
  * The JDBC utility class implements the algorithm which is used to
  * over come the impedance mismatch problem, i.e. it manipulates the
@@ -13,16 +18,21 @@ import java.util.List;
  */
 
 public class JDBCUtility {
+	
+	Logger log = LoggerFactory.getLogger(JDBCUtility.class);
 
 	private String driver = null;
 	
 	public JDBCUtility(String driver) throws Exception{
 		
-		if(driver.equals("org.postgresql.Driver") || driver.equals("org.h2.Driver")||driver.equals("com.mysql.jdbc.Driver"))
+		if(driver.equals("org.postgresql.Driver") || driver.equals("org.h2.Driver")||driver.equals("com.mysql.jdbc.Driver")||driver.equals("oracle.jdbc.driver.OracleDriver"))
 		{	
 			this.driver = driver;
 		}else{
-			throw new Exception("DBMS not supported (yet).");
+			this.driver = driver;
+			log.warn("WARNING: the specified driver doesn't correspond to any of the drivers officially supported by Quest.");
+			log.warn("WARNING: If you database is not fully compliant with SQL 99 you might experience problems using Quest.");
+			log.warn("WARNING: Contact the authors for further support.");
 		}
 	}
 	
@@ -39,8 +49,14 @@ public class JDBCUtility {
 	 */
 	public String getConcatination(String uribase, List<String> list){
 		
-		if(driver.equals("org.postgresql.Driver") || driver.equals("org.h2.Driver")){
+		if(driver.equals("com.mysql.jdbc.Driver")){
 			
+			String str = "CONCAT('" + uribase + "-',";
+			str += list.get(0);
+			for (int i = 1; i < list.size(); i++)
+				str += ",'-', " + list.get(i);
+			return str.concat(")");
+		} else {
 			String result = "'"+uribase+"-' ||";
 			String vars = "";
 			for (int i = 0; i < list.size(); i++) {
@@ -51,15 +67,7 @@ public class JDBCUtility {
 			}
 			result = result + vars;
 			return result;
-			
-		}else if(driver.equals("com.mysql.jdbc.Driver")){
-			
-			String str = "CONCAT('" + uribase + "-',";
-			str += list.get(0);
-			for (int i = 1; i < list.size(); i++)
-				str += ",'-', " + list.get(i);
-			return str.concat(")");
 		}
-		return "";
+//		return "";
 	}
 }
