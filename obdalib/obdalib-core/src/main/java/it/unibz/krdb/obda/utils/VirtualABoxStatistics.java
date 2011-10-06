399e1ccbbf514f7e157e1e20b4df660f3f7d49f9
Josef Hardi <josef.hardi@gmail.com>
2011-10-06 08:43:26 +0000

    Add a feature to show the OBDA model statistics info. The feature resides at the menu bar "OBDA > Display the OBDA model statistics...". A dialog box will appear showing the total generated triples summed from each mapping id. The information iterates through all the data sources contained in the OBDA model.
    BUG=#106
    
    Ticket URL=https://babbage.inf.unibz.it/trac/obdapublic/ticket/106

---
+++ obdalib/obdalib-core/src/main/java/it/unibz/krdb/obda/utils/VirtualABoxStatistics.java
@@ -0,0 +1,174 @@
+package it.unibz.krdb.obda.utils;
+
+import it.unibz.krdb.obda.gui.swing.exception.NoDatasourceSelectedException;
+import it.unibz.krdb.obda.model.OBDADataSource;
+import it.unibz.krdb.obda.model.OBDAMappingAxiom;
+import it.unibz.krdb.obda.model.OBDAModel;
+import it.unibz.krdb.obda.model.OBDASQLQuery;
+import it.unibz.krdb.obda.model.impl.CQIEImpl;
+import it.unibz.krdb.obda.utils.VirtualABoxStatistics;
+import it.unibz.krdb.sql.JDBCConnectionManager;
+
+import java.net.URI;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A utility class about the ABox materialization.
+ */
+public class VirtualABoxStatistics {
+
+	private OBDAModel model;
+	
+	private HashMap<String, HashMap<String, Integer>> statistics = new HashMap<String, HashMap<String, Integer>>();
+	
+	private JDBCConnectionManager conn = JDBCConnectionManager.getJDBCConnectionManager();
+	
+	Logger log = LoggerFactory.getLogger(VirtualABoxStatistics.class);
+	
+	/**
+	 * Inserts the OBDA model to this utility class.
+	 * 
+	 * @param model
+	 * 			The mandatory OBDA model.
+	 */
+	public VirtualABoxStatistics(OBDAModel model) {
+		this.model = model;
+		refresh();
+	}
+		
+	/**
+	 * Returns the complete statistics from the OBDA model.
+	 * 
+	 * @return The complete statistics.
+	 */
+	public HashMap<String, HashMap<String, Integer>> getStatistics() {
+		return statistics;
+	}
+	
+	/**
+	 * Returns the triples counts from all the mappings that associate
+	 * to a certain data source.
+	 * 
+	 * @param datasourceId
+	 * 			The data source identifier.
+	 * @return A data statistics.
+	 */
+	public HashMap<String, Integer> getStatistics(String datasourceId) {
+		return statistics.get(datasourceId);
+	}
+	
+	/**
+	 * Returns one triple count from a particular mapping.
+	 * 
+	 * @param datasourceId
+	 * 			The data source identifier.
+	 * @param mappingId
+	 * 			The mapping identifier.
+	 * @return The number of triples.
+	 */
+	public int getStatistics(String datasourceId, String mappingId) {
+		final HashMap<String, Integer> mappingStat = getStatistics(datasourceId);
+		int triplesCount = mappingStat.get(mappingId).intValue();
+		
+		return triplesCount;
+	}
+	
+	/**
+	 * Gets the total number of triples from all the data sources and mappings.
+	 * 
+	 * @return The total number of triples.
+	 * @throws Exception 
+	 */
+	public int getTotalTriples() throws Exception {
+		int total = 0;
+		for (HashMap<String, Integer> mappingStat : statistics.values()) {
+			for (Integer triplesCount : mappingStat.values()) {
+				int triples = triplesCount.intValue();
+				if (triples == -1) {
+					throw new Exception("An error was occurred in the counting process.");
+				}
+				total = total + triples;
+			}
+		}
+		return total;
+	}
+	
+	@Override
+	public String toString() {
+		String str = "";
+		for (String datasourceId : statistics.keySet()) {
+			str += "Data Source Name: " + datasourceId + "\n";
+			str += "Mappings: \n";
+			HashMap<String, Integer> mappingStat = statistics.get(datasourceId);
+			for (String mappingId : mappingStat.keySet()){
+				int count = mappingStat.get(mappingId);
+				str += String.format("- %s produces %s %s.\n", mappingId, count, (count == 1 ? "triple" : "triples"));
+			}
+			str += "\n";
+		}
+		return str;
+	}
+	
+	public void refresh() {
+		final List<OBDADataSource> sourceList = model.getSources();
+		
+		for (OBDADataSource database : sourceList) {
+			URI sourceUri = database.getSourceID();
+			ArrayList<OBDAMappingAxiom> mappingList = model.getMappings(sourceUri);
+			
+			HashMap<String, Integer> mappingStat = new HashMap<String, Integer>();
+			for (OBDAMappingAxiom mapping : mappingList) {
+				String mappingId = mapping.getId();
+				int triplesCount = 0;
+				try {
+					OBDASQLQuery sourceQuery = (OBDASQLQuery)mapping.getSourceQuery();
+					int tuples = getTuplesCount(database, sourceQuery);
+
+					CQIEImpl targetQuery = (CQIEImpl)mapping.getTargetQuery();
+					int atoms = getAtomCount(targetQuery);
+
+					triplesCount = tuples * atoms;
+				} catch (Exception e) {
+					triplesCount = -1;  // fails to count
+					log.error(e.getMessage());
+				}
+				mappingStat.put(mappingId, triplesCount);
+			}
+			String sourceId = sourceUri.toString();		
+			statistics.put(sourceId, mappingStat);
+		}
+	}
+
+	private int getTuplesCount(OBDADataSource database, OBDASQLQuery query) throws NoDatasourceSelectedException, ClassNotFoundException, SQLException {	
+		
+		String sql = String.format("select COUNT(*) %s", getSelectionString(query));
+
+		ResultSet rs = conn.executeQuery(database, sql);
+
+		int count = 0;
+		while (rs.next()){
+			count = rs.getInt(1);
+		}
+		return count;
+	}
+		
+	private int getAtomCount(CQIEImpl query) {
+		return query.getBody().size();
+	}
+	
+	private String getSelectionString(OBDASQLQuery query) {
+		final String sql = query.toString().toLowerCase(); // make it lower case to help identify a string.
+		
+		final int start = sql.indexOf("from");
+		final int end = sql.length();
+		
+		return sql.substring(start, end);
+	}
+}
---
+++ obdalib/obdalib-protege4/src/main/java/it/unibz/krdb/obda/protege4/gui/action/ABoxStatisticsAction.java
@@ -0,0 +1,76 @@
+package it.unibz.krdb.obda.protege4.gui.action;
+
+import it.unibz.krdb.obda.gui.swing.panel.OBDAModelStatisticsPanel;
+import it.unibz.krdb.obda.model.OBDAModel;
+import it.unibz.krdb.obda.model.impl.OBDAModelImpl;
+import it.unibz.krdb.obda.protege4.core.OBDAModelManager;
+import it.unibz.krdb.obda.utils.VirtualABoxStatistics;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+import org.protege.editor.core.ui.action.ProtegeAction;
+import org.protege.editor.owl.OWLEditorKit;
+
+public class ABoxStatisticsAction extends ProtegeAction {
+
+	private static final long serialVersionUID = 3322509244957306932L;
+
+	private OWLEditorKit editorKit = null;
+	private OBDAModel obdaModel = null;
+	private VirtualABoxStatistics statistics = null;
+		
+	@Override
+	public void initialise() throws Exception {
+		editorKit = (OWLEditorKit)getEditorKit();		
+		obdaModel = ((OBDAModelManager)editorKit.get(OBDAModelImpl.class.getName())).getActiveOBDAModel();
+		statistics = new VirtualABoxStatistics(obdaModel);
+	}
+
+	@Override
+	public void dispose() throws Exception {
+		// Does nothing.
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JDialog dialog = new JDialog();
+		dialog.setModal(true);
+		dialog.setSize(520, 400);
+		dialog.setLocationRelativeTo(null);
+		dialog.setTitle("OBDA Model Statistics");
+		
+		OBDAModelStatisticsPanel pnlStatistics = new OBDAModelStatisticsPanel(statistics);
+		JPanel pnlCommandButton = createButtonPanel(dialog); 
+		dialog.setLayout(new BorderLayout());
+		dialog.add(pnlStatistics, BorderLayout.CENTER);
+		dialog.add(pnlCommandButton, BorderLayout.SOUTH);
+		
+		dialog.pack();
+		dialog.setVisible(true);
+	}
+	
+	private JPanel createButtonPanel(final JDialog parent) {
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		
+		JButton cmdCloseInformation = new JButton();
+		cmdCloseInformation.setText("Close Information");
+		cmdCloseInformation.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                parent.setVisible(false);
+                parent.removeAll();
+                parent.dispose();
+            }
+        });		
+		panel.add(cmdCloseInformation);
+		
+		return panel;
+	}
+}
