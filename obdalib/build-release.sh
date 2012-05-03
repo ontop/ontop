c2f5546df36a9de240dca21bcd3904fb88337686
Mariano Rodriguez-Muro <rodriguez@inf.unibz.it>
2012-05-03 08:49:26 +0000

    added a script to build the realease package of the obda plugin

---
+++ obdalib/build-release.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+export JDBC_PLUGINS_PATH=/Applications/Protege_4.1-obdalib-1.7/plugins
+export REVISION=1440
+rm -fr obdalib-protege41/dist
+svn update
+mvn clean
+mvn install -DskipTests
+cd obdalib-protege41/
+mvn bundle:bundle -DskipTests
+rm -fr dist
+mkdir dist
+cp target/it.unibz.inf.obda.p4plugin-1.7a1.jar dist/it.unibz.inf.obda.p4plugin-1.7-alpha-b$REVISION.jar
+cp $JDBC_PLUGINS_PATH/org.protege.osgi.jdbc.jar dist/
+cp $JDBC_PLUGINS_PATH/org.protege.osgi.jdbc.prefs.jar dist/
+cd dist
+zip it.unibz.inf.obda.p4plugin-1.7-alpha-b$REVISION.zip *.jar
+ls -lah
+
+
+
+
