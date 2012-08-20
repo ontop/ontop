9756da22482a71a6db7772fbef6210d1aa83b9d8
Victor Zhou <yvtungchow@gmail.com>
2012-08-20 02:19:48 +0000

    DMManager that has been tested by me. Construction method take as input DatabaseMetaData and DBMetadata, Method generateDirectMapping() will generate all the direct mappings, Method getDirectMapping will return a list of DirectMappingAxiom: for each DirectMappingAxiom, .getSQL() will get the SQL Query String, and .getCQ() will get the corresponding CQ Query String.

--- obdalib/obdalib-owlapi3/src/main/java/it/unibz/krdb/obda/owlapi3/directmapping/DirectMappingManager.java
+++ obdalib/obdalib-owlapi3/src/main/java/it/unibz/krdb/obda/owlapi3/directmapping/DirectMappingManager.java
@@ -17,35 +17,32 @@ public class DirectMappingManager {
 	
 	private DatabaseMetaData md;
 	private DBMetadata obdamd;
-	private List<DirectMappingAxiomBasic> DMList;
+	private List<DirectMappingAxiom> DMList;
 	
 	public DirectMappingManager(){
-		this.DMList=new ArrayList<DirectMappingAxiomBasic>();
+		this.DMList=new ArrayList<DirectMappingAxiom>();
 	}
 	
 	public DirectMappingManager(DatabaseMetaData md, DBMetadata obdamd){
-		this.DMList=new ArrayList<DirectMappingAxiomBasic>();
+		this.DMList=new ArrayList<DirectMappingAxiom>();
 		this.md = md;
 		this.obdamd = obdamd;
 	}
 	
-	public List<OBDARDBMappingAxiom> getDirectMapping(OBDADataFactoryImpl dfi) throws RecognitionException{
-		List<OBDARDBMappingAxiom> templist= new ArrayList<OBDARDBMappingAxiom>();
-		generateDirectMapping();
-		for(int i=0;i<DMList.size();i++){
-			DirectMappingAxiomBasic tempDMAxiomBasic=DMList.get(i);
-			DatalogProgramParser dpp = new DatalogProgramParser();
-			templist.add(dfi.getRDBMSMappingAxiom(tempDMAxiomBasic.getSQL(), dpp.parse(tempDMAxiomBasic.getCQ())));
-		}
-		return templist;
+	public List<DirectMappingAxiom> getDirectMapping() throws RecognitionException{
+		return DMList;
 	}
 	
-	private void generateDirectMapping(){
+	public void generateDirectMapping(){
 		for(int i=0;i<this.obdamd.getTableList().size();i++){
 			if(existPK(this.obdamd.getTableList().get(i))){
-				DirectMappingAxiomBasic temp = new DirectMappingAxiomBasic(obdamd.getTableList().get(i),md);
+				DirectMappingAxiomBasic temp = new DirectMappingAxiomBasic(obdamd.getTableList().get(i),md, obdamd);
 				temp.generateCQ();
-				DMList.add(new DirectMappingAxiomBasic(temp));
+				this.DMList.add(new DirectMappingAxiom(temp));
+			}else if(numFK(this.obdamd.getTableList().get(i))>1){
+				DirectMappingAxiomN_Ary temp = new DirectMappingAxiomN_Ary(obdamd.getTableList().get(i),md, obdamd);
+				temp.generateCQ();
+				this.DMList.add(new DirectMappingAxiom(temp));
 			}
 		}
 	}
@@ -53,12 +50,21 @@ public class DirectMappingManager {
 	private boolean existPK(DataDefinition dd){
 		boolean existPK = false;
 		for(int i=0;i<dd.getAttributes().size();i++){
-			if(dd.getAttribute(i).bPrimaryKey){
+			if(dd.getAttribute(i+1).bPrimaryKey){
 				existPK = true;
 			}
 		}
 		return existPK;
 	}
 	
+	private int numFK(DataDefinition dd){
+		int numFK = 0;
+		for(int i=0;i<dd.getAttributes().size();i++){
+			if(dd.getAttribute(i+1).bForeignKey){
+				numFK ++;
+			}
+		}
+		return numFK;
+	}
 
 }
