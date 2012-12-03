3febfec068b75d9bfdb5a7112c799f7b7232338d
Sarah Komla-Ebri <sarahkeb89@gmail.com>
2012-12-03 16:17:40 +0000

    Test to svn connection

---
+++ obdalib/reformulation-core/src/main/java/it/unibz/krdb/obda/owlrefplatform/core/dag/TestDAG.java
@@ -0,0 +1,13 @@
+package it.unibz.krdb.obda.owlrefplatform.core.dag;
+
+public class TestDAG {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
