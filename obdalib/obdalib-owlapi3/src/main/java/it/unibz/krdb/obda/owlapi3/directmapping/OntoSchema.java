bb4cf99436643a185304bbbfdff97e12b6419d0e
Josef Hardi <josef.hardi@gmail.com>
2013-01-10 13:46:15 +0000

    Restore the directmapping package for the automatic mapping generator feature.

---
+++ obdalib/obdalib-owlapi3/src/main/java/it/unibz/krdb/obda/owlapi3/directmapping/OntoSchema.java
@@ -0,0 +1,128 @@
+package it.unibz.krdb.obda.owlapi3.directmapping;
+
+
+import it.unibz.krdb.sql.DataDefinition;
+import it.unibz.krdb.sql.api.Attribute;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Set;
+
+import org.semanticweb.owlapi.model.AddAxiom;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+
+
+
+public class OntoSchema {
+	
+	private String tablename;
+	private List<Attribute> attrList;
+	private String baseURI;
+	
+	public OntoSchema(){
+		this.baseURI=new String(":");
+	}
+	
+	public OntoSchema(DataDefinition dd){
+		this.tablename=dd.getName();
+		this.attrList=dd.getAttributes();
+	}
+	
+	public void setBaseURI(String uri){
+		this.baseURI=new String(uri);
+	}
+	
+	public void setTN(String tn){
+		this.tablename=new String(tn);
+	}
+	
+	
+	//Add all the classes, data properties and object properties to the root ontology
+	//Existing class/property (Class/Property sharing same name in database and ontology) won't be added
+	public void enrichOntology(OWLOntology rootOntology) throws OWLOntologyStorageException{
+		addClass(rootOntology);
+		for(int i=0;i<this.attrList.size();i++){
+			Attribute att = this.attrList.get(i);
+			addProperty(rootOntology, att);
+		}
+	}
+	
+	
+	
+	public void addClass(OWLOntology rootOntology) throws OWLOntologyStorageException{
+		if(!existClass(rootOntology)){
+			OWLOntologyManager manager =rootOntology.getOWLOntologyManager();
+			OWLDataFactory dataFactory = manager.getOWLDataFactory();
+			OWLClass newclass = dataFactory.getOWLClass(IRI.create(baseURI+this.tablename));
+			OWLClass thing = dataFactory.getOWLThing();
+			OWLAxiom axiom = dataFactory.getOWLSubClassOfAxiom(newclass, thing);
+			AddAxiom addAxiom = new AddAxiom(rootOntology, axiom);
+			manager.applyChange(addAxiom);
+			manager.saveOntology(rootOntology);
+		}
+	}
+	
+	private boolean existClass(OWLOntology rootOntology){
+		boolean existClass=false;
+		for(OWLClass cls : rootOntology.getClassesInSignature()){
+			if(cls.toString().contains(tablename+">")){
+				existClass=true;
+			}
+		}
+		return existClass;
+	}
+	
+	public void addProperty(OWLOntology rootOntology, Attribute at) throws OWLOntologyStorageException{
+		OWLOntologyManager manager =rootOntology.getOWLOntologyManager();
+		OWLDataFactory dataFactory = manager.getOWLDataFactory();
+		if(!existDataProperty(rootOntology, at)){
+			OWLDataProperty newdproperty = dataFactory.getOWLDataProperty(IRI.create(baseURI+this.tablename+"#"+new String(at.name)));
+			OWLDataProperty topDataProperty = dataFactory.getOWLTopDataProperty();
+			OWLAxiom axiom = dataFactory.getOWLSubDataPropertyOfAxiom(newdproperty, topDataProperty);
+			AddAxiom addAxiom = new AddAxiom(rootOntology, axiom);
+			manager.applyChange(addAxiom);
+			manager.saveOntology(rootOntology);
+		}
+		if(at.bForeignKey && !existObjectProperty(rootOntology, at)){
+			OWLObjectProperty newoproperty = dataFactory.getOWLObjectProperty(IRI.create(baseURI+this.tablename+"#ref-"+new String(at.name)));
+			OWLObjectProperty topObjectProperty = dataFactory.getOWLTopObjectProperty();
+			OWLAxiom axiom = dataFactory.getOWLSubObjectPropertyOfAxiom(newoproperty, topObjectProperty);
+			AddAxiom addAxiom = new AddAxiom(rootOntology, axiom);
+			manager.applyChange(addAxiom);
+			manager.saveOntology(rootOntology);
+		}
+	}
+	
+	private boolean existDataProperty(OWLOntology rootOntology, Attribute at){
+		boolean existDataProperty=false;
+		for(OWLDataProperty dtpp : rootOntology.getDataPropertiesInSignature()){
+			if(dtpp.toString().contains(this.tablename+"#"+at.name+">")){
+				existDataProperty=true;
+			}
+		}
+		return existDataProperty;
+	}
+	
+	private boolean existObjectProperty(OWLOntology rootOntology, Attribute at){
+		boolean existObjectProperty=false;
+		for(OWLObjectProperty obpp : rootOntology.getObjectPropertiesInSignature()){
+			if(obpp.toString().contains(this.tablename+"#ref-"+at.name+">")){
+				existObjectProperty=true;
+			}
+		}
+		return existObjectProperty;
+	}
+	
+
+
+	
+
+}
