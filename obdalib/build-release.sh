7a55adb641fd1b39644b319cb6484f9da61b08c3
Mindaugas Slusnys <mindaugas.slusnys1@unibz.it>
2013-02-05 13:57:45 +0000

    Adding sesame packing script

--- obdalib/build-release.sh
+++ obdalib/build-release.sh
@@ -2,7 +2,9 @@
 
 export JDBC_PLUGINS_PATH=/Applications/Protege_4.1-obdalib-1.7-alpha/plugins
 export REVISION=2039
-echo "pluginVersion=1.7-alpha.b$REVISION" >  obdalib-core/src/main/resources/it/unibz/krdb/obda/utils/version.properties 
+
+# Packing the -ontopPro- distribution
+echo "pluginVersion=1.7-alpha.b$REVISION" >  obdalib-core/src/main/resources/it/unibz/krdb/obda/utils/version.properties
 rm -fr obdalib-protege41/dist
 svn update
 mvn clean
@@ -16,11 +18,32 @@ cp $JDBC_PLUGINS_PATH/org.protege.osgi.jdbc.jar dist/
 cp $JDBC_PLUGINS_PATH/org.protege.osgi.jdbc.prefs.jar dist/
 cd dist
 zip it.unibz.inf.obda.p4plugin-1.7-alpha-b$REVISION.zip *.jar
-ls -lah   
+ls -lah
 cd ..
 cd ..
-cat obdalib-core/src/main/resources/it/unibz/krdb/obda/utils/version.properties 
+cat obdalib-core/src/main/resources/it/unibz/krdb/obda/utils/version.properties
 
+# Packing the sesame distribution
+cd quest-distribution/
+mkdir -p dist/WEB-INF/lib
+mvn assembly:assembly -DskipTests
+cp target/quest-distribution-1.7-alpha-sesame-bin.jar dist/WEB-INF/lib
+cp target/quest-distribution-1.7-alpha-dependencies.zip dist/
+cp src/main/resources/openrdf/openrdf-sesame.war dist/
+cp src/main/resources/openrdf/openrdf-workbench.war dist/
+cp src/main/resources/jetty/jetty-distribution-8.1.9.zip dist/
 
+cd dist
+unzip quest-distribution-1.7-alpha-dependencies.zip -d WEB-INF/lib/
+jar -uf openrdf-sesame.war WEB-INF/lib/*
+jar -uf openrdf-workbench.war WEB-INF/lib/*
 
+mkdir -p jetty-distribution-8.1.9/webapps
+cp openrdf-sesame.war jetty-distribution-8.1.9/webapps
+cp openrdf-workbench.war jetty-distribution-8.1.9/webapps
 
+zip jetty-distribution-8.1.9.zip jetty-distribution-8.1.9/webapps/*
+
+rm -fr jetty-distribution-8.1.9
+rm -fr WEB-INF
+rm -f quest-distribution-1.7-alpha-dependencies.zip
