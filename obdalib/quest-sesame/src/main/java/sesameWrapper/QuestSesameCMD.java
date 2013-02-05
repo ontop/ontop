6e58f3d034cf45631de99f341eb442d3ca26cb11
Mariano Rodriguez-Muro <rodriguez@inf.unibz.it>
2013-02-05 17:49:56 +0000

    * Fixed exception printout with isClosed

--- obdalib/quest-sesame/src/main/java/sesameWrapper/QuestSesameCMD.java
+++ obdalib/quest-sesame/src/main/java/sesameWrapper/QuestSesameCMD.java
@@ -17,82 +17,78 @@ public class QuestSesameCMD {
 	 */
 	public static void main(String[] args) {
 		Repository repo = null;
-		org.openrdf.repository.RepositoryConnection conn = null; 
-		//check argument correctness
-		if (args.length != 3 && args.length !=4) {
-			   System.out.println("Usage:");
-			   System.out.println(" QuestSesameCMD owlfile obdafile queryfile [outputfile]");
-			   System.out.println("");
-			   System.out.println(" owlfile    The full path to the OWL file");
-			   System.out.println(" obdafile   The full path to the OBDA file");
-			   System.out.println(" queryfile  The full path to the file with the SPARQL query");
-			   System.out.println(" outputfile [OPTIONAL] The full path to output file");
-			   System.out.println("");
-			   return;
-			  }
-		
-		//get parameter values
+		org.openrdf.repository.RepositoryConnection conn = null;
+		// check argument correctness
+		if (args.length != 3 && args.length != 4) {
+			System.out.println("Usage:");
+			System.out.println(" QuestSesameCMD owlfile obdafile queryfile [outputfile]");
+			System.out.println("");
+			System.out.println(" owlfile    The full path to the OWL file");
+			System.out.println(" obdafile   The full path to the OBDA file");
+			System.out.println(" queryfile  The full path to the file with the SPARQL query");
+			System.out.println(" outputfile [OPTIONAL] The full path to output file");
+			System.out.println("");
+			return;
+		}
+
+		// get parameter values
 		String owlfile = args[0].trim();
 		String obdafile = args[1].trim();
 		String qfile = args[2].trim();
 		String out = null;
 		if (args.length == 4)
 			out = args[3].trim();
-		
+
 		try {
-			//create and initialize repo
+			// create and initialize repo
 			repo = new SesameVirtualRepo("test_repo", owlfile, obdafile, false, "TreeWitness");
 			repo.initialize();
 			conn = repo.getConnection();
-			System.out.println("test_repo created and initialized...");
-			String query="";
-			//read query from file
+			String query = "";
+			// read query from file
 			FileInputStream input = new FileInputStream(new File(qfile));
 			byte[] fileData = new byte[input.available()];
 
 			input.read(fileData);
 			input.close();
 
-			query =  new String(fileData, "UTF-8");
-			
-			System.out.println("Query: \n"+query);
-			
-			//execute query
+			query = new String(fileData, "UTF-8");
+
+			// execute query
 			TupleQuery tupleQuery = conn.prepareTupleQuery(QueryLanguage.SPARQL, query);
 			TupleQueryResultHandler handler = null;
-			
-			//set handler to output file or printout
+
+			// set handler to output file or printout
 			if (out != null) {
 
 				FileOutputStream output = new FileOutputStream(new File(out));
 				handler = new SPARQLResultsTSVWriter(output);
-				
+
 			} else {
-				
-				 handler = new SPARQLResultsTSVWriter(System.out);
+
+				handler = new SPARQLResultsTSVWriter(System.out);
 			}
-			//evaluate the query
+			// evaluate the query
 			tupleQuery.evaluate(handler);
-			
-			
-			
+
 		} catch (Exception e) {
+			System.out.println("Error executing query:");
 			e.printStackTrace();
 		} finally {
 			try {
-			conn.close();
+				conn.close();
 			} catch (Exception e1) {
-				
+
 			}
-			
+
 			try {
-			repo.shutDown();
+				repo.shutDown();
 			} catch (Exception e1) {
-				
+
 			}
 			System.out.println("Done.");
 		}
-		
+
 	}
 
 }
