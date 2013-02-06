36f18201fa90208cb27862ccce36660739ebc9af
Josef Hardi <josef.hardi@gmail.com>
2013-02-06 15:48:49 +0000

    Updating the script

--- obdalib/build-release.sh
+++ obdalib/build-release.sh
@@ -1,49 +1,89 @@
 #!/bin/sh
 
-export JDBC_PLUGINS_PATH=/Applications/Protege_4.1-obdalib-1.7-alpha/plugins
+export JDBC_PLUGINS_PATH=/Applications/Protege_4.1-obdalib-1.7-alpha2/plugins
+#export PROTEGE_COPY_PATH=
+#export PROTEGE_COPY_FILENAME=
+#export JETTY_COPY_PATH=
+#export JETTY_COPY_FILENAME=jetty-distribution-8.1.9
+#export OPENRDF_SESAME_PATH=
+#export OPENRDF_SESAME_FILENAME=openrdf-sesame
+#export OPENRDF_WORKBENCH_PATH=
+#export OPENRDF_WORKBENCH_FILENAME=openrdf-workbench
+
+export PROTEGE_DIST=ontopPro
+export QUEST_SESAME_DIST=QuestSesame
+export QUEST_JETTY_DIST=QuestJetty
+export OWL_API_DIST=QuestOWL
+
 export REVISION=2039
 
-# Packing the -ontopPro- distribution
-echo "pluginVersion=1.7-alpha.b$REVISION" >  obdalib-core/src/main/resources/it/unibz/krdb/obda/utils/version.properties
-rm -fr obdalib-protege41/dist
 svn update
 mvn clean
+
+# Packing the -ontopPro- distribution
+#
+echo "pluginVersion=1.7-alpha2.b$REVISION" >  obdalib-core/src/main/resources/it/unibz/krdb/obda/utils/version.properties
+rm -fr obdalib-protege41/dist
 mvn install -DskipTests
 cd obdalib-protege41/
 mvn bundle:bundle -DskipTests
-rm -fr dist
-mkdir dist
-cp target/it.unibz.inf.obda.p4plugin-1.7a1.jar dist/it.unibz.inf.obda.p4plugin-1.7-alpha-b$REVISION.jar
-cp $JDBC_PLUGINS_PATH/org.protege.osgi.jdbc.jar dist/
-cp $JDBC_PLUGINS_PATH/org.protege.osgi.jdbc.prefs.jar dist/
-cd dist
-zip it.unibz.inf.obda.p4plugin-1.7-alpha-b$REVISION.zip *.jar
+
+rm -fr ../quest-distribution/$PROTEGE_DIST
+mkdir ../quest-distribution/$PROTEGE_DIST
+cp target/it.unibz.inf.obda.p4plugin-1.7a1.jar ../quest-distribution/$PROTEGE_DIST/it.unibz.inf.obda.p4plugin-1.7-alpha2-b$REVISION.jar
+cp $PROTEGE_COPY_PATH/$PROTEGE_COPY_FILENAME.zip ../quest-distribution/$PROTEGE_DIST/
+
+cd ../quest-distribution/$PROTEGE_DIST/
+
+cp it.unibz.inf.obda.p4plugin-1.7-alpha2-b$REVISION.jar $PROTEGE_COPY_FILENAME/plugins/
+cp $JDBC_PLUGINS_PATH/org.protege.osgi.jdbc.jar $PROTEGE_COPY_FILENAME/plugins/
+cp $JDBC_PLUGINS_PATH/org.protege.osgi.jdbc.prefs.jar $PROTEGE_COPY_FILENAME/plugins/
+
+zip $PROTEGE_COPY_FILENAME.zip $PROTEGE_COPY_FILENAME/plugins/*
+
+rm -fr $PROTEGE_COPY_FILENAME
+
 ls -lah
 cd ..
-cd ..
-cat obdalib-core/src/main/resources/it/unibz/krdb/obda/utils/version.properties
 
 # Packing the sesame distribution
-cd quest-distribution/
-mkdir -p dist/WEB-INF/lib
+#
+rm -fr $QUEST_SESAME_DIST
+mkdir -p $QUEST_SESAME_DIST/WEB-INF/lib
 mvn assembly:assembly -DskipTests
-cp target/quest-distribution-1.7-alpha-sesame-bin.jar dist/WEB-INF/lib
-cp target/quest-distribution-1.7-alpha-dependencies.zip dist/
-cp src/main/resources/openrdf/openrdf-sesame.war dist/
-cp src/main/resources/openrdf/openrdf-workbench.war dist/
-cp src/main/resources/jetty/jetty-distribution-8.1.9.zip dist/
+cp target/quest-distribution-1.7-alpha2-sesame-bin.jar $QUEST_SESAME_DIST/WEB-INF/lib/quest-distribution-1.7-alpha2-sesame-b$REVISION.jar
+unzip target/quest-distribution-1.7-alpha2-dependencies.zip $QUEST_SESAME_DIST/WEB-INF/lib
+cp $OPENRDF_SESAME_PATH/$OPENRDF_SESAME_FILENAME.war $QUEST_SESAME_DIST/
+cp $OPENRDF_WORKBENCH_PATH/$OPENRDF_WORKBENCH_FILENAME.war $QUEST_SESAME_DIST/
 
-cd dist
-unzip quest-distribution-1.7-alpha-dependencies.zip -d WEB-INF/lib/
-jar -uf openrdf-sesame.war WEB-INF/lib/*
-jar -uf openrdf-workbench.war WEB-INF/lib/*
+cd $QUEST_SESAME_DIST
+jar -uf $OPENRDF_SESAME_FILENAME.war WEB-INF/lib/*
+jar -uf $OPENRDF_WORKBENCH_FILENAME.war WEB-INF/lib/*
 
-mkdir -p jetty-distribution-8.1.9/webapps
-cp openrdf-sesame.war jetty-distribution-8.1.9/webapps
-cp openrdf-workbench.war jetty-distribution-8.1.9/webapps
-
-zip jetty-distribution-8.1.9.zip jetty-distribution-8.1.9/webapps/*
-
-rm -fr jetty-distribution-8.1.9
 rm -fr WEB-INF
-rm -f quest-distribution-1.7-alpha-dependencies.zip
+cd ..
+
+# Packaging the sesame jetty distribution
+#
+rm -fr $QUEST_JETTY_DIST
+mkdir $QUEST_JETTY_DIST
+cp $JETTY_COPY_PATH/$JETTY_COPY_FILENAME.zip $QUEST_SESAME_DIST/
+
+$JETTY_FOLDER=$JETTY_COPY_FILENAME
+cd $QUEST_JETTY_DIST
+mkdir -p $JETTY_FOLDER/webapps
+cp ../quest-distribution/$QUEST_SESAME_DIST/$OPENRDF_SESAME_FILENAME.war $JETTY_FOLDER/webapps
+cp ../quest-distribution/$QUEST_SESAME_DIST/$OPENRDF_WORKBENCH_FILENAME.war $JETTY_FOLDER/webapps
+
+zip $JETTY_COPY_FILENAME.zip $JETTY_FOLDER/webapps/*
+
+rm -fr $JETTY_FOLDER
+cd ..
+
+# Packaging the OWL-API distribution
+#
+rm -fr $OWL_API_DIST
+mkdir $OWL_API_DIST
+cp target/quest-distribution-1.7-alpha2-bin.zip $OWL_API_DIST/quest-distribution-1.7-alpha2-b$REVISION.zip
+
+echo "Done."
