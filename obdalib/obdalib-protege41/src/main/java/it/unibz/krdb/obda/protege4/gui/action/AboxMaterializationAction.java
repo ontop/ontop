ce176033eca37f350f26232ef061b8e6a7ae5fa5
Timea Bagosi <timea.bagosi@unibz.it>
2013-03-22 16:04:48 +0000

    add new materialization functionalities

--- obdalib/obdalib-protege41/src/main/java/it/unibz/krdb/obda/protege4/gui/action/AboxMaterializationAction.java
+++ obdalib/obdalib-protege41/src/main/java/it/unibz/krdb/obda/protege4/gui/action/AboxMaterializationAction.java
@@ -1,26 +1,46 @@
 package it.unibz.krdb.obda.protege4.gui.action;
 
 import it.unibz.krdb.obda.gui.swing.utils.OBDAProgessMonitor;
-import it.unibz.krdb.obda.gui.swing.utils.OBDAProgressListener;
 import it.unibz.krdb.obda.model.OBDAModel;
 import it.unibz.krdb.obda.model.impl.OBDAModelImpl;
-import it.unibz.krdb.obda.owlrefplatform.owlapi3.OWLAPI3IndividualIterator;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.OWLAPI3Materializer;
 import it.unibz.krdb.obda.protege4.core.OBDAModelManager;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
+import org.openrdf.rio.RDFWriter;
+import org.openrdf.rio.n3.N3Writer;
+import org.openrdf.rio.rdfxml.util.RDFXMLPrettyWriter;
+import org.openrdf.rio.turtle.TurtleWriter;
 import org.protege.editor.core.ui.action.ProtegeAction;
 import org.protege.editor.owl.OWLEditorKit;
 import org.protege.editor.owl.model.OWLModelManager;
 import org.protege.editor.owl.model.OWLWorkspace;
+import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyFormat;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import sesameWrapper.SesameMaterializer;
+import uk.ac.manchester.cs.owl.owlapi.OWLOntologyImpl;
 
 /***
  * Action to create individuals into the currently open OWL Ontology using the
@@ -34,12 +54,16 @@ public class AboxMaterializationAction extends ProtegeAction {
 
 	private OWLEditorKit editorKit = null;
 	private OBDAModel obdaModel = null;
+	private OWLWorkspace workspace;	
+	private OWLModelManager modelManager;
 	
 	private Logger log = LoggerFactory.getLogger(AboxMaterializationAction.class);
 	
 	@Override
 	public void initialise() throws Exception {
-		editorKit = (OWLEditorKit)getEditorKit();		
+		editorKit = (OWLEditorKit)getEditorKit();
+		workspace = editorKit.getWorkspace();	
+		modelManager = editorKit.getOWLModelManager();
 		obdaModel = ((OBDAModelManager)editorKit.get(OBDAModelImpl.class.getName())).getActiveOBDAModel();
 	}
 
@@ -50,9 +74,110 @@ public class AboxMaterializationAction extends ProtegeAction {
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
+			
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.add(new JLabel("Choose a materialization option: "), BorderLayout.NORTH);
 		
-		final OWLWorkspace workspace = editorKit.getWorkspace();	
+		JPanel radioPanel = new JPanel();
+		radioPanel.setLayout(new BorderLayout());
 		
+		JRadioButton b1 = new JRadioButton("Add individuals to current ontology", true);
+		JRadioButton b2 = new JRadioButton("Write individuals to file in format:\t");
+		String[] fileoptions = {"OWL", "N3", "TTL"};
+		JComboBox combo = new JComboBox(fileoptions);
+		JRadioButton b3 = new JRadioButton("Create a new ontology with the individuals");
+		
+		ButtonGroup group = new ButtonGroup();
+		group.add(b1); group.add(b2); group.add(b3);
+		
+		radioPanel.add(b1, BorderLayout.NORTH);
+		radioPanel.add(b2, BorderLayout.CENTER);
+		radioPanel.add(combo, BorderLayout.EAST);
+		radioPanel.add(b3, BorderLayout.SOUTH);
+		
+		panel.add(radioPanel, BorderLayout.SOUTH);
+		
+		int res = JOptionPane.showOptionDialog(workspace, panel, "Materialization options", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+		if (res == JOptionPane.OK_OPTION)
+		{
+			//we got something to do 
+			//take radio button choice
+			
+			//add to current onto
+			if (b1.isSelected())
+			{
+				materializeOnto(modelManager.getActiveOntology());	
+			}
+			//write to file
+			else if (b2.isSelected())
+			{
+				int outputFormat = combo.getSelectedIndex();
+				try {
+					materializeToFile(outputFormat);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			//create new onto, add
+			else if (b3.isSelected())
+			{
+				OWLOntology newOnto = cloneOnto();
+				materializeOnto(newOnto);
+				//open new onto in new window
+				
+			}
+		}
+		
+	}
+	
+	private void materializeToFile(int format) throws Exception
+	{
+		String fileName = "";
+		final JFileChooser fc = new JFileChooser();
+		fc.setSelectedFile(new File(fileName));
+		fc.showSaveDialog(workspace);
+		try {
+			File file = fc.getSelectedFile();
+			OutputStream out = new FileOutputStream(file);		
+			SesameMaterializer materializer = new SesameMaterializer(this.obdaModel);
+			RDFWriter writer = null;
+			if (format == 0) // owl = rdfxml
+			{
+				 writer = new RDFXMLPrettyWriter(out);
+				
+			} else if (format == 1) // n3
+			{
+				 writer = new N3Writer(out);
+				
+
+			} else if (format == 2) // ttl
+			{
+				writer = new TurtleWriter(out);
+			}
+			writer.startRDF();
+			while(materializer.hasNext())
+				writer.handleStatement(materializer.next());
+			writer.endRDF();
+			out.close();
+		} catch (FileNotFoundException e) {
+			return;
+		}
+	}
+	
+	private OWLOntology cloneOnto()
+	{
+		//create new onto by cloning this one
+		OWLOntology currentOnto = modelManager.getActiveOntology();
+		OWLOntologyManager ontologyManager = modelManager.getOWLOntologyManager();			
+		OWLOntology newOnto = new OWLOntologyImpl(ontologyManager, currentOnto.getOntologyID());
+		Set<OWLAxiom> axioms = currentOnto.getAxioms();
+		for(OWLAxiom axiom: axioms)
+			ontologyManager.addAxiom(newOnto, axiom);
+		return newOnto;
+	}
+	
+	private void materializeOnto(OWLOntology onto)
+	{
 		String message = "The plugin will generate several triples and save them in this current ontology.\n" +
 				"The operation may take some time and may require a lot of memory if the data volume is too high.\n\n" +
 				"Do you want to continue?";
@@ -61,10 +186,10 @@ public class AboxMaterializationAction extends ProtegeAction {
 		
 		if (response == JOptionPane.YES_OPTION) {			
 			try {
-				OWLModelManager modelManager = editorKit.getOWLModelManager();
-				OWLAPI3IndividualIterator individuals = new OWLAPI3IndividualIterator(obdaModel);
-				
-				final MaterializeAction action = new MaterializeAction(modelManager, individuals);
+			
+				OWLAPI3Materializer individuals = new OWLAPI3Materializer(obdaModel);
+				Container container = workspace.getRootPane().getParent();
+				final MaterializeAction action = new MaterializeAction(onto, modelManager, individuals, container);
 				
 				Thread th = new Thread(new Runnable() {
 					@Override
@@ -96,70 +221,5 @@ public class AboxMaterializationAction extends ProtegeAction {
 		}
 	}
 
-	private class MaterializeAction implements OBDAProgressListener {
-
-		private Thread thread = null;
-		private CountDownLatch latch = null;
-		
-		private OWLOntology currentOntology = null;
-		private OWLOntologyManager ontologyManager = null;
-		private OWLAPI3IndividualIterator iterator = null;
-		
-		private boolean bCancel = false;
-
-		public MaterializeAction(OWLModelManager modelManager, OWLAPI3IndividualIterator iterator) {
-			currentOntology = modelManager.getActiveOntology();
-			ontologyManager = modelManager.getOWLOntologyManager();			
-			this.iterator = iterator;
-		}
-
-		public void setCountdownLatch(CountDownLatch cdl){
-			latch = cdl;
-		}
-		
-		public void run() {
-			if (latch == null){
-				try {
-					throw new Exception("No CountDownLatch set");
-				}
-				catch (Exception e) {
-					log.error(e.getMessage(), e);
-					JOptionPane.showMessageDialog(null, "ERROR: could not materialize abox.");
-					return;
-				}
-			}
-			
-			thread = new Thread() {
-				public void run() {
-					try {
-						while(iterator.hasNext()) {
-							ontologyManager.addAxiom(currentOntology, iterator.next());
-						}
-						
-						latch.countDown();
-						Container cont = AboxMaterializationAction.this.getWorkspace().getRootPane().getParent();
-						if(!bCancel){
-							JOptionPane.showMessageDialog(cont, "Task is completed", "Done", JOptionPane.INFORMATION_MESSAGE);
-						}
-					}
-					catch (Exception e) {
-						latch.countDown();
-						log.error("Materialization of Abox failed", e);
-					}
-				}
-			};
-			thread.start();
-		}
-
-		@Override
-		public void actionCanceled() {
-			if (thread != null) {
-				bCancel = true;
-				latch.countDown();
-				thread.interrupt();
-			}
-		}
-
-	}
-
+	
 }
---
+++ obdalib/obdalib-protege41/src/main/java/it/unibz/krdb/obda/protege4/gui/action/MaterializeAction.java
@@ -0,0 +1,83 @@
+package it.unibz.krdb.obda.protege4.gui.action;
+
+import it.unibz.krdb.obda.gui.swing.utils.OBDAProgressListener;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.OWLAPI3Materializer;
+
+import java.awt.Container;
+import java.util.concurrent.CountDownLatch;
+
+import javax.swing.JOptionPane;
+
+import org.protege.editor.owl.model.OWLModelManager;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MaterializeAction implements OBDAProgressListener {
+
+	private Logger log = LoggerFactory.getLogger(MaterializeAction.class);
+	private Thread thread = null;
+	private CountDownLatch latch = null;
+	
+	private OWLOntology currentOntology = null;
+	private OWLOntologyManager ontologyManager = null;
+	private OWLAPI3Materializer materializer = null;
+	private Container cont = null;
+	private boolean bCancel = false;
+
+	public MaterializeAction(OWLOntology currentOntology, OWLModelManager modelManager, OWLAPI3Materializer iterator, Container cont) {
+		this.currentOntology = currentOntology;
+		ontologyManager = modelManager.getOWLOntologyManager();			
+		this.materializer = iterator;
+		this.cont = cont;  
+	}
+
+	public void setCountdownLatch(CountDownLatch cdl){
+		latch = cdl;
+	}
+	
+	public void run() {
+		if (latch == null){
+			try {
+				throw new Exception("No CountDownLatch set");
+			}
+			catch (Exception e) {
+				log.error(e.getMessage(), e);
+				JOptionPane.showMessageDialog(null, "ERROR: could not materialize abox.");
+				return;
+			}
+		}
+		
+		thread = new Thread() {
+			public void run() {
+				try {
+					while(materializer.hasNext()) {
+						ontologyManager.addAxiom(currentOntology, materializer.next());
+					}
+					
+					latch.countDown();
+					if(!bCancel){
+						JOptionPane.showMessageDialog(cont, "Task is completed", "Done", JOptionPane.INFORMATION_MESSAGE);
+					}
+				}
+				catch (Exception e) {
+					latch.countDown();
+					log.error("Materialization of Abox failed", e);
+				}
+			}
+		};
+		thread.start();
+	}
+
+	@Override
+	public void actionCanceled() {
+		if (thread != null) {
+			bCancel = true;
+			latch.countDown();
+			thread.interrupt();
+		}
+	}
+
+}
+
