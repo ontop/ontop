e541eefac9d6ca165d014ff850ff291792eaa08d
Josef Hardi <josef.hardi@gmail.com>
2013-04-12 11:20:55 +0000

    Adding new constants for TRUE and FALSE.

--- obdalib/reformulation-core/src/main/java/it/unibz/krdb/obda/owlrefplatform/core/QuestConstants.java
+++ obdalib/reformulation-core/src/main/java/it/unibz/krdb/obda/owlrefplatform/core/QuestConstants.java
@@ -1,25 +1,26 @@
 package it.unibz.krdb.obda.owlrefplatform.core;
 
-
 public class QuestConstants {
 	
-	//Reformulation Techniques
+	// Reformulation Techniques
 	public static final String PERFECTREFORMULATION = "PerfectRef";
 	public static final String UCQBASED = "UCQ-based";
 	public static final String TW = "TreeWitness";
 	
-	//ABox modes
+	// ABox modes
 	public static final String CLASSIC = "classic";
 	public static final String VIRTUAL = "virtual";
 	
-	//DBtype
+	// DBtype
 	public static final String SEMANTIC = "semantic";
 	public static final String UNIVERSAL = "universal";
 	public static final String DIRECT = "direct";
 	
-	//storage location
+	// Storage location
 	public static final String JDBC = "jdbc";
 	public static final String INMEMORY = "h2-inmemory";
 	
-	
+	// Boolean value
+	public static final String TRUE = "true";
+	public static final String FALSE = "false";
 }
