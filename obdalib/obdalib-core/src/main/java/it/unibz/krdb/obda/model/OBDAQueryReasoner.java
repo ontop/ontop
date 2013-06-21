280e6457b0fc17154c63dce7a21e5c2f0ebe226e
Josef Hardi <josef.hardi@gmail.com>
2013-06-21 07:23:48 +0000

    Code refactor: remove unused classes in obdalib-core package. The removal involves removing test cases in ReformulationTest.

--- obdalib/obdalib-core/src/main/java/it/unibz/krdb/obda/model/OBDAQueryReasoner.java
+++
@@ -1,8 +0,0 @@
-package it.unibz.krdb.obda.model;
-
-public interface OBDAQueryReasoner {
-
-	public OBDAStatement getStatement() throws Exception;
-
-	public OBDAConnection getConnection() throws OBDAException;	
-}
--- obdalib/obdalib-core/src/main/java/it/unibz/krdb/obda/ontology/GeneralClassDescription.java
+++
@@ -1,5 +0,0 @@
-package it.unibz.krdb.obda.ontology;
-
-public interface GeneralClassDescription extends ClassDescription {
-	// NO-OP
-}
