34bc3b5ac1c9eeef37a33e75046808d368088d35
Timea Bagosi <timea.bagosi@unibz.it>
2013-07-05 15:00:29 +0000

    fix: give warning upon importing mapping with bnode, throw exception and warning if duplicate mapping ids occurred

--- obdalib/obdalib-protege41/src/main/java/it/unibz/krdb/obda/protege4/gui/action/R2RMLImportAction.java
+++ obdalib/obdalib-protege41/src/main/java/it/unibz/krdb/obda/protege4/gui/action/R2RMLImportAction.java
@@ -1,5 +1,6 @@
 package it.unibz.krdb.obda.protege4.gui.action;
 
+import it.unibz.krdb.obda.exception.DuplicateMappingException;
 import it.unibz.krdb.obda.io.R2RMLReader;
 import it.unibz.krdb.obda.model.OBDAMappingAxiom;
 import it.unibz.krdb.obda.model.OBDAModel;
@@ -9,7 +10,6 @@ import it.unibz.krdb.obda.protege4.core.OBDAModelManager;
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.net.URI;
-import java.util.List;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
@@ -26,13 +26,14 @@ public class R2RMLImportAction extends ProtegeAction {
 
 	private OWLEditorKit editorKit = null;
 	private OBDAModel obdaModel = null;
-	
+
 	private Logger log = LoggerFactory.getLogger(R2RMLImportAction.class);
-	
+
 	@Override
 	public void initialise() throws Exception {
-		editorKit = (OWLEditorKit)getEditorKit();		
-		obdaModel = ((OBDAModelManager)editorKit.get(OBDAModelImpl.class.getName())).getActiveOBDAModel();
+		editorKit = (OWLEditorKit) getEditorKit();
+		obdaModel = ((OBDAModelManager) editorKit.get(OBDAModelImpl.class
+				.getName())).getActiveOBDAModel();
 	}
 
 	@Override
@@ -42,34 +43,42 @@ public class R2RMLImportAction extends ProtegeAction {
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		
+
 		final OWLWorkspace workspace = editorKit.getWorkspace();
-		
+
 		String message = "The imported mappings will be appended to the existing data source. Continue?";
-		int response = JOptionPane.showConfirmDialog(workspace, message, "Confirmation", JOptionPane.YES_NO_OPTION);
-		
+		int response = JOptionPane.showConfirmDialog(workspace, message,
+				"Confirmation", JOptionPane.YES_NO_OPTION);
+
 		if (response == JOptionPane.YES_OPTION) {
-			
+
 			final JFileChooser fc = new JFileChooser();
 			fc.showOpenDialog(workspace);
+			File file = null;
 			try {
 
-				File file = (fc.getSelectedFile());
-				if (file != null)
-				{
+				file = (fc.getSelectedFile());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			if (file != null) {
 				R2RMLReader reader = new R2RMLReader(file);
 
 				URI sourceID = obdaModel.getSources().get(0).getSourceID();
 
-				obdaModel.addMappings(sourceID, reader.readMappings());
+				try {
+					for (OBDAMappingAxiom mapping : reader.readMappings()) {
+						if (mapping.getTargetQuery().toString().contains("BNODE"))
+							JOptionPane.showMessageDialog(workspace, "The mapping "+mapping.getId()+" contains BNode. -ontoPro- does not support it yet.");
+						else
+							obdaModel.addMapping(sourceID, mapping);
+					}
+				} catch (DuplicateMappingException dm) {
+					JOptionPane.showMessageDialog(workspace, "Duplicate mapping id found. Please correct the Resource node name: "+dm.getLocalizedMessage());
+					throw new RuntimeException("Duplicate mapping found: "+dm.getMessage());
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-
 		}
-        
 
 	}
-
+	}
 }
