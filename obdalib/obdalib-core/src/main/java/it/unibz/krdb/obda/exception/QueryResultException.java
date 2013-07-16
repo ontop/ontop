a21072c67ac656785c49a7da30d8b51f7524edf7
Josef Hardi <josef.hardi@gmail.com>
2013-07-16 15:44:02 +0000

    code tidy.

--- obdalib/obdalib-core/src/main/java/it/unibz/krdb/obda/exception/QueryResultException.java
+++ obdalib/obdalib-core/src/main/java/it/unibz/krdb/obda/exception/QueryResultException.java
@@ -2,28 +2,20 @@ package it.unibz.krdb.obda.exception;
 
 public class QueryResultException extends Exception {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 6941970620516544365L;
+	private static final long serialVersionUID = 1L;
 
 	public QueryResultException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public QueryResultException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public QueryResultException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public QueryResultException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
-
 }
--- obdalib/obdalib-core/src/main/java/it/unibz/krdb/obda/gui/swing/exception/NoDatasourceSelectedException.java
+++ obdalib/obdalib-core/src/main/java/it/unibz/krdb/obda/gui/swing/exception/NoDatasourceSelectedException.java
@@ -1,23 +1,8 @@
-/***
- * Copyright (c) 2008, Mariano Rodriguez-Muro.
- * All rights reserved.
- *
- * The OBDA-API is licensed under the terms of the Lesser General Public
- * License v.3 (see OBDAAPI_LICENSE.txt for details). The components of this
- * work include:
- * 
- * a) The OBDA-API developed by the author and licensed under the LGPL; and, 
- * b) third-party components licensed under terms that may be different from 
- *   those of the LGPL.  Information about such licenses can be found in the 
- *   file named OBDAAPI_3DPARTY-LICENSES.txt.
- */
 package it.unibz.krdb.obda.gui.swing.exception;
 
 public class NoDatasourceSelectedException extends Exception {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -8023276592715555690L;
+
+	private static final long serialVersionUID = 1L;
 
 	public NoDatasourceSelectedException(String msg) {
 		super(msg);
