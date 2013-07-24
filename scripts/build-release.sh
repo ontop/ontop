bd797e984aa6f4bbeefaa5fbf73dd9695fc8982f
Josef Hardi <josef.hardi@gmail.com>
2013-07-24 17:21:29 +0200

    Move the test and build scripts to its own folder.

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -19,7 +19,7 @@ export JDBC_PLUGINS_PATH=$ONTOP_DEP_HOME
 export PROTEGE_COPY_PATH=$ONTOP_DEP_HOME
 export PROTEGE_COPY_FILENAME=protege-4.2-beta.284
 export PROTEGE_MAIN_FOLDER_NAME=Protege_4.2
-export PROTEGE_MAIN_PLUGIN=ontoProPlugin
+export PROTEGE_MAIN_PLUGIN=ontopro-plugin
 
 # location and name for jetty distribution (should be ZIP)
 export JETTY_COPY_PATH=$ONTOP_DEP_HOME
@@ -41,7 +41,7 @@ export QUEST_JETTY_DIST=QuestJetty
 export OWL_API_DIST=QuestOWL
 
 export VERSION=1.8
-export REVISION=2557
+export REVISION=0
 
 #svn update
 mvn clean
@@ -52,7 +52,7 @@ echo ""
 echo "========================================="
 echo " Making -ontopPro- distribution package"
 echo "-----------------------------------------"
-echo "pluginVersion=$VERSION-b$REVISION" >  obdalib-core/src/main/resources/it/unibz/krdb/obda/utils/version.properties
+echo "pluginVersion=$VERSION.$REVISION" >  obdalib-core/src/main/resources/it/unibz/krdb/obda/utils/version.properties
 rm -fr obdalib-protege41/dist
 mvn install -DskipTests
 cd obdalib-protege41/
@@ -60,21 +60,21 @@ mvn bundle:bundle -DskipTests
 
 rm -fr ../quest-distribution/$PROTEGE_DIST
 mkdir ../quest-distribution/$PROTEGE_DIST
-cp target/it.unibz.inf.obda.p4plugin-$VERSION.jar ../quest-distribution/$PROTEGE_DIST/it.unibz.inf.obda.p4plugin-$VERSION-b$REVISION.jar
+cp target/it.unibz.inf.obda.p4plugin-$VERSION.jar ../quest-distribution/$PROTEGE_DIST/it.unibz.inf.obda.p4plugin-$VERSION.$REVISION.jar
 cp $PROTEGE_COPY_PATH/$PROTEGE_COPY_FILENAME.zip ../quest-distribution/$PROTEGE_DIST/
 
 cd ../quest-distribution/$PROTEGE_DIST/
 
 mkdir -p $PROTEGE_MAIN_FOLDER_NAME/plugins
-cp it.unibz.inf.obda.p4plugin-$VERSION-b$REVISION.jar $PROTEGE_MAIN_FOLDER_NAME/plugins/
+cp it.unibz.inf.obda.p4plugin-$VERSION.$REVISION.jar $PROTEGE_MAIN_FOLDER_NAME/plugins/
 cp $JDBC_PLUGINS_PATH/org.protege.osgi.jdbc.jar $PROTEGE_MAIN_FOLDER_NAME/plugins/
 cp $JDBC_PLUGINS_PATH/org.protege.osgi.jdbc.prefs.jar $PROTEGE_MAIN_FOLDER_NAME/plugins/
-zip ../$PROTEGE_DIST/$PROTEGE_MAIN_PLUGIN.zip $PROTEGE_MAIN_FOLDER_NAME/plugins/*.*
+zip ../$PROTEGE_DIST/$PROTEGE_MAIN_PLUGIN-$VERSION.$REVISION.zip $PROTEGE_MAIN_FOLDER_NAME/plugins/*.*
 
 zip $PROTEGE_COPY_FILENAME.zip $PROTEGE_MAIN_FOLDER_NAME/plugins/*
-mv $PROTEGE_COPY_FILENAME.zip $PROTEGE_COPY_FILENAME-ontop-$VERSION-b$REVISION.zip
+mv $PROTEGE_COPY_FILENAME.zip $PROTEGE_MAIN_PLUGIN-with-protege-$VERSION.$REVISION.zip
 
-rm -fr $PROTEGE_COPY_FILENAME
+rm -fr $PROTEGE_MAIN_FOLDER_NAME
 cd ..
 
 # Packing the sesame distribution
@@ -86,7 +86,7 @@ echo "-----------------------------------------"
 rm -fr $QUEST_SESAME_DIST
 mkdir -p $QUEST_SESAME_DIST/WEB-INF/lib
 mvn assembly:assembly -DskipTests
-cp target/quest-distribution-$VERSION-sesame-bin.jar $QUEST_SESAME_DIST/WEB-INF/lib/quest-distribution-$VERSION-sesame-b$REVISION.jar
+cp target/quest-distribution-$VERSION-sesame-bin.jar $QUEST_SESAME_DIST/WEB-INF/lib/ontop-distribution-$VERSION.$REVISION.jar
 unzip -q -d $QUEST_SESAME_DIST/WEB-INF/lib/ target/quest-distribution-$VERSION-dependencies.zip
 cp $OPENRDF_SESAME_PATH/$OPENRDF_SESAME_FILENAME.war $QUEST_SESAME_DIST/
 cp $OPENRDF_WORKBENCH_PATH/$OPENRDF_WORKBENCH_FILENAME.war $QUEST_SESAME_DIST/
@@ -110,7 +110,7 @@ echo " Making Sesame Jetty distribution package"
 echo "-----------------------------------------"
 rm -fr $QUEST_JETTY_DIST
 mkdir $QUEST_JETTY_DIST
-cp $JETTY_COPY_PATH/$JETTY_COPY_FILENAME.zip $QUEST_JETTY_DIST/
+cp $JETTY_COPY_PATH/$JETTY_COPY_FILENAME.zip $QUEST_JETTY_DIST/ontop-with-jetty-$VERSION.$REVISION.zip
 
 export JETTY_FOLDER=$JETTY_INNER_FOLDERNAME
 cd $QUEST_JETTY_DIST
@@ -118,7 +118,7 @@ mkdir -p $JETTY_INNER_FOLDERNAME/webapps
 cp ../$QUEST_SESAME_DIST/$OPENRDF_SESAME_FILENAME.war $JETTY_FOLDER/webapps
 cp ../$QUEST_SESAME_DIST/$OPENRDF_WORKBENCH_FILENAME.war $JETTY_FOLDER/webapps
 
-zip $JETTY_COPY_FILENAME.zip $JETTY_FOLDER/webapps/*
+zip ontop-with-jetty-$VERSION.$REVISION.zip $JETTY_FOLDER/webapps/*
 
 rm -fr $JETTY_FOLDER
 cd ..
@@ -132,7 +132,7 @@ echo "-----------------------------------------"
 rm -fr $OWL_API_DIST
 mkdir $OWL_API_DIST
 echo "[INFO] Copying files..."
-cp target/quest-distribution-$VERSION-bin.zip $OWL_API_DIST/quest-distribution-$VERSION-b$REVISION.zip
+cp target/quest-distribution-$VERSION-bin.zip $OWL_API_DIST/ontop-distribution-$VERSION.$REVISION.zip
 
 echo ""
 echo "Done."
