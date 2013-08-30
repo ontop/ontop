0b97d654019130f9a53a14df224b1d24c09dddaf
marianormuro <mariano.muro@gmail.com>
2013-08-30 14:29:32 +0200

    New test case that puts URI's in WHERE clauses that should be matched to either constants in from a mapping, or templates from a mapping.

---
+++ quest-owlapi3/src/test/java/it/unibz/krdb/obda/reformulation/tests/LungCancerH2TestVirtual.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright (C) 2009-2013, Free University of Bozen Bolzano
+ * This source code is available under the terms of the Affero General Public
+ * License v3.
+ * 
+ * Please see LICENSE.txt for full license terms, including the availability of
+ * proprietary exceptions.
+ */
+package it.unibz.krdb.obda.reformulation.tests;
+
+import it.unibz.krdb.obda.io.ModelIOManager;
+import it.unibz.krdb.obda.model.OBDADataFactory;
+import it.unibz.krdb.obda.model.OBDAModel;
+import it.unibz.krdb.obda.model.impl.OBDADataFactoryImpl;
+import it.unibz.krdb.obda.owlrefplatform.core.QuestConstants;
+import it.unibz.krdb.obda.owlrefplatform.core.QuestPreferences;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWL;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLConnection;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLFactory;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLResultSet;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLStatement;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLIndividual;
+import org.semanticweb.owlapi.model.OWLLiteral;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/***
+ * A simple test that check if the system is able to handle Mappings for
+ * classes/roles and attributes even if there are no URI templates. i.e., the
+ * database stores URI's directly.
+ * 
+ * We are going to create an H2 DB, the .sql file is fixed. We will map directly
+ * there and then query on top.
+ */
+public class LungCancerH2TestVirtual extends TestCase {
+
+	// TODO We need to extend this test to import the contents of the mappings
+	// into OWL and repeat everything taking form OWL
+
+	private OBDADataFactory fac;
+	private Connection conn;
+
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	private OBDAModel obdaModel;
+	private OWLOntology ontology;
+
+	final String owlfile = "src/test/resources/test/lung-cancer3.owl";
+	final String obdafile = "src/test/resources/test/lung-cancer3.obda";
+
+	@Override
+	public void setUp() throws Exception {
+		
+		
+		/*
+		 * Initializing and H2 database with the stock exchange data
+		 */
+		// String driver = "org.h2.Driver";
+		String url = "jdbc:h2:mem:questjunitdb";
+		String username = "sa";
+		String password = "";
+
+		fac = OBDADataFactoryImpl.getInstance();
+
+		conn = DriverManager.getConnection(url, username, password);
+		Statement st = conn.createStatement();
+
+		FileReader reader = new FileReader("src/test/resources/test/lung-cancer3-create-h2.sql");
+		BufferedReader in = new BufferedReader(reader);
+		StringBuilder bf = new StringBuilder();
+		String line = in.readLine();
+		while (line != null) {
+			bf.append(line + "\n");
+			line = in.readLine();
+		}
+
+		st.executeUpdate(bf.toString());
+		conn.commit();
+
+		// Loading the OWL file
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		ontology = manager.loadOntologyFromOntologyDocument((new File(owlfile)));
+
+		// Loading the OBDA data
+		obdaModel = fac.getOBDAModel();
+		
+		ModelIOManager ioManager = new ModelIOManager(obdaModel);
+		ioManager.load(obdafile);
+		
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		try {
+			dropTables();
+			conn.close();
+		} catch (Exception e) {
+			log.debug(e.getMessage());
+		}
+	}
+
+	private void dropTables() throws SQLException, IOException {
+
+		Statement st = conn.createStatement();
+
+		FileReader reader = new FileReader("src/test/resources/test/lung-cancer3-drop-h2.sql");
+		BufferedReader in = new BufferedReader(reader);
+		StringBuilder bf = new StringBuilder();
+		String line = in.readLine();
+		while (line != null) {
+			bf.append(line);
+			line = in.readLine();
+		}
+
+		st.executeUpdate(bf.toString());
+		st.close();
+		conn.commit();
+	}
+
+	private void runTests(Properties p) throws Exception {
+
+		// Creating a new instance of the reasoner
+		QuestOWLFactory factory = new QuestOWLFactory();
+		factory.setOBDAController(obdaModel);
+
+		factory.setPreferenceHolder(p);
+
+		QuestOWL reasoner = (QuestOWL) factory.createReasoner(ontology, new SimpleConfiguration());
+
+		// Now we are ready for querying
+		QuestOWLConnection conn = reasoner.getConnection();
+		QuestOWLStatement st = conn.createStatement();
+
+		String query1 = "PREFIX : <http://example.org/> SELECT * WHERE { ?x :hasNeoplasm <http://example.org/db1/neoplasm/1> }";
+		String query2 = "PREFIX : <http://example.org/> SELECT * WHERE { <http://example.org/db1/1> :hasNeoplasm ?y }";
+		String query3 = "PREFIX : <http://example.org/> SELECT * WHERE { ?y :hasStage <http://example.org/stages/II> }";
+		String query4 = "PREFIX : <http://example.org/> SELECT * WHERE { ?y :hasStage <http://example.org/stages/limited> }";
+		
+		
+		
+//		String query = "PREFIX : <http://example.org/> SELECT * WHERE { ?x :hasNeoplasm ?y }";
+		
+		try {
+			executeQueryAssertResults(query3, st, 1);
+			executeQueryAssertResults(query4, st, 2);
+			executeQueryAssertResults(query1, st, 1);
+			executeQueryAssertResults(query2, st, 1);
+			
+			
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			try {
+
+			} catch (Exception e) {
+				st.close();
+			}
+			conn.close();
+			reasoner.dispose();
+		}
+	}
+	
+	public void executeQueryAssertResults(String query, QuestOWLStatement st, int expectedRows) throws Exception {
+		QuestOWLResultSet rs = st.executeTuple(query);
+		int count = 0;
+		while (rs.nextRow()) {
+			count++;
+			for (int i = 1; i <= rs.getColumCount(); i++) {
+				System.out.print(rs.getSignature().get(i-1));
+				System.out.print("=" + rs.getOWLObject(i));
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+		rs.close();
+		assertEquals(expectedRows, count);
+	}
+
+	public void testViEqSig() throws Exception {
+
+		QuestPreferences p = new QuestPreferences();
+		p.setCurrentValueOf(QuestPreferences.ABOX_MODE, QuestConstants.VIRTUAL);
+		p.setCurrentValueOf(QuestPreferences.OPTIMIZE_EQUIVALENCES, "true");
+		p.setCurrentValueOf(QuestPreferences.OPTIMIZE_TBOX_SIGMA, "true");
+
+		runTests(p);
+	}
+	
+//	public void testClassicEqSig() throws Exception {
+//
+//		QuestPreferences p = new QuestPreferences();
+//		p.setCurrentValueOf(QuestPreferences.ABOX_MODE, QuestConstants.CLASSIC);
+//		p.setCurrentValueOf(QuestPreferences.OPTIMIZE_EQUIVALENCES, "true");
+//		p.setCurrentValueOf(QuestPreferences.OPTIMIZE_TBOX_SIGMA, "true");
+//		p.setCurrentValueOf(QuestPreferences.OBTAIN_FROM_MAPPINGS, "true");
+//
+//		runTests(p);
+//	}
+
+
+}
