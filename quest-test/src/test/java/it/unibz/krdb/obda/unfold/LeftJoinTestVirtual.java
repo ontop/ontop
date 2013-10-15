f7e84c25068a69b47ee0df0b760e1745846d98c6
mislusnys <mindaugas.slusnys1@unibz.it>
2013-10-15 17:34:26 +0200

    Fixed OPTIONAL/Left Join bug where nested LJs were handled incorrectly, added a Test (5 cases)

---
+++ quest-test/src/test/java/it/unibz/krdb/obda/unfold/LeftJoinTestVirtual.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (C) 2009-2013, Free University of Bozen Bolzano
+ * This source code is available under the terms of the Affero General Public
+ * License v3.
+ * 
+ * Please see LICENSE.txt for full license terms, including the availability of
+ * proprietary exceptions.
+ */
+package it.unibz.krdb.obda.unfold;
+
+import it.unibz.krdb.obda.io.ModelIOManager;
+import it.unibz.krdb.obda.model.OBDADataFactory;
+import it.unibz.krdb.obda.model.OBDAModel;
+import it.unibz.krdb.obda.model.impl.OBDADataFactoryImpl;
+import it.unibz.krdb.obda.owlrefplatform.core.QuestConstants;
+import it.unibz.krdb.obda.owlrefplatform.core.QuestPreferences;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWL;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLConnection;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLFactory;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLResultSet;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLStatement;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class LeftJoinTestVirtual extends TestCase {
+
+	private OBDADataFactory fac;
+	private Connection conn;
+
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	private OBDAModel obdaModel;
+	private OWLOntology ontology;
+
+	final String owlfile = "src/test/resources/person.owl";
+	final String obdafile = "src/test/resources/person.obda";
+
+	@Override
+	public void setUp() throws Exception {
+
+		fac = OBDADataFactoryImpl.getInstance();
+		
+		// Loading the OWL file
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		ontology = manager.loadOntologyFromOntologyDocument((new File(owlfile)));
+
+		// Loading the OBDA data
+		obdaModel = fac.getOBDAModel();
+		
+		ModelIOManager ioManager = new ModelIOManager(obdaModel);
+		ioManager.load(obdafile);
+		
+	}
+
+	private void runTests(Properties p) throws Exception {
+
+		// Creating a new instance of the reasoner
+		QuestOWLFactory factory = new QuestOWLFactory();
+		factory.setOBDAController(obdaModel);
+
+		factory.setPreferenceHolder(p);
+
+		QuestOWL reasoner = (QuestOWL) factory.createReasoner(ontology, new SimpleConfiguration());
+
+		// Now we are ready for querying
+		QuestOWLConnection conn = reasoner.getConnection();
+		QuestOWLStatement st = conn.createStatement();
+
+		String query1 = "PREFIX : <http://www.semanticweb.org/mindaugas/ontologies/2013/9/untitled-ontology-58#> SELECT * WHERE {?p a :Person . ?p :name ?name . ?p :age ?age }";
+		String query2 = "PREFIX : <http://www.semanticweb.org/mindaugas/ontologies/2013/9/untitled-ontology-58#> SELECT * WHERE {?p a :Person . ?p :name ?name . OPTIONAL {?p :nick11 ?nick1} OPTIONAL {?p :nick22 ?nick2} }";		
+		String query3 = "PREFIX : <http://www.semanticweb.org/mindaugas/ontologies/2013/9/untitled-ontology-58#> SELECT * WHERE {?p a :Person . ?p :name ?name . OPTIONAL {?p :nick11 ?nick1} }";
+		String query4 = "PREFIX : <http://www.semanticweb.org/mindaugas/ontologies/2013/9/untitled-ontology-58#> SELECT * WHERE {?p a :Person . ?p :name ?name . OPTIONAL {?p :nick1 ?nick1} OPTIONAL {?p :nick2 ?nick2} }";
+		String query5 = "PREFIX : <http://www.semanticweb.org/mindaugas/ontologies/2013/9/untitled-ontology-58#> SELECT * WHERE {?p a :Person . ?p :name ?name . OPTIONAL {?p :age ?age} }";
+		
+		try {
+			executeQueryAssertResults(query1, st, 3);
+			executeQueryAssertResults(query2, st, 4);
+			executeQueryAssertResults(query3, st, 4);
+			executeQueryAssertResults(query4, st, 4);
+			executeQueryAssertResults(query5, st, 4);
+			
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			try {
+
+			} catch (Exception e) {
+				st.close();
+			}
+			conn.close();
+			reasoner.dispose();
+		}
+	}
+	
+	public void executeQueryAssertResults(String query, QuestOWLStatement st, int expectedRows) throws Exception {
+		QuestOWLResultSet rs = st.executeTuple(query);
+		int count = 0;
+		while (rs.nextRow()) {
+			count++;
+			for (int i = 1; i <= rs.getColumCount(); i++) {
+				System.out.print(rs.getSignature().get(i-1));
+				System.out.print("=" + rs.getOWLObject(i));
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+		rs.close();
+		assertEquals(expectedRows, count);
+	}
+	
+//	public void executeGraphQueryAssertResults(String query, QuestOWLStatement st, int expectedRows) throws Exception {
+//		List<OWLAxiom> rs = st.executeGraph(query);
+//		int count = 0;
+//		Iterator<OWLAxiom> axit = rs.iterator();
+//		while (axit.hasNext()) {
+//			System.out.println(axit.next());			
+//			count++;
+//		}		
+//		assertEquals(expectedRows, count);
+//	}
+
+	public void testLeftJoin() throws Exception {
+
+		QuestPreferences p = new QuestPreferences();
+//		p.setCurrentValueOf(QuestPreferences.ABOX_MODE, QuestConstants.VIRTUAL);
+//		p.setCurrentValueOf(QuestPreferences.OPTIMIZE_EQUIVALENCES, "true");
+//		p.setCurrentValueOf(QuestPreferences.OPTIMIZE_TBOX_SIGMA, "true");
+
+		runTests(p);
+	}
+
+}
