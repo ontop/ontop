60a5465f387867c25f4c9393eb8e463aca4cbc8e
tibagosi <timea.bagosi@unibz.it>
2013-10-24 09:53:08 +0200

    test case for inconsistency checking

--- quest-owlapi3/src/test/java/it/unibz/krdb/obda/reformulation/owlapi3/InconsistencyCheckingTest.java
+++ quest-owlapi3/src/test/java/it/unibz/krdb/obda/reformulation/owlapi3/InconsistencyCheckingTest.java
@@ -1,62 +1,79 @@
 package it.unibz.krdb.obda.reformulation.owlapi3;
 
-import static org.junit.Assert.assertFalse;
-import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.*;
-
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.Class;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.ClassAssertion;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.DataProperty;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.DataPropertyRange;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.Declaration;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.DisjointClasses;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.DisjointObjectProperties;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.DisjointDataProperties;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.Integer;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.Datatype;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.ObjectProperty;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.NamedIndividual;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.ObjectPropertyAssertion;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.DataPropertyAssertion;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.FunctionalDataProperty;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.FunctionalObjectProperty;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.Literal;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.Ontology;
 import it.unibz.krdb.obda.owlrefplatform.core.QuestConstants;
 import it.unibz.krdb.obda.owlrefplatform.core.QuestPreferences;
 import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLFactory;
+import junit.framework.TestCase;
 
 import org.junit.Test;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLClassAxiom;
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLDataPropertyRangeAxiom;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.vocab.OWL2Datatype;
 
-import uk.ac.manchester.cs.owl.owlapi.OWLClassAssertionImpl;
-import uk.ac.manchester.cs.owl.owlapi.OWLClassAxiomImpl;
-import uk.ac.manchester.cs.owl.owlapi.OWLClassImpl;
+public class InconsistencyCheckingTest extends TestCase{
 
-public class InconsistencyCheckingTest {
+	private OWLReasoner reasoner;
+	private OWLOntology ontology;
+	private OWLOntologyManager manager;
+	
+	String prefix = "http://www.example.org/";
+	OWLClass c1 = Class(IRI.create(prefix + "Male"));
+	OWLClass c2 = Class(IRI.create(prefix + "Female"));
+	
+	OWLObjectProperty r1 = ObjectProperty(IRI.create(prefix + "hasMother"));
+	OWLObjectProperty r2 = ObjectProperty(IRI.create(prefix + "hasFather"));
+	
+	OWLDataProperty d1 = DataProperty(IRI.create(prefix + "hasName"));
+	OWLDataProperty d2 = DataProperty(IRI.create(prefix + "hasAge"));
+	OWLDataPropertyRangeAxiom ra1 = DataPropertyRange(d1, Datatype(IRI.create(OWL2Datatype.XSD_STRING.getURI())));
+	OWLDataPropertyRangeAxiom ra2 = DataPropertyRange(d2, Integer());
 
-	@Test
-	public void testInconsistency() throws OWLOntologyCreationException {
-
-		String prefix = "http://www.example.org/";
-
-		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-
-		OWLClass c1 = Class(IRI.create(prefix + "Male"));
-		OWLClass c2 = Class(IRI.create(prefix + "Female"));
-		
-		OWLObjectProperty r1 = ObjectProperty(IRI.create(prefix + "#R1"));
-		OWLObjectProperty r1_1 = ObjectProperty(IRI.create(prefix + "#R1_1"));
-		OWLObjectProperty r2 = ObjectProperty(IRI.create(prefix + "#R2"));
-
-		OWLNamedIndividual a = NamedIndividual(IRI.create(prefix + "#a"));
-		OWLNamedIndividual b = NamedIndividual(IRI.create(prefix + "#b"));
-
-		OWLOntology ontology = Ontology(manager, //
+	OWLNamedIndividual a = NamedIndividual(IRI.create(prefix + "a"));
+	OWLNamedIndividual b = NamedIndividual(IRI.create(prefix + "b"));
+	OWLNamedIndividual c = NamedIndividual(IRI.create(prefix + "c"));
+	
+	@Override
+	public void setUp() throws Exception {
+		manager = OWLManager.createOWLOntologyManager();
+		ontology = Ontology(manager, //
 				Declaration(c1),
 				Declaration(c2),
 				Declaration(r1), //
-				Declaration(r1_1), //
 				Declaration(r2), //
-				SubObjectPropertyOf(r1_1, r2), //
-				DisjointObjectProperties(r1, r2), //
-				ClassAssertion(c1, a),
-				ClassAssertion(c2, a),
-				DisjointClasses(c1, c2), 
-				ObjectPropertyAssertion(r1, a, b), //
-				ObjectPropertyAssertion(r2, a, b) //
+				Declaration(d1), //
+				Declaration(d2), //
+				ra1, ra2
 		);
-
+	}
+	
+	private void startReasoner() {
 		QuestPreferences p = new QuestPreferences();
 		p.setCurrentValueOf(QuestPreferences.ABOX_MODE, QuestConstants.CLASSIC);
 		p.setCurrentValueOf(QuestPreferences.OPTIMIZE_EQUIVALENCES, "true");
@@ -64,10 +81,88 @@ public class InconsistencyCheckingTest {
 
 		QuestOWLFactory questOWLFactory = new QuestOWLFactory();
 		questOWLFactory.setPreferenceHolder(p);
-		OWLReasoner reasoner = questOWLFactory.createReasoner(ontology);
+		reasoner = questOWLFactory.createReasoner(ontology);
+	}
+	
+	@Test
+	public void testInitialConsistency() {
+		//initially the ontology is consistent
+		startReasoner();
+		assertTrue(reasoner.isConsistent());
+	}
+	
+	@Test
+	public void testDisjointClassInconsistency() throws OWLOntologyCreationException {
+
+		//Male(a), Female(a), disjoint(Male, Female)
+		manager.addAxiom(ontology, ClassAssertion(c1, a));
+		manager.addAxiom(ontology, ClassAssertion(c2, a));
+		manager.addAxiom(ontology, DisjointClasses(c1, c2));
+		
+		startReasoner();
+		
 		boolean consistent = reasoner.isConsistent();
 		assertFalse(consistent);
 
-	}
+	} 
+	
+	@Test
+	public void testDisjointObjectPropInconsistency() throws OWLOntologyCreationException {
 
+		//hasMother(a, b), hasFather(a,b), disjoint(hasMother, hasFather)
+		manager.addAxiom(ontology,ObjectPropertyAssertion(r1, a, b)); //
+		manager.addAxiom(ontology,ObjectPropertyAssertion(r2, a, b)); //
+		manager.addAxiom(ontology, DisjointObjectProperties(r1, r2));
+		
+		startReasoner();
+		
+		boolean consistent = reasoner.isConsistent();
+		assertFalse(consistent);
+
+	} 
+	
+	@Test
+	public void testDisjointDataPropInconsistency() throws OWLOntologyCreationException {
+
+		//hasName(a, Name), hasAge(a, Age), disjoint(hasName, hasAge) -> disjoint(String, Integer)
+		manager.addAxiom(ontology, DataPropertyAssertion(d1, a, Literal("Anna")));
+		manager.addAxiom(ontology, DataPropertyAssertion(d2, a, Literal(21)));
+		manager.addAxiom(ontology, DisjointDataProperties(d1, d2));
+		
+		startReasoner();
+		
+		boolean consistent = reasoner.isConsistent();
+		assertFalse(consistent);
+
+	} 
+	
+	@Test
+	public void testFunctionalObjPropInconsistency() throws OWLOntologyCreationException {
+
+		//hasMother(a,b), hasMother(a,c), func(hasMother)
+		manager.addAxiom(ontology,ObjectPropertyAssertion(r1, a, b)); //
+		manager.addAxiom(ontology,ObjectPropertyAssertion(r1, a, c)); //
+		manager.addAxiom(ontology, FunctionalObjectProperty(r1));
+		
+		startReasoner();
+		
+		boolean consistent = reasoner.isConsistent();
+		assertFalse(consistent);
+
+	} 
+	
+	@Test
+	public void testFunctionalDataPropInconsistency() throws OWLOntologyCreationException {
+
+		//hasAge(a, 18), hasAge(a, 21), func(hasAge)
+		manager.addAxiom(ontology, DataPropertyAssertion(d1, a, Literal(18)));
+		manager.addAxiom(ontology, DataPropertyAssertion(d1, a, Literal(21)));
+		manager.addAxiom(ontology, FunctionalDataProperty(d2));
+		
+		startReasoner();
+		
+		boolean consistent = reasoner.isConsistent();
+		assertFalse(consistent);
+
+	} 
 }
