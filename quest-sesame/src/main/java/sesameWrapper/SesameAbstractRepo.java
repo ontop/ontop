2ad86dbaa931c17b662bf0c4be4a9ad345d429fd
Guohui Xiao <gh.xiao@gmail.com>
2013-12-12 15:30:04 +0100

    add apache v2 header to all the java files

--- quest-sesame/src/main/java/sesameWrapper/SesameAbstractRepo.java
+++ quest-sesame/src/main/java/sesameWrapper/SesameAbstractRepo.java
@@ -1,5 +1,25 @@
 package sesameWrapper;
 
+/*
+ * #%L
+ * ontop-quest-sesame
+ * %%
+ * Copyright (C) 2009 - 2013 Free University of Bozen-Bolzano
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import it.unibz.krdb.obda.model.OBDAException;
 import it.unibz.krdb.obda.owlrefplatform.core.QuestDBConnection;
 import java.io.File;
