7bb743a833bcea124fde8408a2593445edf73316
Guohui Xiao <gh.xiao@gmail.com>
2014-02-26 17:15:22 +0100

    clean up SQLGenerator

--- quest-test/src/test/java/it/unibz/krdb/obda/unfold/LeftJoinTestVirtual.java
+++ quest-test/src/test/java/it/unibz/krdb/obda/unfold/LeftJoinTestVirtual.java
@@ -134,7 +134,16 @@ public class LeftJoinTestVirtual extends TestCase {
 		String query_multi3 = "PREFIX : <http://www.example.org/test#> SELECT DISTINCT * WHERE {?p :name ?name . OPTIONAL {?p :nick1 ?nick1} }";
 		String query_multi4 = "PREFIX : <http://www.example.org/test#> SELECT DISTINCT * WHERE {?p a :Person . OPTIONAL {?p :name ?name . OPTIONAL {?p :nick1 ?nick1} } }";
 		String query_multi5 = "PREFIX : <http://www.example.org/test#> SELECT DISTINCT * WHERE {?p a :Person . OPTIONAL {?p :name ?name . OPTIONAL {?p :nick1 ?nick1} OPTIONAL {?p :nick2 ?nick2} } }";
-		String query_multi6 = "PREFIX : <http://www.example.org/test#> SELECT DISTINCT * WHERE {?p a :Person . OPTIONAL {?p :name ?name . OPTIONAL {?p :nick1 ?nick1 OPTIONAL {?p :nick2 ?nick2} } } }";
+		
+		String query_multi6 = "PREFIX : <http://www.example.org/test#> "
+				+ "SELECT DISTINCT * "
+				+ "WHERE {"
+				+ "  ?p a :Person . "
+				+ "  OPTIONAL {"
+				+ "    ?p :name ?name . "
+				+ "    OPTIONAL {"
+				+ "      ?p :nick1 ?nick1 "
+				+ "      OPTIONAL {?p :nick2 ?nick2} } } }";
 		
 		
 		String query1 = "PREFIX : <http://www.example.org/test#> "
@@ -163,9 +172,30 @@ public class LeftJoinTestVirtual extends TestCase {
 				+ "  OPTIONAL {?p :nick1 ?nick1} "
 				+ "  OPTIONAL {?p :nick2 ?nick2} }";
 		
-		String query5 = "PREFIX : <http://www.example.org/test#> SELECT DISTINCT * WHERE {?p a :Person . ?p :name ?name . OPTIONAL {?p :age ?age} }";
-		String query6 = "PREFIX : <http://www.example.org/test#> SELECT * WHERE {?p a :Person . ?p :name ?name . OPTIONAL {?p :nick1 ?nick1 OPTIONAL { {?p :nick2 ?nick2 } UNION {?p :nick22 ?nick22} } } }";
-		String query7 = "PREFIX : <http://www.example.org/test#> SELECT DISTINCT * WHERE {?p a :Person . ?p :name ?name . OPTIONAL {?p :nick11 ?nick11 OPTIONAL { {?p :nick33 ?nick33 } UNION {?p :nick22 ?nick22} } } }";
+		String query5 = "PREFIX : <http://www.example.org/test#> "
+				+ "SELECT DISTINCT * "
+				+ "WHERE {"
+				+ "  ?p a :Person . "
+				+ "  ?p :name ?name ."
+				+ "    OPTIONAL {?p :age ?age} }";
+		
+		String query6 = "PREFIX : <http://www.example.org/test#> "
+				+ "SELECT * "
+				+ "WHERE {"
+				+ " ?p a :Person . ?p :name ?name ."
+				+ " OPTIONAL {"
+				+ "   ?p :nick1 ?nick1 "
+				+ "   OPTIONAL {"
+				+ "     {?p :nick2 ?nick2 } UNION {?p :nick22 ?nick22} } } }";
+	
+		String query7 = "PREFIX : <http://www.example.org/test#> "
+				+ "SELECT DISTINCT * "
+				+ "WHERE {"
+				+ "  ?p a :Person . "
+				+ "  ?p :name ?name ."
+				+ "  OPTIONAL {"
+				+ "    ?p :nick11 ?nick11 "
+				+ "    OPTIONAL { {?p :nick33 ?nick33 } UNION {?p :nick22 ?nick22} } } }";
 		//@formatter.on
 		
 		try {
@@ -175,12 +205,12 @@ public class LeftJoinTestVirtual extends TestCase {
 			//executeQueryAssertResults(query_multi3, st, 4);
 			//executeQueryAssertResults(query_multi4, st, 4);
 			//executeQueryAssertResults(query_multi5, st, 4);
-			//executeQueryAssertResults(query_multi6, st, 4);
+			executeQueryAssertResults(query_multi6, st, 4);
 
 			//executeQueryAssertResults(query1, st, 3);
 			//executeQueryAssertResults(query2, st, 4);
 //			executeQueryAssertResults(query3, st, 4);
-			executeQueryAssertResults(query4, st, 4);
+//			executeQueryAssertResults(query4, st, 4);
 //			executeQueryAssertResults(query5, st, 4);
 //			executeQueryAssertResults(query6, st, 6);
 //			executeQueryAssertResults(query7, st, 4);
