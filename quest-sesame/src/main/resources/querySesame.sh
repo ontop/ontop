97a7c2f77213b9fdb379a48278d57433cabd3886
Guohui Xiao <gh.xiao@gmail.com>
2014-05-29 09:19:08 +0200

    fix the package names in the command line scripts

--- quest-sesame/src/main/resources/querySesame.sh
+++ quest-sesame/src/main/resources/querySesame.sh
@@ -1,1 +1,2 @@
-java -Dlogback.configurationFile=log/logback.xml -Djava.ext.dirs=lib/:jdbc/ sesameWrapper.QuestSesameCMD $@
+#!/bin/sh
+java -Dlogback.configurationFile=log/logback.xml -Djava.ext.dirs=lib/:jdbc/ org.semanticweb.ontop.sesame.QuestSesameCMD $@
