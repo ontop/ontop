1fec1cdb6044e8201304ac143b301f2dd0b83c1c
martinrezk <martinrezk@gmail.com>
2014-06-27 13:15:56 +0200

    branch  to solve the types in the ontology

---
+++ quest-test/src/test/java/it/unibz/krdb/obda/testsuite/OntologyTypesTest.java
@@ -0,0 +1,156 @@
+package it.unibz.krdb.obda.testsuite;
+
+/*
+ * #%L
+ * ontop-test
+ * %%
+ * Copyright (C) 2009 - 2014 Free University of Bozen-Bolzano
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import it.unibz.krdb.obda.io.ModelIOManager;
+import it.unibz.krdb.obda.model.OBDADataFactory;
+import it.unibz.krdb.obda.model.OBDAModel;
+import it.unibz.krdb.obda.model.impl.OBDADataFactoryImpl;
+import it.unibz.krdb.obda.owlrefplatform.core.QuestConstants;
+import it.unibz.krdb.obda.owlrefplatform.core.QuestPreferences;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWL;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLConnection;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLFactory;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLResultSet;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.QuestOWLStatement;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class OntologyTypesTest extends TestCase {
+
+	private OBDADataFactory fac;
+	private Connection conn;
+
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	private OBDAModel obdaModel;
+	private OWLOntology ontology;
+
+	final String owlfile = "src/test/resources/ontologyType/ontologyType.owl";
+	final String obdafile = "src/test/resources/ontologyType/ontologyType.obda";
+
+	@Override
+	public void setUp() throws Exception {
+
+		fac = OBDADataFactoryImpl.getInstance();
+		
+		// Loading the OWL file
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		ontology = manager.loadOntologyFromOntologyDocument((new File(owlfile)));
+
+		// Loading the OBDA data
+		obdaModel = fac.getOBDAModel();
+		
+		ModelIOManager ioManager = new ModelIOManager(obdaModel);
+		ioManager.load(obdafile);
+		
+	}
+
+	private void runTests(Properties p) throws Exception {
+
+		// Creating a new instance of the reasoner
+		QuestOWLFactory factory = new QuestOWLFactory();
+		factory.setOBDAController(obdaModel);
+
+		factory.setPreferenceHolder(p);
+
+		QuestOWL reasoner = (QuestOWL) factory.createReasoner(ontology, new SimpleConfiguration());
+
+		// Now we are ready for querying
+		QuestOWLConnection conn = reasoner.getConnection();
+		QuestOWLStatement st = conn.createStatement();
+
+		String query1 = "PREFIX : <http://www.company.com/ARES#>" +
+						"select * {?x :number ?y}";
+		try {
+			executeQueryAssertResults(query1, st, 2);
+			
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			try {
+
+			} catch (Exception e) {
+				st.close();
+			}
+			conn.close();
+			reasoner.dispose();
+		}
+	}
+	
+	public void executeQueryAssertResults(String query, QuestOWLStatement st, int expectedRows) throws Exception {
+		QuestOWLResultSet rs = st.executeTuple(query);
+		int count = 0;
+		while (rs.nextRow()) {
+			count++;
+			for (int i = 1; i <= rs.getColumnCount(); i++) {
+				System.out.print(rs.getSignature().get(i-1));
+				System.out.print("=" + rs.getOWLObject(i));
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+		rs.close();
+		assertEquals(expectedRows, count);
+	}
+	
+//	public void executeGraphQueryAssertResults(String query, QuestOWLStatement st, int expectedRows) throws Exception {
+//		List<OWLAxiom> rs = st.executeGraph(query);
+//		int count = 0;
+//		Iterator<OWLAxiom> axit = rs.iterator();
+//		while (axit.hasNext()) {
+//			System.out.println(axit.next());			
+//			count++;
+//		}		
+//		assertEquals(expectedRows, count);
+//	}
+
+	public void testOntologyType() throws Exception {
+
+		QuestPreferences p = new QuestPreferences();
+//		p.setCurrentValueOf(QuestPreferences.ABOX_MODE, QuestConstants.VIRTUAL);
+//		p.setCurrentValueOf(QuestPreferences.OPTIMIZE_EQUIVALENCES, "true");
+//		p.setCurrentValueOf(QuestPreferences.OPTIMIZE_TBOX_SIGMA, "true");
+
+		runTests(p);
+	}
+
+}
