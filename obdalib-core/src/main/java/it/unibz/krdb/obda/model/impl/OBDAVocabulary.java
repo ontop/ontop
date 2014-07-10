6f5ff526abee9b008b568bc19754169cbb950710
ConstantB <konstantina.bereta@di.uoa.gr>
2014-07-10 19:52:44 +0300

    added some spatial constants to OBDAVocabulary. Will be updated when additional spatial features will be supported

--- obdalib-core/src/main/java/it/unibz/krdb/obda/model/impl/OBDAVocabulary.java
+++ obdalib-core/src/main/java/it/unibz/krdb/obda/model/impl/OBDAVocabulary.java
@@ -35,6 +35,17 @@ public class OBDAVocabulary {
 	public static final ValueConstant TRUE = new ValueConstantImpl("true", COL_TYPE.BOOLEAN);
 	public static final ValueConstant FALSE = new ValueConstantImpl("false", COL_TYPE.BOOLEAN);
 
+	public static final Constant FALSE = new ValueConstantImpl("f", COL_TYPE.BOOLEAN);
+
+	/* Numeric operations */
+
+	public static final String MINUS_STR = "minus";
+	
+	public static final String ADD_STR = "add";
+	
+	public static final String SUBSTRACT_STR = "substract";
+	
+	public static final String MULTIPLY_STR = "multiply";
 	
 	/* Numeric operation predicates */
 	
@@ -43,6 +54,44 @@ public class OBDAVocabulary {
 	public static final Predicate SUBTRACT = new NumericalOperationPredicateImpl("subtract", 2);
 	public static final Predicate MULTIPLY = new NumericalOperationPredicateImpl("multiply", 2);
 		
+	public static final Predicate MINUS = new NumericalOperationPredicateImpl(
+			MINUS_STR, 1);
+	
+	public static final Predicate ADD = new NumericalOperationPredicateImpl(
+			ADD_STR, 2);
+	
+	public static final Predicate SUBSTRACT = new NumericalOperationPredicateImpl(
+			SUBSTRACT_STR, 2);
+	
+	public static final Predicate MULTIPLY = new NumericalOperationPredicateImpl(
+			MULTIPLY_STR, 2);
+	
+	/* Boolean predicate URIs */
+
+	public static final String strAND = "AND";
+
+	public static final String strEQ = "EQ";
+
+	public static final String strGTE = "GTE";
+
+	public static final String strGT = "GT";
+
+	public static final String strLTE = "LTE";
+
+	public static final String strLT = "LT";
+
+	public static final String strNEQ = "NEQ";
+
+	public static final String strNOT = "NOT";
+
+	public static final String strOR = "OR";
+
+	public static final String strIS_NULL = "IS_NULL";
+
+	public static final String strIS_NOT_NULL = "IS_NOT_NULL";
+
+	public static final String strIS_TRUE = "IS_TRUE";
+	
 	/* Boolean predicates */
 
 	public static final BooleanOperationPredicate AND = new BooleanOperationPredicateImpl("AND", 2);
@@ -107,7 +156,42 @@ public class OBDAVocabulary {
 
 	public static final Predicate SPARQL_JOIN = new AlgebraOperatorPredicateImpl("Join");
 	public static final Predicate SPARQL_LEFTJOIN = new AlgebraOperatorPredicateImpl("LeftJoin");
+	
+	
+	/*SPARQL spatial functions*/
+	public static final String NS_STRDF = "<http://strdf.di.uoa.gr/ontology#>";
+	
+	public static final String overlap = "<http://strdf.di.uoa.gr/ontology#overlap>";
+	
 
+	/* SPARQL Algebra predicate */
+
+	public static final Predicate SPARQL_JOIN = new AlgebraOperatorPredicateImpl(
+			SPARQL_JOIN_URI, COL_TYPE.STRING);
+
+	public static final Predicate SPARQL_LEFTJOIN = new AlgebraOperatorPredicateImpl(
+			SPARQL_LEFTJOIN_URI, COL_TYPE.STRING);
+
+	public static final Predicate SPARQL_IS_LITERAL = new BooleanOperationPredicateImpl(
+			SPARQL_IS_LITERAL_URI);
+
+	public static final Predicate SPARQL_IS_URI = new BooleanOperationPredicateImpl(
+			SPARQL_IS_URI_URI);
+
+	public static final Predicate SPARQL_IS_IRI = new BooleanOperationPredicateImpl(
+			SPARQL_IS_IRI_URI);
+
+	public static final Predicate SPARQL_IS_BLANK = new BooleanOperationPredicateImpl(
+			SPARQL_IS_BLANK_URI);
+
+	public static final Predicate SPARQL_LANGMATCHES = new BooleanOperationPredicateImpl(
+			SPARQL_LANGMATCHES_URI, 2);
+
+	public static final Predicate SPARQL_STR = new NonBooleanOperationPredicateImpl(
+			SPARQL_STR_URI);
+
+	public static final Predicate SPARQL_DATATYPE = new NonBooleanOperationPredicateImpl(
+			SPARQL_DATATYPE_URI);
 
 	/* SPARQL built-in functions */
 	
