cf5ee9bb92e31cecaa44b053d8d9afc26977747c
Sarah Komla-Ebri <sarahkeb89@gmail.com>
2014-07-14 17:07:18 +0200

    add comments, removed not used files

--- obdalib-r2rml/src/main/java/it/unibz/krdb/obda/r2rml/MappingConverterCMD.java
+++ obdalib-r2rml/src/main/java/it/unibz/krdb/obda/r2rml/MappingConverterCMD.java
@@ -22,6 +22,7 @@ package it.unibz.krdb.obda.r2rml;
 
 import it.unibz.krdb.obda.exception.InvalidMappingException;
 import it.unibz.krdb.obda.io.ModelIOManager;
+import it.unibz.krdb.obda.io.PrefixManager;
 import it.unibz.krdb.obda.model.OBDADataFactory;
 import it.unibz.krdb.obda.model.OBDADataSource;
 import it.unibz.krdb.obda.model.OBDAModel;
@@ -111,50 +112,60 @@ class MappingConverterCMD {
 
 				ModelIOManager modelIO = new ModelIOManager(model);
 				modelIO.save(out);
-				// PrefixManager pm = model.getPrefixManager();
-				//
-				// TurtleFormatter tf = new TurtleFormatter(pm);
-				// ArrayList<OBDAMappingAxiom> axioms = reader.readMappings();
-				// BufferedWriter writer = null;
-				// writer = new BufferedWriter(new FileWriter(outfile));
-				// writer.write("[PrefixDeclaration]");
-				// writer.newLine();
-				// Map<String, String> map = pm.getPrefixMap();
-				// for (String key : map.keySet()) {
-				// writer.write(key + "\t" + map.get(key));
-				// writer.newLine();
-				// }
-				// writer.newLine();
-				// writer.write("[SourceDeclaration]");
-				// writer.newLine();
-				// writer.write("sourceUri" + "\t" + "customSource");
-				// writer.newLine();
-				// writer.write("connectionUrl" + "\t" +
-				// "jdbc:h2:tcp://localhost/DBName");
-				// writer.newLine();
-				// writer.write("username" + "\t" + "sa");
-				// writer.newLine();
-				// writer.write("password"+ "\t");
-				// writer.newLine();
-				// writer.write("driverClass" + "\t" + "org.h2.Driver");
-				// writer.newLine();
-				// writer.newLine();
-				// writer.write("[MappingDeclaration] @collection [[");
-				// writer.newLine();
-				//
-				// for (OBDAMappingAxiom ax : axioms) {
-				// writer.write("mappingId" + "\t" + ax.getId());
-				// writer.newLine();
-				// CQIE cq = (CQIE) ax.getTargetQuery();
-				// String cqStr = tf.print(cq);
-				// writer.write("target" + "\t" + cqStr);
-				// writer.newLine();
-				// writer.write("source" + "\t" + ax.getSourceQuery());
-				// writer.newLine();
-				// writer.newLine();
-				// }
-				// writer.write("]]");
-				// writer.close();
+				
+				/*Add the not standard prefixes to prefix manager.
+				 * If you want them to have the abbreviation in the obda file
+				 PrefixManager pm = model.getPrefixManager();
+				 pm.addPrefix(":", "http://example.org/");
+				 
+				*/ 
+				 
+				 
+				 /* Deprecated way
+				 TurtleFormatter tf = new TurtleFormatter(pm);
+				 ArrayList<OBDAMappingAxiom> axioms = reader.readMappings();
+				 BufferedWriter writer = null;
+				 writer = new BufferedWriter(new FileWriter(outfile));
+				 writer.write("[PrefixDeclaration]");
+				 writer.newLine();
+				 Map<String, String> map = pm.getPrefixMap();
+				 for (String key : map.keySet()) {
+				 writer.write(key + "\t" + map.get(key));
+				 writer.newLine();
+				 }
+				 writer.newLine();
+				 writer.write("[SourceDeclaration]");
+				 writer.newLine();
+				 writer.write("sourceUri" + "\t" + "customSource");
+				 writer.newLine();
+				 writer.write("connectionUrl" + "\t" +
+				 "jdbc:h2:tcp://localhost/DBName");
+				 writer.newLine();
+				 writer.write("username" + "\t" + "sa");
+				 writer.newLine();
+				 writer.write("password"+ "\t");
+				 writer.newLine();
+				 writer.write("driverClass" + "\t" + "org.h2.Driver");
+				 writer.newLine();
+				 writer.newLine();
+				 writer.write("[MappingDeclaration] @collection [[");
+				 writer.newLine();
+				
+				 for (OBDAMappingAxiom ax : axioms) {
+				 writer.write("mappingId" + "\t" + ax.getId());
+				 writer.newLine();
+				 CQIE cq = (CQIE) ax.getTargetQuery();
+				 String cqStr = tf.print(cq);
+				 writer.write("target" + "\t" + cqStr);
+				 writer.newLine();
+				 writer.write("source" + "\t" + ax.getSourceQuery());
+				 writer.newLine();
+				 writer.newLine();
+				 }
+				 writer.write("]]");
+				 writer.close();
+				 	*/ 
+				
 				System.out
 						.println("OBDA mapping file " + outfile + " written!");
 			}
