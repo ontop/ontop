abd686aac3621f03348771589204f5487f6af25a
martinrezk <martinrezk@gmail.com>
2014-07-24 19:17:27 +0200

    new test example

--- quest-test/src/test/java/org/semanticweb/ontop/sesame/tests/general/AggregatesTestWP5.java
+++ quest-test/src/test/java/org/semanticweb/ontop/sesame/tests/general/AggregatesTestWP5.java
@@ -1,4 +1,4 @@
-package org.semanticweb.ontop.reformulation.tests;
+package org.semanticweb.ontop.sesame.tests.general;
 
 /*
  * #%L
@@ -20,98 +20,137 @@ package org.semanticweb.ontop.reformulation.tests;
  * #L%
  */
 
-import org.semanticweb.ontop.io.ModelIOManager;
+import org.openrdf.model.Model;
+import org.openrdf.model.impl.LinkedHashModel;
+import org.openrdf.query.Binding;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFParser;
+import org.openrdf.rio.Rio;
+import org.openrdf.rio.helpers.StatementCollector;
 import org.semanticweb.ontop.model.OBDADataFactory;
 import org.semanticweb.ontop.model.OBDAModel;
-import org.semanticweb.ontop.model.impl.OBDADataFactoryImpl;
 import org.semanticweb.ontop.owlrefplatform.core.QuestConstants;
 import org.semanticweb.ontop.owlrefplatform.core.QuestPreferences;
-import org.semanticweb.ontop.owlrefplatform.owlapi3.QuestOWL;
-import org.semanticweb.ontop.owlrefplatform.owlapi3.QuestOWLConnection;
-import org.semanticweb.ontop.owlrefplatform.owlapi3.QuestOWLFactory;
 import org.semanticweb.ontop.owlrefplatform.owlapi3.QuestOWLResultSet;
 import org.semanticweb.ontop.owlrefplatform.owlapi3.QuestOWLStatement;
+import org.semanticweb.ontop.sesame.SesameVirtualRepo;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
 
-import java.io.BufferedReader;
+
+
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Properties;
-
 import junit.framework.TestCase;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.OWLAnonymousIndividual;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyIRIMapper;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
+import org.semanticweb.owlapi.util.AutoIRIMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AggregatesTest extends TestCase {
+public class AggregatesTestWP5 extends TestCase {
 
 	private OBDADataFactory fac;
-	private Connection conn;
 
 	Logger log = LoggerFactory.getLogger(this.getClass());
 	private OBDAModel obdaModel;
-	private OWLOntology ontology;
+	private OWLOntology owlontology;
+	Model mappings;
+	QuestPreferences pref;
+	RepositoryConnection con;
 
-	final String owlfile = "src/test/resources/test/stockexchange-unittest.owl";
-	final String obdafile = "src/test/resources/test/stockexchange-mysql-unittest.obda";
+	
+	final String owlfile = "src/test/resources/wp5/optique-demo.rdf";
+	final String mappingfile = "src/test/resources/wp5/mapping.ttl";
 
+	
+	
+	
+	
+	
+	
+	
+	
 	@Override
 	public void setUp() throws Exception {
-				
+	
+		
+	// create owlontology from file
+	OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+	OWLOntologyIRIMapper iriMapper = new AutoIRIMapper(	new File(owlfile).getParentFile(), false);
+	man.addIRIMapper(iriMapper);
+	try{
+		owlontology = man
+				.loadOntologyFromOntologyDocument(new File(owlfile));
+	} catch (Exception e) {
+
+		e.printStackTrace();
+		assertFalse(false);
+	}
+		
+	/*
+	 * Mappings
+	 */
+	try {
+		// create RDF Graph from ttl file
+		RDFParser parser = Rio.createParser(RDFFormat.TURTLE);
+		InputStream in = new FileInputStream(mappingfile);
+		URL documentUrl = new URL("file://" + mappingfile);
+		mappings = new LinkedHashModel();
+		StatementCollector collector = new StatementCollector(mappings);
+		parser.setRDFHandler(collector);
+		parser.parse(in, documentUrl.toString());
+
+	} catch (Exception e) {
+
+		e.printStackTrace();
+		assertFalse(false);
+	}
+		
+	/*
+	 * PREFERENCES
+	 */
+		
+	pref = new QuestPreferences();
+	pref.setCurrentValueOf(QuestPreferences.ABOX_MODE,	QuestConstants.VIRTUAL);
+//	pref.setCurrentValueOf(QuestPreferences.REWRITE, "true");
+//	pref.setCurrentValueOf(QuestPreferences.REFORMULATION_TECHNIQUE, QuestConstants.TW);
+	// set jdbc params in config
+	pref.setCurrentValueOf(QuestPreferences.DBNAME, "siemens-test");
+	pref.setCurrentValueOf(QuestPreferences.JDBC_URL,"jdbc:postgresql://10.254.11.17:5432/siemens-test");
+	pref.setCurrentValueOf(QuestPreferences.DBUSER, "postgres");
+	pref.setCurrentValueOf(QuestPreferences.DBPASSWORD, "postgres");
+	pref.setCurrentValueOf(QuestPreferences.JDBC_DRIVER,"org.postgresql.Driver");	
+		
+	Repository repo;
+	try {
+		repo = new SesameVirtualRepo("virtualExample2", owlontology, mappings, pref);
 		/*
-		 * Initializing and H2 database with the stock exchange data
+		 * Repository must be always initialized first
 		 */
-		String driver = "com.mysql.jdbc.Driver";
-		String url = "jdbc:mysql://10.7.20.39/stockexchange";
-		String username = "fish";
-		String password = "fish";
+		repo.initialize();
 
-		//?sessionVariables=sql_mode='ANSI'
-		
-		fac = OBDADataFactoryImpl.getInstance();
+		/*
+		 * Get the repository connection
+		 */
+		con = repo.getConnection();
 
-		conn = DriverManager.getConnection(url, username, password);
-		conn.setAutoCommit(true);
-		Statement st = conn.createStatement();
-
-		FileReader reader = new FileReader("src/test/resources/test/stockexchange-create-mysql.sql");
-		BufferedReader in = new BufferedReader(reader);
-		StringBuilder bf = new StringBuilder();
-		String line = in.readLine();
-		while (line != null) {
-			bf.append(line + "\n");
-			line = in.readLine();
-		}
-		String[] sqls = bf.toString().split(";");
-		for (String st_sql: sqls) {
-			st.addBatch(st_sql);
-		}
-		//st.executeBatch();
-		//st.executeUpdate(bf.toString());
-		//conn.commit();
-
-		// Loading the OWL file
-		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-		ontology = manager.loadOntologyFromOntologyDocument((new File(owlfile)));
-
-		// Loading the OBDA data
-		obdaModel = fac.getOBDAModel();
-		
-		ModelIOManager ioManager = new ModelIOManager(obdaModel);
-		ioManager.load(obdafile);
+	} catch (Exception e) {
+		e.printStackTrace();
+		assertFalse(false);
+	}
 		
 	}
 
@@ -119,85 +158,32 @@ public class AggregatesTest extends TestCase {
 	public void tearDown() throws Exception {
 		try {
 		//	dropTables();
-			conn.close();
+			con.close();
 		} catch (Exception e) {
 			log.debug(e.getMessage());
 		}
 	}
 
-	private void dropTables() throws SQLException, IOException {
-
-		Statement st = conn.createStatement();
-
-		FileReader reader = new FileReader("src/test/resources/test/stockexchange-drop-mysql.sql");
-		BufferedReader in = new BufferedReader(reader);
-		StringBuilder bf = new StringBuilder();
-		String line = in.readLine();
-		while (line != null) {
-			bf.append(line);
-			line = in.readLine();
-		}
-
-		st.executeUpdate(bf.toString());
-		st.close();
-		conn.commit();
-	}
-
+	
+	
+	
+	
+	
+	//TODO: Groupby using more than 1 variable
+	//TODO: Add tests for the rest of the aggregates!!
+	//TODO: Add value test.... check if the answer is correct, not just rows
+	
+	
+	//test queries
 	private void runTests(QuestPreferences p, String query, int expectedvalue) throws Exception {
-
-		// Creating a new instance of the reasoner
-		QuestOWLFactory factory = new QuestOWLFactory();
-		factory.setOBDAController(obdaModel);
-
-		factory.setPreferenceHolder(p);
-
-		QuestOWL reasoner = (QuestOWL) factory.createReasoner(ontology, new SimpleConfiguration());
-
-		// Now we are ready for querying
-		QuestOWLConnection conn = reasoner.getConnection();
-		QuestOWLStatement st = conn.createStatement();
-
-		//test queries
-		
-		
-		
 		try {
-		
-			
-
-			//TODO: Groupby using more than 1 variable
-			//TODO: Add tests for the rest of the aggregates!!
-			//TODO: Add value test.... check if the answer is correct, not just rows
-			
-			
-			
-			executeQueryAssertResults(query, st, expectedvalue);
-			
-			/*
-			 * String query1 = "PREFIX : <http://www.owl-ontologies.com/Ontology1207768242.owl#> SELECT DISTINCT * WHERE {?x a :Stock }";
-		String query1_1 = "PREFIX : <http://www.owl-ontologies.com/Ontology1207768242.owl#> SELECT (?value AS ?new) WHERE {?x a :Transaction. ?x :amountOfTransaction ?value }";
-		
-		// COUNT
-		
-		
-		String query6 = "PREFIX : <http://www.owl-ontologies.com/Ontology1207768242.owl#> SELECT ?value ?broker WHERE {{?x a :Transaction. ?x :amountOfTransaction ?value } UNION {?x :isExecutedBy ?broker} } "; // GROUP BY ?value  ?broker
-
-		
-		executeQueryAssertResults(query1, st, 10);
-			executeQueryAssertResults(query6, st, 7);
-			executeQueryAssertResults(query1_1, st, 4);
-		
-
-			 * 
-			 */
-			
-			
+			executeQueryAssertResults(query,  expectedvalue);
 		} catch (Exception e) {
+			assertFalse(false);
 			throw e;
 		} finally {
-			conn.close();
-			st.close();
-			reasoner.dispose();
+			con.close();
+		
 		}
 	}
 	
@@ -213,22 +199,55 @@ public class AggregatesTest extends TestCase {
 		assertEquals(expectedValue, count);
 	}
 	
-	public void executeQueryAssertResults(String query, QuestOWLStatement st, int expectedRows) throws Exception {
-		QuestOWLResultSet rs = st.executeTuple(query);
-		int count = 0;
-		while (rs.nextRow()) {
-			count++;
-			for (int i = 1; i <= rs.getColumnCount(); i++) {
-				System.out.print(rs.getSignature().get(i-1));
-				System.out.print("=" + rs.getOWLObject(i));
-				System.out.print(" ");
-			}
-			System.out.println();
+	
+	
+	
+	
+	
+	public void executeQueryAssertResults(String query,  int expectedRows) throws Exception {
+		
+		
+		int resultCount = 0;
+		try {
+			TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL,
+					query);
+			TupleQueryResult result = tupleQuery.evaluate();
+
+			while (result.hasNext()) {
+	            for (Binding binding : result.next()) {
+	                System.out.print(binding.getValue() + ", ");
+	            }
+	            System.out.println();
+	        }
+			
+//			while (result.hasNext()) {
+//				
+//				
+//				System.out.print(result.getBindingNames().get(0));
+//				
+//				//System.out.print("=" + result.  getOWLObject(resultCount+1));
+//				System.out.print(" ");
+//				
+//				//result.next().getValue();
+//				resultCount++;
+//				System.out.println();
+//			}
+			
+			result.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			assertFalse(false);
 		}
-		rs.close();
-		assertEquals(expectedRows, count);
+		
+		
+
+		//assertEquals(expectedRows, count);
 	}
 	
+	
+	
+	/*
 	public void executeGraphQueryAssertResults(String query, QuestOWLStatement st, int expectedRows) throws Exception {
 		List<OWLAxiom> rs = st.executeGraph(query);
 		int count = 0;
@@ -239,6 +258,7 @@ public class AggregatesTest extends TestCase {
 		}		
 		assertEquals(expectedRows, count);
 	}
+	*/
 /*
 	public void testAggrCount() throws Exception {
 
@@ -258,7 +278,7 @@ public class AggregatesTest extends TestCase {
 		p.setCurrentValueOf(QuestPreferences.ABOX_MODE, QuestConstants.VIRTUAL);
 		p.setCurrentValueOf(QuestPreferences.OPTIMIZE_EQUIVALENCES, "true");
 		p.setCurrentValueOf(QuestPreferences.OPTIMIZE_TBOX_SIGMA, "true");
-		String query = "PREFIX : <http://www.owl-ontologies.com/Ontology1207768242.owl#> SELECT ?broker (COUNT(?value) AS ?count) WHERE {?x a :Transaction. ?x :isExecutedBy ?broker. ?x :amountOfTransaction ?value } GROUP BY ?broker";
+		String query = "select ?state (count(?x) as ?c) where {    ?x rdf:type <http://www.siemens.com/optique/demo#Message> .   ?x <http://www.siemens.com/optique/demo#hasState> ?state.} GROUP BY ?state";
 
 		runTests(p,query,1);
 
