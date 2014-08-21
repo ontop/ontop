44c4be73fa543fe3d17e1b9ad99e8fc94f16415b
Mariano Rodriguez Muro <mariano.muro@gmail.com>
2014-08-21 13:03:32 -0400

    * Added small fix to trim leading/trailing from URIs (string values with white spaces would generate URIs with white spaces, invalid)

--- reformulation-core/src/main/java/it/unibz/krdb/obda/owlrefplatform/core/resultset/QuestResultset.java
+++ reformulation-core/src/main/java/it/unibz/krdb/obda/owlrefplatform/core/resultset/QuestResultset.java
@@ -159,7 +159,7 @@ public class QuestResultset implements TupleResultSet {
 						}
 					}
 
-					result = fac.getConstantURI(realValue);
+					result = fac.getConstantURI(realValue.trim());
 
 				} else if (type == COL_TYPE.BNODE) {
 					String rawLabel = set.getString(column);
