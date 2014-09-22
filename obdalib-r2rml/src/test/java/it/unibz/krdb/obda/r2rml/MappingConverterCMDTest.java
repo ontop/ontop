1a0c29a15c0117fe074f2311c8ea6e793b6657fa
Sarah Komla-Ebri <sarahkeb89@gmail.com>
2014-09-22 18:54:00 +0200

    changed MappingConverterCMDTest

--- obdalib-r2rml/src/test/java/it/unibz/krdb/obda/r2rml/MappingConverterCMDTest.java
+++ obdalib-r2rml/src/test/java/it/unibz/krdb/obda/r2rml/MappingConverterCMDTest.java
@@ -6,12 +6,12 @@ public class MappingConverterCMDTest {
 
 	@Test
 	public void testWithOntology() {
-		MappingConverterCMD.main(new String[]{"src/test/resources/dataPropertiesOntologyType.obda", "src/test/resources/dataPropertiesOntologyType.owl"});
+		MappingConverterCMD.main(new String[]{"src/test/resources/npd-v2-ql_a.obda", "src/test/resources/npd-v2-ql_a.owl"});
 	}
 	
 	@Test
 	public void testWithoutOntology() {
-		MappingConverterCMD.main(new String[]{"src/test/resources/dataPropertiesOntologyType.obda"});
+		MappingConverterCMD.main(new String[]{"src/test/resources/npd-v2-ql_a.obda"});
 	}
 	
 	@Test
