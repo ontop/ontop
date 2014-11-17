09a3a7646a80b44c94a057bb5139d23b5b5b3e2b
Guohui Xiao <gh.xiao@gmail.com>
2014-11-17 20:38:54 +0100

    reverts the change in JdbcTypeMapper

--- obdalib-core/src/main/java/it/unibz/krdb/obda/utils/JdbcTypeMapper.java
+++ obdalib-core/src/main/java/it/unibz/krdb/obda/utils/JdbcTypeMapper.java
@@ -54,7 +54,7 @@ public class JdbcTypeMapper {
 		sqlToQuest.put(Types.REAL, COL_TYPE.DOUBLE);
 		sqlToQuest.put(Types.DATE, COL_TYPE.DATE);
 		sqlToQuest.put(Types.TIME, COL_TYPE.TIME);
-		//sqlToQuest.put(Types.TIMESTAMP, COL_TYPE.DATETIME);
+		sqlToQuest.put(Types.TIMESTAMP, COL_TYPE.DATETIME); //GX: needs check
 		sqlToQuest.put(Types.BOOLEAN, COL_TYPE.BOOLEAN);
 		sqlToQuest.put(Types.BIT, COL_TYPE.BOOLEAN);
 //		typeMapper.put(Types.BINARY, dfac.getDataTypePredicateBinary());
