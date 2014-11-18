1aebc8d5964210be0784d95b3401df68f69cb42e
Guohui Xiao <gh.xiao@gmail.com>
2014-11-18 22:40:29 +0100

    sqlSlice for AdpSQLDialectAdapter

--- reformulation-core/src/main/java/it/unibz/krdb/obda/owlrefplatform/core/queryevaluation/AdpSQLDialectAdapter.java
+++ reformulation-core/src/main/java/it/unibz/krdb/obda/owlrefplatform/core/queryevaluation/AdpSQLDialectAdapter.java
@@ -9,6 +9,30 @@ public class AdpSQLDialectAdapter extends SQL99DialectAdapter {
 		// TODO: provide a correct implementation
 		return value ? 	"1" : "0";
 	}
+
+	/**
+	 * same as PostgreSQL
+	 *
+	 */
+	@Override
+	public String sqlSlice(long limit, long offset) {
+		if (limit < 0 || limit == 0) {
+			if (offset < 0) {
+				// If both limit and offset is not specified.
+				return "LIMIT 0";
+			} else {
+				// if the limit is not specified
+				return String.format("LIMIT ALL\nOFFSET %d", offset);
+			}
+		} else {
+			if (offset < 0) {
+				// If the offset is not specified
+				return String.format("LIMIT %d\nOFFSET 0", limit);
+			} else {
+				return String.format("LIMIT %d\nOFFSET %d", limit, offset);
+			}
+		}
+	}
 	
 	/***
 	 * Given an XSD dateTime this method will generate a SQL TIMESTAMP value.
