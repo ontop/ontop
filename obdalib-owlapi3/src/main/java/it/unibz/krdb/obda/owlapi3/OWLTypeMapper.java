12d7b2faffd31607cf50334b382ad611094e733c
kontchakov <roman@dcs.bbk.ac.uk>
2015-02-20 00:19:58 +0000

    xsd:datatypes cleaned

--- obdalib-owlapi3/src/main/java/it/unibz/krdb/obda/owlapi3/OWLTypeMapper.java
+++ obdalib-owlapi3/src/main/java/it/unibz/krdb/obda/owlapi3/OWLTypeMapper.java
@@ -13,19 +13,19 @@ import org.semanticweb.owlapi.vocab.OWL2Datatype;
 public class OWLTypeMapper {
 
 	
-	private static final Map<OWL2Datatype, COL_TYPE> OWLtoCOLTYPE = new HashMap<OWL2Datatype, COL_TYPE>();
-	private static final Map<COL_TYPE, OWL2Datatype> COLTYPEtoOWL = new HashMap<COL_TYPE, OWL2Datatype>();
+	private static final Map<OWL2Datatype, COL_TYPE> OWLtoCOLTYPE = new HashMap<>();
+	private static final Map<COL_TYPE, OWL2Datatype> COLTYPEtoOWL = new HashMap<>();
 	
 	static {
+		registerType(OWL2Datatype.RDF_PLAIN_LITERAL, COL_TYPE.LITERAL);
 		registerType(OWL2Datatype.XSD_DECIMAL, COL_TYPE.DECIMAL);   // 1
 		registerType(OWL2Datatype.XSD_STRING, COL_TYPE.STRING);     // 2
 		registerType(OWL2Datatype.XSD_INTEGER, COL_TYPE.INTEGER);   // 3
 		registerType(OWL2Datatype.XSD_NON_NEGATIVE_INTEGER, COL_TYPE.NON_NEGATIVE_INTEGER);  // 4
 		registerType(OWL2Datatype.XSD_DATE_TIME, COL_TYPE.DATETIME);  // 5
-
-		registerType(OWL2Datatype.XSD_DATE_TIME_STAMP, COL_TYPE.DATETIME_STAMP);  // 21
+		registerType(OWL2Datatype.XSD_DATE_TIME_STAMP, COL_TYPE.DATETIME_STAMP);  // 15
 		
-		// not QL type
+		// not OWL 2 QL types
 		registerType(OWL2Datatype.XSD_INT, COL_TYPE.INT); // 6
 		registerType(OWL2Datatype.XSD_POSITIVE_INTEGER, COL_TYPE.POSITIVE_INTEGER); // 7
 		registerType(OWL2Datatype.XSD_NEGATIVE_INTEGER, COL_TYPE.NEGATIVE_INTEGER); // 8
@@ -38,19 +38,12 @@ public class OWLTypeMapper {
 	
 		
 		// irregularities
-
 		OWLtoCOLTYPE.put(OWL2Datatype.XSD_FLOAT, COL_TYPE.DOUBLE); // 12 // TEMPORARY!!
-
-		OWLtoCOLTYPE.put(OWL2Datatype.RDF_PLAIN_LITERAL, COL_TYPE.LITERAL);
 		OWLtoCOLTYPE.put(OWL2Datatype.RDFS_LITERAL, COL_TYPE.LITERAL);		
-//		OWLtoCOLTYPE.put(OWL2Datatype.XSD_DATE_TIME_STAMP, COL_TYPE.DATETIME);
 
-		COLTYPEtoOWL.put(COL_TYPE.LITERAL, OWL2Datatype.RDF_PLAIN_LITERAL);
 		COLTYPEtoOWL.put(COL_TYPE.DATE, OWL2Datatype.RDF_PLAIN_LITERAL);  // not XSD_DATE;
 		COLTYPEtoOWL.put(COL_TYPE.TIME, OWL2Datatype.RDF_PLAIN_LITERAL);
 		COLTYPEtoOWL.put(COL_TYPE.YEAR, OWL2Datatype.RDF_PLAIN_LITERAL);
-
-
 	}
 	
 	private static void registerType(OWL2Datatype owlType, Predicate.COL_TYPE type) {
