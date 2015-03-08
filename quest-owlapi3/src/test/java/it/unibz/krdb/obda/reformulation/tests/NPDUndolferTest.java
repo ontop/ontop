dc595e94c4d4bc14361a02f882e27de254d7f20e
kontchakov <roman@dcs.bbk.ac.uk>
2015-03-08 10:34:34 +0000

    tw-rewriter data properties fix

--- quest-owlapi3/src/test/java/it/unibz/krdb/obda/reformulation/tests/NPDUndolferTest.java
+++ quest-owlapi3/src/test/java/it/unibz/krdb/obda/reformulation/tests/NPDUndolferTest.java
@@ -27,43 +27,25 @@ public class NPDUndolferTest extends TestCase {
 		ontology = manager.loadOntologyFromOntologyDocument(new File(owlfile));
 	}
 
+	/**
+	 * Query 6 from the NPD benchmark 
+	 * (this query was an indicator for incorrect variable order in SPAQRL Extend)
+	 * 
+	 * @throws Exception
+	 */
+	
 	public void testNpdQ6() throws Exception {
 	
-		QuestOWLFactory fac = new QuestOWLFactory();
-
-		QuestPreferences pref = new QuestPreferences();
-		pref.setCurrentValueOf(QuestPreferences.DBTYPE, QuestConstants.SEMANTIC_INDEX);
-		pref.setCurrentValueOf(QuestPreferences.ABOX_MODE, QuestConstants.CLASSIC);
-
-		fac.setPreferenceHolder(pref);
-
-		QuestOWL quest = fac.createReasoner(ontology);
-		QuestOWLConnection qconn =  quest.getConnection();
-		QuestOWLStatement st = qconn.createStatement();
-		
-		quest.getQuestInstance().getSemanticIndexRepository().createIndexes(qconn.getConnection());
-		
-
 		String query =
 "PREFIX : <http://sws.ifi.uio.no/vocab/npd-v2#>" +
-"PREFIX isc: <http://resource.geosciml.org/classifier/ics/ischart/>" +
 "PREFIX nlxv: <http://sws.ifi.uio.no/vocab/norlex#>" +
 "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
 "PREFIX npd: <http://sws.ifi.uio.no/data/npd-v2/>" +
-"PREFIX void: <http://rdfs.org/ns/void#>" +
 "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>" +
 "PREFIX owl: <http://www.w3.org/2002/07/owl#>" +
-"PREFIX ex: <http://example.org/ex#>" +
-"PREFIX quest: <http://obda.org/quest#>" +
-"PREFIX diskos: <http://sws.ifi.uio.no/data/diskos/>" +
 "PREFIX nlx: <http://sws.ifi.uio.no/data/norlex/>" +
-"PREFIX ptl: <http://sws.ifi.uio.no/vocab/npd-v2-ptl#>" +
 "PREFIX npdv: <http://sws.ifi.uio.no/vocab/npd-v2#>" +
-"PREFIX geos: <http://www.opengis.net/ont/geosparql#>" +
-"PREFIX sql: <http://sws.ifi.uio.no/vocab/sql#>" +
 "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
-"PREFIX dc: <http://purl.org/dc/elements/1.1/>" +
-"PREFIX diskosv: <http://sws.ifi.uio.no/vocab/diskos#>" +
 "SELECT DISTINCT ?wellbore (?length * 0.30 AS ?lenghtM) ?company (?year + 2 AS ?YearB) " +
 "WHERE {" +
 "  ?wc npdv:coreForWellbore" +
@@ -74,16 +56,72 @@ public class NPDUndolferTest extends TestCase {
 "        ] ." +
 "  { ?wc npdv:coresTotalLength ?length } " +
 "  " +
-"  FILTER(?year >= \"2008\"^^xsd:integer &&" +
-"         ?length > 50 " +
+"  FILTER(?year >= \"2008\"^^xsd:integer && ?length > 50 " +
 "  )" +
 "} ORDER BY ?wellbore";
 
-		String unfolding = st.getRewriting(query);
-		assertFalse(unfolding.contains("GTE(company,"));
+		String rewriting = getRewriting(query);
+		assertFalse(rewriting.contains("GTE(company,"));
+	}
+
+	/**
+	 * Davide's query
+	 * (indicator for incomplete treatment of data properties in the rewriting)
+	 * 
+	 * @throws Exception
+	 */
+	
+	public void testNpdQD() throws Exception {
+		
+		String query =
+"PREFIX : <http://sws.ifi.uio.no/vocab/npd-v2#>" +
+"PREFIX nlxv: <http://sws.ifi.uio.no/vocab/norlex#>" +
+"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+"PREFIX npd: <http://sws.ifi.uio.no/data/npd-v2/>" +
+"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>" +
+"PREFIX owl: <http://www.w3.org/2002/07/owl#>" +
+"PREFIX nlx: <http://sws.ifi.uio.no/data/norlex/>" +
+"PREFIX npdv: <http://sws.ifi.uio.no/vocab/npd-v2#>" +
+"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+"SELECT DISTINCT ?wellbore " + 
+"		   WHERE { " +
+"		      ?wc npdv:coreForWellbore [ rdf:type npdv:Wellbore ]. " + 
+"		   }";
+
+		String rewriting = getRewriting(query);
+		assertFalse(rewriting.contains("GTE(company,"));
+	}
+	
+	/**
+	 * constructs a rewriting
+	 * 
+	 * @param query
+	 * @return
+	 * @throws Exception
+	 */
+	
+	private String getRewriting(String query) throws Exception {
+		
+		QuestOWLFactory fac = new QuestOWLFactory();
+
+		QuestPreferences pref = new QuestPreferences();
+		pref.setCurrentValueOf(QuestPreferences.DBTYPE, QuestConstants.SEMANTIC_INDEX);
+		pref.setCurrentValueOf(QuestPreferences.ABOX_MODE, QuestConstants.CLASSIC);
+		pref.setCurrentValueOf(QuestPreferences.REFORMULATION_TECHNIQUE, QuestConstants.TW);
+		pref.setCurrentValueOf(QuestPreferences.REWRITE, QuestConstants.TRUE);
+
+		fac.setPreferenceHolder(pref);
+
+		QuestOWL quest = fac.createReasoner(ontology);
+		QuestOWLConnection qconn =  quest.getConnection();
+		QuestOWLStatement st = qconn.createStatement();
+		
+		String rewriting = st.getRewriting(query);
 		st.close();
 		
 		quest.dispose();
+		
+		return rewriting;
 	}
-
+	
 }
