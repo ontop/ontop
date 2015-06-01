524bec8f7dbed5f39015090c819324b6a2c0d512
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2015-06-01 15:02:49 +0200

    Escapes view names.

--- reformulation-core/src/main/java/org/semanticweb/ontop/owlrefplatform/core/sql/SQLGenerator.java
+++ reformulation-core/src/main/java/org/semanticweb/ontop/owlrefplatform/core/sql/SQLGenerator.java
@@ -789,7 +789,8 @@ public class SQLGenerator implements SQLQueryGenerator {
 
 		//String viewname = String.format(VIEW_ANS_NAME, pred);
 		// String viewname = "Q" + pred + "View";
-		String viewname = sqladapter.nameView(VIEW_PREFIX, pred.getName(), VIEW_ANS_SUFFIX, viewNames);
+		String safePredicateName = escapeName(pred.getName());
+		String viewname = sqladapter.nameView(VIEW_PREFIX, safePredicateName, VIEW_ANS_SUFFIX, viewNames);
 		viewNames.add(viewname);
 
 		List<String> columns = Lists
@@ -806,6 +807,13 @@ public class SQLGenerator implements SQLQueryGenerator {
 				unionView, columns);
 		metadata.add(viewU);
 		sqlAnsViewMap.put(pred, unionView);
+	}
+
+	/**
+	 * Escapes view names.
+	 */
+	private static String escapeName(String name) {
+		return name.replace('.', '_');
 	}
 
 	/***
@@ -2481,7 +2489,7 @@ public class SQLGenerator implements SQLQueryGenerator {
 
 			Predicate tablePredicate = atom.getFunctionSymbol();
 			String tableName = tablePredicate.getName();
-			String safeTableName = tableName.replace('.', '_');
+			String safeTableName = escapeName(tableName);
 			DataDefinition def = metadata.getDefinition(tableName);
 
 			if (def == null) {
