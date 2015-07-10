8a5a67a06fda3a582d609176c08fc3f58c9e5873
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2015-07-10 18:47:05 +0200

    Binding removal implementation under progress.

--- reformulation-core/src/main/java/org/semanticweb/ontop/pivotalrepr/impl/ConstructionNodeTools.java
+++ reformulation-core/src/main/java/org/semanticweb/ontop/pivotalrepr/impl/ConstructionNodeTools.java
@@ -1,15 +1,16 @@
 package org.semanticweb.ontop.pivotalrepr.impl;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import org.semanticweb.ontop.model.*;
 import org.semanticweb.ontop.model.impl.VariableImpl;
 import org.semanticweb.ontop.owlrefplatform.core.basicoperations.ImmutableSubstitutionImpl;
 import org.semanticweb.ontop.pivotalrepr.ConstructionNode;
+import org.semanticweb.ontop.pivotalrepr.ImmutableQueryModifiers;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static org.semanticweb.ontop.owlrefplatform.core.basicoperations.ImmutableSubstitutionUtilities.computeOneWayUnifier;
 
@@ -55,6 +56,31 @@ public class ConstructionNodeTools {
         }
     }
 
+    /**
+     * TODO: explain and find a better name
+     */
+    private static class NewSubstitutions {
+        private final ImmutableSubstitution<VariableOrGroundTerm> substitutionToPropagate;
+        private final ImmutableSubstitution<VariableOrGroundTerm> newBindings;
+
+        protected NewSubstitutions(ImmutableSubstitution<VariableOrGroundTerm> substitutionToPropagate,
+                                   ImmutableSubstitution<VariableOrGroundTerm> newBindings) {
+            this.substitutionToPropagate = substitutionToPropagate;
+            this.newBindings = newBindings;
+        }
+
+        public ImmutableSubstitution<VariableOrGroundTerm> getNewBindings() {
+            return newBindings;
+        }
+
+        public Optional<ImmutableSubstitution<VariableOrGroundTerm>> getOptionalSubstitutionToPropagate() {
+            if (substitutionToPropagate.isEmpty()) {
+                return Optional.absent();
+            }
+            return Optional.of(substitutionToPropagate);
+        }
+    }
+
 
     /**
      * TODO: explain
@@ -71,7 +97,6 @@ public class ConstructionNodeTools {
          */
         ImmutableSubstitution<ImmutableTerm> composedSubstitution = additionalBindingsSubstitution.composeWith(formerConstructionNode.getSubstitution());
         ImmutableMap.Builder<VariableImpl, ImmutableTerm> substitutionMapBuilder = ImmutableMap.builder();
-
 
         ImmutableMap<VariableImpl, ImmutableTerm> compositionMap = composedSubstitution.getImmutableMap();
 
@@ -106,91 +131,218 @@ public class ConstructionNodeTools {
     /**
      * TODO: explain
      *
-     * TODO: determine if the substitution to propagate MUST BE VAR-TO-VAR. Better no!
      */
     public static BindingRemoval newNodeWithLessBindings(ConstructionNode formerConstructionNode,
                                                          ImmutableSubstitution<ImmutableTerm> bindingsToRemove)
-            throws InconsistentBindingException{
-        ImmutableMap.Builder<VariableImpl, ImmutableTerm> substitutionMapBuilder = ImmutableMap.builder();
+            throws InconsistentBindingException {
 
-        ImmutableMap<VariableImpl, ImmutableTerm> formerSubstitutionMap = formerConstructionNode.
-                getDirectBindingSubstitution().getImmutableMap();
+        ImmutableSet<VariableImpl> variablesToRemove = extractVariablesToRemove(formerConstructionNode, bindingsToRemove);
 
-        ImmutableSet.Builder<VariableImpl> projectedVariablesToRemove = ImmutableSet.builder();
-        ImmutableSet.Builder<VariableImpl> projectedVariablesToAdd = ImmutableSet.builder();
+        ImmutableSet<VariableImpl> newVariablesToProject = extractVariablesToProject(variablesToRemove, bindingsToRemove);
+
+        NewSubstitutions newSubstitutions = computeSubstitutionToPropagateAndNewBindings(formerConstructionNode, bindingsToRemove,
+               variablesToRemove, newVariablesToProject);
+
+        ImmutableSubstitution<ImmutableTerm> newBindingSubstitution = computeNewBindingSubstitution(formerConstructionNode, variablesToRemove,
+                newSubstitutions.getNewBindings());
+
+        DataAtom dataAtom = computeNewDataAtom(formerConstructionNode.getProjectionAtom(), variablesToRemove, newVariablesToProject);
+
+
+        Optional<ImmutableQueryModifiers> newOptionalModifiers = computeNewOptionalModifiers(formerConstructionNode.getOptionalModifiers(),
+                bindingsToRemove);
+
+        ConstructionNode newConstructionNode = new ConstructionNodeImpl(dataAtom, newBindingSubstitution, newOptionalModifiers);
+
+        return new BindingRemoval(newConstructionNode, newSubstitutions.getOptionalSubstitutionToPropagate());
+    }
+
+    /**
+     * TODO: explain
+     */
+    private static ImmutableSubstitution<ImmutableTerm> computeNewBindingSubstitution(
+            ConstructionNode formerConstructionNode, ImmutableSet<VariableImpl> variablesToRemove,
+            ImmutableSubstitution<VariableOrGroundTerm> newBindings) {
+        ImmutableMap.Builder<VariableImpl, ImmutableTerm> mapBuilder = ImmutableMap.builder();
+
+        ImmutableMap<VariableImpl, ImmutableTerm> formerSubstitutionMap = formerConstructionNode.getDirectBindingSubstitution().getImmutableMap();
+        for (VariableImpl variable : formerSubstitutionMap.keySet()) {
+            if (!variablesToRemove.contains(variable)) {
+                mapBuilder.put(variable, formerSubstitutionMap.get(variable));
+            }
+        }
+
+        mapBuilder.putAll(newBindings.getImmutableMap());
+
+        return new ImmutableSubstitutionImpl<>(mapBuilder.build());
+
+    }
+
+    /**
+     * TODO: explain
+     */
+    private static ImmutableList<ImmutableSubstitution<VariableOrGroundTerm>> computeBindingUnifiers(
+            ConstructionNode formerConstructionNode, ImmutableSet<VariableImpl> variablesToRemove,
+            ImmutableSubstitution<ImmutableTerm> bindingsToRemove) {
+
+        ImmutableSubstitution<ImmutableTerm> formerSubstitution = formerConstructionNode.getDirectBindingSubstitution();
+
+        ImmutableList.Builder<ImmutableSubstitution<VariableOrGroundTerm>> substitutionListBuilder = ImmutableList.builder();
+
+        for (VariableImpl variable : variablesToRemove) {
+            ImmutableTerm formerTerm = formerSubstitution.get(variable);
+            ImmutableTerm newTerm = bindingsToRemove.get(variable);
+
+            Optional<ImmutableSubstitution<ImmutableTerm>> optionalTermSubstitution = computeOneWayUnifier(
+                    formerTerm, newTerm);
+            /**
+             * If cannot be unified...
+             */
+            if (!optionalTermSubstitution.isPresent()) {
+                throw new InconsistentBindingException("Contradictory bindings found in one child.");
+            }
+            try {
+                substitutionListBuilder.add(
+                        convertToVarOrGroundTermSubstitution(optionalTermSubstitution.get()));
+            } catch (SubstitutionConversionException e) {
+                throw new InconsistentBindingException("Incompatible bindings found in one child.");
+            }
+        }
+
+        return substitutionListBuilder.build();
+    }
+
+    /**
+     * TODO: explain
+     *
+     */
+    private static NewSubstitutions computeSubstitutionToPropagateAndNewBindings(
+            ConstructionNode formerConstructionNode, ImmutableSubstitution<ImmutableTerm> bindingsToRemove,
+            ImmutableSet<VariableImpl> variablesToRemove, ImmutableSet<VariableImpl> newVariablesToProject) {
+
+        ImmutableList<ImmutableSubstitution<VariableOrGroundTerm>> bindingUnifiers = computeBindingUnifiers(
+                formerConstructionNode, variablesToRemove, bindingsToRemove);
 
         Map<VariableImpl, VariableOrGroundTerm> substitutionMapToPropagate = new HashMap<>();
+        ImmutableMap.Builder<VariableImpl, VariableOrGroundTerm> newBindingsMapBuilder = ImmutableMap.builder();
 
 
-        for (VariableImpl variable : formerSubstitutionMap.keySet()) {
-            ImmutableTerm previousTerm = formerSubstitutionMap.get(variable);
+        for (ImmutableSubstitution<VariableOrGroundTerm> unifier : bindingUnifiers) {
+            ImmutableMap<VariableImpl, VariableOrGroundTerm> unificationMap = unifier.getImmutableMap();
 
-            /**
-             * TODO: explain
-             */
-            if (!bindingsToRemove.isDefining(variable)) {
-                substitutionMapBuilder.put(variable, previousTerm);
-            }
-            /**
-             * TODO: explain
-             *
-             * The previous binding will be added to the new substitution.
-             *
-             */
-            else {
-                ImmutableTerm newTerm = bindingsToRemove.get(variable);
+            if (!unifier.isEmpty()) {
+                for (VariableImpl replacedVariable : unificationMap.keySet()) {
+                    VariableOrGroundTerm targetTerm = unificationMap.get(replacedVariable);
 
-                projectedVariablesToRemove.add(variable);
-                // TODO: remove this cast
-                projectedVariablesToAdd.addAll((ImmutableSet<VariableImpl>)(ImmutableSet<?>)newTerm.getReferencedVariables());
-
-                Optional<ImmutableSubstitution<ImmutableTerm>> optionalTermSubstitution = computeOneWayUnifier(
-                        previousTerm, newTerm);
-                /**
-                 * If cannot be unified...
-                 */
-                if (!optionalTermSubstitution.isPresent()) {
-                    throw new InconsistentBindingException("Contradictory bindings found in one child.");
-                }
-
-                // TODO: find a better name
-                ImmutableSubstitution<VariableOrGroundTerm> termSubstitution;
-                try {
-                    termSubstitution = convertToVarOrGroundTermSubstitution(optionalTermSubstitution.get());
-                } catch (SubstitutionConversionException e) {
-                    throw new InconsistentBindingException("Incompatible bindings found in one child.");
-                }
-                if (!termSubstitution.isEmpty()) {
-                    for (VariableImpl replacedVariable : termSubstitution.getImmutableMap().keySet()) {
-                        VariableOrGroundTerm termToPropagate = termSubstitution.getImmutableMap().get(replacedVariable);
-
+                    /**
+                     * New variable ---> goes to the bindings
+                     *
+                     * TODO: understand it better
+                     */
+                    if (newVariablesToProject.contains(replacedVariable)) {
+                        // TODO: check if some conflicts happen
+                        newBindingsMapBuilder.put(replacedVariable, targetTerm);
+                    }
+                    /**
+                     * Sub-tree variable is replaced
+                     * ---> Need to be propagated
+                     */
+                    else {
                         if (!substitutionMapToPropagate.containsKey(replacedVariable)) {
-                            substitutionMapToPropagate.put(replacedVariable, termToPropagate);
+                            substitutionMapToPropagate.put(replacedVariable, targetTerm);
                         }
                         else {
                             /**
                              * Should not have a "conflict" with a ground term. ---> must be a variable.
                              */
                             VariableOrGroundTerm otherTermToPropagate = substitutionMapToPropagate.get(replacedVariable);
-                            if (!otherTermToPropagate.equals(termToPropagate)) {
-                                if (termToPropagate instanceof VariableImpl) {
+                            if (!otherTermToPropagate.equals(targetTerm)) {
+                                if (targetTerm instanceof VariableImpl) {
                                     /**
                                      * Registers the equality to the new substitution.
                                      */
-                                    substitutionMapBuilder.put((VariableImpl) termToPropagate, otherTermToPropagate);
+                                    newBindingsMapBuilder.put((VariableImpl) targetTerm, otherTermToPropagate);
                                 }
                                 else {
-                                    throw new InconsistentBindingException("Should not find a ground term here: " + termToPropagate);
+                                    throw new InconsistentBindingException("Should not find a ground term here: " + targetTerm);
                                 }
                             }
                         }
                     }
                 }
-
             }
         }
 
-        throw new RuntimeException("Not fully implemented yet");
+        ImmutableSubstitution<VariableOrGroundTerm> substitutionToPropagate = new ImmutableSubstitutionImpl<>(
+                ImmutableMap.copyOf(substitutionMapToPropagate));
+        ImmutableSubstitution<VariableOrGroundTerm> newBindings = new ImmutableSubstitutionImpl<>(newBindingsMapBuilder.build());
+
+        return new NewSubstitutions(substitutionToPropagate, newBindings);
+    }
+
+    /**
+     * TODO: explain
+     *
+     */
+    private static ImmutableSet<VariableImpl> extractVariablesToRemove(ConstructionNode formerConstructionNode,
+                                                                       ImmutableSubstitution<ImmutableTerm> bindingsToRemove)
+            throws InconsistentBindingException {
+
+        ImmutableSet<VariableImpl> allVariablesToRemove = bindingsToRemove.getImmutableMap().keySet();
+
+        // Mutable
+        Set<VariableImpl> localVariablesToRemove = new HashSet<>(allVariablesToRemove);
+        localVariablesToRemove.retainAll(formerConstructionNode.getSubstitution().getImmutableMap().keySet());
+
+        /**
+         * Checks that no projected but not-bound variable was proposed to be removed.
+         */
+        ImmutableSet<Variable> projectedVariables = formerConstructionNode.getProjectionAtom().getVariables();
+        for (VariableImpl variable : allVariablesToRemove) {
+            if ((!localVariablesToRemove.contains(variable)) && projectedVariables.contains(variable)) {
+                throw new InconsistentBindingException("The variable to remove " + variable + " is projected but" +
+                        "not bound!");
+            }
+        }
+
+        return ImmutableSet.copyOf(localVariablesToRemove);
+    }
+
+    /**
+     * Extracts the variables that MUST be projected (if not already).
+     *
+     */
+    private static ImmutableSet<VariableImpl> extractVariablesToProject(ImmutableSet<VariableImpl> variablesToRemove,
+                                                                        ImmutableSubstitution<ImmutableTerm> bindingsToRemove) {
+
+        Set<VariableImpl> variablesToProject = new HashSet<>();
+
+        for (VariableImpl variableToRemove : variablesToRemove) {
+            ImmutableTerm targetTerm = bindingsToRemove.get(variableToRemove);
+            // TODO: remove this cast in the future
+            variablesToProject.addAll((Collection<VariableImpl>)(Collection<?>)targetTerm.getReferencedVariables());
+        }
+
+        return ImmutableSet.copyOf(variablesToProject);
+    }
+
+    /**
+     * TODO: explain
+     */
+    private static Optional<ImmutableQueryModifiers> computeNewOptionalModifiers(Optional<ImmutableQueryModifiers> optionalModifiers,
+                                                                                 ImmutableSubstitution<ImmutableTerm> bindingsToRemove) {
+        if (!optionalModifiers.isPresent())
+            return Optional.absent();
+
+        throw new RuntimeException("TODO: implement it");
+    }
+
+    /**
+     * TODO: explain
+     */
+    private static DataAtom computeNewDataAtom(DataAtom projectionAtom, ImmutableSet<VariableImpl> variablesToRemove,
+                                               ImmutableSet<VariableImpl> newVariablesToProject) {
+        throw new RuntimeException("TODO: implement it");
     }
 
     private static ImmutableSubstitution<VariableOrGroundTerm> convertToVarOrGroundTermSubstitution(ImmutableSubstitution<ImmutableTerm> substitution)
