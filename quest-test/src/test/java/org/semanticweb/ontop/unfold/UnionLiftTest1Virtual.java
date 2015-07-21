f0c157035dc804e676a0385ceed4c625bbabe6cb
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2015-07-21 18:33:51 +0200

    Test intermediateQuery built manually added.

--- quest-test/src/test/java/org/semanticweb/ontop/unfold/UnionLiftTest1Virtual.java
+++ quest-test/src/test/java/org/semanticweb/ontop/unfold/UnionLiftTest1Virtual.java
@@ -20,22 +20,24 @@ package org.semanticweb.ontop.unfold;
  * #L%
  */
 
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableMap;
 import org.junit.Before;
 import org.junit.Test;
 import org.semanticweb.ontop.io.ModelIOManager;
-import org.semanticweb.ontop.model.OBDADataFactory;
-import org.semanticweb.ontop.model.OBDAModel;
+import org.semanticweb.ontop.model.*;
+import org.semanticweb.ontop.model.impl.AtomPredicateImpl;
 import org.semanticweb.ontop.model.impl.OBDADataFactoryImpl;
+import org.semanticweb.ontop.model.impl.VariableImpl;
 import org.semanticweb.ontop.owlrefplatform.core.QuestPreferences;
+import org.semanticweb.ontop.owlrefplatform.core.basicoperations.ImmutableSubstitutionImpl;
 import org.semanticweb.ontop.owlrefplatform.owlapi3.QuestOWL;
 import org.semanticweb.ontop.owlrefplatform.owlapi3.QuestOWLConnection;
 import org.semanticweb.ontop.owlrefplatform.owlapi3.QuestOWLFactory;
 import org.semanticweb.ontop.owlrefplatform.owlapi3.QuestOWLResultSet;
 import org.semanticweb.ontop.owlrefplatform.owlapi3.QuestOWLStatement;
-import org.semanticweb.ontop.pivotalrepr.ConstructionNode;
-import org.semanticweb.ontop.pivotalrepr.IntermediateQuery;
-import org.semanticweb.ontop.pivotalrepr.IntermediateQueryBuilder;
-import org.semanticweb.ontop.pivotalrepr.impl.JgraphtIntermediateQueryBuilder;
+import org.semanticweb.ontop.pivotalrepr.*;
+import org.semanticweb.ontop.pivotalrepr.impl.*;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
@@ -48,6 +50,7 @@ import java.sql.Connection;
 import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
+import static org.semanticweb.ontop.pivotalrepr.BinaryAsymmetricOperatorNode.ArgumentPosition.*;
 
 /**
  * Class to check the translation of the combination of Optional/Union in SPARQL into Datalog, and finally 
@@ -55,6 +58,10 @@ import static org.junit.Assert.assertEquals;
  * @author Minda, Guohui, mrezk
  */
 public class UnionLiftTest1Virtual {
+
+	private static final OBDADataFactory DATA_FACTORY = OBDADataFactoryImpl.getInstance();
+	private static final Optional<ImmutableBooleanExpression> NO_EXPRESSION = Optional.absent();
+	private static final Optional<ImmutableQueryModifiers> NO_MODIFIER = Optional.absent();
 
 	private OBDADataFactory fac;
 	private Connection conn;
@@ -135,11 +142,102 @@ public class UnionLiftTest1Virtual {
 
 
 
-    public IntermediateQuery buildQuery1() {
-        IntermediateQueryBuilder builder = new JgraphtIntermediateQueryBuilder();
-        //ConstructionNode root = new
-        return null;
+    public IntermediateQuery buildQuery1() throws Exception {
+		VariableImpl x = (VariableImpl) DATA_FACTORY.getVariable("x");
+		VariableImpl y = (VariableImpl) DATA_FACTORY.getVariable("y");
+		DataAtom rootDataAtom = DATA_FACTORY.getDataAtom(new AtomPredicateImpl("ans1", 2), x, y);
+        ConstructionNode root = new ConstructionNodeImpl(rootDataAtom);
+
+		IntermediateQueryBuilder queryBuilder = new JgraphtIntermediateQueryBuilder();
+		queryBuilder.init(root);
+
+
+		LeftJoinNode topLJ = new LeftJoinNodeImpl(NO_EXPRESSION);
+		queryBuilder.addChild(root, topLJ);
+
+		InnerJoinNode join1 = new InnerJoinNodeImpl(NO_EXPRESSION);
+		queryBuilder.addChild(topLJ, join1, LEFT);
+
+		/**
+		 * Ans 2
+		 */
+
+		DataAtom ans2Atom = DATA_FACTORY.getDataAtom(new AtomPredicateImpl("ans2", 1), x);
+		ConstructionNode topAns2Node = new ConstructionNodeImpl(ans2Atom);
+		queryBuilder.addChild(join1, topAns2Node);
+
+		UnionNode unionAns2 = new UnionNodeImpl();
+		queryBuilder.addChild(topAns2Node, unionAns2);
+
+		VariableImpl a = (VariableImpl) DATA_FACTORY.getVariable("a");
+		ConstructionNode t1Ans2Node = new ConstructionNodeImpl(ans2Atom,
+				new ImmutableSubstitutionImpl<ImmutableTerm>(ImmutableMap.of(x, a)),
+				NO_MODIFIER);
+		queryBuilder.addChild(unionAns2, t1Ans2Node);
+
+		TableNode t1 = new TableNodeImpl(DATA_FACTORY.getDataAtom(new AtomPredicateImpl("t1", 1), a));
+		queryBuilder.addChild(t1Ans2Node, t1);
+
+		VariableImpl b = (VariableImpl) DATA_FACTORY.getVariable("b");
+		ConstructionNode t2Ans2Node = new ConstructionNodeImpl(ans2Atom,
+				new ImmutableSubstitutionImpl<ImmutableTerm>(ImmutableMap.of(x, b)),
+				NO_MODIFIER);
+		queryBuilder.addChild(unionAns2, t2Ans2Node);
+
+		TableNode t2 = new TableNodeImpl(DATA_FACTORY.getDataAtom(new AtomPredicateImpl("t2", 1), b));
+		queryBuilder.addChild(t2Ans2Node, t2);
+
+		/**
+		 * Ans 3
+		 */
+		DataAtom ans3Atom = DATA_FACTORY.getDataAtom(new AtomPredicateImpl("ans3", 1), x);
+		VariableImpl c = (VariableImpl) DATA_FACTORY.getVariable("c");
+		ConstructionNode ans3Node = new ConstructionNodeImpl(ans3Atom,
+				new ImmutableSubstitutionImpl<ImmutableTerm>(ImmutableMap.of(x, c)), NO_MODIFIER);
+		queryBuilder.addChild(join1, ans3Node);
+
+		TableNode t3 = new TableNodeImpl(DATA_FACTORY.getDataAtom(new AtomPredicateImpl("t3", 1), c));
+		queryBuilder.addChild(ans3Node, t3);
+
+
+		/**
+		 * Ans 4
+		 */
+		DataAtom ans4Atom = DATA_FACTORY.getDataAtom(new AtomPredicateImpl("ans4", 2), x, y);
+		ConstructionNode topAns4Node = new ConstructionNodeImpl(ans4Atom);
+		queryBuilder.addChild(topLJ, topAns4Node, RIGHT);
+
+		UnionNode unionAns4 = new UnionNodeImpl();
+		queryBuilder.addChild(topAns4Node, unionAns4);
+
+		VariableImpl d = (VariableImpl) DATA_FACTORY.getVariable("d");
+		VariableImpl e = (VariableImpl) DATA_FACTORY.getVariable("e");
+		ConstructionNode t4Ans4Node = new ConstructionNodeImpl(ans4Atom,
+				new ImmutableSubstitutionImpl<ImmutableTerm>(ImmutableMap.of(x, d, y, e)),
+				NO_MODIFIER);
+		queryBuilder.addChild(unionAns4, t4Ans4Node);
+
+		TableNode t4 = new TableNodeImpl(DATA_FACTORY.getDataAtom(new AtomPredicateImpl("t4", 2), d, e));
+		queryBuilder.addChild(t4Ans4Node, t4);
+
+		VariableImpl f = (VariableImpl) DATA_FACTORY.getVariable("f");
+		VariableImpl g = (VariableImpl) DATA_FACTORY.getVariable("g");
+		ConstructionNode t5Ans4Node = new ConstructionNodeImpl(ans4Atom,
+				new ImmutableSubstitutionImpl<ImmutableTerm>(ImmutableMap.of(x, f, y, g)),
+				NO_MODIFIER);
+		queryBuilder.addChild(unionAns4, t5Ans4Node);
+
+		TableNode t5 = new TableNodeImpl(DATA_FACTORY.getDataAtom(new AtomPredicateImpl("t5", 2), f, g));
+		queryBuilder.addChild(t5Ans4Node, t5);
+
+
+        return queryBuilder.build();
     }
+
+	@Test
+	public void printQuery1() throws Exception {
+		System.out.println("Query 1: \n" + buildQuery1());
+	}
 
 
         @Test
