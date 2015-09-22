dc40cc7fcc01cb0b16f6c318068016534a644265
kontchakov <roman@dcs.bbk.ac.uk>
2015-09-22 15:34:01 +0100

    identifiers test

---
+++ obdalib-core/src/test/java/it/unibz/krdb/sql/TestDatabaseIdentifiers.java
@@ -0,0 +1,30 @@
+package it.unibz.krdb.sql;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class TestDatabaseIdentifiers {
+
+	@Test
+	public void testIdentifiers() throws Exception {
+		try {
+			Connection conn = DriverManager.getConnection(
+					"jdbc:postgresql://localhost/dbconstraints",
+	                "postgres",
+	                "postgres");
+			DatabaseMetaData md = conn.getMetaData();
+			System.out.println(md.getDatabaseProductName());
+			System.out.println("storesLowerCaseIdentifiers: " + md.storesLowerCaseIdentifiers());
+			System.out.println("storesUpperCaseIdentifiers: " + md.storesUpperCaseIdentifiers());
+			System.out.println("storesMixedCaseIdentifiers: " + md.storesMixedCaseIdentifiers());
+			System.out.println("storesLowerCaseQuotedIdentifiers: " + md.storesLowerCaseQuotedIdentifiers());
+			System.out.println("storesUpperCaseQuotedIdentifiers: " + md.storesUpperCaseQuotedIdentifiers());
+			System.out.println("storesMixedCaseQuotedIdentifiers: " + md.storesMixedCaseQuotedIdentifiers());
+		}
+		catch (Exception e) {
+		}	
+	}
+}
