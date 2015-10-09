7c3c9d22a67a630f75541fa7d1838dbc57076701
elemguzel <elemguzel@gmail.com>
2015-10-09 17:42:20 +0200

    some operators tested

--- quest-test/src/test/java/it/unibz/krdb/sql/SAPHANATest.java
+++ quest-test/src/test/java/it/unibz/krdb/sql/SAPHANATest.java
@@ -66,9 +66,9 @@ public class SAPHANATest {
             * Get the book information that is stored in the database 
             */
         String sparqlQuery =
-//                "PREFIX : <http://meraka/moss/exampleBooks.owl#>\n" +
-//                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-//                        "select ?x ?y where {?x rdf:type :Author. ?x :name ?y.} limit 5 offset 2";
+                "PREFIX : <http://meraka/moss/exampleBooks.owl#>\n" +
+                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                        "select ?x ?y where {?x rdf:type :Author. ?x :name ?y.} limit 5 offset 2";
 //                    "PREFIX : <http://meraka/moss/exampleBooks.owl#>\n" +
 //                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
 //                    "select ?title ?author ?genre ?edition where {\n" +
@@ -81,15 +81,31 @@ public class SAPHANATest {
 //        "PREFIX : <http://meraka/moss/exampleBooks.owl#>\n" +
 //                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
 //                "select ?x ?y where {?x rdf:type :Author. ?x :name ?y. FILTER regex(?y,\"Carr\")}";
-                "PREFIX : <http://meraka/moss/exampleBooks.owl#>\n" +
-                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-                        "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
-                    "select ?x ?edition where {\n" +
-                    "?x a :Edition;\n" +
-                        ":editionNumber ?edition .\n" +
-                        "FILTER (xsd:double(?edition) > 1.0)" +
-                        "}";
+//                "PREFIX : <http://meraka/moss/exampleBooks.owl#>\n" +
+//                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+//                        "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+//                    "select ?x ?edition where {\n" +
+//                    "?x a :Edition;\n" +
+//                        ":editionNumber ?edition .\n" +
+//                        "FILTER (xsd:double(?edition) > 1.0)" +
+//                        "}";
+//                "PREFIX : <http://meraka/moss/exampleBooks.owl#>\n" +
+//                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+//                        "select ?x ?y ?yAfter  WHERE {?x rdf:type :Author. ?x :name ?y. \n" +
+////                        "BIND (strlen(?y) as ?yLen)\n" +
+////                        "BIND (substr(?y, 1, 1) as ?ySubStr)\n" +
+////                        "BIND (ucase(?y) as ?yUcase)\n"+
+////                        "BIND (lcase(?y) as ?yLcase)\n"+
+////                        "BIND (strStarts(?y, \"a\") as ?yStart)\n" +
+////                        "BIND (strEnds(?y, \"a\") as ?yEnd)\n" +
+////                        "BIND (strbefore(?y, \"a\") as ?yBefore)\n"+
+//                        "BIND (strafter(?y, \"a\") as ?yAfter)\n" +
+////                        "BIND (contains(?y, \"a\") as ?yContain)\n" +
+//                        "}";
 
+//        "PREFIX : <http://meraka/moss/exampleBooks.owl#>\n" +
+//                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+//                        "select ?x ?y where {?x rdf:type :Author. ?x :name ?y.} ORDER BY DESC(?y)";
         try {
             long t1 = System.currentTimeMillis();
             QuestOWLResultSet rs = st.executeTuple(sparqlQuery);
--- reformulation-core/src/main/java/it/unibz/krdb/obda/owlrefplatform/core/queryevaluation/SAPHANASQLDialectAdapter.java
+++ reformulation-core/src/main/java/it/unibz/krdb/obda/owlrefplatform/core/queryevaluation/SAPHANASQLDialectAdapter.java
@@ -21,4 +21,27 @@ public class SAPHANASQLDialectAdapter extends SQL99DialectAdapter {
             }
         }
     }
+
+//    @Override
+//    public String strStartsOperator(){
+//        return "LEFT(%1$s, LENGTH(%2$s)) LIKE %2$s";
+//    }
+//
+//    @Override
+//    public String strContainsOperator(){
+//        return "LOCATE(%2$s , %1$s) > 0";
+//    }
+//
+//    @Override
+//    public String strBefore(String str, String before) {
+//        return String.format("LEFT(%s,SIGN(LOCATE(%s,%s)) * (LOCATE(%s,%s)-1))", str,   before, str, before, str);
+//    }
+//
+//    @Override
+//    public String strAfter(String str, String after) {
+//        //rtrim is needed to remove the space in
+//        return String.format("RTRIM(SUBSTR(%s,LOCATE(%s,%s)+LENGTH(%s), SIGN(LOCATE(%s,%s))*LENGTH(%s)))",
+//                str, after, str , after, after, str, str);
+//    }
+
 }
