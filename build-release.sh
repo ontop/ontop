804baa9fb0eddb75cdde214353334f1ff9af409b
Guohui Xiao <gh.xiao@gmail.com>
2015-10-12 10:57:38 +0200

    version bumped to 1.16.0-SNAPSHOT

--- build-release.sh
+++ build-release.sh
@@ -49,8 +49,8 @@ export QUEST_SESAME_DIST=QuestSesame
 export QUEST_JETTY_DIST=QuestJetty
 export OWL_API_DIST=QuestOWL
 
-export VERSION=1.15
-export REVISION=1-SNAPSHOT
+export VERSION=1.16
+export REVISION=0
 
 
 # Start building the packages
