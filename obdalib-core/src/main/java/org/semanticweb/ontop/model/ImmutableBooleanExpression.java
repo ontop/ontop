f9108f836c79f953b6c034d1fbb5eecdbec10c53
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2015-11-16 18:35:09 +0100

    ImmutableBooleanExpression.flatten() added.

--- obdalib-core/src/main/java/org/semanticweb/ontop/model/ImmutableBooleanExpression.java
+++ obdalib-core/src/main/java/org/semanticweb/ontop/model/ImmutableBooleanExpression.java
@@ -1,7 +1,15 @@
 package org.semanticweb.ontop.model;
 
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+
 public interface ImmutableBooleanExpression extends BooleanExpression, ImmutableFunctionalTerm {
     @Override
     ImmutableBooleanExpression clone();
+
+    /**
+     * Flattens AND expressions.
+     */
+    ImmutableList<ImmutableBooleanExpression> flatten();
 }
--- obdalib-core/src/main/java/org/semanticweb/ontop/model/impl/ImmutableBooleanExpressionImpl.java
+++ obdalib-core/src/main/java/org/semanticweb/ontop/model/impl/ImmutableBooleanExpressionImpl.java
@@ -1,10 +1,7 @@
 package org.semanticweb.ontop.model.impl;
 
 import com.google.common.collect.ImmutableList;
-import org.semanticweb.ontop.model.BooleanExpression;
-import org.semanticweb.ontop.model.BooleanOperationPredicate;
-import org.semanticweb.ontop.model.ImmutableBooleanExpression;
-import org.semanticweb.ontop.model.ImmutableTerm;
+import org.semanticweb.ontop.model.*;
 
 public class ImmutableBooleanExpressionImpl extends ImmutableFunctionalTermImpl implements ImmutableBooleanExpression {
     protected ImmutableBooleanExpressionImpl(BooleanOperationPredicate functor, ImmutableTerm... terms) {
@@ -22,5 +19,32 @@ public class ImmutableBooleanExpressionImpl extends ImmutableFunctionalTermImpl 
     @Override
     public ImmutableBooleanExpression clone() {
         return this;
+    }
+
+    /**
+     * Recursive
+     */
+    @Override
+    public ImmutableList<ImmutableBooleanExpression> flatten() {
+
+        Predicate functionSymbol = getFunctionSymbol();
+        /**
+         * Only flattens AND expressions.
+         */
+        if (functionSymbol.equals(OBDAVocabulary.AND)) {
+            ImmutableList.Builder<ImmutableBooleanExpression> listBuilder = ImmutableList.builder();
+            for (ImmutableTerm subTerm : getImmutableTerms()) {
+                /**
+                 * Recursive call
+                 */
+                if (subTerm instanceof ImmutableBooleanExpression) {
+                    listBuilder.addAll(((ImmutableBooleanExpression)subTerm).flatten());
+                }
+            }
+            return listBuilder.build();
+        }
+        else {
+            return ImmutableList.of((ImmutableBooleanExpression)this);
+        }
     }
 }
