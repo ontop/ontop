cddf7cab8e17471c652feac0ca26e1727429d52b
Guohui Xiao <gh.xiao@gmail.com>
2016-01-06 00:07:55 +0100

    ontop-test scripts

---
+++ scripts/ontop-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Usage: ./quest-sesame=test.sh 
+
+cd $ONTOP_BUILD_PATH/quest-sesame
+mvn clean
+mvn site
+rm -rf $ONTOP_REPORT_PATH/quest-sesame
+cp -R target/site $ONTOP_REPORT_PATH/quest-sesame
