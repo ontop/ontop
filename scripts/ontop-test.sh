145f506eaf1c3196ccc055637046de479fa24a19
Guohui Xiao <gh.xiao@gmail.com>
2016-01-06 00:08:05 +0100

    ontop-test scripts

--- scripts/ontop-test.sh
+++ scripts/ontop-test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Usage: ./quest-sesame=test.sh 
-
-cd $ONTOP_BUILD_PATH/quest-sesame
+export BUILD_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}/" )" && cd .. && pwd )"
+cd $BUILD_ROOT
 mvn clean
+mvn install -DskipTests=true
+cd $BUILD_ROOT/../quest-sesame
+mvn install -DskipTests=false
 mvn site
-rm -rf $ONTOP_REPORT_PATH/quest-sesame
-cp -R target/site $ONTOP_REPORT_PATH/quest-sesame
+rm -rf $ONTOP_REPORT_PATH/quest-test
+cp -R target/site $ONTOP_REPORT_PATH/quest-test
