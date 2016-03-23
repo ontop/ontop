cafebbfb0bc75fd58105b296e22eaf82d4422851
Sarah Komla-Ebri <sarahkeb89@gmail.com>
2016-03-23 18:01:42 +0100

    modift test case

--- ontop-protege/src/test/java/it/unibz/krdb/obda/gui/PrettyOptionPaneTest.java
+++ ontop-protege/src/test/java/it/unibz/krdb/obda/gui/PrettyOptionPaneTest.java
@@ -8,15 +8,16 @@ public class PrettyOptionPaneTest {
 
     public static void main(String[] args) {
 
-        String longMessage = "Text is super long and it will take a few lines to get everything that is written here. " +
+        String longMessage = "Here we have an test to show when PrettyOptionPane can be really useful. "+
+                "This text is super long and it will take a few lines to get everything that is written here. " +
                 "It looks really horrible when the default JOption pane is used. " +
                 "It is is better to create a PrettyOptionPane that reduces the default size of the window and makes it looks really pretty. ";
 
         // create a jframe
-        JFrame frame = new JFrame("JOptionPane showMessageDialog example");
+        JFrame frame = new JFrame("JOptionPane showPrettyMessageDialog example");
 
-        // show a joptionpane dialog using showMessageDialog
-        OptionPaneUtils.showMessageDialog(frame, longMessage, "Error", JOptionPane.ERROR_MESSAGE);
+        // show a joptionpane dialog using showPrettyMessageDialog
+        OptionPaneUtils.showPrettyMessageDialog(frame, longMessage, "Error", JOptionPane.ERROR_MESSAGE);
         System.exit(0);
     }
 }
