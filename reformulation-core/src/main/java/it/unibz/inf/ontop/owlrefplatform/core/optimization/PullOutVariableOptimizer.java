3b9687b0be0781bfb9746a932df3a9920c96bf3a
Ugur Donmez <ugur.donmez@unibz.it>
2016-06-13 11:41:03 +0200

    PullOutVariableOptimizer improved and new tests

--- reformulation-core/src/main/java/it/unibz/inf/ontop/owlrefplatform/core/optimization/PullOutVariableOptimizer.java
+++ reformulation-core/src/main/java/it/unibz/inf/ontop/owlrefplatform/core/optimization/PullOutVariableOptimizer.java
@@ -11,6 +11,7 @@ import it.unibz.inf.ontop.pivotalrepr.*;
 import it.unibz.inf.ontop.pivotalrepr.proposal.NodeCentricOptimizationResults;
 import it.unibz.inf.ontop.pivotalrepr.proposal.PullVariableOutOfDataNodeProposal;
 import it.unibz.inf.ontop.pivotalrepr.proposal.impl.PullVariableOutOfDataNodeProposalImpl;
+import it.unibz.inf.ontop.pivotalrepr.proposal.impl.SubstitutionUpPropagationProposalImpl;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,12 +26,11 @@ public class PullOutVariableOptimizer implements IntermediateQueryOptimizer {
 
     @Override
     public IntermediateQuery optimize(IntermediateQuery query) {
-        throw new RuntimeException("TODO: reimplement PullOutVariableOptimizer");
-//        try {
-//            return pullOutSomeVariables(query);
-//        } catch (EmptyQueryException e) {
-//            throw new IllegalStateException("Inconsistency: PullOutVariableOptimizer should not empty the query");
-//        }
+        try {
+            return pullOutSomeVariables(query);
+        } catch (EmptyQueryException e) {
+            throw new IllegalStateException("Inconsistency: PullOutVariableOptimizer should not empty the query");
+        }
     }
 
     /**
@@ -39,186 +39,173 @@ public class PullOutVariableOptimizer implements IntermediateQueryOptimizer {
      * Depth-first exploration
      *
      */
-//    private IntermediateQuery pullOutSomeVariables(IntermediateQuery initialQuery)
-//            throws EmptyQueryException {
-//        // Non-final
-//        Optional<QueryNode> optionalCurrentNode = initialQuery.getFirstChild(initialQuery.getRootConstructionNode());
-//
-//        // Non-final
-//        IntermediateQuery currentQuery = initialQuery;
-//
-//        while (optionalCurrentNode.isPresent()) {
-//            final QueryNode currentNode = optionalCurrentNode.get();
-//
-//            /**
-//             * Targets: join-like nodes
-//             */
-//            if (currentNode instanceof JoinLikeNode) {
-//                UpdatedNodeAndQuery<JoinLikeNode> updatedJoinLikeNodeAndQuery = optimizeJoinLikeNode(currentQuery,
-//                        (JoinLikeNode) currentNode);
-//                NextNodeAndQuery nextNodeAndQuery = optimizeJoinLikeNodeChildren(
-//                        updatedJoinLikeNodeAndQuery.getNextQuery(), updatedJoinLikeNodeAndQuery.getUpdatedNode());
-//
-//                optionalCurrentNode = nextNodeAndQuery.getOptionalNextNode();
-//                currentQuery = nextNodeAndQuery.getNextQuery();
-//            }
-//            else if (currentNode instanceof DataNode) {
-//                NextNodeAndQuery nextNodeAndQuery = optimizeDataNode(currentQuery,
-//                        (DataNode) currentNode);
-//                optionalCurrentNode = nextNodeAndQuery.getOptionalNextNode();
-//                currentQuery = nextNodeAndQuery.getNextQuery();
-//
-//            }
-//            else {
-//                optionalCurrentNode = getDepthFirstNextNode(currentQuery, currentNode);
-//            }
-//        }
-//        return currentQuery;
-//    }
-//
-//    /**
-//     * TODO: explain
-//     */
-//    private NextNodeAndQuery optimizeJoinLikeNodeChildren(IntermediateQuery initialQuery, JoinLikeNode initialJoinLikeNode)
-//            throws EmptyQueryException {
-//
-//        // Non-final variables
-//        IntermediateQuery currentQuery = initialQuery;
-//        QueryNode currentJoinLikeNode = initialJoinLikeNode;
-//        Optional<QueryNode> optionalCurrentChildNode = currentQuery.getFirstChild(initialJoinLikeNode);
-//
-//        int startIndex = getStartIndex(initialQuery, Optional.of((QueryNode)initialJoinLikeNode));
-//
-//        Set<Variable> alreadySeenVariables = new HashSet<>();
-//
-//        while (optionalCurrentChildNode.isPresent()) {
-//
-//            QueryNode childNode = optionalCurrentChildNode.get();
-//
-//            /**
-//             * PullOutVariableProposals only concern SubTreeDelimiterNodes
-//             */
-//            if (childNode instanceof SubTreeDelimiterNode) {
-//
-//                /**
-//                 * May update alreadySeenVariables (append-only)!!
-//                 */
-//                Optional<PullVariableOutOfDataNodeProposal> optionalProposal = buildProposal((SubTreeDelimiterNode) childNode,
-//                        alreadySeenVariables, startIndex);
-//
-//                /**
-//                 * Applies the proposal and extracts the next node (and query)
-//                 * from the results
-//                 */
-//                if (optionalProposal.isPresent()) {
-//                    PullVariableOutOfDataNodeProposal proposal = optionalProposal.get();
-//
-//                    NodeCentricOptimizationResults<SubTreeDelimiterNode> results = currentQuery.applyProposal(proposal);
-//
-//                    currentQuery = results.getResultingQuery();
-//                    optionalCurrentChildNode = results.getOptionalNextSibling();
-//
-//                    java.util.Optional<QueryNode> optionalCurrentParent = results.getOptionalClosestAncestor();
-//                    if (!optionalCurrentParent.isPresent()) {
-//                        throw new IllegalStateException("Missing parent of current node after pulling out some variables");
-//                    }
-//                    currentJoinLikeNode = optionalCurrentParent.get();
-//                }
-//                else {
-//                    optionalCurrentChildNode = currentQuery.getNextSibling(childNode);
-//                }
-//            }
-//            else {
-//                optionalCurrentChildNode = currentQuery.getNextSibling(childNode);
-//            }
-//        }
-//
-//        return new NextNodeAndQuery(getDepthFirstNextNode(currentQuery, currentJoinLikeNode),
-//                currentQuery);
-//    }
-//
-//    /**
-//     * TODO: explain!!!!
-//     *
-//     * By default, returns 0 (checks all the arguments)
-//     *
-//     */
-//    protected int getStartIndex(IntermediateQuery query, Optional<QueryNode> optionalParentNode) {
-//        return 0;
-//    }
-//
-//    /**
-//     * TODO: explain
-//     *
-//     * By default, does nothing
-//     *
-//     * Can be overwritten (useful for extensions).
-//     *
-//     */
-//    private UpdatedNodeAndQuery<JoinLikeNode> optimizeJoinLikeNode(IntermediateQuery initialQuery,
-//                                                                     JoinLikeNode initialJoinLikeNode) {
-//        return new UpdatedNodeAndQuery<>(initialJoinLikeNode, initialQuery);
-//    }
-//
-//    /**
-//     * May update alreadySeenVariables!
-//     *
-//     * TODO: explain it further
-//     */
-//    private Optional<PullVariableOutOfDataNodeProposal> buildProposal(SubTreeDelimiterNode delimiterNode,
-//                                                                      Set<Variable> alreadySeenVariables,
-//                                                                      int startIndex) {
-//        ImmutableList.Builder<Integer> variableIndexListBuilder = ImmutableList.builder();
-//
-//        DataAtom dataAtom = delimiterNode.getProjectionAtom();
-//        ImmutableList<? extends VariableOrGroundTerm> arguments = dataAtom.getArguments();
-//
-//        for (int i=startIndex; i < arguments.size(); i++) {
-//            VariableOrGroundTerm argument = arguments.get(i);
-//            if (argument instanceof Variable) {
-//                Variable variable = (Variable) argument;
-//
-//                /**
-//                 * Tracks the indexes of variables to "pull out"
-//                 */
-//                if (!alreadySeenVariables.add(variable)) {
-//                    variableIndexListBuilder.add(i);
-//                }
-//            }
-//        }
-//
-//        ImmutableList<Integer> toReplaceVariableIndexes = variableIndexListBuilder.build();
-//        if (!toReplaceVariableIndexes.isEmpty()) {
-//            PullVariableOutOfDataNodeProposal proposal = new PullVariableOutOfDataNodeProposalImpl(delimiterNode, toReplaceVariableIndexes);
-//            return Optional.of(proposal);
-//        }
-//        else {
-//            return Optional.empty();
-//        }
-//    }
-//
-//    /**
-//     * TODO: explain
-//     *
-//     */
-//    private NextNodeAndQuery optimizeDataNode(IntermediateQuery currentQuery, DataNode currentNode)
-//            throws EmptyQueryException {
-//
-//        int startIndex = getStartIndex(currentQuery, currentQuery.getParent(currentNode));
-//
-//        Optional<PullVariableOutOfDataNodeProposal> optionalProposal = buildProposal(currentNode, new HashSet<Variable>(), startIndex);
-//
-//        if (optionalProposal.isPresent()) {
-//            PullVariableOutOfDataNodeProposal proposal = optionalProposal.get();
-//            NodeCentricOptimizationResults<SubTreeDelimiterNode> results = currentQuery.applyProposal(proposal);
-//
-//            return getNextNodeAndQuery(results);
-//
-//        }
-//        else {
-//            // NB: a DataNode is not expected to have a child
-//            return new NextNodeAndQuery(getDepthFirstNextNode(currentQuery, currentNode), currentQuery);
-//        }
-//    }
+    private IntermediateQuery pullOutSomeVariables(IntermediateQuery initialQuery)
+            throws EmptyQueryException {
+        // Non-final
+        Optional<QueryNode> optionalCurrentNode = initialQuery.getFirstChild(initialQuery.getRootConstructionNode());
+
+        // Non-final
+        IntermediateQuery currentQuery = initialQuery;
+
+        Set<Variable> alreadySeenVariables = new HashSet<>();
+
+        while (optionalCurrentNode.isPresent()) {
+            final QueryNode currentNode = optionalCurrentNode.get();
+
+            /**
+             * Targets: join-like nodes
+             */
+            if (currentNode instanceof JoinLikeNode) {
+                UpdatedNodeAndQuery<JoinLikeNode> updatedJoinLikeNodeAndQuery = optimizeJoinLikeNode(currentQuery,
+                        (JoinLikeNode) currentNode);
+                NextNodeAndQuery nextNodeAndQuery = optimizeJoinLikeNodeChildren(
+                        updatedJoinLikeNodeAndQuery.getNextQuery(), updatedJoinLikeNodeAndQuery.getUpdatedNode());
+
+                optionalCurrentNode = nextNodeAndQuery.getOptionalNextNode();
+                currentQuery = nextNodeAndQuery.getNextQuery();
+            }
+            else if (currentNode instanceof DataNode) {
+                NextNodeAndQuery nextNodeAndQuery = optimizeDataNode(currentQuery,
+                        (DataNode) currentNode);
+                optionalCurrentNode = nextNodeAndQuery.getOptionalNextNode();
+                currentQuery = nextNodeAndQuery.getNextQuery();
+
+            }
+            else {
+                optionalCurrentNode = getDepthFirstNextNode(currentQuery, currentNode);
+            }
+        }
+        return currentQuery;
+    }
+
+    /**
+     * TODO: explain
+     */
+    private NextNodeAndQuery optimizeJoinLikeNodeChildren(IntermediateQuery initialQuery, JoinLikeNode initialJoinLikeNode)
+            throws EmptyQueryException {
+
+        // Non-final variables
+        IntermediateQuery currentQuery = initialQuery;
+        QueryNode currentJoinLikeNode = initialJoinLikeNode;
+        Optional<QueryNode> optionalCurrentChildNode = currentQuery.getFirstChild(initialJoinLikeNode);
+
+        Set<Variable> alreadySeenVariables = new HashSet<>();
+
+        while (optionalCurrentChildNode.isPresent()) {
+
+            QueryNode childNode = optionalCurrentChildNode.get();
+
+            /**
+             * PullOutVariableProposals only concern SubTreeDelimiterNodes
+             */
+            if (childNode instanceof DataNode) {
+
+                /**
+                 * May update alreadySeenVariables (append-only)!!
+                 */
+                Optional<PullVariableOutOfDataNodeProposal> optionalProposal = buildProposal((DataNode) childNode,
+                        alreadySeenVariables);
+
+                /**
+                 * Applies the proposal and extracts the next node (and query)
+                 * from the results
+                 */
+                if (optionalProposal.isPresent()) {
+                    PullVariableOutOfDataNodeProposal proposal = optionalProposal.get();
+
+                    NodeCentricOptimizationResults<DataNode> results = currentQuery.applyProposal(proposal);
+
+                    currentQuery = results.getResultingQuery();
+                    optionalCurrentChildNode = results.getOptionalNextSibling();
+
+                    Optional<QueryNode> optionalCurrentParent = results.getOptionalClosestAncestor();
+                    if (!optionalCurrentParent.isPresent()) {
+                        throw new IllegalStateException("Missing parent of current node after pulling out some variables");
+                    }
+                    currentJoinLikeNode = optionalCurrentParent.get();
+                }
+                else {
+                    optionalCurrentChildNode = currentQuery.getNextSibling(childNode);
+                }
+            }
+            else {
+                optionalCurrentChildNode = currentQuery.getNextSibling(childNode);
+            }
+        }
+
+        return new NextNodeAndQuery(getDepthFirstNextNode(currentQuery, currentJoinLikeNode),
+                currentQuery);
+    }
+
+    /**
+     * TODO: explain
+     *
+     * By default, does nothing
+     *
+     * Can be overwritten (useful for extensions).
+     *
+     */
+    private UpdatedNodeAndQuery<JoinLikeNode> optimizeJoinLikeNode(IntermediateQuery initialQuery,
+                                                                     JoinLikeNode initialJoinLikeNode) {
+        return new UpdatedNodeAndQuery<>(initialJoinLikeNode, initialQuery);
+    }
+
+    /**
+     * May update alreadySeenVariables!
+     *
+     * TODO: explain it further
+     */
+    private Optional<PullVariableOutOfDataNodeProposal> buildProposal(DataNode dataNode,
+                                                                      Set<Variable> alreadySeenVariables) {
+        ImmutableList.Builder<Integer> variableIndexListBuilder = ImmutableList.builder();
+
+        DataAtom dataAtom = dataNode.getProjectionAtom();
+        ImmutableList<? extends VariableOrGroundTerm> arguments = dataAtom.getArguments();
+
+        for (int i=0; i < arguments.size(); i++) {
+            VariableOrGroundTerm argument = arguments.get(i);
+            if (argument instanceof Variable) {
+                Variable variable = (Variable) argument;
+
+                /**
+                 * Tracks the indexes of variables to "pull out"
+                 */
+                if (!alreadySeenVariables.add(variable)) {
+                    variableIndexListBuilder.add(i);
+                }
+            }
+        }
+
+        ImmutableList<Integer> toReplaceVariableIndexes = variableIndexListBuilder.build();
+        if (!toReplaceVariableIndexes.isEmpty()) {
+            PullVariableOutOfDataNodeProposal proposal = new PullVariableOutOfDataNodeProposalImpl(dataNode,
+                    toReplaceVariableIndexes);
+            return Optional.of(proposal);
+        }
+        else {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * TODO: explain
+     *
+     */
+    private NextNodeAndQuery optimizeDataNode(IntermediateQuery currentQuery, DataNode currentNode)
+            throws EmptyQueryException {
+
+        Optional<PullVariableOutOfDataNodeProposal> optionalProposal = buildProposal(currentNode, new HashSet<>());
+
+        if (optionalProposal.isPresent()) {
+            PullVariableOutOfDataNodeProposal proposal = optionalProposal.get();
+            NodeCentricOptimizationResults<DataNode> results = currentQuery.applyProposal(proposal);
+
+            return getNextNodeAndQuery(results);
+        }
+        else {
+            // NB: a DataNode is not expected to have a child
+            return new NextNodeAndQuery(getDepthFirstNextNode(currentQuery, currentNode), currentQuery);
+        }
+    }
 }
 
