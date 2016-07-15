ddb7723118734c633275b88bb02d06528b479e88
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2016-07-15 11:22:06 +0200

    JoinBooleanExpressionExecutor now relies on an empty node removal to get rid of a join node with an unsatisfied condition.

--- reformulation-core/src/main/java/it/unibz/inf/ontop/executor/join/JoinBooleanExpressionExecutor.java
+++ reformulation-core/src/main/java/it/unibz/inf/ontop/executor/join/JoinBooleanExpressionExecutor.java
@@ -5,12 +5,13 @@ import com.google.common.collect.ImmutableList;
 import it.unibz.inf.ontop.executor.NodeCentricInternalExecutor;
 import it.unibz.inf.ontop.model.ImmutableExpression;
 import it.unibz.inf.ontop.pivotalrepr.NonCommutativeOperatorNode.ArgumentPosition;
+import it.unibz.inf.ontop.pivotalrepr.impl.EmptyNodeImpl;
 import it.unibz.inf.ontop.pivotalrepr.impl.InnerJoinNodeImpl;
 import it.unibz.inf.ontop.pivotalrepr.proposal.impl.NodeCentricOptimizationResultsImpl;
 import it.unibz.inf.ontop.pivotalrepr.impl.QueryTreeComponent;
-import it.unibz.inf.ontop.pivotalrepr.proposal.impl.ReactToChildDeletionProposalImpl;
 import it.unibz.inf.ontop.pivotalrepr.*;
 import it.unibz.inf.ontop.pivotalrepr.proposal.*;
+import it.unibz.inf.ontop.pivotalrepr.proposal.impl.RemoveEmptyNodesProposalImpl;
 
 import static it.unibz.inf.ontop.executor.join.JoinExtractionUtils.*;
 
@@ -39,25 +40,22 @@ public class JoinBooleanExpressionExecutor implements NodeCentricInternalExecuto
                     query.getMetadata());
         }
         /**
-         * The filter condition can be satisfied --> the join node and its sub-tree is thus removed from the tree.
+         * The filter condition cannot be satisfied --> the join node and its sub-tree is thus removed from the tree.
          * Returns no join node.
          */
         catch (InsatisfiedExpressionException e) {
-            /**
-             * Will remain the sames, whatever happens
-             */
-            ReactToChildDeletionProposal reactionProposal = new ReactToChildDeletionProposalImpl(
-                    parentNode,
-                    query.getNextSibling(originalTopJoinNode),
-                    query.getOptionalPosition(parentNode, originalTopJoinNode),
-                    query.getProjectedVariables(originalTopJoinNode));
 
-            // Removes the join node
-            treeComponent.removeSubTree(originalTopJoinNode);
-            ReactToChildDeletionResults deletionResults = query.applyProposal(reactionProposal);
+            EmptyNode replacingEmptyNode = new EmptyNodeImpl(query.getProjectedVariables(originalTopJoinNode));
+            treeComponent.replaceSubTree(originalTopJoinNode, replacingEmptyNode);
 
-            return new NodeCentricOptimizationResultsImpl<>(deletionResults.getResultingQuery(),
-                    deletionResults.getOptionalNextSibling(), Optional.of(deletionResults.getClosestRemainingAncestor()));
+            RemoveEmptyNodesProposalImpl<EmptyNode> cleaningProposal
+                    = new RemoveEmptyNodesProposalImpl<>(replacingEmptyNode);
+
+            NodeCentricOptimizationResults<EmptyNode> cleaningResults = query.applyProposal(cleaningProposal, true);
+
+            // Converts it into a NodeCentricOptimizationResults<InnerJoinNode>
+            return new NodeCentricOptimizationResultsImpl<>(cleaningResults.getResultingQuery(),
+                    cleaningResults.getOptionalNextSibling(), cleaningResults.getOptionalClosestAncestor());
         }
 
         /**
