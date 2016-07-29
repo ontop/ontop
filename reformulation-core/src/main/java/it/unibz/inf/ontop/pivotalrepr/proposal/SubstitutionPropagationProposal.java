9058c095f22b6146ac116f61284eda1634f29102
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2016-07-29 18:04:09 +0200

    Navigation results are now tested in SubstitutionPropagationTest.

--- reformulation-core/src/main/java/it/unibz/inf/ontop/pivotalrepr/proposal/SubstitutionPropagationProposal.java
+++ reformulation-core/src/main/java/it/unibz/inf/ontop/pivotalrepr/proposal/SubstitutionPropagationProposal.java
@@ -1,12 +1,15 @@
 package it.unibz.inf.ontop.pivotalrepr.proposal;
 
 import it.unibz.inf.ontop.model.ImmutableTerm;
-import it.unibz.inf.ontop.model.VariableOrGroundTerm;
 import it.unibz.inf.ontop.pivotalrepr.QueryNode;
 import it.unibz.inf.ontop.model.ImmutableSubstitution;
 
 /**
  * Proposal to apply a substitution to a focus node and to propagate it down and up.
+ *
+ * Assumption: the substitution must be directly applicable to the focus node
+ * (i.e. the focus node should not reject it).
+ *
  *
  */
 public interface SubstitutionPropagationProposal<T extends QueryNode> extends SimpleNodeCentricOptimizationProposal<T> {
