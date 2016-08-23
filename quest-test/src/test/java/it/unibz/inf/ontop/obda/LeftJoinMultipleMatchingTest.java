95967a9d6911e374c16485a43d03429114aeab85
Sarah Komla-Ebri <sarahkeb89@gmail.com>
2016-08-23 15:06:04 +0200

    Bug with leftjoin

--- quest-test/src/test/java/it/unibz/inf/ontop/obda/LeftJoinMultipleMatchingTest.java
+++ quest-test/src/test/java/it/unibz/inf/ontop/obda/LeftJoinMultipleMatchingTest.java
@@ -1,7 +1,8 @@
 package it.unibz.inf.ontop.obda;
 
 /**
- * Test case for Rais ontology. Problem with OPTIONAL
+ * Test case for Rais ontology.
+ * Problem with OPTIONAL when the left join is having on the right multiple mappings
  */
 
 import it.unibz.inf.ontop.io.ModelIOManager;
@@ -14,6 +15,7 @@ import it.unibz.inf.ontop.owlrefplatform.owlapi.*;
 import it.unibz.inf.ontop.querymanager.QueryController;
 import it.unibz.inf.ontop.querymanager.QueryControllerGroup;
 import it.unibz.inf.ontop.querymanager.QueryControllerQuery;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.semanticweb.owlapi.apibinding.OWLManager;
@@ -23,22 +25,44 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
-public class RaisTestPostgres {
+public class LeftJoinMultipleMatchingTest {
     private OBDADataFactory fac;
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
     private OBDAModel obdaModel;
     private OWLOntology ontology;
 
-    private final String owlFile = "src/test/resources/rais-ontology.owl";
-    private final String obdaFile = "src/test/resources/rais-ontology.obda";
+    private final String owlFile = "src/test/resources/optional/rais-ontology.owl";
+
+
+    private Connection sqlConnection;
+    private QuestOWLConnection conn;
+    private QuestOWL reasoner;
 
     @Before
     public void setUp() throws Exception {
+
+        sqlConnection= DriverManager.getConnection("jdbc:h2:mem:raisjunit","sa", "");
+        java.sql.Statement s = sqlConnection.createStatement();
+
+        try {
+            String text = new Scanner( new File("src/test/resources/optional/rais-create-h2.sql") ).useDelimiter("\\A").next();
+            s.execute(text);
+
+        } catch(SQLException sqle) {
+            System.out.println("Exception in creating db from script");
+        }
+
+        s.close();
+
 
         fac = OBDADataFactoryImpl.getInstance();
 
@@ -46,28 +70,44 @@ public class RaisTestPostgres {
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
         ontology = manager.loadOntologyFromOntologyDocument((new File(owlFile)));
 
-        // Loading the OBDA data
-        obdaModel = fac.getOBDAModel();
 
-        ModelIOManager ioManager = new ModelIOManager(obdaModel);
-        ioManager.load(obdaFile);
+
 
     }
 
+
+    @After
+    public void tearDown() throws Exception{
+        conn.close();
+        reasoner.dispose();
+        if (!sqlConnection.isClosed()) {
+            java.sql.Statement s = sqlConnection.createStatement();
+            try {
+                s.execute("DROP ALL OBJECTS DELETE FILES");
+            } catch (SQLException sqle) {
+                System.out.println("Table not found, not dropping");
+            } finally {
+                s.close();
+                sqlConnection.close();
+            }
+        }
+    }
+
     private void runTests(QuestPreferences p) throws Exception {
+
         // Creating a new instance of the reasoner
         QuestOWLFactory factory = new QuestOWLFactory();
         QuestOWLConfiguration config = QuestOWLConfiguration.builder().obdaModel(obdaModel).preferences(p).build();
-        QuestOWL reasoner = factory.createReasoner(ontology, config);
+        reasoner = factory.createReasoner(ontology, config);
 
         // Now we are ready for querying
-        QuestOWLConnection conn = reasoner.getConnection();
+        conn = reasoner.getConnection();
         QuestOWLStatement st = conn.createStatement();
 
 
         QueryController qc = new QueryController();
         QueryIOManager qman = new QueryIOManager(qc);
-        qman.load("src/test/resources/rais-ontology.q");
+        qman.load("src/test/resources/optional/rais-ontology.q");
 
         for (QueryControllerGroup group : qc.getGroups()) {
             for (QueryControllerQuery query : group.getQueries()) {
@@ -101,6 +141,15 @@ public class RaisTestPostgres {
     @Test
     public void testRaisQueries() throws Exception {
 
+        String obdaFile = "src/test/resources/optional/rais-ontology-unmodified.obda";
+
+        // Loading the OBDA data
+        obdaModel = fac.getOBDAModel();
+
+        ModelIOManager ioManager = new ModelIOManager(obdaModel);
+        ioManager.load(obdaFile);
+
+
         QuestPreferences p = new QuestPreferences();
 
         runTests(p);
@@ -109,6 +158,13 @@ public class RaisTestPostgres {
 
     @Test
     public void testOptionalQuery() throws Exception {
+        // Loading the OBDA data
+        obdaModel = fac.getOBDAModel();
+
+        ModelIOManager ioManager = new ModelIOManager(obdaModel);
+
+        ioManager.load("src/test/resources/optional/rais-ontology-small.obda");
+
 
         QuestPreferences p = new QuestPreferences();
 
@@ -119,9 +175,8 @@ public class RaisTestPostgres {
                 "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
                 "PREFIX xml: <http://www.w3.org/XML/1998/namespace#>\n" +
                 "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-                "SELECT DISTINCT ?ao ?title ?date WHERE {\n" +
+                "SELECT DISTINCT ?ao ?date WHERE {\n" +
                 "?ao a rais:ArchiveObject .\n" +
-                "?ao rais:title ?title.\n" +
                 " OPTIONAL{ ?ao rais:archivalDate ?date.}\n" +
                 "}\n";
 
@@ -133,31 +188,30 @@ public class RaisTestPostgres {
         // Creating a new instance of the reasoner
     	QuestOWLFactory factory = new QuestOWLFactory();
         QuestOWLConfiguration config = QuestOWLConfiguration.builder().obdaModel(obdaModel).preferences(p).build();
-        QuestOWL reasoner = factory.createReasoner(ontology, config);
+        reasoner = factory.createReasoner(ontology, config);
 
         // Now we are ready for querying
-        QuestOWLConnection conn = reasoner.getConnection();
+        conn = reasoner.getConnection();
         QuestOWLStatement st = conn.createStatement();
 
+        log.debug("Executing query: ");
+        log.debug("Query: \n{}", query);
 
-                log.debug("Executing query: ");
-                log.debug("Query: \n{}", query);
+        long start = System.nanoTime();
+        QuestOWLResultSet res = st.executeTuple(query);
+        long end = System.nanoTime();
 
-                long start = System.nanoTime();
-                QuestOWLResultSet res = st.executeTuple(query);
-                long end = System.nanoTime();
+        double time = (end - start) / 1000;
 
-                double time = (end - start) / 1000;
+        int count = 0;
+        while (res.nextRow()) {
+            count += 1;
+        }
+        log.debug("Total result: {}", count);
 
-                int count = 0;
-                while (res.nextRow()) {
-                    count += 1;
-                }
-                log.debug("Total result: {}", count);
+        assertFalse(count == 0);
 
-                assertFalse(count == 0);
-
-                log.debug("Elapsed time: {} ms", time);
+        log.debug("Elapsed time: {} ms", time);
 
         return count;
 
