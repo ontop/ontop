cca665143f2f2df34b3679d79c26c74b9f8f07d0
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2016-11-06 13:51:43 +0100

    RedundantSelfJoinTest.testJoiningConditionRemoval() added. Not passed.

--- reformulation-core/src/test/java/it/unibz/inf/ontop/reformulation/tests/RedundantSelfJoinTest.java
+++ reformulation-core/src/test/java/it/unibz/inf/ontop/reformulation/tests/RedundantSelfJoinTest.java
@@ -24,7 +24,9 @@ import it.unibz.inf.ontop.pivotalrepr.proposal.impl.InnerJoinOptimizationProposa
 import it.unibz.inf.ontop.model.*;
 import it.unibz.inf.ontop.pivotalrepr.*;
 
+import static it.unibz.inf.ontop.model.ExpressionOperation.EQ;
 import static it.unibz.inf.ontop.model.ExpressionOperation.LT;
+import static it.unibz.inf.ontop.model.ExpressionOperation.OR;
 import static it.unibz.inf.ontop.model.impl.OBDAVocabulary.NULL;
 import static it.unibz.inf.ontop.pivotalrepr.NonCommutativeOperatorNode.ArgumentPosition.LEFT;
 import static it.unibz.inf.ontop.pivotalrepr.NonCommutativeOperatorNode.ArgumentPosition.RIGHT;
@@ -806,6 +808,54 @@ public class RedundantSelfJoinTest {
         assertTrue(optionalClosestAncestor.isPresent());
         assertTrue(optionalClosestAncestor.get().isSyntacticallyEquivalentTo(newRootNode));
         assertFalse(results.getOptionalNextSibling().isPresent());
+    }
+
+    @Test
+    public void testJoiningConditionRemoval() throws EmptyQueryException {
+
+        IntermediateQueryBuilder queryBuilder = new DefaultIntermediateQueryBuilder(metadata);
+        DistinctVariableOnlyDataAtom projectionAtom = DATA_FACTORY.getDistinctVariableOnlyDataAtom(ANS1_PREDICATE, M, N, O);
+        ConstructionNode constructionNode = new ConstructionNodeImpl(projectionAtom.getVariables());
+        queryBuilder.init(projectionAtom, constructionNode);
+
+        ImmutableExpression joiningCondition = DATA_FACTORY.getImmutableExpression(OR,
+                DATA_FACTORY.getImmutableExpression(EQ, O, ONE),
+                DATA_FACTORY.getImmutableExpression(EQ, O, TWO));
+
+        InnerJoinNode joinNode = new InnerJoinNodeImpl(Optional.of(joiningCondition));
+        queryBuilder.addChild(constructionNode, joinNode);
+        ExtensionalDataNode dataNode1 =  new ExtensionalDataNodeImpl(DATA_FACTORY.getDataAtom(TABLE1_PREDICATE, M, N, ONE));
+        ExtensionalDataNode dataNode2 =  new ExtensionalDataNodeImpl(DATA_FACTORY.getDataAtom(TABLE1_PREDICATE, M, N1, O));
+
+        queryBuilder.addChild(joinNode, dataNode1);
+        queryBuilder.addChild(joinNode, dataNode2);
+
+        IntermediateQuery query = queryBuilder.build();
+        System.out.println("\nBefore optimization: \n" +  query);
+
+        IntermediateQueryBuilder expectedQueryBuilder = new DefaultIntermediateQueryBuilder(metadata);
+        DistinctVariableOnlyDataAtom projectionAtom1 = DATA_FACTORY.getDistinctVariableOnlyDataAtom(ANS1_PREDICATE, M, N, O);
+        ConstructionNode constructionNode1 = new ConstructionNodeImpl(projectionAtom1.getVariables(),
+                new ImmutableSubstitutionImpl<>(ImmutableMap.of(O, ONE)), Optional.empty());
+        expectedQueryBuilder.init(projectionAtom1, constructionNode1);
+
+        ExtensionalDataNode dataNode5 =  new ExtensionalDataNodeImpl(DATA_FACTORY.getDataAtom(TABLE1_PREDICATE, M, N, ONE));
+        expectedQueryBuilder.addChild(constructionNode1, dataNode5);
+
+        IntermediateQuery expectedQuery = expectedQueryBuilder.build();
+
+        System.out.println("\n Expected query : \n" +  expectedQuery);
+
+        NodeCentricOptimizationResults<InnerJoinNode> results = query.applyProposal(new InnerJoinOptimizationProposalImpl(joinNode));
+        IntermediateQuery optimizedQuery = results.getResultingQuery();
+
+        System.out.println("\n After optimization: \n" +  optimizedQuery);
+
+        assertTrue(IQSyntacticEquivalenceChecker.areEquivalent(optimizedQuery, expectedQuery));
+
+        assertFalse(results.getOptionalNewNode().isPresent());
+        assertTrue(results.getOptionalReplacingChild().isPresent());
+        assertTrue(results.getOptionalReplacingChild().get().isSyntacticallyEquivalentTo(dataNode5));
     }
 
 
