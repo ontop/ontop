07a5cb5b2cb7a26392be2eb305258f054b9a217a
Sarah Komla-Ebri <sarahkeb89@gmail.com>
2017-02-07 13:10:14 +0100

    simplify

--- ontop-docker-test-env/src/test/resources/containers/load-scripts-db2.sh
+++ ontop-docker-test-env/src/test/resources/containers/load-scripts-db2.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-db2 "create db ontop"
-db2 "connect to ontop"
-for f in /tmp/sql_scripts/all/*.sql; do db2 -tmf $f; done
-db2 "commit work"
-db2 "connect reset"
-for f in /tmp/sql_scripts/db2/*.sql; do db2 -tmf $f; done
+for f in /tmp/sql_scripts/*.sql; do db2 -tmf $f; done
+
+
