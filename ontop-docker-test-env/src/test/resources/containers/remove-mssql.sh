981092188f0614c2f4e597fe1e1784e6b5942dab
Sarah Komla-Ebri <sarahkeb89@gmail.com>
2017-02-24 18:52:39 +0100

    add mssql on docker

---
+++ ontop-docker-test-env/src/test/resources/containers/remove-mssql.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+docker kill ontop_mssql_running
+docker rm ontop_mssql_running
+docker rmi ontop_mssql
---
+++ ontop-docker-test-env/src/test/resources/containers/run-mssql.sh
@@ -0,0 +1,1 @@
+docker run --name ontop_mssql_running -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Mssql1.0' -p 1433:1433 -d ontop_mssql
