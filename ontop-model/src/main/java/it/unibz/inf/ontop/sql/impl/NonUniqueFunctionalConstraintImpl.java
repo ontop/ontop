c541802362b7d661fa3e021194af4d61188b9fcb
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2017-04-19 17:52:37 +0200

    NonUniqueFunctionalConstraint added.
    Former-commit-id: 26b22ff801aa76b6900bf38bd525b7c345629fd2 [formerly 667a6e82710c4f1b50da053fb7fb93fb3def4bed]
    Former-commit-id: 0267c33f1d04ae622be2d3555e8874066e7e5a00

---
+++ ontop-model/src/main/java/it/unibz/inf/ontop/sql/impl/NonUniqueFunctionalConstraintImpl.java
@@ -0,0 +1,59 @@
+package it.unibz.inf.ontop.sql.impl;
+
+import com.google.common.collect.ImmutableSet;
+import it.unibz.inf.ontop.sql.Attribute;
+import it.unibz.inf.ontop.sql.NonUniqueFunctionalConstraint;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NonUniqueFunctionalConstraintImpl implements NonUniqueFunctionalConstraint {
+
+    private final ImmutableSet<Attribute> determinants;
+    private final ImmutableSet<Attribute> dependents;
+
+    private NonUniqueFunctionalConstraintImpl(ImmutableSet<Attribute> determinants,
+                                             ImmutableSet<Attribute> dependents) {
+        this.determinants = determinants;
+        this.dependents = dependents;
+    }
+
+    @Override
+    public ImmutableSet<Attribute> getDeterminants() {
+        return determinants;
+    }
+
+    @Override
+    public ImmutableSet<Attribute> getDependents() {
+        return dependents;
+    }
+
+
+    public static class BuilderImpl implements Builder {
+
+        private final Set<Attribute> determinants;
+        private final Set<Attribute> dependents;
+
+        public BuilderImpl() {
+            determinants = new HashSet<>();
+            dependents = new HashSet<>();
+        }
+
+
+        @Override
+        public void addDeterminant(Attribute determinant) {
+            determinants.add(determinant);
+        }
+
+        @Override
+        public void addDependent(Attribute dependent) {
+            dependents.add(dependent);
+        }
+
+        @Override
+        public NonUniqueFunctionalConstraint build() {
+            return new NonUniqueFunctionalConstraintImpl(ImmutableSet.copyOf(determinants),
+                    ImmutableSet.copyOf(dependents));
+        }
+    }
+}
