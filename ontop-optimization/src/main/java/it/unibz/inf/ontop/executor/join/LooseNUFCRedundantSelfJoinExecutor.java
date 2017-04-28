f61ffe518249f52d127994c72ee141b4f852453a
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2017-04-28 20:17:46 +0200

    First complete implementation of LooseNUFCRedundantSelfJoinExecutor. Passing dedicated tests.
    Former-commit-id: d32e313375655df2af3aac74240af35c265e9631

--- ontop-optimization/src/main/java/it/unibz/inf/ontop/executor/join/LooseNUFCRedundantSelfJoinExecutor.java
+++ ontop-optimization/src/main/java/it/unibz/inf/ontop/executor/join/LooseNUFCRedundantSelfJoinExecutor.java
@@ -9,11 +9,16 @@ import it.unibz.inf.ontop.model.*;
 import it.unibz.inf.ontop.model.impl.ImmutabilityTools;
 import it.unibz.inf.ontop.model.impl.ImmutableUnificationTools;
 import it.unibz.inf.ontop.pivotalrepr.DataNode;
+import it.unibz.inf.ontop.pivotalrepr.InnerJoinNode;
 import it.unibz.inf.ontop.pivotalrepr.IntermediateQuery;
+import it.unibz.inf.ontop.pivotalrepr.QueryNode;
 import it.unibz.inf.ontop.sql.*;
 import it.unibz.inf.ontop.utils.ImmutableCollectors;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static it.unibz.inf.ontop.injection.OntopModelSettings.CardinalityPreservationMode.LOOSE;
 import static it.unibz.inf.ontop.model.impl.OntopModelSingletons.DATA_FACTORY;
@@ -39,7 +44,7 @@ public class LooseNUFCRedundantSelfJoinExecutor extends RedundantSelfJoinExecuto
     }
 
     @Override
-    protected Optional<PredicateLevelProposal> proposePerPredicate(ImmutableCollection<DataNode> initialNodes,
+    protected Optional<PredicateLevelProposal> proposePerPredicate(InnerJoinNode joinNode, ImmutableCollection<DataNode> initialNodes,
                                                                    AtomPredicate predicate, DBMetadata dbMetadata,
                                                                    ImmutableList<Variable> priorityVariables,
                                                                    IntermediateQuery query)
@@ -70,7 +75,7 @@ public class LooseNUFCRedundantSelfJoinExecutor extends RedundantSelfJoinExecuto
         }
         ImmutableList<ImmutableSubstitution<VariableOrGroundTerm>> dependentUnifiers = dependentUnifierBuilder.build();
 
-        ImmutableList<DataNode> nodesToRemove = selectNodesToRemove(query, initialNodes, constraintNodeMap);
+        ImmutableSet<DataNode> nodesToRemove = selectNodesToRemove(query, joinNode, constraintNodeMap, predicate);
 
         return (dependentUnifiers.isEmpty() && nodesToRemove.isEmpty())
                 ? Optional.empty()
@@ -183,35 +188,109 @@ public class LooseNUFCRedundantSelfJoinExecutor extends RedundantSelfJoinExecuto
      *
      * TODO: consider the case of predicates with multiple non-unique functional dependencies
      */
-    private ImmutableList<DataNode> selectNodesToRemove(
-            IntermediateQuery query, ImmutableCollection<DataNode> samePredicateNodes,
-            ImmutableMap<NonUniqueFunctionalConstraint, ImmutableCollection<Collection<DataNode>>> constraintNodeMap) {
+    private ImmutableSet<DataNode> selectNodesToRemove(
+            IntermediateQuery query, InnerJoinNode joinNode,
+            ImmutableMap<NonUniqueFunctionalConstraint, ImmutableCollection<Collection<DataNode>>> constraintNodeMap, AtomPredicate predicate) {
 
-        if ((settings.getCardinalityPreservationMode() != LOOSE) || (constraintNodeMap.size() != 1)) {
-            return ImmutableList.of();
+        if (settings.getCardinalityPreservationMode() != LOOSE) {
+            return ImmutableSet.of();
         }
 
-        Map.Entry<NonUniqueFunctionalConstraint, ImmutableCollection<Collection<DataNode>>> constraintEntry =
-                constraintNodeMap.entrySet().iterator().next();
-        NonUniqueFunctionalConstraint constraint = constraintEntry.getKey();
-        ImmutableCollection<Collection<DataNode>> clusters = constraintEntry.getValue();
+        ImmutableSet<Variable> requiredAndCooccuringVariables = extractRequiredAndCooccuringVariables(query, joinNode);
 
-
-        ImmutableSet<DataNode> requiredNodes = findRequiredNodes(query, samePredicateNodes, constraint, clusters);
-
-        // TODO: implement it seriously
-        return ImmutableList.of();
+        /*
+         * Expects that different unique constraints can only remove independent data nodes (-> no conflict)
+         *
+         * TODO: show why this works
+         */
+        return constraintNodeMap.entrySet().stream()
+                .flatMap(e -> selectNodesToRemovePerConstraint(requiredAndCooccuringVariables, e.getKey(), e.getValue(),
+                        predicate))
+                .collect(ImmutableCollectors.toSet());
     }
 
-    private ImmutableSet<DataNode> findRequiredNodes(IntermediateQuery query, ImmutableCollection<DataNode> samePredicateNodes,
-                                                     NonUniqueFunctionalConstraint constraint,
-                                                     ImmutableCollection<Collection<DataNode>> clusters) {
-        DataNode firstDataNode = samePredicateNodes.iterator().next();
-        // Since all the nodes are assumed to be the children of the same join node, this concern all these data nodes
-        ImmutableSet<Variable> requiredVariablesByAncestors = query.getVariablesRequiredByAncestors(firstDataNode);
+    private ImmutableSet<Variable> extractRequiredAndCooccuringVariables(IntermediateQuery query, InnerJoinNode joinNode) {
+        Stream<Variable> requiredVariablesByAncestorStream = Stream.concat(
+                query.getVariablesRequiredByAncestors(joinNode).stream(),
+                joinNode.getLocallyRequiredVariables().stream());
 
-        // TODO: implement seriously
-        return ImmutableSet.copyOf(samePredicateNodes);
+        /*
+         * NB: takes into multiple occurrences of a variable within the same data node. For other nodes, count
+         * only the first occurrence.
+         */
+        ImmutableMultiset<Variable> childrenVariableBag = query.getChildren(joinNode).stream()
+                .flatMap(c -> (c instanceof DataNode)
+                        // Multiset
+                        ? ((DataNode) c).getProjectionAtom().getArguments().stream()
+                        .filter(t -> t instanceof Variable)
+                        .map(v -> (Variable) v)
+                        // Set
+                        : query.getVariables(c).stream())
+                .collect(ImmutableCollectors.toMultiset());
+
+        Stream<Variable> cooccuringVariableStream = childrenVariableBag.entrySet().stream()
+                .filter(e -> e.getCount() > 1)
+                .map(Multiset.Entry::getElement);
+
+        return Stream.concat(requiredVariablesByAncestorStream, cooccuringVariableStream)
+                .collect(ImmutableCollectors.toSet());
+    }
+
+    private Stream<DataNode> selectNodesToRemovePerConstraint(ImmutableSet<Variable> requiredAndCooccuringVariables,
+                                                              NonUniqueFunctionalConstraint constraint,
+                                                              ImmutableCollection<Collection<DataNode>> clusters,
+                                                              AtomPredicate predicate) {
+        ImmutableList<Integer> determinantIndexes = constraint.getDeterminants().stream()
+                .map(Attribute::getIndex)
+                .collect(ImmutableCollectors.toList());
+
+        ImmutableList<Integer> dependentIndexes = constraint.getDependents().stream()
+                .map(Attribute::getIndex)
+                .collect(ImmutableCollectors.toList());
+
+
+        ImmutableSet<Integer> independentIndexes = IntStream.range(1, predicate.getArity() + 1)
+                .filter(i -> !dependentIndexes.contains(i))
+                .filter(i -> !determinantIndexes.contains(i))
+                .boxed()
+                .collect(ImmutableCollectors.toSet());
+
+        return clusters.stream()
+                .flatMap(cluster -> selectNodesToRemovePerCluster(cluster, requiredAndCooccuringVariables,
+                        independentIndexes));
+
+    }
+
+    private Stream<DataNode> selectNodesToRemovePerCluster(Collection<DataNode> cluster,
+                                                           ImmutableSet<Variable> requiredAndCooccuringVariables,
+                                                           ImmutableSet<Integer> independentIndexes) {
+        int clusterSize = cluster.size();
+
+        if (clusterSize < 2)
+            return Stream.empty();
+
+        ImmutableSet<DataNode> removableDataNodes = cluster.stream()
+                .filter(n -> isRemovable(n, independentIndexes, requiredAndCooccuringVariables))
+                .collect(ImmutableCollectors.toSet());
+
+        /*
+         * One node must be kept
+         */
+        return removableDataNodes.size() < clusterSize
+                ? removableDataNodes.stream()
+                : removableDataNodes.stream().skip(1);
+    }
+
+    /**
+     * TODO: explain
+     */
+    private boolean isRemovable(DataNode node, ImmutableSet<Integer> independentIndexes,
+                                ImmutableSet<Variable> requiredAndCooccuringVariables) {
+        ImmutableList<? extends VariableOrGroundTerm> arguments = node.getProjectionAtom().getArguments();
+
+        return independentIndexes.stream()
+                .map(i -> arguments.get(i - 1))
+                .allMatch(t -> (t instanceof Variable) && (!requiredAndCooccuringVariables.contains(t)));
     }
 
 
