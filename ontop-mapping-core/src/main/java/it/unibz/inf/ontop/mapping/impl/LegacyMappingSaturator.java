e25c4f684e943b58a6f8df5434e65c67fb23abef
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2017-05-13 20:47:26 +0200

    Triple mapping assertion generation added to LegacyMappingSaturator.
    Former-commit-id: e7e888f5347acc12519166873c42b426de34822d [formerly d879800e9422070007eafcb12ae61ecd041a7b9f]
    Former-commit-id: 29cc9bbfe6b6bb03c15e21869d85a7cd12fece79

--- ontop-mapping-core/src/main/java/it/unibz/inf/ontop/mapping/impl/LegacyMappingSaturator.java
+++ ontop-mapping-core/src/main/java/it/unibz/inf/ontop/mapping/impl/LegacyMappingSaturator.java
@@ -9,14 +9,21 @@ import it.unibz.inf.ontop.mapping.datalog.Datalog2QueryMappingConverter;
 import it.unibz.inf.ontop.mapping.datalog.Mapping2DatalogConverter;
 import it.unibz.inf.ontop.model.CQIE;
 import it.unibz.inf.ontop.model.DBMetadata;
+import it.unibz.inf.ontop.model.Function;
+import it.unibz.inf.ontop.model.Term;
+import it.unibz.inf.ontop.model.impl.OBDAVocabulary;
 import it.unibz.inf.ontop.owlrefplatform.core.basicoperations.CQContainmentCheckUnderLIDs;
 import it.unibz.inf.ontop.owlrefplatform.core.basicoperations.LinearInclusionDependencies;
 import it.unibz.inf.ontop.owlrefplatform.core.dagjgrapht.TBoxReasoner;
 import it.unibz.inf.ontop.owlrefplatform.core.mappingprocessing.TMappingExclusionConfig;
 import it.unibz.inf.ontop.owlrefplatform.core.mappingprocessing.TMappingProcessor;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static it.unibz.inf.ontop.model.impl.OntopModelSingletons.DATA_FACTORY;
 
 /**
  * Uses the old Datalog-based mapping saturation code
@@ -49,7 +56,56 @@ public class LegacyMappingSaturator implements MappingSaturator {
         List<CQIE> saturatedMappingRules = TMappingProcessor.getTMappings(inputMappingRules, saturatedTBox, true,
                 foreignKeyCQC, tMappingExclusionConfig);
 
-        return datalog2MappingConverter.convertMappingRules(ImmutableList.copyOf(saturatedMappingRules),
+        List<CQIE> allMappingRules = new ArrayList<>(saturatedMappingRules);
+        allMappingRules.addAll(generateTripleMappings(saturatedMappingRules));
+
+        return datalog2MappingConverter.convertMappingRules(ImmutableList.copyOf(allMappingRules),
                 dbMetadata, mapping.getExecutorRegistry(), mapping.getMetadata());
+    }
+
+    /***
+     * Creates mappings with heads as "triple(x,y,z)" from mappings with binary
+     * and unary atoms"
+     *
+     * TODO: clean it
+     */
+    private static List<CQIE> generateTripleMappings(List<CQIE> saturatedRules) {
+        List<CQIE> newmappings = new LinkedList<CQIE>();
+
+        for (CQIE mapping : saturatedRules) {
+            Function newhead = null;
+            Function currenthead = mapping.getHead();
+            if (currenthead.getArity() == 1) {
+				/*
+				 * head is Class(x) Forming head as triple(x,uri(rdf:type),
+				 * uri(Class))
+				 */
+                Function rdfTypeConstant = DATA_FACTORY.getUriTemplate(DATA_FACTORY.getConstantLiteral(OBDAVocabulary.RDF_TYPE));
+
+                String classname = currenthead.getFunctionSymbol().getName();
+                Term classConstant = DATA_FACTORY.getUriTemplate(DATA_FACTORY.getConstantLiteral(classname));
+
+                newhead = DATA_FACTORY.getTripleAtom(currenthead.getTerm(0), rdfTypeConstant, classConstant);
+            }
+            else if (currenthead.getArity() == 2) {
+				/*
+				 * head is Property(x,y) Forming head as triple(x,uri(Property),
+				 * y)
+				 */
+                String propname = currenthead.getFunctionSymbol().getName();
+                Function propConstant = DATA_FACTORY.getUriTemplate(DATA_FACTORY.getConstantLiteral(propname));
+
+                newhead = DATA_FACTORY.getTripleAtom(currenthead.getTerm(0), propConstant, currenthead.getTerm(1));
+            }
+            else {
+				/*
+				 * head is triple(x,uri(Property),y)
+				 */
+                newhead = (Function) currenthead.clone();
+            }
+            CQIE newmapping = DATA_FACTORY.getCQIE(newhead, mapping.getBody());
+            newmappings.add(newmapping);
+        }
+        return newmappings;
     }
 }
