516c95df480cceace9b5aa75076f981b8a1121ad
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2017-05-24 11:08:37 +0200

    MappingVocabularyExtractor: no COL_TYPE -> data property.
    Former-commit-id: 62dcf9ae09ae60fe6c21f56c0024a9b6f72096a5

--- ontop-mapping-sql-core/src/main/java/it/unibz/inf/ontop/ontology/utils/MappingVocabularyExtractor.java
+++ ontop-mapping-sql-core/src/main/java/it/unibz/inf/ontop/ontology/utils/MappingVocabularyExtractor.java
@@ -26,12 +26,12 @@ public class MappingVocabularyExtractor {
                     ontologyVocabulary.createClass(f.getFunctionSymbol().getName());
                 else {
                     Predicate.COL_TYPE secondArgType = f.getFunctionSymbol().getType(1);
-                        if (secondArgType == null) {
-                            // TODO: this should only happen for data properties (no ambiguity for object property).
-                            throw new IllegalArgumentException("In the target of a mapping assertion, the atom "
-                                    + f + " does not have a type for its second argument. This type is required.");
-                        }
-                        else if (secondArgType.equals(Predicate.COL_TYPE.OBJECT))
+//                        if (secondArgType == null) {
+//                            // TODO: this should only happen for data properties (no ambiguity for object property).
+//                            throw new IllegalArgumentException("In the target of a mapping assertion, the atom "
+//                                    + f + " does not have a type for its second argument. This type is required.");
+//                        }
+                        if ((secondArgType != null) && secondArgType.equals(Predicate.COL_TYPE.OBJECT))
                             ontologyVocabulary.createObjectProperty(f.getFunctionSymbol().getName());
                         else
                             ontologyVocabulary.createDataProperty(f.getFunctionSymbol().getName());
