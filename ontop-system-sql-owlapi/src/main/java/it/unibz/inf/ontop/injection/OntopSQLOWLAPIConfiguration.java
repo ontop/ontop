a4b687ddb05077768045ecabee72d948d0a38c4d
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2017-05-25 20:15:49 +0200

    OntopMappingSQLAllOWLAPIConfiguration and its implementation added (new module ontop-mapping-sql-owlapi).
    Former-commit-id: c7157ade79a7ba1dc719b4acb5d2823469d83f06 [formerly 499cf85fd6982cc5f57b6900937d5c22a439ec1f]
    Former-commit-id: 02c401237d3268293fa918995d8da331dfb8c465

--- ontop-system-sql-owlapi/src/main/java/it/unibz/inf/ontop/injection/OntopSQLOWLAPIConfiguration.java
+++ ontop-system-sql-owlapi/src/main/java/it/unibz/inf/ontop/injection/OntopSQLOWLAPIConfiguration.java
@@ -3,14 +3,14 @@ package it.unibz.inf.ontop.injection;
 import it.unibz.inf.ontop.injection.impl.OntopSQLOWLAPIConfigurationImpl;
 
 public interface OntopSQLOWLAPIConfiguration extends OntopStandaloneSQLConfiguration,
-        OntopSystemOWLAPIConfiguration, OntopMappingOWLAPIConfiguration {
+        OntopSystemOWLAPIConfiguration, OntopMappingSQLAllOWLAPIConfiguration {
 
     static Builder<? extends Builder> defaultBuilder() {
         return new OntopSQLOWLAPIConfigurationImpl.BuilderImpl<>();
     }
 
     interface Builder<B extends Builder<B>> extends OntopStandaloneSQLConfiguration.Builder<B>,
-            OntopMappingOWLAPIConfiguration.Builder<B> {
+            OntopMappingSQLAllOWLAPIConfiguration.Builder<B> {
 
         @Override
         OntopSQLOWLAPIConfiguration build();
