81fe103d5ce9195df79d58b4e529814661791d3d
juliencorman <cormanjulien@gmail.com>
2017-07-12 13:00:17 +0200

    Added fixPointFlattener tests
    Former-commit-id: e349ac55c3c556b8931433750f49d6c0764e4463

--- core/optimization/src/test/java/it/unibz/inf/ontop/reformulation/tests/FixPointUnionAndConstructionFlattenerTest.java
+++ core/optimization/src/test/java/it/unibz/inf/ontop/reformulation/tests/FixPointUnionAndConstructionFlattenerTest.java
@@ -1,6 +1,5 @@
 package it.unibz.inf.ontop.reformulation.tests;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import it.unibz.inf.ontop.iq.IntermediateQuery;
 import it.unibz.inf.ontop.iq.IntermediateQueryBuilder;
@@ -10,24 +9,17 @@ import it.unibz.inf.ontop.iq.node.ConstructionNode;
 import it.unibz.inf.ontop.iq.node.ExtensionalDataNode;
 import it.unibz.inf.ontop.iq.node.InnerJoinNode;
 import it.unibz.inf.ontop.iq.node.UnionNode;
-import it.unibz.inf.ontop.iq.node.impl.ImmutableQueryModifiersImpl;
 import it.unibz.inf.ontop.model.atom.DistinctVariableOnlyDataAtom;
 import it.unibz.inf.ontop.model.impl.URITemplatePredicateImpl;
 import it.unibz.inf.ontop.model.predicate.AtomPredicate;
-import it.unibz.inf.ontop.model.predicate.Predicate;
 import it.unibz.inf.ontop.model.predicate.URITemplatePredicate;
 import it.unibz.inf.ontop.model.term.*;
-import it.unibz.inf.ontop.owlrefplatform.core.optimization.ConstructionNodeRemovalOptimizer;
 import it.unibz.inf.ontop.owlrefplatform.core.optimization.FixPointUnionAndConstructionFlattener;
-import it.unibz.inf.ontop.owlrefplatform.core.optimization.FlattenUnionOptimizer;
 import org.junit.Test;
-
-import java.util.Optional;
 
 import static it.unibz.inf.ontop.OptimizationTestingTools.*;
 import static it.unibz.inf.ontop.model.OntopModelSingletons.ATOM_FACTORY;
 import static it.unibz.inf.ontop.model.OntopModelSingletons.DATA_FACTORY;
-import static it.unibz.inf.ontop.model.predicate.Predicate.COL_TYPE.INTEGER;
 import static junit.framework.TestCase.assertTrue;
 public class FixPointUnionAndConstructionFlattenerTest {
 
@@ -168,137 +160,137 @@ public class FixPointUnionAndConstructionFlattenerTest {
         assertTrue(IQSyntacticEquivalenceChecker.areEquivalent(optimizedQuery, query2));
     }
 
-//    @Test
-//    public void fixpointFlattenerTest3() throws EmptyQueryException {
-//
-//        IntermediateQueryBuilder queryBuilder1 = createQueryBuilder(EMPTY_METADATA);
-//        DistinctVariableOnlyDataAtom projectionAtom1 = ATOM_FACTORY.getDistinctVariableOnlyDataAtom(ANS2_PREDICATE,
-//                X1, X2);
-//        ConstructionNode constructionNode1 = IQ_FACTORY.createConstructionNode(
-//                projectionAtom1.getVariables(),
-//                DATA_FACTORY.getSubstitution(X1, Y1, X2, Y2)
-//        );
-//        ConstructionNode constructionNode2 = IQ_FACTORY.createConstructionNode(
-//                ImmutableSet.of(Y1, Y2),
-//                DATA_FACTORY.getSubstitution(Y1, Z1, Y2, Z2)
-//        );
-//        ConstructionNode constructionNode3 = IQ_FACTORY.createConstructionNode(
-//                ImmutableSet.of(Z1, Z2),
-//                DATA_FACTORY.getSubstitution(Z1, generateURI1(X), Z2, generateURI2(Y))
-//        );
-//
-//        ExtensionalDataNode dataNode1 = IQ_FACTORY.createExtensionalDataNode(ATOM_FACTORY.getDataAtom
-//                (TABLE1_PREDICATE, X, Y));
-//
-//        UnionNode unionNode1 = IQ_FACTORY.createUnionNode(ImmutableSet.of(X));
-//        UnionNode unionNode2 = IQ_FACTORY.createUnionNode(ImmutableSet.of(X, Y));
-//        UnionNode unionNode3 = IQ_FACTORY.createUnionNode(ImmutableSet.of(X, Y, Z));
-//        InnerJoinNode innerJoinNode = IQ_FACTORY.createInnerJoinNode();
-//
-//        ExtensionalDataNode dataNode1 = IQ_FACTORY.createExtensionalDataNode(ATOM_FACTORY.getDataAtom(TABLE1_PREDICATE, X, Y));
-//        ExtensionalDataNode dataNode2 = IQ_FACTORY.createExtensionalDataNode(ATOM_FACTORY.getDataAtom(TABLE2_PREDICATE, X, Y));
-//        ExtensionalDataNode dataNode3 = IQ_FACTORY.createExtensionalDataNode(ATOM_FACTORY.getDataAtom
-//                (TABLE3_PREDICATE, X, Y));
-//        ExtensionalDataNode dataNode4 = IQ_FACTORY.createExtensionalDataNode(ATOM_FACTORY.getDataAtom
-//                (TABLE4_PREDICATE, X, Y, Z));
-//        ExtensionalDataNode dataNode5 = IQ_FACTORY.createExtensionalDataNode(ATOM_FACTORY.getDataAtom
-//                (TABLE5_PREDICATE, X, Y, Z));
-//
-//        queryBuilder1.init(projectionAtom1, constructionNode1);
-//        queryBuilder1.addChild(constructionNode1, constructionNode2);
-//        queryBuilder1.addChild(constructionNode2, constructionNode3);
-//        queryBuilder1.addChild(constructionNode3, dataNode1);
-//
-//        IntermediateQuery query1 = queryBuilder1.build();
-//
-//        System.out.println("\nBefore optimization: \n" + query1);
-//
-//        FixPointUnionAndConstructionFlattener fixPointUnionAndConstructionFlattener = new FixPointUnionAndConstructionFlattener();
-//        IntermediateQuery optimizedQuery = fixPointUnionAndConstructionFlattener.optimize(query1);
-//
-//        System.out.println("\nAfter optimization: \n" + optimizedQuery);
-//
-//        ConstructionNode constructionNode4 = IQ_FACTORY.createConstructionNode(
-//                projectionAtom1.getVariables(),
-//                DATA_FACTORY.getSubstitution(X1, generateCompositeURI2(Z1, Z2), X2, Z2)
-//        );
-//        IntermediateQueryBuilder queryBuilder2 = createQueryBuilder(EMPTY_METADATA);
-//
-//        queryBuilder2.init(projectionAtom1, constructionNode4);
-//        queryBuilder2.addChild(constructionNode4, constructionNode3);
-//        queryBuilder2.addChild(constructionNode3, dataNode1);
-//
-//        IntermediateQuery query2 = queryBuilder2.build();
-//        System.out.println("\nExpected: \n" + query2);
-//
-//        assertTrue(IQSyntacticEquivalenceChecker.areEquivalent(optimizedQuery, query2));
-//    }
-//    @Test
-//    public void removeConstructionNodeTest14() throws EmptyQueryException {
-//
-//        IntermediateQueryBuilder queryBuilder1 = createQueryBuilder(EMPTY_METADATA);
-//        DistinctVariableOnlyDataAtom projectionAtom1 = ATOM_FACTORY.getDistinctVariableOnlyDataAtom(ANS1_PREDICATE, X);
-//        ConstructionNode constructionNode1 = IQ_FACTORY.createConstructionNode(
-//                projectionAtom1.getVariables(),
-//                DATA_FACTORY.getSubstitution(X, X1)
-//        );
-//        ConstructionNode constructionNode2 = IQ_FACTORY.createConstructionNode(
-//                ImmutableSet.of(X1),
-//                DATA_FACTORY.getSubstitution(X1, X2)
-//        );
-//        ConstructionNode constructionNode3 = IQ_FACTORY.createConstructionNode(
-//                ImmutableSet.of(X2),
-//                DATA_FACTORY.getSubstitution(X2, Y)
-//        );
-//        ConstructionNode constructionNode4 = IQ_FACTORY.createConstructionNode(
-//                ImmutableSet.of(Y),
-//                DATA_FACTORY.getSubstitution(Y, Y1)
-//        );
-//
-//        InnerJoinNode innerJoinNode = IQ_FACTORY.createInnerJoinNode();
-//        ExtensionalDataNode dataNode1 = IQ_FACTORY.createExtensionalDataNode(ATOM_FACTORY.getDataAtom
-//                (TABLE1_PREDICATE, Z1, X2));
-//        ExtensionalDataNode dataNode2 = IQ_FACTORY.createExtensionalDataNode(ATOM_FACTORY.getDataAtom
-//                (TABLE2_PREDICATE, Z2, Y1));
-//
-//        queryBuilder1.init(projectionAtom1, constructionNode1);
-//        queryBuilder1.addChild(constructionNode1, constructionNode2);
-//        queryBuilder1.addChild(constructionNode2, innerJoinNode);
-//        queryBuilder1.addChild(innerJoinNode, dataNode1);
-//        queryBuilder1.addChild(innerJoinNode, constructionNode3);
-//        queryBuilder1.addChild(constructionNode3, constructionNode4);
-//        queryBuilder1.addChild(constructionNode4, dataNode2);
-//
-//        IntermediateQuery query1 = queryBuilder1.build();
-//
-//        System.out.println("\nBefore optimization: \n" + query1);
-//
-//        ConstructionNodeRemovalOptimizer constructionNodeRemovalOptimizer = new ConstructionNodeRemovalOptimizer();
-//        IntermediateQuery optimizedQuery = constructionNodeRemovalOptimizer.optimize(query1);
-//
-//        System.out.println("\nAfter optimization: \n" + optimizedQuery);
-//
-//        ConstructionNode constructionNode5 = IQ_FACTORY.createConstructionNode(
-//                projectionAtom1.getVariables(),
-//                DATA_FACTORY.getSubstitution(X, X2)
-//        );
-//        ConstructionNode constructionNode6 = IQ_FACTORY.createConstructionNode(
-//                ImmutableSet.of(X2),
-//                DATA_FACTORY.getSubstitution(X2, Y1)
-//        );
-//        IntermediateQueryBuilder queryBuilder2 = createQueryBuilder(EMPTY_METADATA);
-//
-//        queryBuilder2.init(projectionAtom1, constructionNode5);
-//        queryBuilder2.addChild(constructionNode5, innerJoinNode);
-//        queryBuilder2.addChild(innerJoinNode, dataNode1);
-//        queryBuilder2.addChild(innerJoinNode, constructionNode6);
-//        queryBuilder2.addChild(constructionNode6, dataNode2);
-//
-//        IntermediateQuery query2 = queryBuilder2.build();
-//        System.out.println("\nExpected: \n" + query2);
-//
-//        assertTrue(IQSyntacticEquivalenceChecker.areEquivalent(optimizedQuery, query2));
-//    }
+    @Test
+    public void fixpointFlattenerTest3() throws EmptyQueryException {
+
+        IntermediateQueryBuilder queryBuilder1 = createQueryBuilder(EMPTY_METADATA);
+        DistinctVariableOnlyDataAtom projectionAtom1 = ATOM_FACTORY.getDistinctVariableOnlyDataAtom(ANS1_PREDICATE, X);
+        ConstructionNode constructionNode1 = IQ_FACTORY.createConstructionNode(
+                projectionAtom1.getVariables(),
+                DATA_FACTORY.getSubstitution(X, X1)
+        );
+        ConstructionNode constructionNode2 = IQ_FACTORY.createConstructionNode(
+                ImmutableSet.of(X1),
+                DATA_FACTORY.getSubstitution(X1, X2)
+        );
+        UnionNode unionNode1 = IQ_FACTORY.createUnionNode(ImmutableSet.of(X2));
+        UnionNode unionNode2 = IQ_FACTORY.createUnionNode(ImmutableSet.of(X2));
+        ConstructionNode constructionNode3 = IQ_FACTORY.createConstructionNode(
+                ImmutableSet.of(X2),
+                DATA_FACTORY.getSubstitution(X2, Y)
+        );
+        ConstructionNode constructionNode4 = IQ_FACTORY.createConstructionNode(
+                ImmutableSet.of(Y),
+                DATA_FACTORY.getSubstitution(Y, Y1)
+        );
+
+        ExtensionalDataNode dataNode1 = IQ_FACTORY.createExtensionalDataNode(ATOM_FACTORY.getDataAtom
+                (TABLE1_PREDICATE, Z1, X2));
+        ExtensionalDataNode dataNode2 = IQ_FACTORY.createExtensionalDataNode(ATOM_FACTORY.getDataAtom
+                (TABLE2_PREDICATE, Z2, X2));
+        ExtensionalDataNode dataNode3 = IQ_FACTORY.createExtensionalDataNode(ATOM_FACTORY.getDataAtom
+                (TABLE3_PREDICATE, Y1, Y2));
+
+        queryBuilder1.init(projectionAtom1, constructionNode1);
+        queryBuilder1.addChild(constructionNode1, constructionNode2);
+        queryBuilder1.addChild(constructionNode2, unionNode1);
+        queryBuilder1.addChild(unionNode1, dataNode1);
+        queryBuilder1.addChild(unionNode1, unionNode2);
+        queryBuilder1.addChild(unionNode2, dataNode2);
+        queryBuilder1.addChild(unionNode2, constructionNode3);
+        queryBuilder1.addChild(constructionNode3, constructionNode4);
+        queryBuilder1.addChild(constructionNode4, dataNode3);
+
+        IntermediateQuery query1 = queryBuilder1.build();
+
+        System.out.println("\nBefore optimization: \n" + query1);
+
+        FixPointUnionAndConstructionFlattener flattener = new FixPointUnionAndConstructionFlattener();
+        IntermediateQuery optimizedQuery = flattener.optimize(query1);
+
+        System.out.println("\nAfter optimization: \n" + optimizedQuery);
+
+        ConstructionNode constructionNode5 = IQ_FACTORY.createConstructionNode(
+                projectionAtom1.getVariables(),
+                DATA_FACTORY.getSubstitution(X, X2)
+        );
+        ConstructionNode constructionNode6 = IQ_FACTORY.createConstructionNode(
+                ImmutableSet.of(X2),
+                DATA_FACTORY.getSubstitution(X2, Y1)
+        );
+        IntermediateQueryBuilder queryBuilder2 = createQueryBuilder(EMPTY_METADATA);
+
+        queryBuilder2.init(projectionAtom1, constructionNode5);
+        queryBuilder2.addChild(constructionNode5, unionNode1);
+        queryBuilder2.addChild(unionNode1, dataNode1);
+        queryBuilder2.addChild(unionNode1, dataNode2);
+        queryBuilder2.addChild(unionNode1, constructionNode6);
+        queryBuilder2.addChild(constructionNode6, dataNode3);
+
+        IntermediateQuery query2 = queryBuilder2.build();
+        System.out.println("\nExpected: \n" + query2);
+
+        assertTrue(IQSyntacticEquivalenceChecker.areEquivalent(optimizedQuery, query2));
+    }
+
+
+    @Test
+    public void fixpointFlattenerTest4() throws EmptyQueryException {
+
+        IntermediateQueryBuilder queryBuilder1 = createQueryBuilder(EMPTY_METADATA);
+        DistinctVariableOnlyDataAtom projectionAtom1 = ATOM_FACTORY.getDistinctVariableOnlyDataAtom(ANS1_PREDICATE, X);
+        ConstructionNode constructionNode1 = IQ_FACTORY.createConstructionNode(
+                projectionAtom1.getVariables()
+        );
+        UnionNode unionNode1 = IQ_FACTORY.createUnionNode(ImmutableSet.of(X));
+        UnionNode unionNode2 = IQ_FACTORY.createUnionNode(ImmutableSet.of(X, Y));
+        ConstructionNode constructionNode2 = IQ_FACTORY.createConstructionNode(
+                ImmutableSet.of(X, Y)
+        );
+        ConstructionNode constructionNode3 = IQ_FACTORY.createConstructionNode(
+                ImmutableSet.of(X, Y),
+                DATA_FACTORY.getSubstitution(X, X1, Y, Y1)
+        );
+
+        ExtensionalDataNode dataNode1 = IQ_FACTORY.createExtensionalDataNode(ATOM_FACTORY.getDataAtom
+                (TABLE1_PREDICATE, X, Y));
+        ExtensionalDataNode dataNode2 = IQ_FACTORY.createExtensionalDataNode(ATOM_FACTORY.getDataAtom
+                (TABLE2_PREDICATE, X, Y));
+        ExtensionalDataNode dataNode3 = IQ_FACTORY.createExtensionalDataNode(ATOM_FACTORY.getDataAtom
+                (TABLE3_PREDICATE, X1, Y1));
+
+        queryBuilder1.init(projectionAtom1, constructionNode1);
+        queryBuilder1.addChild(constructionNode1, unionNode1);
+        queryBuilder1.addChild(unionNode1, dataNode1);
+        queryBuilder1.addChild(unionNode1, unionNode2);
+        queryBuilder1.addChild(unionNode2, dataNode2);
+        queryBuilder1.addChild(unionNode2, constructionNode2);
+        queryBuilder1.addChild(constructionNode2, constructionNode3);
+        queryBuilder1.addChild(constructionNode3, dataNode3);
+
+        IntermediateQuery query1 = queryBuilder1.build();
+
+        System.out.println("\nBefore optimization: \n" + query1);
+
+        FixPointUnionAndConstructionFlattener flattener = new FixPointUnionAndConstructionFlattener();
+        IntermediateQuery optimizedQuery = flattener.optimize(query1);
+
+        System.out.println("\nAfter optimization: \n" + optimizedQuery);
+
+        IntermediateQueryBuilder queryBuilder2 = createQueryBuilder(EMPTY_METADATA);
+
+        queryBuilder2.init(projectionAtom1, constructionNode1);
+        queryBuilder2.addChild(constructionNode1, unionNode1);
+        queryBuilder2.addChild(unionNode1, dataNode1);
+        queryBuilder2.addChild(unionNode1, dataNode2);
+        queryBuilder2.addChild(unionNode1, constructionNode3);
+        queryBuilder2.addChild(constructionNode3, dataNode3);
+
+        IntermediateQuery query2 = queryBuilder2.build();
+        System.out.println("\nExpected: \n" + query2);
+
+        assertTrue(IQSyntacticEquivalenceChecker.areEquivalent(optimizedQuery, query2));
+    }
 
     private ImmutableFunctionalTerm generateURI1(VariableOrGroundTerm argument) {
         return DATA_FACTORY.getImmutableFunctionalTerm(URI_PREDICATE, URI_TEMPLATE_STR_1, argument);
@@ -310,17 +302,5 @@ public class FixPointUnionAndConstructionFlattenerTest {
 
     private ImmutableFunctionalTerm generateCompositeURI2(ImmutableTerm argument1, ImmutableTerm argument2) {
         return DATA_FACTORY.getImmutableFunctionalTerm(URI_2PREDICATE, URI_TEMPLATE_STR_2_2, argument1, argument2);
-    }
-
-    private ImmutableFunctionalTerm generateInt(VariableOrGroundTerm argument) {
-        return DATA_FACTORY.getImmutableFunctionalTerm(
-                DATA_FACTORY.getDatatypeFactory().getTypePredicate(INTEGER),
-                argument);
-    }
-
-    private ImmutableFunctionalTerm generateString(VariableOrGroundTerm argument) {
-        return DATA_FACTORY.getImmutableFunctionalTerm(
-                DATA_FACTORY.getDatatypeFactory().getTypePredicate(Predicate.COL_TYPE.STRING),
-                argument);
     }
 }
