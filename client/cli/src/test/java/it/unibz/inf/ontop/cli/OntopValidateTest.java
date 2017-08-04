330095ec824f48c0573de0407f0b54ced89371ca
Sarah Komla-Ebri <sarahkeb89@gmail.com>
2017-08-04 15:45:18 +0200

    fix cli validation

--- client/cli/src/test/java/it/unibz/inf/ontop/cli/OntopValidateTest.java
+++ client/cli/src/test/java/it/unibz/inf/ontop/cli/OntopValidateTest.java
@@ -1,14 +1,21 @@
 package it.unibz.inf.ontop.cli;
 
+import org.junit.ClassRule;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.rules.ExternalResource;
 
 @Ignore
 public class OntopValidateTest {
+
+    @ClassRule
+    public static ExternalResource h2Connection = new H2ExternalResourceForBookExample();
+
     @Test
     public void testOntopValidateCMD (){
-        String[] argv = {"validate", "-m", "src/test/resources/booktutorial.obda",
-                "-t", "client/cli/src/test/resources/booktutorial.owl"};
+        String[] argv = {"validate", "-m", "client/cli/src/test/resources/books/exampleBooks.obda",
+                "-t", "client/cli/src/test/resources/books/exampleBooks.owl",
+        "-p",  "client/cli/src/test/resources/books/exampleBooks.properties" };
         Ontop.main(argv);
     }
 }
