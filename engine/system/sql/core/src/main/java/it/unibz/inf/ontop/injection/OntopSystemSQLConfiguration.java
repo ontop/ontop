d6fe5b353c771db8e2cfddd7d0c999c4b65d2168
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2017-09-21 17:12:03 +0200

    QueryReformulationMarriageTest renamed OfflineOnlineMarriageTest. New test for the query engine.

--- engine/system/sql/core/src/main/java/it/unibz/inf/ontop/injection/OntopSystemSQLConfiguration.java
+++ engine/system/sql/core/src/main/java/it/unibz/inf/ontop/injection/OntopSystemSQLConfiguration.java
@@ -1,9 +1,24 @@
 package it.unibz.inf.ontop.injection;
 
 
+import it.unibz.inf.ontop.injection.impl.OntopSystemSQLConfigurationImpl;
+
 public interface OntopSystemSQLConfiguration extends OntopSystemConfiguration, OntopReformulationSQLConfiguration {
 
     @Override
     OntopSystemSQLSettings getSettings();
+
+    /**
+     * This builder will require a OBDA specification to be directly assigned
+     */
+    static Builder<? extends Builder> defaultBuilder() {
+        return new OntopSystemSQLConfigurationImpl.BuilderImpl<>();
+    }
+
+    interface Builder<B extends Builder<B>> extends OntopReformulationSQLConfiguration.Builder<B> {
+
+        @Override
+        OntopSystemSQLConfiguration build();
+    }
 
 }
--- engine/system/sql/core/src/main/java/it/unibz/inf/ontop/injection/impl/OntopSystemSQLConfigurationImpl.java
+++ engine/system/sql/core/src/main/java/it/unibz/inf/ontop/injection/impl/OntopSystemSQLConfigurationImpl.java
@@ -1,6 +1,8 @@
 package it.unibz.inf.ontop.injection.impl;
 
 import com.google.inject.Module;
+import it.unibz.inf.ontop.exception.InvalidOntopConfigurationException;
+import it.unibz.inf.ontop.injection.OntopReformulationSQLSettings;
 import it.unibz.inf.ontop.injection.OntopSystemSQLConfiguration;
 import it.unibz.inf.ontop.injection.OntopSystemSQLSettings;
 
@@ -15,6 +17,11 @@ public class OntopSystemSQLConfigurationImpl extends OntopReformulationSQLConfig
     OntopSystemSQLConfigurationImpl(OntopSystemSQLSettings settings, OntopSystemSQLOptions options,
                                     SpecificationLoader specificationLoader) {
         super(settings, options.sqlTranslationOptions, specificationLoader);
+        this.settings = settings;
+    }
+
+    OntopSystemSQLConfigurationImpl(OntopSystemSQLSettings settings, OntopSystemSQLOptions options) {
+        super(settings, options.sqlTranslationOptions);
         this.settings = settings;
     }
 
@@ -40,6 +47,33 @@ public class OntopSystemSQLConfigurationImpl extends OntopReformulationSQLConfig
          */
         OntopSystemSQLOptions(OntopReformulationSQLOptions sqlTranslationOptions) {
             this.sqlTranslationOptions = sqlTranslationOptions;
+        }
+    }
+
+    static abstract class OntopSystemSQLBuilderMixin<B extends OntopSystemSQLConfiguration.Builder<B>>
+            extends OntopReformulationSQLBuilderMixin<B>
+            implements OntopSystemSQLConfiguration.Builder<B> {
+
+        final OntopSystemSQLOptions generateSystemSQLOptions() {
+            return new OntopSystemSQLOptions(generateSQLReformulationOptions());
+        }
+    }
+
+    /**
+     * Requires the OBDA specification to be already assigned
+     */
+    public static class BuilderImpl<B extends OntopSystemSQLConfiguration.Builder<B>>
+            extends OntopSystemSQLBuilderMixin<B> {
+
+        @Override
+        public OntopSystemSQLConfiguration build() {
+            if (!isOBDASpecificationAssigned())
+                throw new InvalidOntopConfigurationException("An OBDA specification must be assigned " +
+                        "to directly instantiate such a OntopReformulationSQLConfiguration");
+
+            OntopSystemSQLSettings settings = new OntopSystemSQLSettingsImpl(generateProperties());
+            OntopSystemSQLOptions options = generateSystemSQLOptions();
+            return new OntopSystemSQLConfigurationImpl(settings, options);
         }
     }
 }
