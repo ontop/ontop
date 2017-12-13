5a97d7b49ea3fdeaf61a554cd9c34c2a33aaf687
juliencorman <cormanjulien@gmail.com>
2017-12-13 16:50:03 +0100

    Prevented generation of triple mapping for predicates used internally for subqueries

--- core/model/src/main/java/it/unibz/inf/ontop/datalog/DatalogFactory.java
+++ core/model/src/main/java/it/unibz/inf/ontop/datalog/DatalogFactory.java
@@ -10,29 +10,32 @@ import java.util.Optional;
 
 public interface DatalogFactory {
 
-    public CQIE getCQIE(Function head, Function... body);
+    CQIE getCQIE(Function head, Function... body);
 
-    public CQIE getCQIE(Function head, List<Function> body);
+    CQIE getCQIE(Function head, List<Function> body);
 
-    public CQIE getFreshCQIECopy(CQIE rule);
+    CQIE getFreshCQIECopy(CQIE rule);
 
 
-    public DatalogProgram getDatalogProgram();
+    DatalogProgram getDatalogProgram();
 
-    public DatalogProgram getDatalogProgram(MutableQueryModifiers modifiers);
+    DatalogProgram getDatalogProgram(MutableQueryModifiers modifiers);
 
-    public DatalogProgram getDatalogProgram(MutableQueryModifiers modifiers, Collection<CQIE> rules);
+    DatalogProgram getDatalogProgram(MutableQueryModifiers modifiers, Collection<CQIE> rules);
 
     	/* SPARQL meta-predicates */
 
-    public Function getSPARQLJoin(Function t1, Function t2);
+    Function getSPARQLJoin(Function t1, Function t2);
 
-    public Function getSPARQLJoin(Function t1, Function t2, Function joinCondition);
+    Function getSPARQLJoin(Function t1, Function t2, Function joinCondition);
 
     /**
      * Follows the ugly encoding of complex left-join expressions (with a filter on the left side)
      */
-    public Function getSPARQLLeftJoin(List<Function> atoms, List<Function> atoms2, Optional<Function> optionalCondition);
+    Function getSPARQLLeftJoin(List<Function> atoms, List<Function> atoms2, Optional<Function> optionalCondition);
 
-    public Function getSPARQLLeftJoin(Term t1, Term t2);
+    Function getSPARQLLeftJoin(Term t1, Term t2);
+
+    String getSubqueryPredicatePrefix();
+
 }
--- mapping/core/src/test/java/it/unibz/inf/ontop/utils/MappingTestingTools.java
+++ mapping/core/src/test/java/it/unibz/inf/ontop/utils/MappingTestingTools.java
@@ -3,6 +3,9 @@ package it.unibz.inf.ontop.utils;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Injector;
 import it.unibz.inf.ontop.datalog.Datalog2QueryMappingConverter;
+import it.unibz.inf.ontop.datalog.IntermediateQuery2DatalogTranslator;
+import it.unibz.inf.ontop.datalog.impl.IntermediateQuery2DatalogTranslatorImpl;
+import it.unibz.inf.ontop.injection.OntopMappingSettings;
 import it.unibz.inf.ontop.injection.SpecificationFactory;
 import it.unibz.inf.ontop.injection.OntopMappingConfiguration;
 import it.unibz.inf.ontop.injection.IntermediateQueryFactory;
@@ -14,7 +17,8 @@ import it.unibz.inf.ontop.iq.tools.ExecutorRegistry;
 import it.unibz.inf.ontop.dbschema.DBMetadataTestingTools;
 import it.unibz.inf.ontop.spec.mapping.MappingMetadata;
 import it.unibz.inf.ontop.spec.mapping.PrefixManager;
-import it.unibz.inf.ontop.spec.mapping.transformer.MappingNormalizer;
+import it.unibz.inf.ontop.spec.mapping.transformer.*;
+import it.unibz.inf.ontop.spec.mapping.transformer.impl.DefaultMappingTransformer;
 
 import java.util.stream.Stream;
 
@@ -28,6 +32,15 @@ public class MappingTestingTools {
     public static final SpecificationFactory MAPPING_FACTORY;
     public static final MappingNormalizer MAPPING_NORMALIZER;
     public static final Datalog2QueryMappingConverter DATALOG_2_QUERY_MAPPING_CONVERTER;
+    public static final DefaultMappingTransformer DEFAULT_MAPPING_TRANSFORMER;
+    public static final MappingCanonicalRewriter MAPPING_CANONICAL_REWRITER;
+    public static final MappingSaturator MAPPING_SATURATOR;
+    public static final ABoxFactIntoMappingConverter A_BOX_FACT_INTO_MAPPING_CONVERTER;
+    public static final MappingMerger MAPPING_MERGER;
+    public static final OntopMappingSettings ONTOP_MAPPING_SETTINGS;
+    public static final MappingSameAsInverseRewriter SAME_AS_INVERSE_REWRITER;
+    public static final MappingEquivalenceFreeRewriter EQUIVALENCE_FREE_REWRITER;
+    public static final IntermediateQuery2DatalogTranslator INTERMEDIATE_QUERY_2_DATALOG_TRANSLATOR;
 
     public static final PrefixManager EMPTY_PREFIX_MANAGER;
     public static final UriTemplateMatcher EMPTY_URI_TEMPLATE_MATCHER;
@@ -50,6 +63,15 @@ public class MappingTestingTools {
         EMPTY_URI_TEMPLATE_MATCHER = UriTemplateMatcher.create(Stream.of());
         EMPTY_PREFIX_MANAGER = MAPPING_FACTORY.createPrefixManager(ImmutableMap.of());
         EMPTY_MAPPING_METADATA = MAPPING_FACTORY.createMetadata(EMPTY_PREFIX_MANAGER, EMPTY_URI_TEMPLATE_MATCHER);
+        DEFAULT_MAPPING_TRANSFORMER = injector.getInstance(DefaultMappingTransformer.class);
+        MAPPING_CANONICAL_REWRITER = injector.getInstance(MappingCanonicalRewriter.class);
+        MAPPING_SATURATOR= injector.getInstance(MappingSaturator.class);
+        A_BOX_FACT_INTO_MAPPING_CONVERTER = injector.getInstance(ABoxFactIntoMappingConverter.class);
+        ONTOP_MAPPING_SETTINGS = injector.getInstance(OntopMappingSettings.class);
+        MAPPING_MERGER = injector.getInstance(MappingMerger.class);
+        SAME_AS_INVERSE_REWRITER = injector.getInstance(MappingSameAsInverseRewriter.class);
+        EQUIVALENCE_FREE_REWRITER = injector.getInstance(MappingEquivalenceFreeRewriter.class);
+        INTERMEDIATE_QUERY_2_DATALOG_TRANSLATOR = injector.getInstance(IntermediateQuery2DatalogTranslator.class);
     }
 
     public static IntermediateQueryBuilder createQueryBuilder(DBMetadata dbMetadata) {
