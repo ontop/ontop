6599866910f5a123fe5b2bce6f46fcc5a785e957
Roman Kontchakov <roman@dcs.bbk.ac.uk>
2017-12-15 17:43:16 +0000

    cleanup

--- test/semantic-index/src/test/java/it/unibz/inf/ontop/si/dag/DAGTest.java
+++ test/semantic-index/src/test/java/it/unibz/inf/ontop/si/dag/DAGTest.java
@@ -21,22 +21,142 @@ package it.unibz.inf.ontop.si.dag;
  */
 
 
-import it.unibz.inf.ontop.si.SemanticIndexHelper;
 import it.unibz.inf.ontop.spec.ontology.*;
 import it.unibz.inf.ontop.spec.ontology.Equivalences;
 import it.unibz.inf.ontop.spec.ontology.TBoxReasoner;
+import it.unibz.inf.ontop.spec.ontology.impl.DatatypeImpl;
+import it.unibz.inf.ontop.spec.ontology.impl.TBoxReasonerImpl;
+import it.unibz.inf.ontop.spec.ontology.owlapi.OWLAPITranslatorUtility;
 import junit.framework.TestCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class DAGTest extends TestCase {
 
-	SemanticIndexHelper helper	= new SemanticIndexHelper();
+	public final static Logger log = LoggerFactory.getLogger(DAGTest.class);
+
+	public static final String owlloc = "src/test/resources/test/semanticIndex_ontologies/";
+	private Ontology onto;
+
+	private static final String owl_exists = "::__exists__::";
+	private static final String owl_inverse_exists = "::__inverse__exists__::";
+	private static final String owl_inverse = "::__inverse__::";
+
+
+	public List<List<Description>> get_results(String resname) {
+		String resfile = owlloc + resname + ".si";
+		File results = new File(resfile);
+		Document doc = null;
+
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			doc = db.parse(results);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		doc.getDocumentElement().normalize();
+		List<Description> cls = get_dag_type(doc, "classes");
+		List<Description> roles = get_dag_type(doc, "rolles");
+
+		List<List<Description>> rv = new ArrayList<List<Description>>(2);
+		rv.add(cls);
+		rv.add(roles);
+		return rv;
+	}
+
+	/**
+	 * Extract particular type of DAG nodes from XML document
+	 *
+	 * @param doc  XML document containing encoded DAG nodes
+	 * @param type type of DAGNodes to extract
+	 * @return a list of DAGNodes
+	 */
+	private List<Description> get_dag_type(Document doc, String type) {
+		List<Description> rv = new LinkedList<Description>();
+		Node root = doc.getElementsByTagName(type).item(0);
+		NodeList childNodes = root.getChildNodes();
+		for (int i = 0; i < childNodes.getLength(); i++) {
+			if (childNodes.item(i).getNodeType() == Node.ELEMENT_NODE) {
+
+				Element node = (Element) childNodes.item(i);
+				String uri = node.getAttribute("uri");
+
+				boolean inverse = false;
+				boolean exists = false;
+				Description description;
+
+				if (uri.startsWith(owl_exists)) {
+					uri = uri.substring(owl_exists.length());
+					exists = true;
+				}
+				else if (uri.startsWith(owl_inverse_exists)) {
+					uri = uri.substring(owl_inverse_exists.length());
+					inverse = true;
+					exists = true;
+				}
+				else if (uri.startsWith(owl_inverse)) {
+					uri = uri.substring(owl_inverse.length());
+					inverse = true;
+				}
+
+				if (type.equals("classes")) {
+					if (exists) {
+						if (onto.containsObjectProperty(uri)) {
+							ObjectPropertyExpression prop = onto.getObjectProperty(uri);
+							if (inverse)
+								prop = prop.getInverse();
+							description = prop.getDomain();
+						}
+						else {
+							DataPropertyExpression prop = onto.getDataProperty(uri);
+							description = prop.getDomainRestriction(DatatypeImpl.rdfsLiteral);
+						}
+					}
+					else
+						description = onto.getClass(uri);
+				}
+				else {
+					if (onto.containsObjectProperty(uri)) {
+						ObjectPropertyExpression prop = onto.getObjectProperty(uri);
+						if (inverse)
+							description = prop.getInverse();
+						else
+							description = prop;
+					}
+					else {
+						description = onto.getDataProperty(uri);
+					}
+				}
+
+				Description _node = description;
+				rv.add(_node);
+			}
+		}
+		return rv;
+	}
 
 	private void test_dag_index_nodes(String testname) throws Exception {
-		TBoxReasoner reasoner = helper.load_dag(testname);
-		List<List<Description>> exp_idx = helper.get_results(testname);
+
+		onto = OWLAPITranslatorUtility.loadOntologyFromFile(owlloc + testname + ".owl");
+		TBoxReasoner reasoner = TBoxReasonerImpl.create(onto);
+		List<List<Description>> exp_idx = get_results(testname);
 
 		List<Description> classes= new LinkedList<Description>();
 		for(Equivalences<ClassExpression> node : reasoner.classes().dag()) {
@@ -71,14 +191,11 @@ public class DAGTest extends TestCase {
 		for (Description node : exp_idx.get(1)) {
 			roles.contains(node);
 		}
-		
-		
 	}
 
 	public void test_1_0_0() throws Exception {
 		String testname = "test_1_0_0";
 		test_dag_index_nodes(testname);
-
 	}
 
 	public void test_1_0_1() throws Exception {
