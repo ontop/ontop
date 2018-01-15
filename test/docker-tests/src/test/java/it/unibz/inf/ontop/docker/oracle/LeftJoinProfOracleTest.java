d2078024bdffb1b28cdfc9c7010fd2c87d52768d
Sarah Komla-Ebri <sarahkeb89@gmail.com>
2018-01-15 18:45:26 +0100

    use for oracle test the common mappings for left join

--- test/docker-tests/src/test/java/it/unibz/inf/ontop/docker/oracle/LeftJoinProfOracleTest.java
+++ test/docker-tests/src/test/java/it/unibz/inf/ontop/docker/oracle/LeftJoinProfOracleTest.java
@@ -5,8 +5,8 @@ import it.unibz.inf.ontop.docker.AbstractLeftJoinProfTest;
 public class LeftJoinProfOracleTest extends AbstractLeftJoinProfTest {
 
 
-    private static final String owlFileName = "/oracle/redundant_join/redundant_join_fk_test.owl";
-    private static final String obdaFileName = "/oracle/redundant_join/redundant_join_fk_test.obda";
+    private static final String owlFileName = "/redundant_join/redundant_join_fk_test.owl";
+    private static final String obdaFileName = "/redundant_join/redundant_join_fk_test.obda";
     private static final String propertyFileName = "/oracle/oracle.properties";
 
 
