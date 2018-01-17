d02dbd04781b6c5cf5a5069f28db3c514107a0ad
Roman Kontchakov <roman@dcs.bbk.ac.uk>
2018-01-17 15:57:02 +0000

    top and bot classes and properties proper treatment

--- binding/owlapi/src/test/java/it/unibz/inf/ontop/owlapi/ClassifiedTBoxTest.java
+++ binding/owlapi/src/test/java/it/unibz/inf/ontop/owlapi/ClassifiedTBoxTest.java
@@ -4,6 +4,7 @@ package it.unibz.inf.ontop.owlapi;
 import it.unibz.inf.ontop.spec.ontology.ClassifiedTBox;
 import it.unibz.inf.ontop.spec.ontology.owlapi.OWLAPITranslatorOWL2QL;
 import org.junit.Test;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,8 +18,7 @@ public class ClassifiedTBoxTest {
 
     @Test
     public void test_wrong_property() throws Exception {
-//        ClassifiedTBox ontology = OWLAPITranslatorOWL2QL.loadOntologyFromFileAndClassify("src/test/resources/ontology/venice.owl");
-
+        ClassifiedTBox ontology = OWL2QLTranslatorTest.loadOntologyFromFileAndClassify("src/test/resources/ontology/venice.owl");
     }
 
 }
