7763f1168fe158ed2db5a604691cb8f4e38909f6
juliencorman <cormanjulien@gmail.com>
2018-06-25 15:21:16 +0200

    Updated doc and protege plugin generation

--- client/protege/build-plugin.sh
+++ client/protege/build-plugin.sh
@@ -16,5 +16,4 @@ cd $pwd
 mvn bundle:bundle
 
 # Copy the jar to Protege plugins.
-# You may need to unzip first: cd "$pwd/build/dependencies"; unzip protege-5.0.0-beta-21-platform-independent.zip -d protege;
-cp "$pwd/target/it.unibz.inf.ontop.protege-$version.jar" "$pwd/../../build/dependencies/Protege-5.2.0/plugins"
+cp "$pwd/target/it.unibz.inf.ontop.protege-$version.jar" "$pwd/../../build/dependencies/protege/Protege-5.2.0/plugins"
