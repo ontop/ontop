8d773c14122bb115ba8a0912221fae0e1f354a22
juliencorman <cormanjulien@gmail.com>
2018-06-25 13:24:03 +0200

    Protege plugin building script

--- client/protege/build-plugin.sh
+++ client/protege/build-plugin.sh
@@ -3,6 +3,8 @@
 # Builds the Protege plugin for debug purposes
 # Source: http://github.com/ontop/ontop/wiki/ObdalibPluginDebug
 
+# Retrieve Ontop's version from the pom.xml file (assumption: can be found in the first line matching '<version>(.*)<\version>')
+version=`grep -m 1 'version' < pom.xml | sed -r 's/.*<version>(.*)<\/version>.*/\1/' | tr -d '\n'`
 pwd=`pwd`
 
 # Build ontop bundles
@@ -14,5 +16,5 @@ cd $pwd
 mvn bundle:bundle
 
 # Copy the jar to Protege plugins.
-# You may need to unzip first: cd "$pwd/build/dependencies"; unzip protege-5.0.0-beta-21-platform-independent.zip -d protege
-cp "$pwd/target/it.unibz.inf.ontop.protege-3.0.0-beta-2-SNAPSHOT.jar" "$pwd/../../build/dependencies/Protege-5.2.0/plugins"
+# You may need to unzip first: cd "$pwd/build/dependencies"; unzip protege-5.0.0-beta-21-platform-independent.zip -d protege;
+cp "$pwd/target/it.unibz.inf.ontop.protege-$version.jar" "$pwd/../../build/dependencies/Protege-5.2.0/plugins"
