b02e5216dad604fea10e717c11e5ce20a5970192
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2018-09-05 11:24:55 +0200

    R2RMLExportAction messages updated.

--- client/protege/src/main/java/it/unibz/inf/ontop/protege/gui/action/R2RMLExportAction.java
+++ client/protege/src/main/java/it/unibz/inf/ontop/protege/gui/action/R2RMLExportAction.java
@@ -91,12 +91,12 @@ public class R2RMLExportAction extends ProtegeAction {
 
                     final File file = fc.getSelectedFile();
 
-                    Thread th = new Thread("Bootstrapper Action Thread"){
+                    Thread th = new Thread("R2RML Export Action Thread"){
                         @Override
                         public void run() {
                             try {
                                 OBDAProgressMonitor monitor = new OBDAProgressMonitor(
-                                        "Bootstrapping ontology and mappings...", workspace);
+                                        "Exporting the mapping to R2RML...", workspace);
                                 R2RMLExportThread t = new R2RMLExportThread();
                                 monitor.addProgressListener(t);
                                 monitor.start();
