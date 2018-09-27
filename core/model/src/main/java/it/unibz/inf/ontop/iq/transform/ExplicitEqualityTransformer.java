4f302cb325eb3eea6c20942ed86e7027fe1f3809
juliencorman <cormanjulien@gmail.com>
2018-09-27 20:29:11 +0200

    implemented CnLifter, within the explicitEqualityTransformer

--- core/model/src/main/java/it/unibz/inf/ontop/iq/transform/ExplicitEqualityTransformer.java
+++ core/model/src/main/java/it/unibz/inf/ontop/iq/transform/ExplicitEqualityTransformer.java
@@ -4,13 +4,13 @@ import com.google.common.collect.ImmutableList;
 import it.unibz.inf.ontop.iq.IQTree;
 import it.unibz.inf.ontop.iq.node.QueryNode;
 
-public interface ExplicitEqualityTransformer extends IQTransformer{
-
-    interface CNLifter<T extends QueryNode>{
-        IQTree liftCn (T root, ImmutableList<IQTree> children);
-    }
-
-    interface Updater<T extends IQTree>{
-        IQTree update (T tree);
-    }
+public interface ExplicitEqualityTransformer extends IQTreeTransformer{
+//
+//    interface CNLifter<T extends QueryNode>{
+//        IQTree liftCn (T root, ImmutableList<IQTree> children);
+//    }
+//
+//    interface Updater<T extends IQTree>{
+//        IQTree update (T tree);
+//    }
 }
--- core/model/src/main/java/it/unibz/inf/ontop/iq/transform/impl/ExplicitEqualityTransformerImpl.java
+++ core/model/src/main/java/it/unibz/inf/ontop/iq/transform/impl/ExplicitEqualityTransformerImpl.java
@@ -7,120 +7,175 @@ import it.unibz.inf.ontop.iq.IQTree;
 import it.unibz.inf.ontop.iq.UnaryIQTree;
 import it.unibz.inf.ontop.iq.node.*;
 import it.unibz.inf.ontop.iq.transform.ExplicitEqualityTransformer;
+import it.unibz.inf.ontop.iq.transform.IQTreeTransformer;
 import it.unibz.inf.ontop.utils.ImmutableCollectors;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
-public class ExplicitEqualityTransformerImpl extends DefaultRecursiveIQTransformer implements ExplicitEqualityTransformer {
+public class ExplicitEqualityTransformerImpl implements ExplicitEqualityTransformer {
 
+    private final ImmutableList<IQTreeTransformer> preTransformers;
+    private final ImmutableList<IQTreeTransformer> postTransformers;
+    private final IntermediateQueryFactory iqFactory;
 
     @Inject
     public ExplicitEqualityTransformerImpl(IntermediateQueryFactory iqFactory) {
-        super(iqFactory);
+        this.iqFactory = iqFactory;
+        this.preTransformers = ImmutableList.of(new CnLifter());
+        this.postTransformers = ImmutableList.of(new CnLifter());
     }
 
-    private IQTree transform(QueryNode root,
-                             ImmutableList<IQTree> children,
-                             CNLifter CNLifter,
-                             Updater updater){
+//    private IQTree transform(QueryNode root,
+//                             ImmutableList<IQTree> children,
+//                             CNLifter CNLifter,
+//                             Updater updater){
+//
+//        ImmutableList<IQTree> transformedChildren = children.stream()
+//                .map(t -> t.acceptTransformer(this))
+//                .collect(ImmutableCollectors.toList());
+//
+//        IQTree combination = CNLifter.liftCn(root, transformedChildren);
+//        return updater.update(combination);
 
-        ImmutableList<IQTree> transformedChildren = children.stream()
-                .map(t -> t.acceptTransformer(this))
-                .collect(ImmutableCollectors.toList());
 
-        IQTree combination = CNLifter.liftCn(root, transformedChildren);
-        return updater.update(combination);
-
+    @Override
+    public IQTree transform(IQTree tree) {
+        return new CompositeRecursiveIQTreeTransformer(preTransformers, postTransformers, iqFactory).transform(tree);
     }
 
-    protected IQTree transformUnaryNode(UnaryOperatorNode rootNode, IQTree child) {
-        transform()
-        IQTree transformedChild = child.acceptTransformer(this);
-        return combine(rootNode, transformedChild);
-    }
-
-    protected IQTree transformNaryCommutativeNode(NaryOperatorNode rootNode, ImmutableList<IQTree> children) {
-        return combine(
-                rootNode,
-                children.stream()
-                        .map(t -> t.acceptTransformer(this))
-                        .collect(ImmutableCollectors.toList())
-        );
-    }
-
-    private IQTree combine(UnaryOperatorNode rootNode, IQTree transformedChild) {
-        Optional<ConstructionNode> idleCn = getIdleCn(transformedChild);
-        return idleCn.isPresent() ?
-                liftIdleCn(idleCn.get(), rootNode, trimIdleCn(transformedChild)) :
-                iqFactory.createUnaryIQTree(rootNode, transformedChild);
-    }
-
-    private IQTree combine(NaryOperatorNode rootNode, ImmutableList<IQTree> transformedChildren) {
-
-        ImmutableList<ConstructionNode> idleCns = transformedChildren.stream()
-                .map(t -> getIdleCn(t))
-                .filter(o -> o.isPresent())
-                .map(o -> o.get())
-                .collect(ImmutableCollectors.toList());
-
-        return idleCns.isEmpty() ?
-                iqFactory.createNaryIQTree(rootNode, transformedChildren) :
-                liftIdleCns(
-                        mergeIdleCns(idleCns),
-                        rootNode,
-                        transformedChildren.stream()
-                                .map(t -> trimIdleCn(t))
-                                .collect(ImmutableCollectors.toList())
-                );
-    }
-
-    private IQTree trimIdleCn(IQTree tree) {
-        return getIdleCn(tree).isPresent() ?
-                ((UnaryIQTree) tree).getChild() :
-                tree;
-    }
-
-    private Optional<ConstructionNode> getIdleCn(IQTree tree) {
-        QueryNode root = tree.getRootNode();
-        if (root instanceof ConstructionNode) {
-            ConstructionNode cn = ((ConstructionNode) root);
-            if (cn.getSubstitution().isEmpty()) {
-                return Optional.of(cn);
-            }
-        }
-        return Optional.empty();
-    }
-
-    private ConstructionNode mergeIdleCns(ImmutableList<ConstructionNode> cns) {
-        return iqFactory.createConstructionNode(cns.stream()
-                .flatMap(c -> c.getVariables().stream())
-                .collect(ImmutableCollectors.toSet())
-        );
-    }
-
-    private IQTree liftIdleCn(ConstructionNode constructionNode, UnaryOperatorNode rootNode, IQTree trimmedChild) {
-        return iqFactory.createUnaryIQTree(
-                constructionNode,
-                iqFactory.createUnaryIQTree(
-                        rootNode,
-                        trimmedChild
-                ));
-    }
-
-    private IQTree liftIdleCns(ConstructionNode cn, NaryOperatorNode root, ImmutableList<IQTree> trimmedChildren) {
-        return iqFactory.createUnaryIQTree(
-                cn,
-                iqFactory.createNaryIQTree(
-                        root,
-                        trimmedChildren
-                ));
-    }
-
-    class UnionCNLifter implements CNLifter<UnionNode> {
+    /**
+     * Affects (left) joins and data nodes.
+     * - (left) join: if the same variable is returned by both operands (implicit equality), rename it in the right operand and make the euality explicit
+     * - data node: create a variable and make the equality explicit (create a filter).
+     */
+    class LocalExplicitEqualityEnforcer extends DefaultNonRecursiveIQTreeTransformer {
 
         @Override
-        public IQTree combine(UnionNode root, ImmutableList<IQTree> children) {
+        public IQTree transformIntensionalData(IntensionalDataNode dataNode) {
+            return transformDatanode(dataNode);
+        }
 
+        @Override
+        public IQTree transformExtensionalData(ExtensionalDataNode dataNode) {
+            return transformDatanode(dataNode);
+        }
+
+        @Override
+        public IQTree transformLeftJoin(IQTree tree, LeftJoinNode rootNode, IQTree leftChild, IQTree rightChild) {
+        }
+
+        @Override
+        public IQTree transformInnerJoin(IQTree tree, InnerJoinNode rootNode, ImmutableList<IQTree> children) {
+        }
+
+        private IQTree transformDatanode(DataNode dataNode) {
+
+        }
+    }
+
+    /**
+     * Affects filters and (left) joins.
+     * For each child, deletes its root if it is a filter node.
+     * Then:
+     * - join and filter: merge the boolean expression
+     * - left join: merge boolean expressions coming from the right, and lift the ones coming from the left.
+     * This lift is only performed for optimization purposes: may avoid a subquery during SQL generation.
+     */
+    class FilterChildNormalizer extends DefaultNonRecursiveIQTreeTransformer {
+
+        @Override
+        public IQTree transformLeftJoin(IQTree tree, LeftJoinNode rootNode, IQTree leftChild, IQTree rightChild) {
+        }
+
+        @Override
+        public IQTree transformInnerJoin(IQTree tree, InnerJoinNode rootNode, ImmutableList<IQTree> children) {
+        }
+
+        @Override
+        public IQTree transformFilter(IQTree tree, FilterNode rootNode, IQTree child) {
+        }
+    }
+
+    /**
+     * For each child, deletes its root if it is a substitution-free construction node (i.e. a simple projection).
+     * Then lift the projection if needed (i.e. create a substitution-free construction node above the current one)
+     */
+    class CnLifter extends DefaultNonRecursiveIQTreeTransformer {
+
+        @Override
+        protected IQTree transformUnaryNode(IQTree tree, UnaryOperatorNode rootNode, IQTree child) {
+            ImmutableList<ConstructionNode> idleCns = getIdleCns(Stream.of(child));
+            return idleCns.isEmpty() ?
+                    tree :
+                    iqFactory.createUnaryIQTree(
+                            idleCns.iterator().next(),
+                            iqFactory.createUnaryIQTree(
+                                    rootNode,
+                                    trimIdleCn(child)
+                            ));
+        }
+
+        @Override
+        protected IQTree transformNaryCommutativeNode(IQTree tree, NaryOperatorNode rootNode, ImmutableList<IQTree> children) {
+            ImmutableList<ConstructionNode> idleCns = getIdleCns(children.stream());
+            return idleCns.isEmpty() ?
+                    tree :
+                    iqFactory.createUnaryIQTree(
+                            mergeProjections(idleCns),
+                            iqFactory.createNaryIQTree(
+                                    rootNode,
+                                    children.stream()
+                                            .map(t -> trimIdleCn(t))
+                                            .collect(ImmutableCollectors.toList())
+                            ));
+        }
+
+        @Override
+        protected IQTree transformBinaryNonCommutativeNode(IQTree tree, BinaryNonCommutativeOperatorNode rootNode, IQTree leftChild, IQTree rightChild) {
+            ImmutableList<IQTree> children = ImmutableList.of(leftChild, rightChild);
+            ImmutableList<ConstructionNode> idleCns = getIdleCns(Stream.of(leftChild, rightChild));
+            return idleCns.isEmpty() ?
+                    tree :
+                    iqFactory.createUnaryIQTree(
+                            mergeProjections(idleCns),
+                            iqFactory.createBinaryNonCommutativeIQTree(
+                                    rootNode,
+                                    trimIdleCn(leftChild),
+                                    trimIdleCn(rightChild)
+                            ));
+        }
+
+        private ImmutableList<ConstructionNode> getIdleCns(Stream<IQTree> trees) {
+            return trees
+                    .map(t -> getIdleCn(t))
+                    .filter(o -> o.isPresent())
+                    .map(o -> o.get())
+                    .collect(ImmutableCollectors.toList());
+        }
+
+        private ConstructionNode mergeProjections(ImmutableList<ConstructionNode> idleCns) {
+            return iqFactory.createConstructionNode(idleCns.stream()
+                    .flatMap(c -> c.getVariables().stream())
+                    .collect(ImmutableCollectors.toSet())
+            );
+        }
+
+        private Optional<ConstructionNode> getIdleCn(IQTree tree) {
+            QueryNode root = tree.getRootNode();
+            if (root instanceof ConstructionNode) {
+                ConstructionNode cn = ((ConstructionNode) root);
+                if (cn.getSubstitution().isEmpty()) {
+                    return Optional.of(cn);
+                }
+            }
+            return Optional.empty();
+        }
+
+        private IQTree trimIdleCn(IQTree tree) {
+            return getIdleCn(tree).isPresent() ?
+                    ((UnaryIQTree) tree).getChild() :
+                    tree;
         }
     }
 }
