dd37c4d5d10650ca709ebf5bd8a75fbeb44e67ac
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2018-11-24 16:50:01 +0100

    Bugfix: wrong assignment of constants to variables in the SQLOntopBindingSet.
    The underlying value order was not reliable (not guaranteed to follow the signature).

--- engine/system/sql/core/src/main/java/it/unibz/inf/ontop/answering/resultset/impl/SQLOntopBindingSet.java
+++ engine/system/sql/core/src/main/java/it/unibz/inf/ontop/answering/resultset/impl/SQLOntopBindingSet.java
@@ -7,27 +7,33 @@ import it.unibz.inf.ontop.exception.OntopInternalBugException;
 import it.unibz.inf.ontop.model.term.DBConstant;
 import it.unibz.inf.ontop.model.term.ImmutableTerm;
 import it.unibz.inf.ontop.model.term.RDFConstant;
+import it.unibz.inf.ontop.model.term.Variable;
 import it.unibz.inf.ontop.substitution.ImmutableSubstitution;
 import it.unibz.inf.ontop.utils.ImmutableCollectors;
 
 public class SQLOntopBindingSet extends AbstractOntopBindingSet implements OntopBindingSet {
 
 
-    private final ImmutableSubstitution<DBConstant> SQLVar2DBConstant;
-    private final ImmutableSubstitution SPARQLVar2Term;
+    private final ImmutableList<Variable> signature;
+    private final ImmutableSubstitution<DBConstant> sqlVar2DBConstant;
+    private final ImmutableSubstitution<ImmutableTerm> sparqlVar2Term;
 
 
-    SQLOntopBindingSet(ImmutableMap<String, Integer> bindingName2Index,
-                       ImmutableSubstitution<DBConstant> SQLVar2DBConstant, ImmutableSubstitution SPARQLVar2Term) {
+    SQLOntopBindingSet(ImmutableList<Variable> signature,
+                       ImmutableMap<String, Integer> bindingName2Index,
+                       ImmutableSubstitution<DBConstant> sqlVar2DBConstant,
+                       ImmutableSubstitution<ImmutableTerm> sparqlVar2Term) {
         super(bindingName2Index);
-        this.SQLVar2DBConstant = SQLVar2DBConstant;
-        this.SPARQLVar2Term = SPARQLVar2Term;
+        this.signature = signature;
+        this.sqlVar2DBConstant = sqlVar2DBConstant;
+        this.sparqlVar2Term = sparqlVar2Term;
     }
 
     @Override
     public ImmutableList<RDFConstant> computeValues() {
-        ImmutableSubstitution<ImmutableTerm> composition = SQLVar2DBConstant.composeWith(SPARQLVar2Term);
-        return composition.getImmutableMap().values().stream()
+        ImmutableSubstitution<ImmutableTerm> composition = sqlVar2DBConstant.composeWith(sparqlVar2Term);
+        return signature.stream()
+                .map(composition::apply)
                 .map(this::evaluate)
                 .collect(ImmutableCollectors.toList());
     }
