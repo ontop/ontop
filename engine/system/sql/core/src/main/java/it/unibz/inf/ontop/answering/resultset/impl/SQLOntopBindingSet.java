c2373db07781c4b01e85264f9fd5da19276f58aa
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2018-11-27 15:26:16 +0100

    Bugfix: JDBC nulls were not converted into NullConstants.

--- engine/system/sql/core/src/main/java/it/unibz/inf/ontop/answering/resultset/impl/SQLOntopBindingSet.java
+++ engine/system/sql/core/src/main/java/it/unibz/inf/ontop/answering/resultset/impl/SQLOntopBindingSet.java
@@ -1,13 +1,13 @@
 package it.unibz.inf.ontop.answering.resultset.impl;
 
 import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Maps;
 import it.unibz.inf.ontop.answering.resultset.OntopBinding;
 import it.unibz.inf.ontop.answering.resultset.OntopBindingSet;
 import it.unibz.inf.ontop.exception.OntopInternalBugException;
 import it.unibz.inf.ontop.model.term.*;
 import it.unibz.inf.ontop.substitution.ImmutableSubstitution;
 
-import java.util.AbstractMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -16,24 +16,24 @@ import java.util.stream.Collectors;
 public class SQLOntopBindingSet extends AbstractOntopBindingSet implements OntopBindingSet {
 
     SQLOntopBindingSet(ImmutableSortedSet<Variable> signature,
-                       ImmutableSubstitution<DBConstant> sqlVar2DBConstant,
+                       ImmutableSubstitution<Constant> sqlVar2Constant,
                        ImmutableSubstitution<ImmutableTerm> sparqlVar2Term) {
-        super(computeBindingMap(signature, sqlVar2DBConstant, sparqlVar2Term));
+        super(computeBindingMap(signature, sqlVar2Constant, sparqlVar2Term));
     }
 
     private static LinkedHashMap<String, OntopBinding> computeBindingMap(ImmutableSortedSet<Variable> signature,
-                                                                    ImmutableSubstitution<DBConstant> sqlVar2DBConstant,
+                                                                    ImmutableSubstitution<Constant> sqlVar2Constant,
                                                                     ImmutableSubstitution<ImmutableTerm> sparqlVar2Term) {
 
-        ImmutableSubstitution<ImmutableTerm> composition = sqlVar2DBConstant.composeWith(sparqlVar2Term);
+        ImmutableSubstitution<ImmutableTerm> composition = sqlVar2Constant.composeWith(sparqlVar2Term);
 
         return signature.stream()
                 .map(v -> getBinding(v,composition))
-                .filter(o -> o.isPresent())
-                .map(o -> o.get())
+                .filter(Optional::isPresent)
+                .map(Optional::get)
                 .collect(Collectors.toMap(
-                        e -> e.getKey(),
-                        e -> e.getValue(),
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
                         (e1, e2) -> e1,
                         LinkedHashMap::new
                 ));
@@ -41,9 +41,8 @@ public class SQLOntopBindingSet extends AbstractOntopBindingSet implements Ontop
 
     private static Optional<Map.Entry<String,OntopBinding>> getBinding(Variable v, ImmutableSubstitution<ImmutableTerm> composition) {
         Optional<RDFConstant> constant = evaluate(composition.apply(v));
-        return constant.isPresent()?
-                Optional.of(new AbstractMap.SimpleImmutableEntry<String, OntopBinding>(v.getName(), new OntopBindingImpl(v, constant.get()))):
-                Optional.empty();
+        return constant
+                .map(rdfConstant -> Maps.immutableEntry(v.getName(), new OntopBindingImpl(v, rdfConstant)));
     }
 
     private static Optional<RDFConstant> evaluate(ImmutableTerm term) {
