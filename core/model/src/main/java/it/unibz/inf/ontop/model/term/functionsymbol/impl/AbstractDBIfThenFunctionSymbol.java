0b25e3a4eaf65552f7d5a1ce2fccfaca3f037b5a
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2018-12-12 19:04:57 +0100

    AbstractDBIfThenFunctionSymbol.inferType() implemented.

--- core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/AbstractDBIfThenFunctionSymbol.java
+++ core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/AbstractDBIfThenFunctionSymbol.java
@@ -47,12 +47,37 @@ public abstract class AbstractDBIfThenFunctionSymbol extends FunctionSymbolImpl 
     }
 
     /**
-     * TODO: implement it seriously
+     * Is supposed to be strongly typed: does not compare the types of the possible values because
+     * they are supposed to be the sames.
      */
     @Override
     public Optional<TermTypeInference> inferType(ImmutableList<? extends ImmutableTerm> terms) {
-        return Optional.empty();
+
+        ImmutableList<TermTypeInference> typeInferences = extractPossibleValues(terms)
+                .map(t -> (t instanceof Variable)
+                        ? Optional.of(TermTypeInference.declareRedirectionToVariable((Variable) t))
+                        : t.inferType())
+                .flatMap(o -> o
+                        .map(Stream::of)
+                        .orElseGet(Stream::empty))
+                .collect(ImmutableCollectors.toList());
+
+        // Gives a preference to type inferences that determine the type (over the ones that redirect it to a variable)
+        return typeInferences.stream()
+                .filter(t -> t.getTermType().isPresent())
+                .map(Optional::of)
+                .findAny()
+                .orElseGet(() -> typeInferences.stream()
+                        .findAny());
     }
+
+    protected Stream<ImmutableTerm> extractPossibleValues(ImmutableList<? extends ImmutableTerm> terms) {
+        return IntStream.range(1, terms.size())
+                .filter(i -> i % 2 == 1)
+                .boxed()
+                .map(terms::get);
+    }
+
 
     @Override
     public Optional<TermTypeInference> inferAndValidateType(ImmutableList<? extends ImmutableTerm> terms)
