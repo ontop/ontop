ad445a15d8c6dd25a8270c205417fe9eb26592a5
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2018-12-14 10:18:38 +0100

    Minor update of StandardNotationDBBooleanFunctionSymbolImpl.

--- core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/StandardNotationDBBooleanFunctionSymbolImpl.java
+++ core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/StandardNotationDBBooleanFunctionSymbolImpl.java
@@ -2,14 +2,16 @@ package it.unibz.inf.ontop.model.term.functionsymbol.impl;
 
 import com.google.common.collect.ImmutableList;
 import it.unibz.inf.ontop.model.type.DBTermType;
+import it.unibz.inf.ontop.model.type.TermType;
 
 public abstract class StandardNotationDBBooleanFunctionSymbolImpl extends DBBooleanFunctionSymbolImpl {
 
     private final String nameInDialect;
     private static final String FUNCTIONAL_TEMPLATE = "%s(%s)";
 
-    protected StandardNotationDBBooleanFunctionSymbolImpl(String nameInDialect, int arity, DBTermType dbBooleanTermType) {
-        super(nameInDialect, arity, dbBooleanTermType);
+    protected StandardNotationDBBooleanFunctionSymbolImpl(String nameInDialect, ImmutableList<TermType> expectedBaseTypes,
+                                                          DBTermType dbBooleanTermType) {
+        super(nameInDialect + expectedBaseTypes.size(), expectedBaseTypes, dbBooleanTermType);
         this.nameInDialect = nameInDialect;
     }
 
