2252a45b133c8f932b14a5a0657712f16ce02382
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2018-12-17 16:09:56 +0100

    Minor simplification of IRIStringTemplateFunctionSymbolImpl.

--- core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/IRIStringTemplateFunctionSymbolImpl.java
+++ core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/IRIStringTemplateFunctionSymbolImpl.java
@@ -1,20 +1,14 @@
 package it.unibz.inf.ontop.model.term.functionsymbol.impl;
 
 import it.unibz.inf.ontop.model.term.functionsymbol.IRIStringTemplateFunctionSymbol;
-import it.unibz.inf.ontop.model.type.TermType;
 import it.unibz.inf.ontop.model.type.TypeFactory;
 import it.unibz.inf.ontop.utils.URITemplates;
 
 
 public class IRIStringTemplateFunctionSymbolImpl extends ObjectStringTemplateFunctionSymbolImpl implements IRIStringTemplateFunctionSymbol {
-    private final TermType returnType;
-    private final String iriTemplate;
 
     private IRIStringTemplateFunctionSymbolImpl(String iriTemplate, int arity, TypeFactory typeFactory) {
         super(iriTemplate, arity, typeFactory);
-        // TODO: require DB string instead
-        this.returnType = typeFactory.getXsdStringDatatype();
-        this.iriTemplate = iriTemplate;
     }
 
     protected static IRIStringTemplateFunctionSymbol createFunctionSymbol(String iriTemplate,
@@ -22,10 +16,5 @@ public class IRIStringTemplateFunctionSymbolImpl extends ObjectStringTemplateFun
         int arity = URITemplates.getArity(iriTemplate);
 
         return new IRIStringTemplateFunctionSymbolImpl(iriTemplate, arity, typeFactory);
-    }
-
-    @Override
-    public String getTemplate() {
-        return iriTemplate;
     }
 }
