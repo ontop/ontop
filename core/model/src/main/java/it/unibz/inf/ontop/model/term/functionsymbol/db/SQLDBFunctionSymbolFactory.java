abd5965e5698047480c475693c3891940d418bfb
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2018-12-20 17:46:13 +0100

    SQL-specific function symbols moved to db/rdb. MockupDBFunctionSymbolFactory added.

--- core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/db/SQLDBFunctionSymbolFactory.java
+++
@@ -1,7 +0,0 @@
-package it.unibz.inf.ontop.model.term.functionsymbol.db;
-
-
-import it.unibz.inf.ontop.model.term.functionsymbol.db.DBFunctionSymbolFactory;
-
-public interface SQLDBFunctionSymbolFactory extends DBFunctionSymbolFactory {
-}
