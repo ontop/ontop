d5df95b8cf188068223b9cd05eeaad80fc60ff8c
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2019-01-02 11:04:20 +0100

    ExpressionOperation.SUBSTR* removed.

--- db/rdb/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/db/impl/AbstractSQLDBFunctionSymbolFactory.java
+++ db/rdb/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/db/impl/AbstractSQLDBFunctionSymbolFactory.java
@@ -16,6 +16,7 @@ public abstract class AbstractSQLDBFunctionSymbolFactory extends AbstractDBFunct
     protected static final String CONCAT_STR = "CONCAT";
     protected static final String REPLACE_STR = "REPLACE";
     protected static final String AND_STR = "AND";
+    protected static final String SUBSTR_STR = "SUBSTR";
     protected static final String SUBSTRING_STR = "SUBSTRING";
     protected static final String CHAR_LENGTH_STR = "CHAR_LENGTH";
     protected static final String RIGHT_STR = "RIGHT";
@@ -85,10 +86,12 @@ public abstract class AbstractSQLDBFunctionSymbolFactory extends AbstractDBFunct
         DBFunctionSymbol subString2FunctionSymbol = new DefaultSQLSimpleTypedDBFunctionSymbol(SUBSTRING_STR, 2, dbStringType,
                 false, abstractRootDBType);
         builder.put(SUBSTRING_STR, 2, subString2FunctionSymbol);
+        builder.put(SUBSTR_STR, 2, subString2FunctionSymbol);
 
         DBFunctionSymbol subString3FunctionSymbol = new DefaultSQLSimpleTypedDBFunctionSymbol(SUBSTRING_STR, 3, dbStringType,
                 false, abstractRootDBType);
         builder.put(SUBSTRING_STR, 3, subString3FunctionSymbol);
+        builder.put(SUBSTR_STR, 3, subString3FunctionSymbol);
 
         DBFunctionSymbol rightFunctionSymbol = new DefaultSQLSimpleTypedDBFunctionSymbol(RIGHT_STR, 2, dbStringType,
                 false, abstractRootDBType);
