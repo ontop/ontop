97389210f3d4788a8be6cd56666f880a53c24508
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2019-02-17 12:08:43 +0100

    SameTermSPARQLFunctionSymbolImpl directly simplifies itself into a standard strict equality.

--- core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/SameTermSPARQLFunctionSymbolImpl.java
+++ core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/SameTermSPARQLFunctionSymbolImpl.java
@@ -1,27 +1,66 @@
 package it.unibz.inf.ontop.model.term.functionsymbol.impl;
 
 import com.google.common.collect.ImmutableList;
+import it.unibz.inf.ontop.iq.node.VariableNullability;
+import it.unibz.inf.ontop.model.term.ImmutableExpression;
+import it.unibz.inf.ontop.model.term.ImmutableFunctionalTerm;
 import it.unibz.inf.ontop.model.term.ImmutableTerm;
 import it.unibz.inf.ontop.model.term.TermFactory;
 import it.unibz.inf.ontop.model.type.RDFDatatype;
 import it.unibz.inf.ontop.model.type.RDFTermType;
+import it.unibz.inf.ontop.model.type.TermTypeInference;
 import it.unibz.inf.ontop.model.vocabulary.SPARQL;
 
 import javax.annotation.Nonnull;
+import java.util.Optional;
 
-public class SameTermSPARQLFunctionSymbolImpl extends AbstractBinaryComparisonSPARQLFunctionSymbol {
+/**
+ * SPARQL's strict equality function symbol
+ */
+public class SameTermSPARQLFunctionSymbolImpl extends SPARQLFunctionSymbolImpl {
+
+    private final RDFDatatype xsdBooleanType;
 
     protected SameTermSPARQLFunctionSymbolImpl(@Nonnull RDFTermType rdfRootType, RDFDatatype xsdBooleanType) {
 
-        super("SP_SAME_TERM", SPARQL.SAME_TERM, rdfRootType, xsdBooleanType);
+        super("SP_SAME_TERM", SPARQL.SAME_TERM, ImmutableList.of(rdfRootType, rdfRootType));
+        this.xsdBooleanType = xsdBooleanType;
+    }
+
+    /**
+     * Always simplifies itself
+     */
+    @Override
+    protected ImmutableTerm buildTermAfterEvaluation(ImmutableList<ImmutableTerm> newTerms, TermFactory termFactory,
+                                                     VariableNullability variableNullability) {
+        ImmutableExpression condition = termFactory.getStrictEquality(newTerms);
+
+        ImmutableFunctionalTerm lexicalTerm = termFactory.getConversion2RDFLexical(condition, xsdBooleanType);
+        ImmutableFunctionalTerm typeTerm = termFactory.getIfElseNull(
+                termFactory.getDBIsNotNull(condition),
+                termFactory.getRDFTermTypeConstant(xsdBooleanType));
+
+        return termFactory.getRDFFunctionalTerm(lexicalTerm, typeTerm)
+                .simplify(variableNullability);
     }
 
     @Override
-    protected ImmutableTerm computeLexicalTerm(ImmutableList<ImmutableTerm> subLexicalTerms,
-                                               ImmutableList<ImmutableTerm> typeTerms, TermFactory termFactory) {
-        return termFactory.getConjunction(
-                termFactory.getStrictEquality(subLexicalTerms),
-                termFactory.getStrictEquality(typeTerms))
-                .simplify();
+    protected boolean tolerateNulls() {
+        return false;
+    }
+
+    @Override
+    protected boolean isAlwaysInjective() {
+        return false;
+    }
+
+    @Override
+    public Optional<TermTypeInference> inferType(ImmutableList<? extends ImmutableTerm> terms) {
+        return Optional.of(TermTypeInference.declareTermType(xsdBooleanType));
+    }
+
+    @Override
+    public boolean canBePostProcessed(ImmutableList<? extends ImmutableTerm> arguments) {
+        return false;
     }
 }
