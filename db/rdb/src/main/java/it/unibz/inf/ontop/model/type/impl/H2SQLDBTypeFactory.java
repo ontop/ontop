4a1112601b940ce5579df475403ca5b169215cad
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2019-02-20 11:39:19 +0100

    Use BOOLEAN as default H2 boolean type.

--- db/rdb/src/main/java/it/unibz/inf/ontop/model/type/impl/H2SQLDBTypeFactory.java
+++ db/rdb/src/main/java/it/unibz/inf/ontop/model/type/impl/H2SQLDBTypeFactory.java
@@ -11,8 +11,6 @@ import java.util.Map;
 
 public class H2SQLDBTypeFactory extends DefaultSQLDBTypeFactory {
 
-    private static final String BOOL_STR = "BOOL";
-
     @AssistedInject
     private H2SQLDBTypeFactory(@Assisted TermType rootTermType, @Assisted TypeFactory typeFactory) {
         super(createH2SQLTypeMap(rootTermType, typeFactory), createH2SQLCodeMap());
@@ -20,14 +18,11 @@ public class H2SQLDBTypeFactory extends DefaultSQLDBTypeFactory {
 
     private static Map<String, DBTermType> createH2SQLTypeMap(TermType rootTermType, TypeFactory typeFactory) {
         Map<String, DBTermType> map = createDefaultSQLTypeMap(rootTermType, typeFactory);
-        // TODO: add the other alias BIT?
-        map.put(BOOL_STR, new BooleanDBTermType(BOOL_STR, rootTermType.getAncestry(), typeFactory.getXsdBooleanDatatype()));
         return map;
     }
 
     private static ImmutableMap<DefaultTypeCode, String> createH2SQLCodeMap() {
         Map<DefaultTypeCode, String> map = createDefaultSQLCodeMap();
-        map.put(DefaultTypeCode.BOOLEAN, BOOL_STR);
         return ImmutableMap.copyOf(map);
     }
 }
