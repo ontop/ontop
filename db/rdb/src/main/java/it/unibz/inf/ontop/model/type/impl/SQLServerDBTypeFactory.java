aef8400cbc18cb9d1155be9e6f7d8fe7ecfc5d03
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2019-03-15 18:35:10 +0100

    SQLServerDBTypeFactory added.

---
+++ db/rdb/src/main/java/it/unibz/inf/ontop/model/type/impl/SQLServerDBTypeFactory.java
@@ -0,0 +1,55 @@
+package it.unibz.inf.ontop.model.type.impl;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.assistedinject.Assisted;
+import com.google.inject.assistedinject.AssistedInject;
+import it.unibz.inf.ontop.model.type.DBTermType;
+import it.unibz.inf.ontop.model.type.TermType;
+import it.unibz.inf.ontop.model.type.TypeFactory;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class SQLServerDBTypeFactory extends DefaultSQLDBTypeFactory {
+
+    protected static final String BIT_STR = "BIT";
+    protected static final String DATETIME_STR = "DATETIME";
+    protected static final String DATETIME2_STR = "DATETIME2";
+
+    @AssistedInject
+    private SQLServerDBTypeFactory(@Assisted TermType rootTermType, @Assisted TypeFactory typeFactory) {
+        super(createSQLServerTypeMap(rootTermType, typeFactory), createSQLServerCodeMap());
+    }
+
+    private static Map<String, DBTermType> createSQLServerTypeMap(TermType rootTermType, TypeFactory typeFactory) {
+        // Non-standard (not part of the R2RML standard).
+        BooleanDBTermType bitType = new BooleanDBTermType(BIT_STR, rootTermType.getAncestry(),
+                typeFactory.getXsdBooleanDatatype());
+        // Name for TIMESTAMP
+        BooleanDBTermType datetimeType = new BooleanDBTermType(DATETIME_STR, rootTermType.getAncestry(),
+                typeFactory.getXsdDatetimeDatatype());
+        BooleanDBTermType datetime2Type = new BooleanDBTermType(DATETIME2_STR, rootTermType.getAncestry(),
+                typeFactory.getXsdDatetimeDatatype());
+
+        Map<String, DBTermType> map = createDefaultSQLTypeMap(rootTermType, typeFactory);
+        map.put(BIT_STR, bitType);
+        map.put(DATETIME_STR, datetimeType);
+        map.put(DATETIME2_STR, datetime2Type);
+        return map;
+    }
+
+    private static ImmutableMap<DefaultTypeCode, String> createSQLServerCodeMap() {
+        Map<DefaultTypeCode, String> map = createDefaultSQLCodeMap();
+        map.put(DefaultTypeCode.BOOLEAN, BIT_STR);
+        map.put(DefaultTypeCode.STRING, NVARCHAR_STR);
+        map.put(DefaultTypeCode.DATETIMESTAMP, DATETIME_STR);
+        // By default float is float(53) which is a float with double precision
+        map.put(DefaultTypeCode.DOUBLE, FLOAT_STR);
+        return ImmutableMap.copyOf(map);
+    }
+
+    @Override
+    public Optional<String> getDBNaNLexicalValue() {
+        return Optional.of("NaN");
+    }
+}
