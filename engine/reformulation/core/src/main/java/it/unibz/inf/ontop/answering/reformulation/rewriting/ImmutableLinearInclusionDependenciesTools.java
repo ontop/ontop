7c19e3d0f6bcc5b1e7af566aaf6b93f3ce401cb8
kontchakov <roman@dcs.bbk.ac.uk>
2019-06-19 10:35:01 +0100

    ABox dependencies code refactor

--- engine/reformulation/core/src/main/java/it/unibz/inf/ontop/answering/reformulation/rewriting/ImmutableLinearInclusionDependenciesTools.java
+++ engine/reformulation/core/src/main/java/it/unibz/inf/ontop/answering/reformulation/rewriting/ImmutableLinearInclusionDependenciesTools.java
@@ -7,12 +7,13 @@ import it.unibz.inf.ontop.model.atom.AtomPredicate;
 import it.unibz.inf.ontop.model.atom.DataAtom;
 import it.unibz.inf.ontop.model.term.TermFactory;
 import it.unibz.inf.ontop.model.term.Variable;
+import it.unibz.inf.ontop.model.term.functionsymbol.Predicate;
 import it.unibz.inf.ontop.spec.ontology.*;
 import it.unibz.inf.ontop.substitution.SubstitutionFactory;
 import it.unibz.inf.ontop.substitution.impl.ImmutableUnificationTools;
 import it.unibz.inf.ontop.utils.CoreUtilsFactory;
 
-import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 public class ImmutableLinearInclusionDependenciesTools {
     private final AtomFactory atomFactory;
@@ -38,38 +39,42 @@ public class ImmutableLinearInclusionDependenciesTools {
 
         final LinearInclusionDependencies.Builder<AtomPredicate> builder = LinearInclusionDependencies.builder(immutableUnificationTools, coreUtilsFactory, substitutionFactory);
 
-        traverseDAG(reasoner.objectPropertiesDAG(), (subprop, prop) -> {
-            if (!subprop.isInverse()) {
-                DataAtom<AtomPredicate> body = translate(subprop, variableXname, variableYname);
-                DataAtom<AtomPredicate> head = translate(prop, variableXname, variableYname);
-                builder.add(head, body);
-            }
-        });
-        traverseDAG(reasoner.dataPropertiesDAG(), (subprop, prop) -> {
-            DataAtom<AtomPredicate> body = translate(subprop, variableXname, variableYname);
-            DataAtom<AtomPredicate> head = translate(prop, variableXname, variableYname);
-            builder.add(head, body);
-        });
-        traverseDAG(reasoner.classesDAG(), (subclass, cla) -> {
-            if (!(cla instanceof OClass) && !(!full && ((cla instanceof ObjectSomeValuesFrom) || (cla instanceof DataSomeValuesFrom))))
-                return;
+        traverseDAG(reasoner.objectPropertiesDAG(),
+                prop -> !prop.isInverse(),
+                subprop -> translate(subprop, variableXname, variableYname),
+                prop -> translate(prop, variableXname, variableYname),
+                builder);
 
-            DataAtom<AtomPredicate> body = translate(subclass, variableYname);
-            // use a different variable name in case the body has an existential as well
-            DataAtom<AtomPredicate> head = translate(cla, variableZname);
-            builder.add(head, body);
-        });
+        traverseDAG(reasoner.dataPropertiesDAG(),
+                e -> true,
+                subprop -> translate(subprop, variableXname, variableYname),
+                prop -> translate(prop, variableXname, variableYname),
+                builder);
+
+        traverseDAG(reasoner.classesDAG(),
+                cla -> !(!(cla instanceof OClass) && !(!full && ((cla instanceof ObjectSomeValuesFrom) || (cla instanceof DataSomeValuesFrom)))),
+                subclass -> translate(subclass, variableYname),
+                // use a different variable name in case the body has an existential as well
+                cla -> translate(cla, variableZname),
+                builder);
 
         return builder.build();
     }
 
-    private static <T> void traverseDAG(EquivalencesDAG<T> dag, BiConsumer<T, T> consumer) {
+    private static <T> void traverseDAG(EquivalencesDAG<T> dag,
+                                        java.util.function.Predicate<T> filter,
+                                        Function<T, DataAtom<AtomPredicate>> translateBody,
+                                        Function<T, DataAtom<AtomPredicate>> translateHead,
+                                        LinearInclusionDependencies.Builder<AtomPredicate> builder) {
         for (Equivalences<T> node : dag)
             for (Equivalences<T> subNode : dag.getSub(node))
                 for (T sub : subNode)
                     for (T e : node)
-                        if (e != sub)
-                            consumer.accept(sub, e);
+                        if (e != sub && filter.test(e)) {
+                            DataAtom<AtomPredicate> body = translateBody.apply(sub);
+                            DataAtom<AtomPredicate> head = translateHead.apply(e);
+                            builder.add(head, body);
+                        }
     }
 
     private DataAtom<AtomPredicate> translate(ObjectPropertyExpression property, String x, String y) {
