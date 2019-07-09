27c2b5f3046ff3b3e1a39f7bd27d4182713ae3a4
kontchakov <roman@dcs.bbk.ac.uk>
2019-07-09 10:36:14 +0100

    LegacyMappingDatatypeFiller

--- mapping/core/src/main/java/it/unibz/inf/ontop/spec/mapping/transformer/impl/LegacyMappingDatatypeFiller.java
+++ mapping/core/src/main/java/it/unibz/inf/ontop/spec/mapping/transformer/impl/LegacyMappingDatatypeFiller.java
@@ -1,25 +1,18 @@
 package it.unibz.inf.ontop.spec.mapping.transformer.impl;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.inject.Inject;
 import it.unibz.inf.ontop.datalog.*;
 import it.unibz.inf.ontop.datalog.impl.DatalogRule2QueryConverter;
 import it.unibz.inf.ontop.dbschema.Attribute;
-import it.unibz.inf.ontop.dbschema.DBMetadata;
 import it.unibz.inf.ontop.dbschema.Relation2Predicate;
 import it.unibz.inf.ontop.exception.UnknownDatatypeException;
 import it.unibz.inf.ontop.injection.IntermediateQueryFactory;
 import it.unibz.inf.ontop.injection.OntopMappingSettings;
 import it.unibz.inf.ontop.injection.ProvenanceMappingFactory;
 import it.unibz.inf.ontop.iq.IQ;
-import it.unibz.inf.ontop.iq.IQTree;
-import it.unibz.inf.ontop.iq.UnaryIQTree;
-import it.unibz.inf.ontop.iq.node.*;
 import it.unibz.inf.ontop.iq.transform.NoNullValueEnforcer;
-import it.unibz.inf.ontop.iq.transform.impl.ChildTransformer;
-import it.unibz.inf.ontop.iq.transform.impl.DefaultNonRecursiveIQTreeTransformer;
 import it.unibz.inf.ontop.model.term.*;
 import it.unibz.inf.ontop.model.term.impl.ImmutabilityTools;
 import it.unibz.inf.ontop.model.type.TypeFactory;
@@ -31,9 +24,6 @@ import it.unibz.inf.ontop.spec.mapping.utils.MappingTools;
 import it.unibz.inf.ontop.utils.ImmutableCollectors;
 
 import java.util.AbstractMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 import static it.unibz.inf.ontop.model.term.functionsymbol.ExpressionOperation.AND;
@@ -141,122 +131,6 @@ public class LegacyMappingDatatypeFiller implements MappingDatatypeFiller {
 
     }
 
-
-    // PINCHED FROM ExplicitEqualityTransformerImpl
-    // TODO: extract as an independent class
-
-    /**
-     * Affects each outermost filter or (left) join n in the tree.
-     * For each child of n, deletes its root if it is a filter node.
-     * Then:
-     * - if n is a join or filter: merge the boolean expressions
-     * - if n is a left join: merge boolean expressions coming from the right, and lift the ones coming from the left.
-     * This lift is only performed for optimization purposes: may save a subquery during SQL generation.
-     */
-    class FilterChildNormalizer extends DefaultNonRecursiveIQTreeTransformer {
-
-        private final ChildTransformer childTransformer;
-
-        public FilterChildNormalizer() {
-            this.childTransformer = new ChildTransformer(iqFactory, this);
-        }
-
-        @Override
-        public IQTree transformLeftJoin(IQTree tree, LeftJoinNode rootNode, IQTree leftChild, IQTree rightChild) {
-
-            Optional<ImmutableExpression> leftChildChildExpression = getOptionalChildExpression(leftChild);
-            Optional<ImmutableExpression> rightChildExpression = getOptionalChildExpression(rightChild);
-
-            if (!leftChildChildExpression.isPresent() && !rightChildExpression.isPresent())
-                return tree;
-
-            IQTree leftJoinTree = iqFactory.createBinaryNonCommutativeIQTree(
-                    rightChildExpression.isPresent()
-                            ? iqFactory.createLeftJoinNode(getConjunction(
-                            rootNode.getOptionalFilterCondition(),
-                            ImmutableList.of(rightChildExpression.get())))
-                            : rootNode,
-                    trimRootFilter(leftChild),
-                    trimRootFilter(rightChild));
-
-            return leftChildChildExpression.isPresent()
-                    ? iqFactory.createUnaryIQTree(iqFactory.createFilterNode(leftChildChildExpression.get()), leftJoinTree)
-                    : leftJoinTree;
-        }
-
-        @Override
-        public IQTree transformInnerJoin(IQTree tree, InnerJoinNode rootNode, ImmutableList<IQTree> children) {
-            ImmutableList<ImmutableExpression> filterChildExpressions = getChildExpressions(children);
-            if (filterChildExpressions.isEmpty())
-                return tree;
-
-            return iqFactory.createNaryIQTree(
-                    iqFactory.createInnerJoinNode(getConjunction(
-                            rootNode.getOptionalFilterCondition(),
-                            filterChildExpressions)),
-                    children.stream()
-                            .map(this::trimRootFilter)
-                            .collect(ImmutableCollectors.toList()));
-        }
-
-        @Override
-        public IQTree transformFilter(IQTree tree, FilterNode rootNode, IQTree child) {
-            ImmutableList<ImmutableExpression> filterChildExpressions = getChildExpressions(ImmutableList.of(child));
-            if (filterChildExpressions.isEmpty())
-                return tree;
-
-            return iqFactory.createUnaryIQTree(
-                    iqFactory.createFilterNode(getConjunction(
-                            Optional.of(rootNode.getFilterCondition()),
-                            filterChildExpressions).get()),
-                    trimRootFilter(child));
-        }
-
-        private ImmutableList<ImmutableExpression> getChildExpressions(ImmutableList<IQTree> children) {
-            return children.stream()
-                    .filter(t -> t.getRootNode() instanceof FilterNode)
-                    .map(t -> ((FilterNode) t.getRootNode()).getFilterCondition())
-                    .collect(ImmutableCollectors.toList());
-        }
-
-        private Optional<ImmutableExpression> getOptionalChildExpression(IQTree child) {
-            QueryNode root = child.getRootNode();
-            return root instanceof FilterNode
-                    ? Optional.of(((FilterNode) root).getFilterCondition())
-                    : Optional.empty();
-        }
-
-        private IQTree trimRootFilter(IQTree tree) {
-            return tree.getRootNode() instanceof FilterNode
-                    ? ((UnaryIQTree) tree).getChild()
-                    : tree;
-        }
-
-        protected IQTree transformUnaryNode(IQTree tree, UnaryOperatorNode rootNode, IQTree child) {
-            return childTransformer.transform(tree);
-        }
-
-        protected IQTree transformNaryCommutativeNode(IQTree tree, NaryOperatorNode rootNode, ImmutableList<IQTree> children) {
-            return childTransformer.transform(tree);
-        }
-
-        protected IQTree transformBinaryNonCommutativeNode(IQTree tree, BinaryNonCommutativeOperatorNode rootNode, IQTree leftChild, IQTree rightChild) {
-            return childTransformer.transform(tree);
-        }
-
-    }
-
-    private Optional<ImmutableExpression> getConjunction(Optional<ImmutableExpression> optExpression, List<ImmutableExpression> expressions) {
-        if (expressions.isEmpty())
-            throw new IllegalArgumentException("Nonempty list of filters expected");
-
-        ImmutableExpression result = (optExpression.isPresent()
-                ? Stream.concat(Stream.of(optExpression.get()), expressions.stream())
-                : expressions.stream())
-                .reduce(null,
-                        (a, b) -> (a == null) ? b : termFactory.getImmutableExpression(AND, a, b));
-        return Optional.of(result);
-    }
 
 
 }
