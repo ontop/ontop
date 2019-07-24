b819eeb043a223d4a4235b5aed71354be583b0db
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2019-07-24 20:08:33 +0200

    OnlyInPresenceOfDistinctProjectOrderByTermsNormalizer added (missing in previous commit).

--- engine/reformulation/sql/src/main/java/it/unibz/inf/ontop/answering/reformulation/generation/normalization/impl/OnlyInPresenceOfDistinctProjectOrderByTermsNormalizer.java
+++ engine/reformulation/sql/src/main/java/it/unibz/inf/ontop/answering/reformulation/generation/normalization/impl/OnlyInPresenceOfDistinctProjectOrderByTermsNormalizer.java
@@ -4,7 +4,7 @@ import com.google.inject.Inject;
 import it.unibz.inf.ontop.injection.CoreSingletons;
 
 public class OnlyInPresenceOfDistinctProjectOrderByTermsNormalizer extends ProjectOrderByTermsNormalizer {
-    
+
     @Inject
     protected OnlyInPresenceOfDistinctProjectOrderByTermsNormalizer(CoreSingletons coreSingletons) {
         super(true, coreSingletons);
