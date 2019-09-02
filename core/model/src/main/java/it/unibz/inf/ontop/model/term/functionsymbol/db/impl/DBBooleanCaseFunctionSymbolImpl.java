de32db7340b2dd3b68bde58cbf1d4015164906a2
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2019-09-02 17:58:43 +0200

    Boolean case can something be reduced to a disjunction.

--- core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/db/impl/DBBooleanCaseFunctionSymbolImpl.java
+++ core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/db/impl/DBBooleanCaseFunctionSymbolImpl.java
@@ -9,12 +9,15 @@ import it.unibz.inf.ontop.model.type.DBTermType;
 import it.unibz.inf.ontop.utils.ImmutableCollectors;
 
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class DBBooleanCaseFunctionSymbolImpl extends DefaultDBCaseFunctionSymbol implements DBBooleanFunctionSymbol {
     protected DBBooleanCaseFunctionSymbolImpl(int arity, DBTermType dbBooleanType, DBTermType rootDBTermType,
                                               boolean doOrderingMatter) {
-        super("BOOL_CASE" + arity, arity, dbBooleanType, rootDBTermType, doOrderingMatter);
+        super("BOOL_CASE" + arity + (doOrderingMatter ? "" : "_UNORDERED"), arity, dbBooleanType, rootDBTermType,
+                doOrderingMatter);
     }
 
     @Override
@@ -58,6 +61,14 @@ public class DBBooleanCaseFunctionSymbolImpl extends DefaultDBCaseFunctionSymbol
                         : termFactory.getIsTrue((NonFunctionalTerm) t))
                 .collect(ImmutableCollectors.toList());
 
+        /*
+         * Tries to simplify the CASE into a disjunction
+         */
+        Optional<ImmutableExpression> optionalSimplification = tryToReduceToDisjunction(twoVLExpressions, termFactory);
+        if (optionalSimplification.isPresent())
+            return optionalSimplification.get()
+                    .simplify2VL(variableNullability);
+
         ImmutableTerm newTerm = simplify(twoVLExpressions, termFactory, variableNullability);
         // Makes sure that the returned expression has been inform that "2VL simplifications" can be applied
         // Prevents an infinite loop
@@ -71,4 +82,34 @@ public class DBBooleanCaseFunctionSymbolImpl extends DefaultDBCaseFunctionSymbol
             return termFactory.getDBBooleanConstant(false);
         return newTerm;
     }
+
+    /**
+     * Transforms, for instance, CASE_5(c1, IS_TRUE(TRUE), c2, IS_TRUE(TRUE), IS_TRUE(FALSE))
+     *   into OR_2(c1,c2)
+     *
+     *   TODO: shall we generalize to the inverse case where the default value is TRUE?
+     */
+    private Optional<ImmutableExpression> tryToReduceToDisjunction(ImmutableList<ImmutableExpression> twoVLExpressions,
+                                                           TermFactory termFactory) {
+        if (!doOrderingMatter) {
+            ImmutableTerm defaultValue = extractDefaultValue(twoVLExpressions, termFactory);
+            ImmutableExpression falseExpression = termFactory.getIsTrue(termFactory.getDBBooleanConstant(false));
+            ImmutableExpression trueExpression = termFactory.getIsTrue(termFactory.getDBBooleanConstant(true));
+
+            if (defaultValue.equals(falseExpression)
+                    && IntStream.range(0, twoVLExpressions.size() - 1)
+                    .filter(i -> i % 2 == 1)
+                    .boxed()
+                    .map(twoVLExpressions::get)
+                    .allMatch(e -> e.equals(trueExpression)))
+
+                return termFactory.getDisjunction(
+                        IntStream.range(0, twoVLExpressions.size())
+                                .filter(i -> i % 2 == 0)
+                                .boxed()
+                                .map(twoVLExpressions::get));
+        }
+        return Optional.empty();
+    }
+
 }
