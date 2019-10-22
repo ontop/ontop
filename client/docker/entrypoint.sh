ae7739fb9ad5cb5dfe5f99e30d56d03d0fbe4dba
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2019-10-22 18:08:02 +0200

    Minor text improvements related to the endpoint.

--- client/docker/entrypoint.sh
+++ client/docker/entrypoint.sh
@@ -46,5 +46,8 @@ else
   LOGBACK_CONFIG_FILE=${ONTOP_HOME}/log/logback.xml
 fi
 
+echo java ${ONTOP_JAVA_ARGS} -cp "${ONTOP_HOME}/lib/*:${ONTOP_HOME}/jdbc/*" -Dlogback.configurationFile="${LOGBACK_CONFIG_FILE}" \
+ it.unibz.inf.ontop.cli.Ontop endpoint "${args_array[@]}"
+
 java ${ONTOP_JAVA_ARGS} -cp "${ONTOP_HOME}/lib/*:${ONTOP_HOME}/jdbc/*" -Dlogback.configurationFile="${LOGBACK_CONFIG_FILE}" \
  it.unibz.inf.ontop.cli.Ontop endpoint "${args_array[@]}"
