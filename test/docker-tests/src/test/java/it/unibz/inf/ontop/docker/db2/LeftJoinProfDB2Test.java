1e46b78a0ab27ed248a63d430f9c8948c4eb3c3c
martinkarbon <martin.karbon16@gmail.com>
2019-11-07 16:28:06 +0100

    LeftJoinProfDB2Test tested and modified

--- test/docker-tests/src/test/java/it/unibz/inf/ontop/docker/db2/LeftJoinProfDB2Test.java
+++ test/docker-tests/src/test/java/it/unibz/inf/ontop/docker/db2/LeftJoinProfDB2Test.java
@@ -1,5 +1,6 @@
 package it.unibz.inf.ontop.docker.db2;
 
+import com.google.common.collect.ImmutableList;
 import it.unibz.inf.ontop.docker.AbstractLeftJoinProfTest;
 
 
@@ -12,5 +13,22 @@ public class LeftJoinProfDB2Test extends AbstractLeftJoinProfTest {
     public LeftJoinProfDB2Test() {
         super(owlFileName, obdaFileName, propertyFileName);
     }
+
+    @Override
+    protected ImmutableList<String> getExpectedValuesDuration1() {
+        return ImmutableList.of("0", "0", "0", "0", "0", "18.000", "20.000", "54.500");
+    }
+
+    @Override
+    protected ImmutableList<String> getExpectedValuesMultitypedAvg1() {
+        return ImmutableList.of("15.500000000000000000000000", "16.000000000000000000000000", "18.875000000000000000000000");
+    }
+
+    @Override
+    protected ImmutableList<String> getExpectedValuesMultitypedSum1(){
+        return ImmutableList.of("31.000", "32.000", "75.500");
+    }
+
+
 
 }
