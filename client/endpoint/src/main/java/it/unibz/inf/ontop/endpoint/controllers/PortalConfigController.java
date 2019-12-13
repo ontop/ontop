e954bc8ea19ddfa5a2600b3d39a8f5e3eead7e98
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2019-12-13 11:43:25 +0100

    The portal is now loaded at launch time.

--- client/endpoint/src/main/java/it/unibz/inf/ontop/endpoint/controllers/PortalConfigController.java
+++ client/endpoint/src/main/java/it/unibz/inf/ontop/endpoint/controllers/PortalConfigController.java
@@ -1,9 +1,7 @@
 package it.unibz.inf.ontop.endpoint.controllers;
 
-import com.google.gson.Gson;
-import com.moandjiezana.toml.Toml;
+import it.unibz.inf.ontop.endpoint.beans.PortalConfigComponent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,38 +9,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Optional;
-
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 @RestController
 public class PortalConfigController {
 
-    private final Optional<String> portalConfigFile;
-    // LAZY
-    private String json;
+    private final String jsonString;
 
     @Autowired
-    public PortalConfigController(@Value("${portal:#{null}}") Optional<String> portalConfigFile) {
-        this.portalConfigFile = portalConfigFile;
-        this.json = null;
+    public PortalConfigController(PortalConfigComponent.PortalConfigSerialization portalConfigSerialization) {
+        this.jsonString = portalConfigSerialization.jsonString;
     }
 
     @RequestMapping(value = "/ontop/portalConfig")
     @ResponseBody
-    public ResponseEntity<String> reformulate() throws FileNotFoundException {
-        if ((json == null) && portalConfigFile.isPresent()) {
-            Toml toml = new Toml().read(new FileReader(portalConfigFile.get()));
-            Gson gson = new Gson();
-            json = gson.toJson(toml.toMap());
-        } else if (json == null) {
-            json = "{}";
-        }
+    public ResponseEntity<String> reformulate() {
         HttpHeaders headers = new HttpHeaders();
         headers.set(CONTENT_TYPE, "application/json; charset=UTF-8");
-        return new ResponseEntity<>(json, headers, HttpStatus.OK);
+        return new ResponseEntity<>(jsonString, headers, HttpStatus.OK);
     }
 
 }
