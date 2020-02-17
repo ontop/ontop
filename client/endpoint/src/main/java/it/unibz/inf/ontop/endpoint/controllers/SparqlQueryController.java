7d52662848682732389fbc316580287ef175f4be
Guillem Rull <guillem.rull@gmail.com>
2020-02-17 10:48:04 +0100

    Implemented streaming on Sparql endpoint.

--- client/endpoint/src/main/java/it/unibz/inf/ontop/endpoint/controllers/SparqlQueryController.java
+++ client/endpoint/src/main/java/it/unibz/inf/ontop/endpoint/controllers/SparqlQueryController.java
@@ -35,7 +35,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -65,73 +68,63 @@ public class SparqlQueryController {
     @RequestMapping(value = "/sparql",
             method = {RequestMethod.GET}
     )
-    @ResponseBody
-    public HttpEntity<String> query_get(
+    public void query_get(
             @RequestHeader(ACCEPT) String accept,
             @RequestParam(value = "query") String query,
             @RequestParam(value = "default-graph-uri", required = false) String[] defaultGraphUri,
-            @RequestParam(value = "named-graph-uri", required = false) String[] namedGraphUri) {
-        return execQuery(accept, query, defaultGraphUri, namedGraphUri);
+            @RequestParam(value = "named-graph-uri", required = false) String[] namedGraphUri,
+            HttpServletResponse response) {
+        execQuery(accept, query, defaultGraphUri, namedGraphUri, response);
     }
 
     @RequestMapping(value = "/sparql",
             method = RequestMethod.POST,
             consumes = APPLICATION_FORM_URLENCODED_VALUE)
-    @ResponseBody
-    public HttpEntity<String> query_post_URL_encoded(
+    public void query_post_URL_encoded(
             @RequestHeader(ACCEPT) String accept,
             @RequestParam(value = "query") String query,
             @RequestParam(value = "default-graph-uri", required = false) String[] defaultGraphUri,
-            @RequestParam(value = "named-graph-uri", required = false) String[] namedGraphUri) {
-        return execQuery(accept, query, defaultGraphUri, namedGraphUri);
+            @RequestParam(value = "named-graph-uri", required = false) String[] namedGraphUri,
+            HttpServletResponse response) {
+        execQuery(accept, query, defaultGraphUri, namedGraphUri, response);
     }
 
     @RequestMapping(value = "/sparql",
             method = RequestMethod.POST,
             consumes = "application/sparql-query")
-    @ResponseBody
-    public HttpEntity<String> query_post_directly(
+    public void query_post_directly(
             @RequestHeader(ACCEPT) String accept,
             @RequestBody String query,
             @RequestParam(value = "default-graph-uri", required = false) String[] defaultGraphUri,
-            @RequestParam(value = "named-graph-uri", required = false) String[] namedGraphUri) {
-        return execQuery(accept, query, defaultGraphUri, namedGraphUri);
+            @RequestParam(value = "named-graph-uri", required = false) String[] namedGraphUri,
+            HttpServletResponse response) {
+        execQuery(accept, query, defaultGraphUri, namedGraphUri, response);
     }
 
-    private ResponseEntity<String> execQuery(String accept,
-                                             String query, String[] defaultGraphUri, String[] namedGraphUri) {
-
-        HttpHeaders headers = new HttpHeaders();
-        HttpStatus status = HttpStatus.OK;
-
+    private void execQuery(String accept, String query, String[] defaultGraphUri, String[] namedGraphUri,
+                           HttpServletResponse response) {
         try (RepositoryConnection connection = repository.getConnection()) {
             Query q = connection.prepareQuery(QueryLanguage.SPARQL, query);
-            ByteArrayOutputStream bao = new ByteArrayOutputStream();
-            final String result;
+            OutputStream bao = response.getOutputStream();
 
             if (q instanceof TupleQuery) {
                 TupleQuery selectQuery = (TupleQuery) q;
+                response.setCharacterEncoding("UTF-8");
 
                 if ("*/*".equals(accept) || accept.contains("json")) {
-                    headers.set(HttpHeaders.CONTENT_TYPE, "application/sparql-results+json");
+                    response.setHeader(HttpHeaders.CONTENT_TYPE, "application/sparql-results+json;charset=UTF-8");
                     selectQuery.evaluate(new SPARQLResultsJSONWriter(bao));
-                    result = bao.toString();
                 } else if (accept.contains("xml")) {
-                    headers.set(HttpHeaders.CONTENT_TYPE, "application/sparql-results+xml");
+                    response.setHeader(HttpHeaders.CONTENT_TYPE, "application/sparql-results+xml;charset=UTF-8");
                     selectQuery.evaluate(new SPARQLResultsXMLWriter(bao));
-                    result = bao.toString();
                 } else if (accept.contains("csv")) {
-                    headers.set(HttpHeaders.CONTENT_TYPE, "text/sparql-results+csv");
+                    response.setHeader(HttpHeaders.CONTENT_TYPE, "text/sparql-results+csv;charset=UTF-8");
                     selectQuery.evaluate(new SPARQLResultsCSVWriter(bao));
-                    result = bao.toString();
                 } else if (accept.contains("tsv")) {
-                    headers.set(HttpHeaders.CONTENT_TYPE, "text/sparql-results+tsv");
+                    response.setHeader(HttpHeaders.CONTENT_TYPE, "text/sparql-results+tsv;charset=UTF-8");
                     selectQuery.evaluate(new SPARQLResultsTSVWriter(bao));
-                    result = bao.toString();
                 } else {
-                    result = "";
-                    status = HttpStatus.BAD_REQUEST;
-                    //throw new IllegalArgumentException("unsupported ACCEPT : " + accept);
+                    response.setStatus(HttpStatus.BAD_REQUEST.value());
                 }
 
             } else if (q instanceof BooleanQuery) {
@@ -139,56 +132,47 @@ public class SparqlQueryController {
                 boolean b = askQuery.evaluate();
 
                 if ("*/*".equals(accept) || accept.contains("json")) {
-                    headers.set(HttpHeaders.CONTENT_TYPE, "application/sparql-results+json");
+                    response.setHeader(HttpHeaders.CONTENT_TYPE, "application/sparql-results+json");
                     BooleanQueryResultWriter writer = new SPARQLBooleanJSONWriter(bao);
                     writer.handleBoolean(b);
-                    result = bao.toString();
                 } else if (accept.contains("xml")) {
-                    headers.set(HttpHeaders.CONTENT_TYPE, "application/sparql-results+xml");
+                    response.setHeader(HttpHeaders.CONTENT_TYPE, "application/sparql-results+xml");
                     BooleanQueryResultWriter writer = new SPARQLBooleanXMLWriter(bao);
                     writer.handleBoolean(b);
-                    result = bao.toString();
                 } else if (accept.contains("text")) {
-                    headers.set(HttpHeaders.CONTENT_TYPE, "text/boolean");
+                    response.setHeader(HttpHeaders.CONTENT_TYPE, "text/boolean");
                     BooleanQueryResultWriter writer = new BooleanTextWriter(bao);
                     writer.handleBoolean(b);
-                    result = bao.toString();
                 } else {
-                    result = "";
-                    status = HttpStatus.BAD_REQUEST;
-                    //throw new IllegalArgumentException("unsupported ACCEPT : " + accept);
+                    response.setStatus(HttpStatus.BAD_REQUEST.value());
                 }
             } else if (q instanceof GraphQuery) {
                 GraphQuery graphQuery = (GraphQuery) q;
+                response.setCharacterEncoding("UTF-8");
+
                 if ("*/*".equals(accept) || accept.contains("turtle")) {
-                    headers.set(HttpHeaders.CONTENT_TYPE, "text/turtle");
+                    response.setHeader(HttpHeaders.CONTENT_TYPE, "text/turtle;charset=UTF-8");
                     graphQuery.evaluate(new TurtleWriter(bao));
-                    result = bao.toString();
                 } else if (accept.contains("json")) {
-                    headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
+                    response.setHeader(HttpHeaders.CONTENT_TYPE, "application/json;charset=UTF-8");
                     graphQuery.evaluate(new RDFJSONWriter(bao, RDFFormat.JSONLD));
-                    result = bao.toString();
                 } else if (accept.contains("xml")) {
-                    headers.set(HttpHeaders.CONTENT_TYPE, "application/rdf+xml");
+                    response.setHeader(HttpHeaders.CONTENT_TYPE, "application/rdf+xml;charset=UTF-8");
                     graphQuery.evaluate(new RDFXMLWriter(bao));
-                    result = bao.toString();
                 } else {
-                    //throw new IllegalArgumentException("unsupported ACCEPT : " + accept);
-                    result = "";
-                    status = HttpStatus.BAD_REQUEST;
+                    response.setStatus(HttpStatus.BAD_REQUEST.value());
                 }
             } else if (q instanceof Update) {
-                //else if (q instanceof Update)
-                result = "";
-                status = HttpStatus.NOT_IMPLEMENTED;
+                response.setStatus(HttpStatus.NOT_IMPLEMENTED.value());
             } else {
-                result = "";
-                status = HttpStatus.BAD_REQUEST;
+                response.setStatus(HttpStatus.BAD_REQUEST.value());
             }
-            return new ResponseEntity<>(result, headers, status);
+            bao.flush();
+        }
+        catch (IOException ex) {
+            throw new Error(ex);
         }
     }
-
 
     @ExceptionHandler({MalformedQueryException.class})
     public ResponseEntity<String> handleMalformedQueryException(Exception ex) {
