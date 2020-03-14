9109f353285904ebce2cbe507aaf8ec463f20df5
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2020-03-14 11:01:44 +0100

    OntopR2RMLPrettify: comments updated.

--- client/cli/src/main/java/it/unibz/inf/ontop/cli/OntopR2RMLPrettify.java
+++ client/cli/src/main/java/it/unibz/inf/ontop/cli/OntopR2RMLPrettify.java
@@ -1,25 +1,5 @@
 package it.unibz.inf.ontop.cli;
 
-/*
- * #%L
- * ontop-cli
- * %%
- * Copyright (C) 2009 - 2014 Free University of Bozen-Bolzano
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 
 import com.github.rvesse.airline.annotations.Command;
 import com.github.rvesse.airline.annotations.Option;
@@ -40,13 +20,13 @@ import java.io.OutputStream;
 public class OntopR2RMLPrettify implements OntopCommand {
 
     @Option(type = OptionType.COMMAND, name = {"-i", "--input"}, title = "input.ttl",
-            description = "Input mapping file in Ontop native format (.obda)")
+            description = "Input mapping file in the turtle R2RML format (.ttl)")
     @Required
     protected String inputMappingFile;
 
 
     @Option(type = OptionType.COMMAND, name = {"-o", "--output"}, title = "pretty.ttl",
-            description = "Input mapping file in Ontop native format (.obda)")
+            description = "Output mapping file in the turtle R2RML format (.ttl)")
     @Required
     protected String outputMappingFile;
 
