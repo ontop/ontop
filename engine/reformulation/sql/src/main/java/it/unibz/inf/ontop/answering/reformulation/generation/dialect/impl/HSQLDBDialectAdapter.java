19879fa5c66f5f072598ffe09460970093a26a40
kontchakov <roman@dcs.bbk.ac.uk>
2020-04-21 15:31:37 +0100

    dead code removed

--- engine/reformulation/sql/src/main/java/it/unibz/inf/ontop/answering/reformulation/generation/dialect/impl/HSQLDBDialectAdapter.java
+++
@@ -1,7 +0,0 @@
-package it.unibz.inf.ontop.answering.reformulation.generation.dialect.impl;
-
-
-public class HSQLDBDialectAdapter extends SQL99DialectAdapter {
-
-
-}
--- engine/reformulation/sql/src/main/java/it/unibz/inf/ontop/answering/reformulation/generation/dialect/impl/MySQLDialectAdapter.java
+++
@@ -1,7 +0,0 @@
-package it.unibz.inf.ontop.answering.reformulation.generation.dialect.impl;
-
-
-public class MySQLDialectAdapter extends SQL99DialectAdapter {
-
-
-}
