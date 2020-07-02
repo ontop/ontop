411d0ac8fc2fcfac327f79319794a456db7139ff
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2020-07-02 22:18:37 +0200

    Cast for floating constants.

--- binding/owlapi/src/test/java/it/unibz/inf/ontop/owlapi/BindWithFunctionsTest.java
+++ binding/owlapi/src/test/java/it/unibz/inf/ontop/owlapi/BindWithFunctionsTest.java
@@ -1020,6 +1020,33 @@ public class BindWithFunctionsTest {
         checkReturnedValues(queryBind, expectedValues);
     }
 
+    @Test
+    public void testConstantIntegerDivide() throws Exception {
+        String queryBind = "SELECT (\"1\"^^xsd:integer / \"2\"^^xsd:integer AS ?w)  {} ";
+
+        List<String> expectedValues = new ArrayList<>();
+        expectedValues.add("\"0.5\"^^xsd:decimal");
+        checkReturnedValues(queryBind, expectedValues);
+    }
+
+    @Test
+    public void testCoalesceDivideByZeroInt() throws Exception {
+        String queryBind = "SELECT (COALESCE(\"1\"^^xsd:integer / \"0\"^^xsd:integer, \"other\") AS ?w)  {} ";
+
+        List<String> expectedValues = new ArrayList<>();
+        expectedValues.add("\"other\"^^xsd:string");
+        checkReturnedValues(queryBind, expectedValues);
+    }
+
+    @Test
+    public void testCoalesceDivideByZeroDecimal() throws Exception {
+        String queryBind = "SELECT (COALESCE(\"1\"^^xsd:decimal / \"0\"^^xsd:decimal, \"other\") AS ?w)  {} ";
+
+        List<String> expectedValues = new ArrayList<>();
+        expectedValues.add("\"other\"^^xsd:string");
+        checkReturnedValues(queryBind, expectedValues);
+    }
+
     private void checkReturnedValues(String query, List<String> expectedValues) throws Exception {
 
         OntopOWLFactory factory = OntopOWLFactory.defaultFactory();
