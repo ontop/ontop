5b18979569eaf34089021dcec06bc7a87a8a06cb
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2020-07-04 12:16:52 +0200

    Issue325Test added.

---
+++ binding/rdf4j/src/test/java/it/unibz/inf/ontop/rdf4j/repository/Issue325Test.java
@@ -0,0 +1,30 @@
+package it.unibz.inf.ontop.rdf4j.repository;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class Issue325Test extends AbstractRDF4JTest {
+
+    private static final String R2RML_FILE = "/issue325/mapping.r2rml.ttl";
+    private static final String SQL_SCRIPT = "/issue325/example.sql";
+
+    @BeforeClass
+    public static void before() throws IOException, SQLException {
+        initR2RML(SQL_SCRIPT, R2RML_FILE);
+    }
+
+    @AfterClass
+    public static void after() throws SQLException {
+        release();
+    }
+    @Test
+    public void testQuery() {
+        runQueryAndCount("SELECT * WHERE {\n" +
+                "\t?s <http://ex.com#ex2> ?s2\n" +
+                "}");
+    }
+}
