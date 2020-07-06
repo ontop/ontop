97e99d8d3a17fab7135d8e8f1b7b0adf7e8c0416
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2020-07-06 23:03:34 +0200

    R2RML to OBDA conversion now supports named graphs.

--- mapping/sql/core/src/main/java/it/unibz/inf/ontop/spec/mapping/serializer/TargetQueryRenderer.java
+++ mapping/sql/core/src/main/java/it/unibz/inf/ontop/spec/mapping/serializer/TargetQueryRenderer.java
@@ -1,27 +1,10 @@
 package it.unibz.inf.ontop.spec.mapping.serializer;
 
-/*
- * #%L
- * ontop-obdalib-core
- * %%
- * Copyright (C) 2009 - 2014 Free University of Bozen-Bolzano
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 import com.google.common.collect.ImmutableList;
 import it.unibz.inf.ontop.exception.OntopInternalBugException;
+import it.unibz.inf.ontop.model.atom.AtomPredicate;
+import it.unibz.inf.ontop.model.atom.QuadPredicate;
+import it.unibz.inf.ontop.model.atom.TriplePredicate;
 import it.unibz.inf.ontop.spec.mapping.TargetAtom;
 import it.unibz.inf.ontop.model.term.*;
 import it.unibz.inf.ontop.model.term.functionsymbol.FunctionSymbol;
@@ -52,17 +35,23 @@ public class TargetQueryRenderer {
 
         TurtleWriter turtleWriter = new TurtleWriter();
         for (TargetAtom atom : body) {
-            String originalString = atom.getProjectionAtom().getPredicate().toString();
-            if (originalString.equals("triple")) {
-                ImmutableTerm subjectTerm = atom.getSubstitutedTerm(0);
-                String subject = displayTerm(subjectTerm, prefixManager);
-                ImmutableTerm predicateTerm = atom.getSubstitutedTerm(1);
-                String predicate = displayTerm(predicateTerm, prefixManager);
-                ImmutableTerm objectTerm = atom.getSubstitutedTerm(2);
-                String object = displayTerm(objectTerm, prefixManager);
+            AtomPredicate atomPredicate = atom.getProjectionAtom().getPredicate();
+            ImmutableTerm subjectTerm = atom.getSubstitutedTerm(0);
+            String subject = displayTerm(subjectTerm, prefixManager);
+            ImmutableTerm predicateTerm = atom.getSubstitutedTerm(1);
+            String predicate = displayTerm(predicateTerm, prefixManager);
+            ImmutableTerm objectTerm = atom.getSubstitutedTerm(2);
+            String object = displayTerm(objectTerm, prefixManager);
+            if (atomPredicate instanceof TriplePredicate) {
                 turtleWriter.put(subject, predicate, object);
-            } else {
-                throw new UnsupportedOperationException("unsupported predicate! " + originalString);
+            }
+            else if (atomPredicate instanceof QuadPredicate) {
+                ImmutableTerm graphTerm = atom.getSubstitutedTerm(3);
+                String graph = displayTerm(graphTerm, prefixManager);
+                turtleWriter.put(subject, predicate, object, graph);
+            }
+            else {
+                throw new UnsupportedOperationException("unsupported predicate! " + atomPredicate);
             }
         }
         return turtleWriter.print();
--- mapping/sql/core/src/main/java/it/unibz/inf/ontop/spec/mapping/serializer/TurtleWriter.java
+++ mapping/sql/core/src/main/java/it/unibz/inf/ontop/spec/mapping/serializer/TurtleWriter.java
@@ -1,27 +1,11 @@
 package it.unibz.inf.ontop.spec.mapping.serializer;
 
-/*
- * #%L
- * ontop-obdalib-core
- * %%
- * Copyright (C) 2009 - 2014 Free University of Bozen-Bolzano
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Optional;
 
 /**
  * A utility class to store the Turtle main components, i.e., subject, predicate
@@ -56,6 +40,7 @@ import java.util.HashMap;
  */
 class TurtleWriter {
 
+	private Multimap<Optional<String>, String> graphToSubjects = ArrayListMultimap.create();
 	private HashMap<String, ArrayList<String>> subjectToPredicates = new HashMap<String, ArrayList<String>>();
 	private HashMap<String, ArrayList<String>> predicateToObjects = new HashMap<String, ArrayList<String>>();
 
@@ -70,21 +55,38 @@ class TurtleWriter {
 	 *            The object term of the Function.
 	 */
 	void put(String subject, String predicate, String object) {
+		Optional<String> graph = Optional.empty();
+		if (!graphToSubjects.containsEntry(graph, subject))
+			graphToSubjects.put(graph, subject);
+		insertSPO(subject, predicate, object);
+	}
+
+	void insertSPO(String subjectKey, String predicate, String object) {
 		// Subject to Predicates map
-		ArrayList<String> predicateList = subjectToPredicates.get(subject);
+		ArrayList<String> predicateList = subjectToPredicates.get(subjectKey);
 		if (predicateList == null) {
 			predicateList = new ArrayList<String>();
 		}
 		insert(predicateList, predicate);
-		subjectToPredicates.put(subject, predicateList);
+		subjectToPredicates.put(subjectKey, predicateList);
+
+		String predicateKey = computePredicateKey(subjectKey, predicate);
 
 		// Predicate to Objects map
-		ArrayList<String> objectList = predicateToObjects.get(predicate + "_" + subject); // predicate that appears in 2 different subjects should not have all objects assigned  to both subjects
+		ArrayList<String> objectList = predicateToObjects.get(predicateKey); // predicate that appears in 2 different subjects should not have all objects assigned  to both subjects
 		if (objectList == null) {
 			objectList = new ArrayList<String>();
 		}
-		objectList.add(object);
-		predicateToObjects.put(predicate + "_" + subject, objectList);
+		if (!objectList.contains(object))
+			objectList.add(object);
+		predicateToObjects.put(predicateKey, objectList);
+	}
+
+	public void put(String subject, String predicate, String object, String graph) {
+		Optional<String> optionalGraph = Optional.of(graph);
+		if (!graphToSubjects.containsEntry(optionalGraph, subject))
+			graphToSubjects.put(optionalGraph, subject);
+		insertSPO(computeSubjectKey(graph, subject), predicate, object);
 	}
 
 	// Utility method to insert the predicate
@@ -105,31 +107,49 @@ class TurtleWriter {
 	 */
 	String print() {
 		StringBuilder sb = new StringBuilder();
-		for (String subject : subjectToPredicates.keySet()) {
-			sb.append(subject);
-			sb.append(" ");
-			boolean semiColonSeparator = false;
-			for (String predicate : subjectToPredicates.get(subject)) {
-				if (semiColonSeparator) {
-					sb.append(" ; ");
-				}
-				sb.append(predicate);
-				sb.append(" ");
-				semiColonSeparator = true;
 
-				boolean commaSeparator = false;
-				for (String object : predicateToObjects.get(predicate+ "_" + subject)) {
-					if (commaSeparator) {
-						sb.append(" , ");
+		for (Optional<String> graph : graphToSubjects.keySet()) {
+			graph.ifPresent(g -> sb.append(String.format("GRAPH %s { ", g)));
+
+			for (String subject : graphToSubjects.get(graph)) {
+				String subjectKey = graph
+						.map(g -> computeSubjectKey(g, subject))
+						.orElse(subject);
+				sb.append(subject);
+				sb.append(" ");
+				boolean semiColonSeparator = false;
+				for (String predicate : subjectToPredicates.get(subjectKey)) {
+					if (semiColonSeparator) {
+						sb.append(" ; ");
 					}
-					sb.append(object);
-					commaSeparator = true;
+					sb.append(predicate);
+					sb.append(" ");
+					semiColonSeparator = true;
+
+					boolean commaSeparator = false;
+					for (String object : predicateToObjects.get(computePredicateKey(subjectKey, predicate))) {
+						if (commaSeparator) {
+							sb.append(" , ");
+						}
+						sb.append(object);
+						commaSeparator = true;
+					}
 				}
+				sb.append(" ");
+				sb.append(".");
+				sb.append(" ");
 			}
-			sb.append(" ");
-			sb.append(".");
-			sb.append(" ");
+			graph.ifPresent(g -> sb.append(" } "));
 		}
 		return sb.toString();
 	}
+
+	private String computeSubjectKey(String graph, String subject) {
+		return subject + "_" + graph;
+	}
+
+	private String computePredicateKey(String subjectKey, String predicate) {
+		return predicate+ "_" + subjectKey;
+	}
+
 }
