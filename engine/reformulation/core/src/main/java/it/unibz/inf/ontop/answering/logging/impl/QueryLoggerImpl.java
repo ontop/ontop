3fa3c0e74a5a040a4b8b83f760155106b0b5b2b0
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2020-07-10 18:22:10 +0200

    Jackson is now used by the query logger.

--- engine/reformulation/core/src/main/java/it/unibz/inf/ontop/answering/logging/impl/QueryLoggerImpl.java
+++ engine/reformulation/core/src/main/java/it/unibz/inf/ontop/answering/logging/impl/QueryLoggerImpl.java
@@ -1,5 +1,7 @@
 package it.unibz.inf.ontop.answering.logging.impl;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.Inject;
 import it.unibz.inf.ontop.answering.logging.QueryLogger;
@@ -9,29 +11,49 @@ import it.unibz.inf.ontop.injection.OntopReformulationSettings;
 import it.unibz.inf.ontop.iq.IQ;
 import it.unibz.inf.ontop.spec.ontology.InconsistentOntologyException;
 import org.apache.commons.rdf.api.IRI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 public class QueryLoggerImpl implements QueryLogger {
 
+    public static final String OUTPUT_STREAM_JSON_ERROR = "Problem with the output stream for the query logger. Disabled";
+    public static final String EXECUTION_BEFORE_UNBLOCKING_DURATION_KEY = "executionBeforeUnblockingDuration";
     protected static final String REFORMATION_EXC_MSG = "query:exception-reformulation";
     protected static final String EVALUATION_EXC_MSG = "query:exception-evaluation";
     protected static final String CONNECTION_EXC_MSG = "query:exception-connection";
     protected static final String CONVERSION_EXC_MSG = "query:exception-conversion";
     protected static final String SPARQL_QUERY_KEY = "sparqlQuery";
     protected static final String REFORMULATED_QUERY_KEY = "reformulatedQuery";
+    protected static final String TIMESTAMP_KEY = "@timestamp";
+    protected static final String MESSAGE_KEY = "message";
+    protected static final String QUERY_ID_KEY = "queryId";
+    private static final String APPLICATION_KEY = "application";
+    private static final String PAYLOAD_KEY = "payload";
 
     protected static final String CLASSES_KEY = "classesUsedInQuery";
     protected static final String PROPERTIES_KEY = "propertiesUsedInQuery";
     protected static final String TABLES_KEY = "tables";
 
     private static final DateFormat  DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+    private static final Logger REGULAR_LOGGER = LoggerFactory.getLogger(QueryLoggerImpl.class);
+    public static final String QUERY_RESULT_SET_UNBLOCKED = "query:result-set-unblocked";
+    public static final String QUERY_LAST_RESULT_FETCHED = "query:last-result-fetched";
+    public static final String EXECUTION_AND_FETCHING_DURATION_KEY = "executionAndFetchingDuration";
+    public static final String RESULT_COUNT_KEY = "resultCount";
+    public static final String TOTAL_DURATION_KEY = "totalDuration";
+    public static final String EXCEPTION_KEY = "exception";
+    public static final String REFORMULATION_DURATION_KEY = "reformulationDuration";
+    public static final String REFORMULATION_CACHE_HIT_KEY = "reformulationCacheHit";
+    public static final String QUERY_REFORMULATED = "query:reformulated";
+
 
     private final UUID queryId;
     private final long creationTime;
@@ -39,6 +61,7 @@ public class QueryLoggerImpl implements QueryLogger {
     private final OntopReformulationSettings settings;
     private final boolean disabled;
     private final String applicationName;
+    private final JsonFactory jsonFactory;
     private long reformulationTime;
     private long unblockedResulSetTime;
     private final ClassAndPropertyExtractor classAndPropertyExtractor;
@@ -62,7 +85,6 @@ public class QueryLoggerImpl implements QueryLogger {
     protected QueryLoggerImpl(PrintStream outputStream, OntopReformulationSettings settings,
                               ClassAndPropertyExtractor classAndPropertyExtractor,
                               RelationNameExtractor relationNameExtractor) {
-        this.disabled = !settings.isQueryLoggingEnabled();
         this.outputStream = outputStream;
         this.settings = settings;
         this.classAndPropertyExtractor = classAndPropertyExtractor;
@@ -72,6 +94,9 @@ public class QueryLoggerImpl implements QueryLogger {
         applicationName = settings.getApplicationName();
         reformulationTime = -1;
         unblockedResulSetTime = -1;
+        jsonFactory = new JsonFactory();
+
+        this.disabled = !settings.isQueryLoggingEnabled();
     }
 
     @Override
@@ -79,32 +104,45 @@ public class QueryLoggerImpl implements QueryLogger {
         if (disabled)
             return;
 
-        String reformulatedQueryString = settings.isReformulatedQueryIncludedIntoQueryLog()
-                ? serializeEntry(REFORMULATED_QUERY_KEY, reformulatedQuery.toString())
-                : "";
-
-        reformulationTime = System.currentTimeMillis();
-        // TODO: use a proper framework
-        String json = String.format(
-                "{\"@timestamp\": \"%s\", " +
-                        "\"application\": \"%s\", " +
-                        "\"message\": \"query:reformulated\", " +
-                        "\"payload\": { " +
-                        "\"queryId\": \"%s\", " +
-                        "%s %s %s %s %s" +
-                        "\"reformulationDuration\": %d, " +
-                        "\"reformulationCacheHit\": %b } }",
-                serializeTimestamp(reformulationTime),
-                applicationName,
-                queryId,
-                serializeEntry(SPARQL_QUERY_KEY, sparqlQueryString),
-                reformulatedQueryString,
-                serializeArrayEntry(CLASSES_KEY, classes),
-                serializeArrayEntry(PROPERTIES_KEY, properties),
-                serializeArrayEntry(TABLES_KEY, relationNames),
-                reformulationTime - creationTime,
-                wasCached);
-        outputStream.println(json);
+        try (JsonGenerator js = jsonFactory.createGenerator(outputStream)) {
+            js.writeStartObject();
+            js.writeStringField(TIMESTAMP_KEY, serializeTimestamp(System.currentTimeMillis()));
+            js.writeStringField(MESSAGE_KEY, QUERY_REFORMULATED);
+            js.writeStringField(APPLICATION_KEY, applicationName);
+            js.writeObjectFieldStart(PAYLOAD_KEY);
+            js.writeStringField(QUERY_ID_KEY, queryId.toString());
+            // Classes
+            if (classes != null) {
+                js.writeArrayFieldStart(CLASSES_KEY);
+                for (IRI klass : classes)
+                    js.writeString(klass.getIRIString());
+                js.writeEndArray();
+            }
+            // Properties
+            if (properties != null) {
+                js.writeArrayFieldStart(PROPERTIES_KEY);
+                for (IRI p : properties)
+                    js.writeString(p.getIRIString());
+                js.writeEndArray();
+            }
+            // Relations
+            if (relationNames != null) {
+                js.writeArrayFieldStart(TABLES_KEY);
+                for (String n : relationNames)
+                    js.writeString(n);
+                js.writeEndArray();
+            }
+            js.writeNumberField(REFORMULATION_DURATION_KEY, reformulationTime - creationTime);
+            js.writeBooleanField(REFORMULATION_CACHE_HIT_KEY, wasCached);
+            if (sparqlQueryString != null)
+                js.writeStringField(SPARQL_QUERY_KEY, sparqlQueryString);
+            if (settings.isReformulatedQueryIncludedIntoQueryLog())
+                js.writeStringField(REFORMULATED_QUERY_KEY, reformulatedQuery.toString());
+            js.writeEndObject();
+            js.writeEndObject();
+        } catch (IOException ex) {
+            REGULAR_LOGGER.error(OUTPUT_STREAM_JSON_ERROR + ex);
+        }
     }
 
     @Override
@@ -115,14 +153,19 @@ public class QueryLoggerImpl implements QueryLogger {
         if (reformulationTime == -1)
             throw new IllegalStateException("Reformulation should have been declared as finished");
 
-        // TODO: use a proper framework
-        String json = String.format(
-                "{\"@timestamp\": \"%s\", \"application\": \"%s\", \"message\": \"query:result-set-unblocked\", \"payload\": { \"queryId\": \"%s\", \"executionBeforeUnblockingDuration\": %d } }",
-                serializeTimestamp(unblockedResulSetTime),
-                applicationName,
-                queryId,
-                unblockedResulSetTime - reformulationTime);
-        outputStream.println(json);
+        try (JsonGenerator js = jsonFactory.createGenerator(outputStream)) {
+            js.writeStartObject();
+            js.writeStringField(TIMESTAMP_KEY, serializeTimestamp(unblockedResulSetTime));
+            js.writeStringField(MESSAGE_KEY, QUERY_RESULT_SET_UNBLOCKED);
+            js.writeStringField(APPLICATION_KEY, applicationName);
+            js.writeObjectFieldStart(PAYLOAD_KEY);
+            js.writeStringField(QUERY_ID_KEY, queryId.toString());
+            js.writeNumberField(EXECUTION_BEFORE_UNBLOCKING_DURATION_KEY, unblockedResulSetTime - reformulationTime);
+            js.writeEndObject();
+            js.writeEndObject();
+        } catch (IOException e) {
+            REGULAR_LOGGER.error(OUTPUT_STREAM_JSON_ERROR + e);
+        }
     }
 
     @Override
@@ -134,16 +177,21 @@ public class QueryLoggerImpl implements QueryLogger {
         if (unblockedResulSetTime == -1)
             throw new IllegalStateException("Result set should have been declared as unblocked");
 
-        // TODO: use a proper framework
-        String json = String.format(
-                "{\"@timestamp\": \"%s\", \"application\": \"%s\" \"message\": \"query:last-result-fetched\", \"payload\": { \"queryId\": \"%s\", \"executionAndFetchingDuration\": %d, \"totalDuration\": %d, \"resultCount\": %d } }",
-                serializeTimestamp(lastResultFetchedTime),
-                applicationName,
-                queryId,
-                lastResultFetchedTime - reformulationTime,
-                lastResultFetchedTime - creationTime,
-                resultCount);
-        outputStream.println(json);
+        try (JsonGenerator js = jsonFactory.createGenerator(outputStream)) {
+            js.writeStartObject();
+            js.writeStringField(TIMESTAMP_KEY, serializeTimestamp(lastResultFetchedTime));
+            js.writeStringField(MESSAGE_KEY, QUERY_LAST_RESULT_FETCHED);
+            js.writeStringField(APPLICATION_KEY, applicationName);
+            js.writeObjectFieldStart(PAYLOAD_KEY);
+            js.writeStringField(QUERY_ID_KEY, queryId.toString());
+            js.writeNumberField(EXECUTION_AND_FETCHING_DURATION_KEY, lastResultFetchedTime - reformulationTime);
+            js.writeNumberField(TOTAL_DURATION_KEY, lastResultFetchedTime - creationTime);
+            js.writeNumberField(RESULT_COUNT_KEY, resultCount);
+            js.writeEndObject();
+            js.writeEndObject();
+        } catch (IOException e) {
+            REGULAR_LOGGER.error(OUTPUT_STREAM_JSON_ERROR + e);
+        }
     }
 
     @Override
@@ -198,37 +246,22 @@ public class QueryLoggerImpl implements QueryLogger {
         if (disabled)
             return;
 
-        // TODO: use a proper framework
-        String json = String.format(
-                "{\"@timestamp\": \"%s\", \"application\": \"%s\" \"message\": \"%s\", \"payload\": { \"queryId\": \"%s\", \"exception\": %s} }",
-                serializeTimestamp(System.currentTimeMillis()),
-                applicationName,
-                exceptionType,
-                queryId,
-                e.getMessage());
-        outputStream.println(json);
+        try (JsonGenerator js = jsonFactory.createGenerator(outputStream)) {
+            js.writeStartObject();
+            js.writeStringField(TIMESTAMP_KEY, serializeTimestamp(System.currentTimeMillis()));
+            js.writeStringField(MESSAGE_KEY, exceptionType);
+            js.writeStringField(APPLICATION_KEY, applicationName);
+            js.writeObjectFieldStart(PAYLOAD_KEY);
+            js.writeStringField(QUERY_ID_KEY, queryId.toString());
+            js.writeStringField(EXCEPTION_KEY, e.getMessage());
+            js.writeEndObject();
+            js.writeEndObject();
+        } catch (IOException ex) {
+            REGULAR_LOGGER.error(OUTPUT_STREAM_JSON_ERROR + ex);
+        }
     }
 
     protected String serializeTimestamp(long time) {
         return DATE_FORMAT.format(new Timestamp(time));
-    }
-
-    protected String serializeEntry(String key, @Nullable String value) {
-        return (value == null)
-                ? ""
-                : String.format("\"%s\": \"%s\", ", key, escapeDoubleQuotes(value));
-    }
-
-    protected String escapeDoubleQuotes(String value) {
-        return value.replaceAll("\"", "\\\"");
-    }
-
-    protected String serializeArrayEntry(String key, ImmutableSet<? extends Object> arguments) {
-        if (arguments == null)
-            return "";
-
-        return String.format("[%s]", arguments.stream()
-                .map(a -> escapeDoubleQuotes(a.toString()))
-                .collect(Collectors.joining(", ")));
     }
 }
