ef49adad2752dbc389d88713d013ed486b0dc25d
Guohui Xiao <xiao@inf.unibz.it>
2020-07-31 00:41:21 +0200

    4.0.0

--- client/docker/build-image.sh
+++ client/docker/build-image.sh
@@ -14,5 +14,5 @@ unzip -o ontop-cli*.zip -d ontop
 cd ontop
 rm -r ontop.bat ontop ontop-completion.sh jdbc
 cp ${ONTOP_HOME}/client/docker/entrypoint.sh .
-docker build -t ontop/ontop-endpoint:4.0 -f ${ONTOP_HOME}/client/docker/Dockerfile .
+docker build -t ontop/ontop-endpoint:4.0.0 -f ${ONTOP_HOME}/client/docker/Dockerfile .
 cd ${CURRENT_DIR}
