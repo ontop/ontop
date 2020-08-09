dad2b81f2f4242aa17a9894191b24dfe15bed6a7
Guohui Xiao <xiao@inf.unibz.it>
2020-08-09 17:47:43 +0200

    add optimization for unwrapping ST_AsText

--- core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/geof/GeofSfWithinFunctionSymbolImpl.java
+++ core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/geof/GeofSfWithinFunctionSymbolImpl.java
@@ -2,11 +2,14 @@ package it.unibz.inf.ontop.model.term.functionsymbol.impl.geof;
 
 import com.google.common.collect.ImmutableList;
 import it.unibz.inf.ontop.model.term.ImmutableTerm;
+import it.unibz.inf.ontop.model.term.NonGroundFunctionalTerm;
 import it.unibz.inf.ontop.model.term.TermFactory;
+import it.unibz.inf.ontop.model.term.functionsymbol.FunctionSymbol;
 import it.unibz.inf.ontop.model.type.RDFDatatype;
 import org.apache.commons.rdf.api.IRI;
 
 import javax.annotation.Nonnull;
+import java.util.Optional;
 
 public class GeofSfWithinFunctionSymbolImpl extends AbstractGeofBooleanFunctionSymbolImpl {
 
@@ -16,6 +19,14 @@ public class GeofSfWithinFunctionSymbolImpl extends AbstractGeofBooleanFunctionS
 
     @Override
     protected ImmutableTerm computeDBBooleanTerm(ImmutableList<ImmutableTerm> subLexicalTerms, ImmutableList<ImmutableTerm> typeTerms, TermFactory termFactory) {
-        return termFactory.getDBSTWithin(subLexicalTerms.get(0), subLexicalTerms.get(1));
+        return termFactory.getDBSTWithin(unwrapSTAsText(subLexicalTerms.get(0)), unwrapSTAsText(subLexicalTerms.get(1))).simplify();
+    }
+
+    private ImmutableTerm unwrapSTAsText(ImmutableTerm term) {
+        return Optional.of(term)
+                .filter(t -> t instanceof NonGroundFunctionalTerm).map(NonGroundFunctionalTerm.class::cast)
+                .filter(t -> t.getFunctionSymbol().getName().startsWith("ST_ASTEXT"))
+                .map(t -> t.getTerm(0))
+                .orElse(term);
     }
 }
