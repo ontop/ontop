529fbe087136f635915078c0a235cd775ef56db0
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2020-09-18 17:29:45 +0200

    FactExtractorWithSaturatedTBox: implemented.

--- mapping/core/src/main/java/it/unibz/inf/ontop/spec/fact/impl/FactExtractorWithSaturatedTBox.java
+++ mapping/core/src/main/java/it/unibz/inf/ontop/spec/fact/impl/FactExtractorWithSaturatedTBox.java
@@ -1,19 +1,22 @@
 package it.unibz.inf.ontop.spec.fact.impl;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import it.unibz.inf.ontop.exception.MinorOntopInternalBugException;
 import it.unibz.inf.ontop.injection.OntopMappingSettings;
 import it.unibz.inf.ontop.model.term.IRIConstant;
 import it.unibz.inf.ontop.model.term.ObjectConstant;
 import it.unibz.inf.ontop.model.term.TermFactory;
 import it.unibz.inf.ontop.model.vocabulary.OWL;
+import it.unibz.inf.ontop.model.vocabulary.RDF;
 import it.unibz.inf.ontop.model.vocabulary.RDFS;
 import it.unibz.inf.ontop.spec.ontology.*;
 import it.unibz.inf.ontop.utils.ImmutableCollectors;
 
+import java.util.Map;
 import java.util.UUID;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -21,7 +24,9 @@ import java.util.stream.Stream;
 public class FactExtractorWithSaturatedTBox extends AbstractFactExtractor {
 
     private final TermFactory termFactory;
-    private final IRIConstant someValuesFrom, subClassOf, subPropertyOf, domain, range;
+    private final IRIConstant someValuesFrom, subClassOf, subPropertyOf, domain, range, rdfType;
+    private final IRIConstant rdfsClass, owlClass, owlRestriction, onProperty, owlThing;
+    private final IRIConstant rdfProperty, objectProperty, dataProperty;
 
     @Inject
     protected FactExtractorWithSaturatedTBox(TermFactory termFactory, OntopMappingSettings settings) {
@@ -32,29 +37,58 @@ public class FactExtractorWithSaturatedTBox extends AbstractFactExtractor {
         subPropertyOf = termFactory.getConstantIRI(RDFS.SUBPROPERTYOF);
         domain = termFactory.getConstantIRI(RDFS.DOMAIN);
         range = termFactory.getConstantIRI(RDFS.RANGE);
+        rdfType = termFactory.getConstantIRI(RDF.TYPE);
+        rdfsClass = termFactory.getConstantIRI(RDFS.CLASS);
+        owlClass = termFactory.getConstantIRI(OWL.CLASS);
+        owlRestriction = termFactory.getConstantIRI(OWL.RESTRICTION);
+        onProperty = termFactory.getConstantIRI(OWL.ON_PROPERTY);
+        owlThing = termFactory.getConstantIRI(OWL.THING);
+        rdfProperty = termFactory.getConstantIRI(RDF.PROPERTY);
+        dataProperty = termFactory.getConstantIRI(OWL.DATA_PROPERTY);
+        objectProperty = termFactory.getConstantIRI(OWL.OBJECT_PROPERTY);
     }
 
     @Override
     protected Stream<RDFFact> extractTbox(ClassifiedTBox tbox) {
         // TODO: also consider simple RDF properties
+
+        ImmutableMap<DescriptionBT, ObjectConstant> expressionIdMap = Stream.concat(Stream.concat(
+                generateIdEntries(tbox.classesDAG(), this::generateIdFromClassExpression),
+                generateIdEntries(tbox.dataPropertiesDAG(), this::generateIdFromDataPropertyExpression)),
+                generateIdEntries(tbox.objectPropertiesDAG(), this::generateIdFromObjectPropertyExpression))
+                .collect(ImmutableCollectors.toMap());
+
+        // TODO: also consider class disjointness
+
+        EquivalencesDAG<ClassExpression> classDag = tbox.classesDAG();
+
         return Stream.concat(
                 Stream.concat(
                         extractFromDAG(
-                                tbox.classesDAG(),
-                                this::generateIdFromClassExpression,
-                                this::convertClassExpression,
+                                classDag,
+                                e -> convertClassExpression(e, expressionIdMap),
+                                expressionIdMap,
+                                ClassExpression.class,
                                 subClassOf),
                         extractFromDAG(
                                 tbox.dataPropertiesDAG(),
-                                this::generateIdFromDataPropertyExpression,
-                                this::convertDataPropertyExpression,
-                                subPropertyOf
-                                )),
+                                e -> convertDataPropertyExpression(e, expressionIdMap, classDag),
+                                expressionIdMap,
+                                DataPropertyExpression.class,
+                                subPropertyOf)),
                 extractFromDAG(
                         tbox.objectPropertiesDAG(),
-                        this::generateIdFromObjectPropertyExpression,
-                        this::convertObjectPropertyExpression,
+                        e -> convertObjectPropertyExpression(e, expressionIdMap, classDag),
+                        expressionIdMap,
+                        ObjectPropertyExpression.class,
                         subPropertyOf));
+    }
+
+    private <T extends DescriptionBT> Stream<Map.Entry<DescriptionBT, ObjectConstant>> generateIdEntries(
+            EquivalencesDAG<T> dag, Function<T, ObjectConstant> idGenerator) {
+        return dag.stream()
+                .flatMap(e -> e.getMembers().stream())
+                .map(m -> Maps.immutableEntry(m, idGenerator.apply(m)));
     }
 
     private ObjectConstant generateIdFromClassExpression(ClassExpression expression) {
@@ -75,41 +109,115 @@ public class FactExtractorWithSaturatedTBox extends AbstractFactExtractor {
                 : termFactory.getConstantIRI(expression.getIRI());
     }
 
-    private Stream<RDFFact> convertClassExpression(ClassExpression e, ObjectConstant objectConstant) {
-        throw new RuntimeException("TODO: continue");
-    }
-
-    private Stream<RDFFact> convertDataPropertyExpression(DataPropertyExpression e, ObjectConstant objectConstant) {
-        throw new RuntimeException("TODO: continue");
-    }
-
-    private Stream<RDFFact> convertObjectPropertyExpression(ObjectPropertyExpression e, ObjectConstant objectConstant) {
-        throw new RuntimeException("TODO: continue");
-    }
-
     private <T extends DescriptionBT> Stream<RDFFact> extractFromDAG(EquivalencesDAG<T> dag,
-                                                                     Function<T, ObjectConstant> idGenerator,
-                                                                     BiFunction<T, ObjectConstant, Stream<RDFFact>> expressionConverter,
-                                                                     IRIConstant subPredicateProperty) {
-        ImmutableMap<T, ObjectConstant> expressionIdMap = dag.stream()
-                .flatMap(e -> e.getMembers().stream())
-                .collect(ImmutableCollectors.toMap(
-                        m -> m,
-                        idGenerator));
-
-        Stream<RDFFact> expressionFacts = expressionIdMap.entrySet().stream()
-                .flatMap(e -> expressionConverter.apply(e.getKey(), e.getValue()));
-
-        // All subPropertyOf or subClassOf (after transitive closure)
-        Stream<RDFFact> subFacts = dag.stream()
-                .flatMap(supEq -> supEq.getMembers().stream()
-                        .flatMap(sup -> dag.getSub(supEq).stream()
-                                .flatMap(subEq -> subEq.getMembers().stream()
-                                        .map(sub -> RDFFact.createTripleFact(
-                                                expressionIdMap.get(sub),
-                                                subPredicateProperty,
-                                                expressionIdMap.get(sup))))));
-
-        return Stream.concat(expressionFacts, subFacts);
+                                                                     Function<T, Stream<RDFFact>> expressionConverter,
+                                                                     ImmutableMap<DescriptionBT, ObjectConstant> expressionIdMap,
+                                                                     Class<T> expressionClass,
+                                                                     IRIConstant subClassOrSubProperty) {
+        return Stream.concat(
+                expressionIdMap.keySet().stream()
+                        .filter(expressionClass::isInstance)
+                        .flatMap(e -> expressionConverter.apply((T) e)),
+                extractSub(dag, subClassOrSubProperty, expressionIdMap));
     }
+
+    private <T extends DescriptionBT> Stream<RDFFact> extractSub(EquivalencesDAG<T> dag, IRIConstant subPredicateProperty, ImmutableMap<DescriptionBT, ObjectConstant> expressionIdMap) {
+        return dag.stream()
+                    .flatMap(supEq -> supEq.getMembers().stream()
+                            .flatMap(sup -> dag.getSub(supEq).stream()
+                                    .flatMap(subEq -> subEq.getMembers().stream()
+                                            .map(sub -> RDFFact.createTripleFact(
+                                                    expressionIdMap.get(sub),
+                                                    subPredicateProperty,
+                                                    expressionIdMap.get(sup))))));
+    }
+
+    /**
+     * Everything except subClassOf
+     */
+    private Stream<RDFFact> convertClassExpression(ClassExpression e,
+                                                   ImmutableMap<DescriptionBT, ObjectConstant> expressionIdMap) {
+        ObjectConstant classId = expressionIdMap.get(e);
+
+        Stream<RDFFact> common = Stream.of(
+                RDFFact.createTripleFact(classId, rdfType, rdfsClass),
+                RDFFact.createTripleFact(classId, rdfType, owlClass));
+
+        if (e instanceof OClass)
+            return common;
+        else if (e instanceof ObjectSomeValuesFrom) {
+            return Stream.concat(
+                    common,
+                    extractFactsFromRestriction(
+                            classId,
+                            expressionIdMap.get(((ObjectSomeValuesFrom) e).getProperty())));
+        }
+        else if (e instanceof DataPropertyExpression) {
+            return Stream.concat(
+                    common,
+                    extractFactsFromRestriction(
+                            classId,
+                            expressionIdMap.get(((DataSomeValuesFrom) e).getProperty())));
+        }
+        else
+            throw new MinorOntopInternalBugException("Unexpected class expression");
+    }
+
+    private Stream<RDFFact> extractFactsFromRestriction(ObjectConstant classId, ObjectConstant propertyId) {
+        return Stream.of(
+                RDFFact.createTripleFact(classId, rdfType, owlRestriction),
+                RDFFact.createTripleFact(classId, onProperty, propertyId),
+                RDFFact.createTripleFact(classId, someValuesFrom, owlThing));
+    }
+
+    private Stream<RDFFact> convertDataPropertyExpression(DataPropertyExpression e,
+                                                          ImmutableMap<DescriptionBT, ObjectConstant> expressionIdMap,
+                                                          EquivalencesDAG<ClassExpression> classDag) {
+        ObjectConstant propertyId = expressionIdMap.get(e);
+        Stream<RDFFact> basics = Stream.of(
+                RDFFact.createTripleFact(propertyId, rdfType, rdfProperty),
+                RDFFact.createTripleFact(propertyId, rdfType, dataProperty));
+
+        // TODO: consider range?
+        return Stream.concat(
+                basics,
+                e.getAllDomainRestrictions().stream()
+                        .flatMap(c -> extractSubDomainOrRange(propertyId, c, domain, expressionIdMap, classDag)));
+
+    }
+
+    /**
+     * Everything except subPropertyOf
+     */
+    private Stream<RDFFact> extractSubDomainOrRange(ObjectConstant propertyId, ClassExpression classExpression,
+                                                    IRIConstant rangeOrDomainProperty,
+                                                    ImmutableMap<DescriptionBT, ObjectConstant> expressionIdMap,
+                                                    EquivalencesDAG<ClassExpression> classDag) {
+        Equivalences<ClassExpression> eq = classDag.getVertex(classExpression);
+
+        Stream<RDFFact> siblings = eq.stream()
+                .map(s -> RDFFact.createTripleFact(propertyId, rangeOrDomainProperty, expressionIdMap.get(s)));
+
+        Stream<RDFFact> subs = classDag.getSub(eq).stream()
+                .flatMap(subEq -> subEq.stream()
+                        .map(sub -> RDFFact.createTripleFact(propertyId, rangeOrDomainProperty, expressionIdMap.get(sub))));
+
+        return Stream.concat(siblings, subs);
+    }
+
+    /**
+     * Everything except subPropertyOf
+     */
+    private Stream<RDFFact> convertObjectPropertyExpression(ObjectPropertyExpression e,
+                                                            ImmutableMap<DescriptionBT, ObjectConstant> expressionIdMap,
+                                                            EquivalencesDAG<ClassExpression> classDag) {
+        ObjectConstant propertyId = expressionIdMap.get(e);
+        return Stream.concat(Stream.concat(
+                Stream.of(
+                    RDFFact.createTripleFact(propertyId, rdfType, rdfProperty),
+                    RDFFact.createTripleFact(propertyId, rdfType, objectProperty)),
+                extractSubDomainOrRange(propertyId, e.getDomain(), domain, expressionIdMap, classDag)),
+                extractSubDomainOrRange(propertyId, e.getRange(), range, expressionIdMap, classDag));
+    }
+
 }
