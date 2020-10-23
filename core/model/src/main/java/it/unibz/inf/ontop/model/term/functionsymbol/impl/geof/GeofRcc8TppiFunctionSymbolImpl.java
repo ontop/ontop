3177921ff59d4bc621b422c7bf8837906bb25ef3
Albulen Pano <alpano@unibz.it>
2020-10-23 17:58:04 +0200

    Add Draft AbstractRelate Version

--- core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/geof/GeofRcc8TppiFunctionSymbolImpl.java
+++ core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/geof/GeofRcc8TppiFunctionSymbolImpl.java
@@ -8,15 +8,19 @@ import org.apache.commons.rdf.api.IRI;
 
 import javax.annotation.Nonnull;
 
-public class GeofRcc8TppiFunctionSymbolImpl  extends AbstractGeofBooleanFunctionSymbolImpl {
+public class GeofRcc8TppiFunctionSymbolImpl  extends AbstractGeofBooleanFunctionSymbolImplUsingRelate {
 
     public GeofRcc8TppiFunctionSymbolImpl(@Nonnull IRI functionIRI, RDFDatatype wktLiteralType, RDFDatatype xsdBooleanType) {
         super("GEOF_RCC8_TPPI", functionIRI, ImmutableList.of(wktLiteralType, wktLiteralType), xsdBooleanType);
     }
 
+    //    @Override
+//    protected ImmutableTerm computeDBBooleanTerm(ImmutableList<ImmutableTerm> subLexicalTerms, ImmutableList<ImmutableTerm> typeTerms, TermFactory termFactory) {
+//        final String matrix_pattern = "TTTFTTFFT";
+//        return termFactory.getDBRelate(subLexicalTerms.get(0), subLexicalTerms.get(1),termFactory.getDBStringConstant(matrix_pattern));
+//    }
     @Override
-    protected ImmutableTerm computeDBBooleanTerm(ImmutableList<ImmutableTerm> subLexicalTerms, ImmutableList<ImmutableTerm> typeTerms, TermFactory termFactory) {
-        final String matrix_pattern = "TTTFTTFFT";
-        return termFactory.getDBRelate(subLexicalTerms.get(0), subLexicalTerms.get(1),termFactory.getDBStringConstant(matrix_pattern));
+    public TriFunction<ImmutableTerm, ImmutableTerm, ImmutableTerm, ImmutableTerm> getDBFunction(TermFactory termFactory) {
+        return termFactory::getDBRelate;
     }
 }
