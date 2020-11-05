e4c34c277df5a1ff1c278e21d04b2c6b9c9b8c61
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2020-11-05 10:52:07 +0100

    Skeleton to TBoxFactTest.

---
+++ binding/rdf4j/src/test/java/it/unibz/inf/ontop/rdf4j/repository/TBoxFactTest.java
@@ -0,0 +1,78 @@
+package it.unibz.inf.ontop.rdf4j.repository;
+
+import com.google.common.collect.ImmutableList;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.query.impl.MapBindingSet;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class TBoxFactTest extends AbstractRDF4JTest {
+
+    // TODO: use another SQL script
+    private static final String CREATE_DB_FILE = "/label_comment.sql";
+    // TODO: use another mapping file
+    private static final String OBDA_FILE = "/label_comment.obda";
+    // TODO: use an ontology file
+    private static final String OWL_FILE = null;
+
+    @BeforeClass
+    public static void before() throws IOException, SQLException {
+        initOBDA(CREATE_DB_FILE, OBDA_FILE, OWL_FILE);
+    }
+
+    @AfterClass
+    public static void after() throws SQLException {
+        release();
+    }
+
+    /**
+     * TODO: replace MY_CLASS_TO_TEST by a concrete class
+     */
+    @Test
+    public void testSubclasses() {
+        String query = "SELECT  *\n" +
+                "WHERE {\n" +
+                "   ?v rdfs:subClassOf MY_CLASS_TO_TEST .\n" +
+                "}\n" +
+                "ORDER BY ?v";
+
+        // TODO: provide the expected results
+        ImmutableList<String> results = ImmutableList.of("testdata");
+        runQueryAndCompare(query, results);
+    }
+
+    @Test
+    public void testRDFSClasses() {
+        String query = "SELECT  *\n" +
+                "WHERE {\n" +
+                "   ?v a rdfs:Class .\n" +
+                "}\n" +
+                "ORDER BY ?v";
+
+        runQueryAndCompare(query, getExpectedClasses());
+    }
+
+    @Test
+    public void testOWLClasses() {
+        String query = "SELECT  *\n" +
+                "WHERE {\n" +
+                "   ?v a owl:Class .\n" +
+                "}\n" +
+                "ORDER BY ?v";
+
+        runQueryAndCompare(query, getExpectedClasses());
+    }
+
+    /**
+     * TODO: provide the list of expected classes
+     */
+    private ImmutableList<String> getExpectedClasses() {
+        return ImmutableList.of("testdata");
+    }
+
+
+}
