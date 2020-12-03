61bf7f2961a067b4e8aadc1ab896ec9a107ff922
kontchakov <roman@dcs.bbk.ac.uk>
2020-12-03 12:00:34 +0000

    SafeSeparatorFragment

--- core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/db/impl/ObjectStringTemplateFunctionSymbolImpl.java
+++ core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/db/impl/ObjectStringTemplateFunctionSymbolImpl.java
@@ -17,26 +17,23 @@ import it.unibz.inf.ontop.utils.ImmutableCollectors;
 
 import javax.annotation.Nullable;
 import java.util.Optional;
-import java.util.UUID;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static it.unibz.inf.ontop.model.term.functionsymbol.db.impl.SafeSeparatorFragment.NOT_A_SAFE_SEPARATOR_REGEX;
-import static it.unibz.inf.ontop.model.term.functionsymbol.db.impl.SafeSeparatorFragment.SOME_SAFE_SEPARATORS;
 
 public abstract class ObjectStringTemplateFunctionSymbolImpl extends FunctionSymbolImpl
         implements ObjectStringTemplateFunctionSymbol {
 
     private final String template;
     private final DBTermType lexicalType;
-    private final Pattern pattern;
+    @Nullable
+    private Pattern injectivePattern; // lazy initalization
     private final boolean isInjective;
     private final ImmutableList<TemplateComponent> components;
-    // Use for checking compatibility
     private final ImmutableList<SafeSeparatorFragment> safeSeparatorFragments;
 
 
@@ -44,19 +41,12 @@ public abstract class ObjectStringTemplateFunctionSymbolImpl extends FunctionSym
 
     protected ObjectStringTemplateFunctionSymbolImpl(ImmutableList<TemplateComponent> components, TypeFactory typeFactory) {
         super(extractStringTemplate(components), createBaseTypes(components, typeFactory));
-        this.template = extractStringTemplate(components);
         this.lexicalType = typeFactory.getDBTypeFactory().getDBStringType();
         this.components = components;
-        this.pattern = extractPattern(components);
+        this.template = extractStringTemplate(components);
         this.safeSeparatorFragments = SafeSeparatorFragment.split(template);
-        this.isInjective = isInjective();
-    }
-
-    /**
-     * Must not produce false positive
-     */
-    private boolean isInjective() {
-        return safeSeparatorFragments.stream()
+        // must not produce false positives
+        this.isInjective = safeSeparatorFragments.stream()
                 .map(SafeSeparatorFragment::getFragment)
                 .allMatch(ObjectStringTemplateFunctionSymbolImpl::atMostOnePlaceholder);
     }
@@ -193,47 +183,20 @@ public abstract class ObjectStringTemplateFunctionSymbolImpl extends FunctionSym
      * Is guaranteed not to return false negative.
      */
     protected boolean areCompatible(ImmutableList<SafeSeparatorFragment> otherFragments) {
-//        if (template.equals(otherTemplate))
-//            return true;
-
-/*
-        String prefix = extractPrefix(template);
-        String otherPrefix = extractPrefix(otherTemplate);
-        int minPrefixLength = Math.min(prefix.length(), otherPrefix.length());
-        // Prefix comparison
-        if (minPrefixLength > 0 &&
-                !prefix.substring(0, minPrefixLength).equals(otherPrefix.substring(0, minPrefixLength)))
-            return false;
- */
-/*
-        String suffix = extractSuffix(template);
-        String otherSuffix = extractSuffix(otherTemplate);
-        int minSuffixLength = Math.min(suffix.length(), otherSuffix.length());
-        if (!suffix.substring(suffix.length() - minSuffixLength).equals(otherSuffix.substring(otherSuffix.length() - minSuffixLength)))
-            return false;
-*/
-        ImmutableList<SafeSeparatorFragment> fragments = this.safeSeparatorFragments; // .substring(minPrefixLength), template.length() - minSuffixLength
-//        ImmutableList<SafeSeparatorFragment> otherFragments = SafeSeparatorFragment.split(otherTemplate); // .substring(minPrefixLength) , otherTemplate.length() - minSuffixLength
+        ImmutableList<SafeSeparatorFragment> fragments = this.safeSeparatorFragments;
 
         if (fragments == otherFragments)
             return true;
 
-        if (fragments.size() != otherFragments.size())
-            return false;
+        return fragments.size() == otherFragments.size()
 
-        // Checks that both templates use the same safe separators in the same order
-        boolean separatorsMatch = IntStream.range(0, fragments.size())
-            .allMatch(i -> fragments.get(i).getSeparator() == otherFragments.get(i).getSeparator());
+            && IntStream.range(0, fragments.size())
+                .allMatch(i -> fragments.get(i).getSeparator() == otherFragments.get(i).getSeparator())
 
-        return separatorsMatch && IntStream.range(0, fragments.size())
-                .allMatch(i -> matchPatterns(fragments.get(i), otherFragments.get(i)));
+            && IntStream.range(0, fragments.size())
+                .allMatch(i -> SafeSeparatorFragment.matchFragments(fragments.get(i), otherFragments.get(i)));
     }
 
-    private static boolean matchPatterns(SafeSeparatorFragment subTemplate1, SafeSeparatorFragment subTemplate2) {
-        return subTemplate1.getFragment().equals(subTemplate2.getFragment())
-                || subTemplate1.getFragment().indexOf('{') >= 0 && subTemplate1.extractPattern().matcher(subTemplate2.getFragment()).find()
-                || subTemplate2.getFragment().indexOf('{') >= 0 && subTemplate2.extractPattern().matcher(subTemplate1.getFragment()).find();
-    }
 
 
     protected static Pattern extractPattern(ImmutableList<TemplateComponent> components) {
@@ -246,24 +209,6 @@ public abstract class ObjectStringTemplateFunctionSymbolImpl extends FunctionSym
         return Pattern.compile("^" + patternString + "$");
     }
 
-    private static String extractOnlyAlwaysSafeSeparators(String template) {
-        return template.chars()
-                .mapToObj(c -> (char) c)
-                .filter(SOME_SAFE_SEPARATORS::contains)
-                .collect(Collector.of(StringBuilder::new,
-                        StringBuilder::append,
-                        StringBuilder::append,
-                        StringBuilder::toString));
-    }
-
-
-    private static String extractPrefix(String template) {
-        int index = template.indexOf('{');
-        return index >= 0
-                ? template.substring(0, index)
-                : template;
-    }
-
     @Override
     protected IncrementalEvaluation evaluateStrictEqWithNonNullConstant(ImmutableList<? extends ImmutableTerm> terms,
                                                                         NonNullConstant otherTerm, TermFactory termFactory,
@@ -271,7 +216,9 @@ public abstract class ObjectStringTemplateFunctionSymbolImpl extends FunctionSym
         String otherValue = otherTerm.getValue();
 
         if (isInjective(terms, variableNullability, termFactory)) {
-            Matcher matcher = pattern.matcher(otherTerm.getValue());
+            if (injectivePattern == null)
+                this.injectivePattern = extractPattern(components);
+            Matcher matcher = injectivePattern.matcher(otherTerm.getValue());
             if (matcher.find()) {
                 ImmutableExpression newExpression = termFactory.getConjunction(
                         IntStream.range(0, getArity())
@@ -345,7 +292,7 @@ class SafeSeparatorFragment {
     @Nullable
     private Pattern pattern;
 
-    public Pattern extractPattern() {
+    private Pattern extractPattern() {
         if (pattern == null) {
             StringBuilder patternString = new StringBuilder();
             int start = 0, end;
@@ -362,6 +309,16 @@ class SafeSeparatorFragment {
         return pattern;
     }
 
+    private boolean matchDifferent(String other) {
+        return fragment.indexOf('{') >= 0
+                && extractPattern().matcher(other).find();
+    }
+
+    public static boolean matchFragments(SafeSeparatorFragment subTemplate1, SafeSeparatorFragment subTemplate2) {
+        return subTemplate1.getFragment().equals(subTemplate2.getFragment())
+                || subTemplate1.matchDifferent(subTemplate2.getFragment())
+                || subTemplate2.matchDifferent(subTemplate1.getFragment());
+    }
 
     public static String makeRegexSafe(String s) {
         return s.replaceAll("[<(\\[{\\\\^=$!|\\]})?*+.>]", "\\\\$0");
