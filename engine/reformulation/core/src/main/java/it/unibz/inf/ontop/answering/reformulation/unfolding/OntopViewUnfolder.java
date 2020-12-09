beab561b556e3580f768b08d2963a2a9689bb71a
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2020-12-09 21:54:36 +0100

    OntopViewUnfolder added.

---
+++ engine/reformulation/core/src/main/java/it/unibz/inf/ontop/answering/reformulation/unfolding/OntopViewUnfolder.java
@@ -0,0 +1,10 @@
+package it.unibz.inf.ontop.answering.reformulation.unfolding;
+
+import it.unibz.inf.ontop.iq.optimizer.IQOptimizer;
+
+/**
+ * Unfolds Ontop views of the highest level
+ *  (among the views present in the tree)
+ */
+public interface OntopViewUnfolder extends IQOptimizer {
+}
