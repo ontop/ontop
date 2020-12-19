fed3f00175b76655baf736965d0dc3437163c373
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2020-12-19 16:00:12 +0100

    One more example of missed distinct lift.

--- core/optimization/src/test/java/it/unibz/inf/ontop/iq/NormalizationTest.java
+++ core/optimization/src/test/java/it/unibz/inf/ontop/iq/NormalizationTest.java
@@ -2262,7 +2262,7 @@ public class NormalizationTest {
     }
 
     @Test
-    public void testDistinctLiftNullableUC() {
+    public void testDistinctLiftNullableUC1() {
         OptimizationTestingTools.OfflineMetadataProviderBuilder3 builder = createMetadataProviderBuilder();
         DatabaseRelationDefinition table50 = builder.createRelationWithUC(50, 2, true);
 
@@ -2274,6 +2274,33 @@ public class NormalizationTest {
         ExtensionalDataNode dataNode2 = IQ_FACTORY.createExtensionalDataNode(table50, ImmutableMap.of(0, A, 1, B));
 
         InnerJoinNode innerJoinNode = IQ_FACTORY.createInnerJoinNode();
+
+        NaryIQTree initialTree = IQ_FACTORY.createNaryIQTree(
+                innerJoinNode,
+                ImmutableList.of(firstChild, dataNode2));
+
+        DistinctVariableOnlyDataAtom projectionAtom = ATOM_FACTORY.getDistinctVariableOnlyDataAtom(ANS1_AR2_PREDICATE, A, B);
+        IQ initialIQ = IQ_FACTORY.createIQ(projectionAtom, initialTree);
+
+        IQTree expectedTree = IQ_FACTORY.createUnaryIQTree(
+                distinctNode,
+                IQ_FACTORY.createNaryIQTree(innerJoinNode,
+                        ImmutableList.of(dataNode1, dataNode2)));
+        normalizeAndCompare(initialIQ, IQ_FACTORY.createIQ(projectionAtom, expectedTree));
+    }
+    @Test
+    public void testDistinctLiftNullableUC2() {
+        OptimizationTestingTools.OfflineMetadataProviderBuilder3 builder = createMetadataProviderBuilder();
+        DatabaseRelationDefinition table50 = builder.createRelationWithUC(50, 2, true);
+
+        DistinctNode distinctNode = IQ_FACTORY.createDistinctNode();
+
+        ExtensionalDataNode dataNode1 = IQ_FACTORY.createExtensionalDataNode(TABLE1_AR1, ImmutableMap.of(0, A));
+        IQTree firstChild = IQ_FACTORY.createUnaryIQTree(distinctNode, dataNode1);
+
+        ExtensionalDataNode dataNode2 = IQ_FACTORY.createExtensionalDataNode(table50, ImmutableMap.of(0, B));
+
+        InnerJoinNode innerJoinNode = IQ_FACTORY.createInnerJoinNode(TERM_FACTORY.getDBIsNotNull(B));
 
         NaryIQTree initialTree = IQ_FACTORY.createNaryIQTree(
                 innerJoinNode,
