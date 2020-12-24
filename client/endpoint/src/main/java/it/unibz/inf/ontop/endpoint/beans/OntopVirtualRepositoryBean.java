33f8d7ba43fd4ec75b3b3f3cbc368ce00e17ee89
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2020-12-24 12:05:09 +0100

    CLI: Ontop views are now given to the configuration.

--- client/endpoint/src/main/java/it/unibz/inf/ontop/endpoint/beans/OntopVirtualRepositoryBean.java
+++ client/endpoint/src/main/java/it/unibz/inf/ontop/endpoint/beans/OntopVirtualRepositoryBean.java
@@ -19,6 +19,7 @@ public class OntopVirtualRepositoryBean {
                                                              @Value("${properties}") String properties,
                                                              @Value("${constraint:#{null}}") String constraint,
                                                              @Value("${db-metadata:#{null}}") String dbMetadata,
+                                                             @Value("${ontop-views:#{null}}") String ontopViews,
                                                              @Value("${db-user:#{null}}") String dbUser,
                                                              @Value("${db-password:#{null}}") String dbPassword,
                                                              @Value("${db-url:#{null}}") String dbUrl) throws RepositoryException {
@@ -41,6 +42,9 @@ public class OntopVirtualRepositoryBean {
 
         if (dbMetadata !=null && !dbMetadata.isEmpty())
             builder.dbMetadataFile(dbMetadata);
+
+        if (ontopViews !=null && !ontopViews.isEmpty())
+            builder.ontopViewFile(ontopViews);
 
         if (dbUser != null && !dbUser.isEmpty())
             builder.jdbcUser(dbUser);
