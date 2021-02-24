719e756db941014b7da1639ff88fbec5e485715a
Albulen Pano <albulen.pano@ontopic.biz>
2021-02-24 13:40:18 +0100

    Add constraints (exc. FK) in SQL Views

---
+++ db/rdb/src/test/java/it/unibz/inf/ontop/dbschema/SQLViewWithConstraintsPersonTest.java
@@ -0,0 +1,102 @@
+package it.unibz.inf.ontop.dbschema;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.inject.Injector;
+import it.unibz.inf.ontop.injection.OntopSQLCoreConfiguration;
+import it.unibz.inf.ontop.utils.ImmutableCollectors;
+import org.junit.Test;
+
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+public class SQLViewWithConstraintsPersonTest {
+    private static final String VIEW_FILE = "src/test/resources/person/sql_views_add_constraints.json";
+    private static final String DBMETADATA_FILE = "src/test/resources/person/person_with_constraints.db-extract.json";
+
+    ImmutableSet<OntopViewDefinition> viewDefinitions = loadViewDefinitions(VIEW_FILE, DBMETADATA_FILE);
+
+    public SQLViewWithConstraintsPersonTest() throws Exception {
+    }
+
+    /**
+     * Both the parent "id" and added "status" constraints are present in the views
+     */
+    @Test
+    public void testPersonAddUniqueConstraint() throws Exception {
+        ImmutableSet<String> constraints = viewDefinitions.stream()
+                .map(RelationDefinition::getUniqueConstraints)
+                .flatMap(Collection::stream)
+                .map(UniqueConstraint::getAttributes)
+                .flatMap(Collection::stream)
+                .map(v -> v.getID().getName())
+                .collect(ImmutableCollectors.toSet());
+
+        assertEquals(ImmutableSet.of("status"), constraints);
+    }
+
+    /**
+     * The dependent of the FD is correctly added by a viewfile
+     */
+    @Test
+    public void testPersonAddFunctionalDependencyDependent() throws Exception {
+        ImmutableSet<String> otherFD = viewDefinitions.stream()
+                .map(RelationDefinition::getOtherFunctionalDependencies)
+                .flatMap(Collection::stream)
+                .map(FunctionalDependency::getDependents)
+                .flatMap(Collection::stream)
+                .map(d -> d.getID().getName())
+                .collect(ImmutableCollectors.toSet());
+
+        assertEquals(ImmutableSet.of("status"), otherFD);
+    }
+
+    /**
+     * The determinant of the FD is correctly added by a viewfile
+     */
+    @Test
+    public void testPersonAddFunctionalDependencyDeterminant() throws Exception {
+        ImmutableSet<String> otherFD = viewDefinitions.stream()
+                .map(RelationDefinition::getOtherFunctionalDependencies)
+                .flatMap(Collection::stream)
+                .map(FunctionalDependency::getDeterminants)
+                .flatMap(Collection::stream)
+                .map(d -> d.getID().getName())
+                .collect(ImmutableCollectors.toSet());
+
+        assertEquals(ImmutableSet.of("id"), otherFD);
+    }
+
+    protected ImmutableSet<OntopViewDefinition> loadViewDefinitions(String viewFilePath,
+                                                                    String dbMetadataFilePath)
+            throws Exception {
+
+        OntopSQLCoreConfiguration configuration = OntopSQLCoreConfiguration.defaultBuilder()
+                .jdbcUrl("jdbc:h2:mem:nowhere")
+                .jdbcDriver("org.h2.Driver")
+                .build();
+
+        Injector injector = configuration.getInjector();
+        SerializedMetadataProvider.Factory serializedMetadataProviderFactory = injector.getInstance(SerializedMetadataProvider.Factory.class);
+        OntopViewMetadataProvider.Factory viewMetadataProviderFactory = injector.getInstance(OntopViewMetadataProvider.Factory.class);
+
+        SerializedMetadataProvider dbMetadataProvider;
+        try (Reader dbMetadataReader = new FileReader(dbMetadataFilePath)) {
+            dbMetadataProvider = serializedMetadataProviderFactory.getMetadataProvider(dbMetadataReader);
+        }
+
+        OntopViewMetadataProvider viewMetadataProvider;
+        try (Reader viewReader = new FileReader(viewFilePath)) {
+            viewMetadataProvider = viewMetadataProviderFactory.getMetadataProvider(dbMetadataProvider, viewReader);
+        }
+
+        ImmutableMetadata metadata = ImmutableMetadata.extractImmutableMetadata(viewMetadataProvider);
+
+        return metadata.getAllRelations().stream()
+                .filter(r -> r instanceof OntopViewDefinition)
+                .map(r -> (OntopViewDefinition) r)
+                .collect(ImmutableCollectors.toSet());
+    }
+}
