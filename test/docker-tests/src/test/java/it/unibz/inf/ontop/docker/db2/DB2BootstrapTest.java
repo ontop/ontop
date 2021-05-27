4e31ade402f9ed831090e7e12a6b1fa743ffe7fb
kontchakov <roman@dcs.bbk.ac.uk>
2021-05-27 09:01:02 +0100

    test and fix for quoted column aliases in bootstrapper

--- test/docker-tests/src/test/java/it/unibz/inf/ontop/docker/db2/DB2BootstrapTest.java
+++ test/docker-tests/src/test/java/it/unibz/inf/ontop/docker/db2/DB2BootstrapTest.java
@@ -1,22 +1,9 @@
 package it.unibz.inf.ontop.docker.db2;
 
-import it.unibz.inf.ontop.injection.OntopSQLOWLAPIConfiguration;
-import it.unibz.inf.ontop.owlapi.OntopOWLFactory;
-import it.unibz.inf.ontop.owlapi.OntopOWLReasoner;
-import it.unibz.inf.ontop.owlapi.connection.OWLConnection;
-import it.unibz.inf.ontop.owlapi.connection.OWLStatement;
-import it.unibz.inf.ontop.spec.mapping.bootstrap.DirectMappingBootstrapper;
-import it.unibz.inf.ontop.spec.mapping.serializer.impl.OntopNativeMappingSerializer;
+import it.unibz.inf.ontop.docker.AbstractBootstrapTest;
 import org.junit.Test;
-import org.semanticweb.owlapi.io.FileDocumentTarget;
-import org.semanticweb.owlapi.model.OWLException;
-import org.semanticweb.owlapi.model.OWLOntology;
 
-import java.io.File;
-
-import static it.unibz.inf.ontop.spec.mapping.bootstrap.DirectMappingBootstrapper.BootstrappingResults;
-
-public class DB2BootstrapTest {
+public class DB2BootstrapTest extends AbstractBootstrapTest {
 
     static private final String baseIRI = "http://db2-bootstrap-test";
     static private final String owlOutputFile = "src/test/resources/db2/bootstrap/output.owl";
@@ -26,43 +13,7 @@ public class DB2BootstrapTest {
 
     @Test
     public void testBootstrap() throws Exception {
-        bootstrap();
-        loadGeneratedFiles();
-    }
-
-    private void loadGeneratedFiles() {
-
-        OntopOWLFactory factory = OntopOWLFactory.defaultFactory();
-        OntopSQLOWLAPIConfiguration config = OntopSQLOWLAPIConfiguration.defaultBuilder()
-                .ontologyFile(owlOutputFile)
-                .nativeOntopMappingFile(obdaOutputFile)
-                .propertyFile(propertyFile)
-                .enableTestMode()
-                .build();
-        try {
-            OntopOWLReasoner reasoner = factory.createReasoner(config);
-            OWLConnection conn = reasoner.getConnection();
-            OWLStatement st = conn.createStatement();
-        }
-        catch (OWLException e) {
-            throw new RuntimeException("Error occurred while loading bootstrapped files: " + e);
-        }
-    }
-
-    private void bootstrap() throws Exception {
-        OntopSQLOWLAPIConfiguration config = OntopSQLOWLAPIConfiguration.defaultBuilder()
-                .propertyFile(propertyFile)
-                .build();
-
-        DirectMappingBootstrapper bootstrapper = DirectMappingBootstrapper.defaultBootstrapper();
-        BootstrappingResults results = bootstrapper.bootstrap(config, baseIRI);
-
-        File obdaFile = new File(obdaOutputFile);
-        OntopNativeMappingSerializer writer = new OntopNativeMappingSerializer();
-        writer.write(obdaFile, results.getPPMapping());
-
-        File ontologyFile = new File(owlOutputFile);
-        OWLOntology onto = results.getOntology();
-        onto.getOWLOntologyManager().saveOntology(onto, new FileDocumentTarget(ontologyFile));
+        bootstrap(propertyFile, baseIRI, owlOutputFile, obdaOutputFile);
+        loadGeneratedFiles(owlOutputFile, obdaOutputFile, propertyFile);
     }
 }
