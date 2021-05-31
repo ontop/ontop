95847594fba316b6b6978cde9a6d55dacc21f876
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2021-05-31 11:53:09 +0200

    Minor import missing.

--- db/rdb/src/test/java/it/unibz/inf/ontop/dbschema/BasicViewWithConstraintsParsingTest.java
+++ db/rdb/src/test/java/it/unibz/inf/ontop/dbschema/BasicViewWithConstraintsParsingTest.java
@@ -1,5 +1,6 @@
 package it.unibz.inf.ontop.dbschema;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import it.unibz.inf.ontop.utils.ImmutableCollectors;
 import org.junit.Test;
