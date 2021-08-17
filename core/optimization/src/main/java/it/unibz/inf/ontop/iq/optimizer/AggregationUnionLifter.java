6b7722e8b9a89f003f2e9f9f5389d5a082a3705b
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2021-08-17 18:43:58 +0200

    Skeleton of AggregationUnionLifterImpl added.

---
+++ core/optimization/src/main/java/it/unibz/inf/ontop/iq/optimizer/AggregationUnionLifter.java
@@ -0,0 +1,7 @@
+package it.unibz.inf.ontop.iq.optimizer;
+
+/**
+ * Tries to lift (part of) the UNION above the aggregation node
+ */
+public interface AggregationUnionLifter  extends IQOptimizer {
+}
