cf670cb3c3e8956691ebd51c57157162368b3a95
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2021-12-16 09:43:50 +0100

    2 options never supported by the bootstrapper removed.

--- client/cli/src/main/java/it/unibz/inf/ontop/cli/AbstractOntopCommand.java
+++ client/cli/src/main/java/it/unibz/inf/ontop/cli/AbstractOntopCommand.java
@@ -12,16 +12,6 @@ abstract class AbstractOntopCommand implements OntopCommand {
     @BashCompletion(behaviour = CompletionBehaviour.FILENAMES)
     String propertiesFile;
 
-    @Option(type = OptionType.COMMAND, name = {"-d", "--db-metadata"}, title = "db-metadata file",
-            description = "User-supplied db-metadata file")
-    @BashCompletion(behaviour = CompletionBehaviour.FILENAMES)
-    String dbMetadataFile;
-
-    @Option(type = OptionType.COMMAND, name = {"-v", "--ontop-views"}, title = "Ontop view file",
-            description = "User-supplied view file")
-    @BashCompletion(behaviour = CompletionBehaviour.FILENAMES)
-    String ontopViewFile;
-
     @Option(type = OptionType.COMMAND, name = {"--db-password"}, title = "DB password",
             description = "DB password (overrides the properties)")
     @BashCompletion(behaviour = CompletionBehaviour.FILENAMES)
