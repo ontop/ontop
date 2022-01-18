55895547d279ef8c0a58fd126d63695613b885e4
kontchakov <roman@dcs.bbk.ac.uk>
2022-01-18 12:23:03 +0000

    dead code removal

--- binding/owlapi/src/test/java/it/unibz/inf/ontop/utils/OWLAPITestingTools.java
+++ binding/owlapi/src/test/java/it/unibz/inf/ontop/utils/OWLAPITestingTools.java
@@ -4,8 +4,6 @@ import com.google.inject.Injector;
 import it.unibz.inf.ontop.injection.OntopModelConfiguration;
 import it.unibz.inf.ontop.spec.ontology.owlapi.OWLAPITranslatorOWL2QL;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -26,25 +24,10 @@ public class OWLAPITestingTools {
         OWLAPI_TRANSLATOR = injector.getInstance(OWLAPITranslatorOWL2QL.class);
     }
 
-    private static String readFromFile(String filename) throws IOException {
-        FileReader reader = new FileReader(filename);
-        BufferedReader in = new BufferedReader(reader);
-        StringBuilder bf = new StringBuilder();
-        String line = in.readLine();
-        while (line != null) {
-            bf.append(line).append('\n');
-            line = in.readLine();
-        }
-        in.close();
-        return bf.toString();
-    }
-
-    private String readFile(String filePath) throws IOException {
-        return Files.lines(Paths.get(filePath)).collect(joining());
-    }
-
     public static void executeFromFile(Connection conn, String filename) throws SQLException, IOException {
-        String s = readFromFile(filename);
+        String s = Files
+                .lines(Paths.get(filename))
+                .collect(joining("\n"));
         try (Statement st = conn.createStatement()) {
             st.executeUpdate(s);
         }
