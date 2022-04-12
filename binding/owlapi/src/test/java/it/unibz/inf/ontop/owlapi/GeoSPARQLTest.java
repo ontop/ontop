7e70ef2aa9ebbe2445b1b80a632f69d1660c3303
apano-on <albulen.pano@unibz.it>
2022-04-12 11:04:58 +0200

    Preserve SRID with nested geosparql expressions

--- binding/owlapi/src/test/java/it/unibz/inf/ontop/owlapi/GeoSPARQLTest.java
+++ binding/owlapi/src/test/java/it/unibz/inf/ontop/owlapi/GeoSPARQLTest.java
@@ -20,6 +20,7 @@ import java.sql.DriverManager;
 
 import static it.unibz.inf.ontop.utils.OWLAPITestingTools.executeFromFile;
 import static junit.framework.TestCase.*;
+import static org.junit.Assert.assertEquals;
 
 public class GeoSPARQLTest {
 
@@ -1397,8 +1398,24 @@ public class GeoSPARQLTest {
                 "geo:asWKT ?w2 .\n" +
                 "BIND(geof:distance(?w1,?w2,uom:metre) as ?x) .\n" +
                 "} LIMIT 1\n";
-        Double val = runQueryAndReturnDoubleX(query);
-        assertEquals(111657.03929352836, val);
+        double val = runQueryAndReturnDoubleX(query);
+        assertEquals(111657.03929352836, val, 1);
+    }
+
+    @Test // ST_BUFFER as sub-argument of ST_WITHIN
+    public void testAskWithinBuffer() throws Exception {
+        String query = "PREFIX : <http://ex.org/> \n" +
+                "PREFIX geo: <http://www.opengis.net/ont/geosparql#>\n" +
+                "PREFIX geof: <http://www.opengis.net/def/function/geosparql/>\n" +
+                "PREFIX uom: <http://www.opengis.net/def/uom/OGC/1.0/>\n" +
+                "ASK WHERE {\n" +
+                "<http://ex.org/feature/1> a :Feature; geo:hasGeometry/geo:asWKT ?xWkt.\n" +
+                "<http://ex.org/feature/2> a :Feature; geo:hasGeometry/geo:asWKT ?yWkt.\n" +
+                "BIND(geof:buffer(?yWkt, 3500000, uom:metre) AS ?bWkt) .\n" +
+                "FILTER (geof:sfWithin(?xWkt, ?bWkt))\n" +
+                "}\n";
+        boolean val = runQueryAndReturnBooleanX(query);
+        assertTrue(val);
     }
 
     private boolean runQueryAndReturnBooleanX(String query) throws Exception {
