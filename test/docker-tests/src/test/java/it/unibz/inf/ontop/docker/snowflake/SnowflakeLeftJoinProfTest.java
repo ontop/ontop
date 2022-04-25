58db22e282fada90215c297d25634b46186773f5
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2022-04-25 14:05:43 +0200

    SnowflakeLeftJoinProfTest updated.

--- test/docker-tests/src/test/java/it/unibz/inf/ontop/docker/snowflake/SnowflakeLeftJoinProfTest.java
+++ test/docker-tests/src/test/java/it/unibz/inf/ontop/docker/snowflake/SnowflakeLeftJoinProfTest.java
@@ -1,5 +1,6 @@
 package it.unibz.inf.ontop.docker.snowflake;
 
+import com.google.common.collect.ImmutableList;
 import it.unibz.inf.ontop.docker.AbstractLeftJoinProfTest;
 import it.unibz.inf.ontop.owlapi.OntopOWLReasoner;
 import it.unibz.inf.ontop.owlapi.connection.OntopOWLConnection;
@@ -35,5 +36,35 @@ public class SnowflakeLeftJoinProfTest extends AbstractLeftJoinProfTest {
     public static void after() throws OWLException {
         CONNECTION.close();
         REASONER.dispose();
+    }
+
+    @Override
+    protected ImmutableList<String> getExpectedValuesAvgStudents1() {
+        return  ImmutableList.of("11.200000");
+    }
+
+    @Override
+    protected ImmutableList<String> getExpectedValuesAvgStudents2() {
+        return   ImmutableList.of("10.333333","12.000000", "13.000000");
+    }
+
+    @Override
+    protected ImmutableList<String> getExpectedValuesAvgStudents3() {
+        return ImmutableList.of("0", "0", "0", "0", "0", "10.333333","12.000000", "13.000000");
+    }
+
+    @Override
+    protected ImmutableList<String> getExpectedValuesDuration1() {
+        return ImmutableList.of("0", "0", "0", "0", "0", "18.00000", "20.00000", "84.50000");
+    }
+
+    @Override
+    protected ImmutableList<String> getExpectedValuesMultitypedSum1() {
+        return ImmutableList.of("31.00000", "32.00000", "115.50000");
+    }
+
+    @Override
+    protected ImmutableList<String> getExpectedValuesMultitypedAvg1() {
+        return ImmutableList.of("15.50000000000", "16.00000000000", "19.25000000000");
     }
 }
