d082155ad4c9ac067ee5bde94e9dca02c776052c
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2022-04-25 13:21:01 +0200

    Snowflake: VALUES needs to be wrapped where serialized.

---
+++ test/docker-tests/src/test/java/it/unibz/inf/ontop/docker/snowflake/SnowflakeLeftJoinProfTest.java
@@ -0,0 +1,39 @@
+package it.unibz.inf.ontop.docker.snowflake;
+
+import it.unibz.inf.ontop.docker.AbstractLeftJoinProfTest;
+import it.unibz.inf.ontop.owlapi.OntopOWLReasoner;
+import it.unibz.inf.ontop.owlapi.connection.OntopOWLConnection;
+import it.unibz.inf.ontop.owlapi.connection.OntopOWLStatement;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.semanticweb.owlapi.model.OWLException;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+
+@Ignore
+public class SnowflakeLeftJoinProfTest extends AbstractLeftJoinProfTest {
+
+    private static final String owlFileName = "/redundant_join/redundant_join_fk_test.owl";
+    private static final String obdaFileName = "/snowflake/redundant_join/redundant_join_fk_test.obda";
+    private static final String propertyFileName = "/snowflake/redundant_join/redundant_join_fk_test.properties";
+
+    private static OntopOWLReasoner REASONER;
+    private static OntopOWLConnection CONNECTION;
+
+    @BeforeClass
+    public static void before() throws OWLOntologyCreationException {
+        REASONER = createReasoner(owlFileName, obdaFileName, propertyFileName);
+        CONNECTION = REASONER.getConnection();
+    }
+
+    @Override
+    protected OntopOWLStatement createStatement() throws OWLException {
+        return CONNECTION.createStatement();
+    }
+
+    @AfterClass
+    public static void after() throws OWLException {
+        CONNECTION.close();
+        REASONER.dispose();
+    }
+}
