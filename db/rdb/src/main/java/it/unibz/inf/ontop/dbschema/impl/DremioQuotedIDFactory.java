834b7e25e54a264a2e4eee98facc1f5f7dce735c
DRovara <damianrovara@gmail.com>
2022-10-11 10:53:10 +0200

    DremioQuotedIDFactory no longer uses a builder, as it's not necessary for lists of fixed sizes

--- db/rdb/src/main/java/it/unibz/inf/ontop/dbschema/impl/DremioQuotedIDFactory.java
+++ db/rdb/src/main/java/it/unibz/inf/ontop/dbschema/impl/DremioQuotedIDFactory.java
@@ -23,17 +23,17 @@ public class DremioQuotedIDFactory extends SQLStandardQuotedIDFactory {
     @Override
     public RelationID createRelationID(String... components) {
         Objects.requireNonNull(components[components.length - 1]);
-        ImmutableList.Builder<QuotedID> builder = ImmutableList.builder();
 
-        builder.add(createFromString(components[components.length - 1]));
-
-        builder.add(createFromString(
+        QuotedID schemaId = createFromString(
                 String.join(".", Arrays.stream(components)
                         .limit(components.length - 1) //First (N-1) components are schema, last is table name
                         .map(name -> name.replace("\"", "")) //Remove quotes in-between
-                        .toArray(String[]::new)))
+                        .toArray(String[]::new))
         );
 
-        return new RelationIDImpl(builder.build());
+        return new RelationIDImpl(ImmutableList.of(
+                createFromString(components[components.length - 1]),
+                schemaId
+        ));
     }
 }
