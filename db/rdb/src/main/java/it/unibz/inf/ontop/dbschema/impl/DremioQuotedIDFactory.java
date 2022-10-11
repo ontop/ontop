1fecfbef7dbad5b5200c351cf8b5259c52a1be3d
DRovara <damianrovara@gmail.com>
2022-10-11 09:02:46 +0200

    Implemented DremioQuotedIDFactory so that it creates relation names with quotes around the whole schema

--- db/rdb/src/main/java/it/unibz/inf/ontop/dbschema/impl/DremioQuotedIDFactory.java
+++ db/rdb/src/main/java/it/unibz/inf/ontop/dbschema/impl/DremioQuotedIDFactory.java
@@ -1,6 +1,8 @@
 package it.unibz.inf.ontop.dbschema.impl;
 
+import com.google.common.collect.ImmutableList;
 import it.unibz.inf.ontop.dbschema.QuotedID;
+import it.unibz.inf.ontop.dbschema.RelationID;
 
 import javax.annotation.Nonnull;
 import java.util.Objects;
@@ -15,5 +17,25 @@ public class DremioQuotedIDFactory extends SQLStandardQuotedIDFactory {
             return new QuotedIDImpl(s.substring(1, s.length() - 1), QUOTATION_STRING, false);
 
         return new QuotedIDImpl(s, NO_QUOTATION, false);
+    }
+
+    @Override
+    public RelationID createRelationID(String... components) {
+        Objects.requireNonNull(components[components.length - 1]);
+        ImmutableList.Builder<QuotedID> builder = ImmutableList.builder();
+
+        builder.add(createFromString(components[components.length - 1]));
+
+        StringBuilder current = new StringBuilder();
+        for (int i = components.length - 2; i >= 0; i--) {
+            if (components[i] == null)
+                continue;
+            if(current.length() > 0)
+                current.insert(0, ".");
+            current.insert(0, components[i].replace("\"", ""));
+        }
+        builder.add(createFromString(current.toString()));
+
+        return new RelationIDImpl(builder.build());
     }
 }
