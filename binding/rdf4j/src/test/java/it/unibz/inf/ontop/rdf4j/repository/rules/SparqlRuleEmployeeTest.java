488eaeb399d1ff7005cd7f759e76743bb5d51fbb
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2022-10-27 12:21:49 +0200

    Tests added for WITH, USING, USING NAMED, FROM and FROM NAMED.

--- binding/rdf4j/src/test/java/it/unibz/inf/ontop/rdf4j/repository/rules/SparqlRuleEmployeeTest.java
+++ binding/rdf4j/src/test/java/it/unibz/inf/ontop/rdf4j/repository/rules/SparqlRuleEmployeeTest.java
@@ -1,5 +1,6 @@
 package it.unibz.inf.ontop.rdf4j.repository.rules;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import it.unibz.inf.ontop.rdf4j.repository.AbstractRDF4JTest;
 import org.junit.AfterClass;
@@ -26,7 +27,7 @@ public class SparqlRuleEmployeeTest extends AbstractRDF4JTest {
     }
 
     @Test
-    public void testEmployeeFullNames() {
+    public void testDefaultGraphEmployeeFullNames() {
         String query = "PREFIX : <http://employee.example.org/voc#>\n" +
                 "SELECT  ?v \n" +
                 "WHERE {\n" +
@@ -34,6 +35,30 @@ public class SparqlRuleEmployeeTest extends AbstractRDF4JTest {
                 " ?x :fullName ?v . \n" +
                 "}";
         runQueryAndCompare(query, ImmutableSet.of("Roger Smith", "Anna Gross"));
+    }
+
+    @Test
+    public void testCompany2EmployeeFullNames() {
+        String query = "PREFIX : <http://employee.example.org/voc#>\n" +
+                "SELECT  ?v \n" +
+                "FROM <http://employee.example.org/data/graph/company2>\n" +
+                "WHERE {\n" +
+                " ?x a :Employee . \n" +
+                " ?x :fullName ?v . \n" +
+                "}";
+        runQueryAndCompare(query, ImmutableSet.of("Papov Vlad", "Doe John"));
+    }
+
+    @Test
+    public void testCompany2EmployeeFullNamesNewGraph() {
+        String query = "PREFIX : <http://employee.example.org/voc#>\n" +
+                "SELECT  ?v \n" +
+                "FROM <http://employee.example.org/data/newGraph>\n" +
+                "WHERE {\n" +
+                " ?x a :Employee . \n" +
+                " ?x :fullName ?v . \n" +
+                "}";
+        runQueryAndCompare(query, ImmutableSet.of("Papov Vlad", "Doe John"));
     }
 
     @Test
@@ -84,6 +109,90 @@ public class SparqlRuleEmployeeTest extends AbstractRDF4JTest {
                 " ?event a :Event ; :label ?v ." +
                 "}";
         runQueryAndCompare(query, ImmutableSet.of("Deployment on Kubernetes", "CRM update"));
+    }
+
+    @Test
+    public void testNotAClass() {
+        String query = "PREFIX : <http://employee.example.org/voc#>\n" +
+                "SELECT  ?v \n" +
+                "WHERE {\n" +
+                " ?v a :NotAClass ." +
+                "}";
+        runQueryAndCompare(query, ImmutableSet.of());
+    }
+
+    @Test
+    public void testCapitalizedNames() {
+        String query = "PREFIX : <http://employee.example.org/voc#>\n" +
+                "SELECT  ?v \n" +
+                "WHERE {\n" +
+                " ?x :capitalizedName ?v . \n" +
+                "}";
+        runQueryAndCompare(query, ImmutableSet.of("PAPOV", "DOE"));
+    }
+
+    @Test
+    public void testLowerCaseNames() {
+        String query = "PREFIX : <http://employee.example.org/voc#>\n" +
+                "SELECT  ?v \n" +
+                "WHERE {\n" +
+                " ?x :lowerCaseName ?v . \n" +
+                "}";
+        runQueryAndCompare(query, ImmutableSet.of("taylor", "doe"));
+    }
+
+    /**
+     * NB: does not involve a rule, just testing FROM, making it removes duplicates
+     */
+    @Test
+    public void testCompany2Employees() {
+        String query = "PREFIX : <http://employee.example.org/voc#>\n" +
+                "SELECT  ?v \n" +
+                "FROM <http://employee.example.org/data/graph/company2>\n" +
+                "FROM <http://employee.example.org/data/graph/company2-source2>\n" +
+                "WHERE {\n" +
+                " ?x a :Employee .\n" +
+                " ?x :lastName ?v . \n" +
+                "}\n" +
+                "ORDER BY ?v";
+        runQueryAndCompare(query, ImmutableList.of("Doe", "Papov", "Taylor"));
+    }
+
+    /**
+     * NB: does not involve a rule, just testing FROM NAMED
+     */
+    @Test
+    public void testCompany2Employees2() {
+        String query = "PREFIX : <http://employee.example.org/voc#>\n" +
+                "SELECT  ?v \n" +
+                "FROM NAMED <http://employee.example.org/data/graph/company2>\n" +
+                "FROM NAMED <http://employee.example.org/data/graph/company2-source2>\n" +
+                "WHERE {\n" +
+                " GRAPH ?g { \n" +
+                "   ?x a :Employee .\n" +
+                "   ?x :lastName ?v . \n" +
+                "  }\n" +
+                "}\n" +
+                "ORDER BY ?v";
+        runQueryAndCompare(query, ImmutableList.of("Doe", "Doe", "Papov", "Taylor"));
+    }
+
+    /**
+     * NB: does not involve a rule, just testing FROM NAMED
+     */
+    @Test
+    public void testCompany2Employees3() {
+        String query = "PREFIX : <http://employee.example.org/voc#>\n" +
+                "SELECT  ?v \n" +
+                "FROM NAMED <http://employee.example.org/data/graph/company2-source2>\n" +
+                "WHERE {\n" +
+                " GRAPH ?g { \n" +
+                "   ?x a :Employee .\n" +
+                "   ?x :lastName ?v . \n" +
+                "  }\n" +
+                "}\n" +
+                "ORDER BY ?v";
+        runQueryAndCompare(query, ImmutableList.of("Doe", "Taylor"));
     }
 
 }
