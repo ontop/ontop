54f9f1c3f63fc4aaa21730b7a029484d516faf7f
Davide Lanti <davide.lanti@unibz.it>
2022-12-13 11:57:28 +0100

    Add cli tests (almost useless, need automationgit push)

--- client/cli/src/test/java/it/unibz/inf/ontop/cli/OntopBootstrapTest.java
+++ client/cli/src/test/java/it/unibz/inf/ontop/cli/OntopBootstrapTest.java
@@ -24,5 +24,43 @@ public class OntopBootstrapTest {
                 "-t", "src/test/resources/output/exampleBooks-bootstrapped.owl");
     }
 
+    @Test
+    public void testOntopBootstrapDMSingleSchema(){
+        String[] argv = {"bootstrap",
+                "-b", "http://www.example.org/",
+                "-d", "src/test/resources/single_schema/boot-conf.json",
+                "-p", "src/test/resources/single_schema/single_schema.properties",
+                "-m", "src/test/resources/single_schema/boot-single_schema.obda",
+                "-t", "src/test/resources/single_schema/boot-single_schema.owl"
+        };
+        Ontop.main(argv);
+    }
+
+    @Test
+    public void testOntopBootstrapMPSingleSchema(){
+        String[] argv = {"bootstrap",
+                "-b", "http://www.example.org/",
+                "-a",
+                "-d", "src/test/resources/single_schema/boot-conf.json",
+                "-p", "src/test/resources/single_schema/single_schema.properties",
+                "-m", "src/test/resources/single_schema/boot-single_schema.obda",
+                "-t", "src/test/resources/single_schema/boot-single_schema.owl",
+                "-w", "src/test/resources/single_schema/single_schema_workload.json"
+        };
+        Ontop.main(argv);
+    }
+
+    @Test
+    public void testMPBootstrapWorkload(){
+        String[] argv = {"bootstrap",
+                "-b", "http://www.example.org/",
+                "-a",
+                "-p", "src/test/resources/flights/flight_2.properties",
+                "-m", "src/test/resources/output/flight_2.obda",
+                "-t", "src/test/resources/output/flight_2.owl",
+                "-w", "src/test/resources/flights/flights_queries.json"
+        };
+        Ontop.main(argv);
+    }
 
 }
