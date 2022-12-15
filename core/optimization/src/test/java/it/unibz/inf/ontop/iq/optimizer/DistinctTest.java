caea0d4ce8a51113a30c7a397a236c03ecfb72c4
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2022-12-15 17:43:40 +0100

    More unit tests added to DistinctTest.

--- core/optimization/src/test/java/it/unibz/inf/ontop/iq/optimizer/DistinctTest.java
+++ core/optimization/src/test/java/it/unibz/inf/ontop/iq/optimizer/DistinctTest.java
@@ -1,14 +1,16 @@
 package it.unibz.inf.ontop.iq.optimizer;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import it.unibz.inf.ontop.iq.IQ;
 import it.unibz.inf.ontop.iq.IQTree;
-import it.unibz.inf.ontop.iq.node.ConstructionNode;
-import it.unibz.inf.ontop.iq.node.ExtensionalDataNode;
+import it.unibz.inf.ontop.iq.node.*;
 import it.unibz.inf.ontop.model.atom.DistinctVariableOnlyDataAtom;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
 
 import static it.unibz.inf.ontop.DependencyTestDBMetadata.PK_TABLE1_AR2;
 import static it.unibz.inf.ontop.OptimizationTestingTools.*;
@@ -62,6 +64,271 @@ public class DistinctTest {
                 IQ_FACTORY.createUnaryIQTree(
                         IQ_FACTORY.createDistinctNode(),
                         dataNode));
+
+        normalizeAndCompare(initialTree, expectedTree, projectionAtom);
+    }
+
+    @Test
+    public void testDistinctUnion1() {
+        DistinctVariableOnlyDataAtom projectionAtom = ATOM_FACTORY.getDistinctVariableOnlyDataAtom(ANS1_AR1_PREDICATE, A);
+
+        ExtensionalDataNode dataNode = IQ_FACTORY.createExtensionalDataNode(PK_TABLE1_AR2, ImmutableMap.of(1, B));
+
+        FilterNode filterNode = IQ_FACTORY.createFilterNode(TERM_FACTORY.getDBStartsWith(ImmutableList.of(B, ONE_STR)));
+
+        ConstructionNode constructionNode = IQ_FACTORY.createConstructionNode(projectionAtom.getVariables(),
+                SUBSTITUTION_FACTORY.getSubstitution(A, ONE_STR));
+
+        IQTree filterSubTree = IQ_FACTORY.createUnaryIQTree(filterNode, dataNode);
+        IQTree subTree1 = IQ_FACTORY.createUnaryIQTree(constructionNode, filterSubTree);
+
+        ValuesNode valuesNode = IQ_FACTORY.createValuesNode(ImmutableList.of(A),
+                ImmutableList.of(
+                        ImmutableList.of(TWO_STR),
+                        ImmutableList.of(TWO_STR),
+                        ImmutableList.of(THREE_STR)));
+
+        UnionNode unionNode = IQ_FACTORY.createUnionNode(projectionAtom.getVariables());
+        DistinctNode distinctNode = IQ_FACTORY.createDistinctNode();
+
+        IQTree initialTree = IQ_FACTORY.createUnaryIQTree(
+                distinctNode,
+                IQ_FACTORY.createNaryIQTree(
+                        unionNode,
+                        ImmutableList.of(subTree1, valuesNode)));
+
+        IQTree newSubTree1 = IQ_FACTORY.createUnaryIQTree(
+                constructionNode,
+                IQ_FACTORY.createUnaryIQTree(
+                        IQ_FACTORY.createSliceNode(0, 1),
+                        filterSubTree));
+
+        ValuesNode newValuesNode = IQ_FACTORY.createValuesNode(ImmutableList.of(A),
+                ImmutableList.of(
+                        ImmutableList.of(TWO_STR),
+                        ImmutableList.of(THREE_STR)));
+
+        IQTree expectedTree = IQ_FACTORY.createNaryIQTree(
+                        unionNode,
+                        ImmutableList.of(newSubTree1, newValuesNode));
+
+        normalizeAndCompare(initialTree, expectedTree, projectionAtom);
+    }
+
+    @Test
+    public void testDistinctUnion2() {
+        DistinctVariableOnlyDataAtom projectionAtom = ATOM_FACTORY.getDistinctVariableOnlyDataAtom(ANS1_AR1_PREDICATE, A);
+
+        ExtensionalDataNode dataNode = IQ_FACTORY.createExtensionalDataNode(PK_TABLE1_AR2, ImmutableMap.of(1, B));
+
+        FilterNode filterNode = IQ_FACTORY.createFilterNode(TERM_FACTORY.getDBStartsWith(ImmutableList.of(B, ONE_STR)));
+
+        ConstructionNode constructionNode = IQ_FACTORY.createConstructionNode(projectionAtom.getVariables(),
+                SUBSTITUTION_FACTORY.getSubstitution(
+                        A,
+                        // Non-deterministic -> blocks
+                        TERM_FACTORY.getDBRowUniqueStr()));
+
+        IQTree filterSubTree = IQ_FACTORY.createUnaryIQTree(filterNode, dataNode);
+        IQTree subTree1 = IQ_FACTORY.createUnaryIQTree(constructionNode, filterSubTree);
+
+        ValuesNode valuesNode = IQ_FACTORY.createValuesNode(ImmutableList.of(A),
+                ImmutableList.of(
+                        ImmutableList.of(TWO_STR),
+                        ImmutableList.of(TWO_STR),
+                        ImmutableList.of(THREE_STR)));
+
+        UnionNode unionNode = IQ_FACTORY.createUnionNode(projectionAtom.getVariables());
+        DistinctNode distinctNode = IQ_FACTORY.createDistinctNode();
+
+        IQTree initialTree = IQ_FACTORY.createUnaryIQTree(
+                distinctNode,
+                IQ_FACTORY.createNaryIQTree(
+                        unionNode,
+                        ImmutableList.of(subTree1, valuesNode)));
+
+        ValuesNode newValuesNode = IQ_FACTORY.createValuesNode(ImmutableList.of(A),
+                ImmutableList.of(
+                        ImmutableList.of(TWO_STR),
+                        ImmutableList.of(THREE_STR)));
+
+        IQTree expectedTree = IQ_FACTORY.createUnaryIQTree(
+                distinctNode,
+                IQ_FACTORY.createNaryIQTree(
+                        unionNode,
+                        ImmutableList.of(subTree1, newValuesNode)));
+
+        normalizeAndCompare(initialTree, expectedTree, projectionAtom);
+    }
+
+    @Test
+    public void testDistinctUnion3() {
+        DistinctVariableOnlyDataAtom projectionAtom = ATOM_FACTORY.getDistinctVariableOnlyDataAtom(ANS1_AR1_PREDICATE, A);
+
+        ExtensionalDataNode dataNode = IQ_FACTORY.createExtensionalDataNode(PK_TABLE1_AR2, ImmutableMap.of(1, B));
+
+        FilterNode subFilterNode = IQ_FACTORY.createFilterNode(TERM_FACTORY.getDBStartsWith(ImmutableList.of(B, ONE_STR)));
+
+        ConstructionNode constructionNode = IQ_FACTORY.createConstructionNode(projectionAtom.getVariables(),
+                SUBSTITUTION_FACTORY.getSubstitution(A, ONE_STR));
+
+        IQTree filterSubTree = IQ_FACTORY.createUnaryIQTree(subFilterNode, dataNode);
+        IQTree subTree1 = IQ_FACTORY.createUnaryIQTree(constructionNode, filterSubTree);
+
+        ValuesNode valuesNode = IQ_FACTORY.createValuesNode(ImmutableList.of(A),
+                ImmutableList.of(
+                        ImmutableList.of(TWO_STR),
+                        ImmutableList.of(TWO_STR),
+                        ImmutableList.of(THREE_STR)));
+
+        UnionNode unionNode = IQ_FACTORY.createUnionNode(projectionAtom.getVariables());
+
+        FilterNode topFilterNode = IQ_FACTORY.createFilterNode(
+                TERM_FACTORY.getDBNonStrictNumericEquality(TERM_FACTORY.getDBRand(UUID.randomUUID()), ONE));
+
+        DistinctNode distinctNode = IQ_FACTORY.createDistinctNode();
+
+        IQTree initialTree = IQ_FACTORY.createUnaryIQTree(
+                distinctNode,
+                IQ_FACTORY.createUnaryIQTree(
+                    topFilterNode,
+                    IQ_FACTORY.createNaryIQTree(
+                            unionNode,
+                            ImmutableList.of(subTree1, valuesNode))));
+
+        IQTree newSubTree1 = IQ_FACTORY.createUnaryIQTree(
+                constructionNode,
+                IQ_FACTORY.createUnaryIQTree(
+                        IQ_FACTORY.createSliceNode(0, 1),
+                        filterSubTree));
+
+        ValuesNode newValuesNode = IQ_FACTORY.createValuesNode(ImmutableList.of(A),
+                ImmutableList.of(
+                        ImmutableList.of(TWO_STR),
+                        ImmutableList.of(THREE_STR)));
+
+        IQTree expectedTree = IQ_FACTORY.createUnaryIQTree(
+                topFilterNode,
+                IQ_FACTORY.createNaryIQTree(
+                    unionNode,
+                    ImmutableList.of(newSubTree1, newValuesNode)));
+
+        normalizeAndCompare(initialTree, expectedTree, projectionAtom);
+    }
+
+    @Test
+    public void testDistinctUnion4() {
+        DistinctVariableOnlyDataAtom projectionAtom = ATOM_FACTORY.getDistinctVariableOnlyDataAtom(ANS1_AR1_PREDICATE, A);
+
+        ExtensionalDataNode dataNode = IQ_FACTORY.createExtensionalDataNode(PK_TABLE1_AR2, ImmutableMap.of(1, B));
+
+        FilterNode subFilterNode = IQ_FACTORY.createFilterNode(TERM_FACTORY.getDBStartsWith(ImmutableList.of(B, ONE_STR)));
+
+        ConstructionNode constructionNode = IQ_FACTORY.createConstructionNode(projectionAtom.getVariables(),
+                SUBSTITUTION_FACTORY.getSubstitution(A, ONE_STR));
+
+        IQTree filterSubTree = IQ_FACTORY.createUnaryIQTree(subFilterNode, dataNode);
+        IQTree subTree1 = IQ_FACTORY.createUnaryIQTree(constructionNode, filterSubTree);
+
+        ValuesNode valuesNode = IQ_FACTORY.createValuesNode(ImmutableList.of(A),
+                ImmutableList.of(
+                        ImmutableList.of(TWO_STR),
+                        ImmutableList.of(TWO_STR),
+                        ImmutableList.of(THREE_STR)));
+
+        UnionNode unionNode = IQ_FACTORY.createUnionNode(projectionAtom.getVariables());
+
+        FilterNode topFilterNode = IQ_FACTORY.createFilterNode(
+                TERM_FACTORY.getDBNonStrictNumericEquality(TERM_FACTORY.getDBRand(UUID.randomUUID()), ONE));
+
+        OrderByNode orderByNode = IQ_FACTORY.createOrderByNode(
+                ImmutableList.of(IQ_FACTORY.createOrderComparator(A, true)));
+
+        DistinctNode distinctNode = IQ_FACTORY.createDistinctNode();
+
+        IQTree initialTree = IQ_FACTORY.createUnaryIQTree(
+                distinctNode,
+                IQ_FACTORY.createUnaryIQTree(
+                    orderByNode,
+                    IQ_FACTORY.createUnaryIQTree(
+                            topFilterNode,
+                            IQ_FACTORY.createNaryIQTree(
+                                    unionNode,
+                                    ImmutableList.of(subTree1, valuesNode)))));
+
+        IQTree newSubTree1 = IQ_FACTORY.createUnaryIQTree(
+                constructionNode,
+                IQ_FACTORY.createUnaryIQTree(
+                        IQ_FACTORY.createSliceNode(0, 1),
+                        filterSubTree));
+
+        ValuesNode newValuesNode = IQ_FACTORY.createValuesNode(ImmutableList.of(A),
+                ImmutableList.of(
+                        ImmutableList.of(TWO_STR),
+                        ImmutableList.of(THREE_STR)));
+
+        IQTree expectedTree = IQ_FACTORY.createUnaryIQTree(
+                orderByNode,
+                IQ_FACTORY.createUnaryIQTree(
+                    topFilterNode,
+                    IQ_FACTORY.createNaryIQTree(
+                            unionNode,
+                            ImmutableList.of(newSubTree1, newValuesNode))));
+
+        normalizeAndCompare(initialTree, expectedTree, projectionAtom);
+    }
+
+    @Test
+    public void testDistinctUnion5() {
+        DistinctVariableOnlyDataAtom projectionAtom = ATOM_FACTORY.getDistinctVariableOnlyDataAtom(ANS1_AR1_PREDICATE, A);
+
+        ExtensionalDataNode dataNode = IQ_FACTORY.createExtensionalDataNode(PK_TABLE1_AR2, ImmutableMap.of(1, B));
+
+        FilterNode subFilterNode = IQ_FACTORY.createFilterNode(TERM_FACTORY.getDBStartsWith(ImmutableList.of(B, ONE_STR)));
+
+        ConstructionNode constructionNode = IQ_FACTORY.createConstructionNode(projectionAtom.getVariables(),
+                SUBSTITUTION_FACTORY.getSubstitution(A, ONE_STR));
+
+        IQTree filterSubTree = IQ_FACTORY.createUnaryIQTree(subFilterNode, dataNode);
+        IQTree subTree1 = IQ_FACTORY.createUnaryIQTree(constructionNode, filterSubTree);
+
+        ValuesNode valuesNode = IQ_FACTORY.createValuesNode(ImmutableList.of(A),
+                ImmutableList.of(
+                        ImmutableList.of(TWO_STR),
+                        ImmutableList.of(TWO_STR),
+                        ImmutableList.of(THREE_STR)));
+
+        UnionNode unionNode = IQ_FACTORY.createUnionNode(projectionAtom.getVariables());
+
+        OrderByNode orderByNode = IQ_FACTORY.createOrderByNode(
+                ImmutableList.of(IQ_FACTORY.createOrderComparator(A, true)));
+
+        DistinctNode distinctNode = IQ_FACTORY.createDistinctNode();
+
+        IQTree initialTree = IQ_FACTORY.createUnaryIQTree(
+                distinctNode,
+                IQ_FACTORY.createUnaryIQTree(
+                        orderByNode,
+                        IQ_FACTORY.createNaryIQTree(
+                                unionNode,
+                                ImmutableList.of(subTree1, valuesNode))));
+
+        IQTree newSubTree1 = IQ_FACTORY.createUnaryIQTree(
+                constructionNode,
+                IQ_FACTORY.createUnaryIQTree(
+                        IQ_FACTORY.createSliceNode(0, 1),
+                        filterSubTree));
+
+        ValuesNode newValuesNode = IQ_FACTORY.createValuesNode(ImmutableList.of(A),
+                ImmutableList.of(
+                        ImmutableList.of(TWO_STR),
+                        ImmutableList.of(THREE_STR)));
+
+        IQTree expectedTree = IQ_FACTORY.createUnaryIQTree(
+                orderByNode,
+                IQ_FACTORY.createNaryIQTree(
+                        unionNode,
+                        ImmutableList.of(newSubTree1, newValuesNode)));
 
         normalizeAndCompare(initialTree, expectedTree, projectionAtom);
     }
