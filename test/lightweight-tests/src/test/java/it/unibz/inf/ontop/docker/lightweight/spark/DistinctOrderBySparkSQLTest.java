837c458e02e4c1f0d306dabd0f2328a5098e92a7
apano-on <albulen.pano@unibz.it>
2023-02-07 15:44:06 +0100

    Test OrderBy With Distinct for Spark

---
+++ test/lightweight-tests/src/test/java/it/unibz/inf/ontop/docker/lightweight/spark/DistinctOrderBySparkSQLTest.java
@@ -0,0 +1,169 @@
+package it.unibz.inf.ontop.docker.lightweight.spark;
+
+import com.google.common.collect.ImmutableSet;
+import it.unibz.inf.ontop.docker.lightweight.AbstractDockerRDF4JTest;
+import it.unibz.inf.ontop.docker.lightweight.SparkSQLLightweightTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+@SparkSQLLightweightTest
+public class DistinctOrderBySparkSQLTest extends AbstractDockerRDF4JTest {
+
+    private static final String OWL_FILE = "/prof/prof.owl";
+    private static final String PROPERTIES_FILE = "/prof/spark/prof-spark.properties";
+    private static final String OBDA_FILE = "/prof/spark/prof-spark.obda";
+
+    @BeforeAll
+    public static void before() throws IOException, SQLException {
+        initOBDA(OBDA_FILE, OWL_FILE, PROPERTIES_FILE);
+    }
+
+    @AfterAll
+    public static void after() throws SQLException {
+        release();
+    }
+
+    @Test
+    public void testDistinctWithOrderBy1() {
+
+        String query =  "PREFIX : <http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#>\n" +
+                "\n" +
+                "SELECT DISTINCT ?v\n" +
+                "WHERE {\n" +
+                "   ?v :firstName ?n .\n" +
+                "} ORDER BY DESC(?v)";
+
+        executeAndCompareValues(query, ImmutableSet.of("<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/8>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/7>",
+                        "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/6>",
+                        "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/5>",
+                        "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/4>",
+                        "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/3>",
+                        "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/2>",
+                        "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/1>"));
+    }
+
+    @Test
+    public void testDistinctWithOrderBy2() {
+
+        String query =  "PREFIX : <http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#>\n" +
+                "\n" +
+                "SELECT DISTINCT ?v\n" +
+                "WHERE {\n" +
+                "   ?v :firstName ?n .\n" +
+                "} ORDER BY DESC(?n)";
+
+        executeAndCompareValues(query, ImmutableSet.of("<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/8>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/7>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/6>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/5>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/4>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/3>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/2>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/1>"));
+    }
+
+    @Test
+    public void testDistinctWithOrderBy3() {
+
+        String query =  "PREFIX : <http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#>\n" +
+                "\n" +
+                "SELECT DISTINCT ?v\n" +
+                "WHERE {\n" +
+                "   ?s :firstName ?v .\n" +
+                "} ORDER BY DESC(?s)";
+
+        executeAndCompareValues(query, ImmutableSet.of("\"Roger\"^^xsd:string", "\"Michael\"^^xsd:string",
+                "\"Mary\"^^xsd:string", "\"John\"^^xsd:string", "\"Johann\"^^xsd:string", "\"Frank\"^^xsd:string",
+                "\"Diego\"^^xsd:string", "\"Barbara\"^^xsd:string"));
+    }
+
+    @Test
+    public void testDistinctWithOrderBy4() {
+
+        String query =  "PREFIX : <http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#>\n" +
+                "\n" +
+                "SELECT DISTINCT ?v\n" +
+                "WHERE {\n" +
+                "   ?s :firstName ?v .\n" +
+                "} ORDER BY DESC(?v)";
+
+        executeAndCompareValues(query, ImmutableSet.of("\"Roger\"^^xsd:string", "\"Michael\"^^xsd:string",
+                "\"Mary\"^^xsd:string", "\"John\"^^xsd:string", "\"Johann\"^^xsd:string", "\"Frank\"^^xsd:string",
+                "\"Diego\"^^xsd:string", "\"Barbara\"^^xsd:string"));
+    }
+
+    @Test
+    public void testNoDistinctWithOrderBy1() {
+
+        String query =  "PREFIX : <http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#>\n" +
+                "\n" +
+                "SELECT ?v\n" +
+                "WHERE {\n" +
+                "   ?v :firstName ?n .\n" +
+                "} ORDER BY DESC(?v)";
+
+        executeAndCompareValues(query, ImmutableSet.of("<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/8>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/7>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/6>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/5>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/4>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/3>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/2>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/1>"));
+    }
+
+    @Test
+    public void testNoDistinctWithOrderBy2() {
+
+        String query =  "PREFIX : <http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#>\n" +
+                "\n" +
+                "SELECT ?v\n" +
+                "WHERE {\n" +
+                "   ?v :firstName ?n .\n" +
+                "} ORDER BY DESC(?n)";
+
+        executeAndCompareValues(query, ImmutableSet.of("<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/8>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/7>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/6>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/5>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/4>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/3>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/2>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/1>"));
+    }
+
+    @Test
+    public void testNoDistinctWithOrderBy3() {
+
+        String query =  "PREFIX : <http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#>\n" +
+                "\n" +
+                "SELECT ?v\n" +
+                "WHERE {\n" +
+                "   ?s :firstName ?v .\n" +
+                "} ORDER BY DESC(?s)";
+
+        executeAndCompareValues(query, ImmutableSet.of("\"Roger\"^^xsd:string", "\"Michael\"^^xsd:string",
+                "\"Mary\"^^xsd:string", "\"John\"^^xsd:string", "\"Johann\"^^xsd:string", "\"Frank\"^^xsd:string",
+                "\"Diego\"^^xsd:string", "\"Barbara\"^^xsd:string"));
+    }
+
+    @Test
+    public void testNoDistinctWithOrderBy4() {
+
+        String query =  "PREFIX : <http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#>\n" +
+                "\n" +
+                "SELECT ?v\n" +
+                "WHERE {\n" +
+                "   ?s :firstName ?v .\n" +
+                "} ORDER BY DESC(?v)";
+
+        executeAndCompareValues(query, ImmutableSet.of("\"Roger\"^^xsd:string", "\"Michael\"^^xsd:string",
+                "\"Mary\"^^xsd:string", "\"John\"^^xsd:string", "\"Johann\"^^xsd:string", "\"Frank\"^^xsd:string",
+                "\"Diego\"^^xsd:string", "\"Barbara\"^^xsd:string"));
+    }
+}
