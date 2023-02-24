9c58fb64028fabab1ccb38275063c33a57ce3bb9
kontchakov <roman@dcs.bbk.ac.uk>
2023-02-24 17:32:18 +0000

    javadocs

--- core/model/src/main/java/it/unibz/inf/ontop/substitution/Substitution.java
+++ core/model/src/main/java/it/unibz/inf/ontop/substitution/Substitution.java
@@ -133,7 +133,7 @@ public interface Substitution<T extends ImmutableTerm>  {
     /**
      * Returns the operations object that contains methods for dealing with
      * substitutions of the ImmutableTerm range.
-     * 
+     *
      * @return operations object
      */
 
@@ -159,44 +159,188 @@ public interface Substitution<T extends ImmutableTerm>  {
     default Substitution<ImmutableTerm> compose(Substitution<? extends ImmutableTerm> f) { return onImmutableTerms().compose(this, f); }
 
 
+    /**
+     * Restricts the domain to a given set of variables.
+     *
+     * @param set a set of variables
+     * @return new substitution
+     */
 
     Substitution<T> restrictDomainTo(Set<Variable> set);
 
+    /**
+     * Removes a given set of variables from the domain.
+     *
+     * @param set a set of variables
+     * @return new substitution
+     */
+
     Substitution<T> removeFromDomain(Set<Variable> set);
+
+    /**
+     * Restricts the range to the terms of a given type.
+     *
+     * @param <S> possibly a supertype of the new range type
+     * @param type a type literal
+     * @return new substitution
+     */
 
     <S extends ImmutableTerm> Substitution<S> restrictRangeTo(Class<? extends S> type);
 
 
     ImmutableMap<T, Collection<Variable>> inverseMap();
 
+    /**
+     * Returns a builder object to construct a new substitution on the basis of this.
+     *
+     * @return a builder
+     */
     Builder<T, ? extends Builder<T, ?>> builder();
 
+    /**
+     * Represents a series of transformation of a substitution into another substitution (or stream/map).
+     *
+     * @param <T> the range type
+     * @param <B> the builder type (to handle injectivity)
+     */
+
     interface Builder<T extends ImmutableTerm, B extends Builder<T, ? extends B>> {
+        /**
+         * Completes the transformation pipeline and constructs a substitution.
+         *
+         * @return the substitution
+         */
         Substitution<T> build();
 
+        /**
+         * Restricts the domain to a given set of variables.
+         *
+         * @param set a set of variables
+         * @return the builder object
+         */
         B restrictDomainTo(Set<Variable> set);
 
+        /**
+         * Removes a given set of variables from the domain.
+         *
+         * @param set a set of variables
+         * @return the builder object
+         */
         B removeFromDomain(Set<Variable> set);
 
+
+        /**
+         * Restricts the domain and range based on a binary predicate.
+         *
+         * @param predicate a predicate
+         * @return the builder object
+         */
         B restrict(BiPredicate<Variable, T> predicate);
 
+        /**
+         * Restricts the range to the terms satisfying a given predicate.
+         *
+         * @param predicate a predicate
+         * @return the builder object
+         */
         B restrictRange(Predicate<T> predicate);
 
+        /**
+         * Restricts the range to the terms of a given type.
+         *
+         * @param <S> possibly a supertype of the new range type
+         * @param type a type literal
+         * @return the builder object
+         */
         <S extends ImmutableTerm> Builder<S, ?> restrictRangeTo(Class<? extends S> type);
 
-        <U, S extends ImmutableTerm> Builder<S, ?> transform(Function<Variable, U> lookup, BiFunction<T, U, S> function);
-
+        /**
+         * Transforms the terms in the range according a given function
+         *
+         * @param function maps terms in the range to new range terms
+         * @return the builder object
+         * @param <S> type of the transformed range terms
+         */
         <S extends ImmutableTerm> Builder<S, ?> transform(Function<T, S> function);
 
+        /**
+         * Transforms the terms in the range according a given total look-up and function.
+         * The look-up function is assumed to be defined on the whole domain.
+         *
+         * @param lookup maps domain variables to values of the look-up type
+         * @param function maps terms in the range and look-up values to new range terms
+         * @return the builder object
+         * @param <U> type of the look-up objects
+         * @param <S> type of the transformed range terms
+         */
+        <U, S extends ImmutableTerm> Builder<S, ?> transform(Function<Variable, U> lookup, BiFunction<T, U, S> function);
+
+        /**
+         * Transforms the terms in the range according a given partial look-up and function.
+         * If the look-up function is undefined on a domain element, the old value
+         * is preserved.
+         *
+         * @param lookup maps domain variables to values of the look-up type
+         * @param function maps terms in the range and look-up values to new range terms
+         * @return the builder object
+         * @param <U> type of the look-up objects
+         */
         <U> Builder<T, ?> transformOrRetain(Function<Variable, U> lookup, BiFunction<T, U, T> function);
 
+        /**
+         * Transforms the terms in the range according a given partial look-up and function.
+         * If the look-up function is undefined on a domain element, the domain element and
+         * old its associated term are removed.
+         *
+         * @param lookup maps domain variables to values of the look-up type
+         * @param function maps look-up values to new range terms
+         * @return the builder object
+         * @param <U> type of the look-up objects
+         * @param <S> type of the transformed range terms
+         */
         <U, S extends ImmutableTerm> Builder<S, ?> transformOrRemove(Function<Variable, U> lookup, Function<U, S> function);
 
+        /**
+         * Transforms the terms in the range according a given partial look-up and function.
+         * If the look-up function is undefined on a domain element, the old value
+         * is preserved. If the look-up function is defined, then the single entry is replaced
+         * by a set of entries determined by the function.
+         *
+         * @param lookup maps domain variables to values of the look-up type
+         * @param function maps look-up values to replacing sets of entries (as a substitution)
+         * @return the builder object
+         * @param <U> type of the look-up objects
+         */
         <U> Builder<T, ?> flatTransform(Function<Variable, U> lookup, Function<U, Substitution<T>> function);
+
+        /**
+         * Completes the transformation pipeline and returns a stream
+         *
+         * @param transformer a function that maps variables and terms to stream elements
+         * @param <S> the type of stream elements
+         * @return the stream
+         */
 
         <S> Stream<S> toStream(BiFunction<Variable, T, S> transformer);
 
+        /**
+         * Completes the transformation pipeline and returns a map from variables to some values
+         *
+         * @param transformer a function that maps variables and terms to map values
+         * @param <S> the type of map values
+         * @return the map
+         */
+
         <S> ImmutableMap<Variable, S> toMap(BiFunction<Variable, T, S> transformer);
+
+        /**
+         * Completes the transformation pipeline and returns a map from variables to some values.
+         * Optional map values and the corresponding variables are removed from the map
+         *
+         * @param transformer a function that maps variables and terms to optional map values
+         * @param <S> the type of map values
+         * @return the map
+         */
 
         <S> ImmutableMap<Variable, S> toMapIgnoreOptional(BiFunction<Variable, T, Optional<S>> transformer);
     }
