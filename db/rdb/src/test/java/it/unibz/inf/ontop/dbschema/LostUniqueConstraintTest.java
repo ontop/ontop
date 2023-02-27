25adf5f9ced97afa11d173c2a01dd29791e3b55e
DRovara <damianrovara@gmail.com>
2023-02-27 09:49:56 +0100

    Created test that reproduces error

--- db/rdb/src/test/java/it/unibz/inf/ontop/dbschema/LostUniqueConstraintTest.java
+++ db/rdb/src/test/java/it/unibz/inf/ontop/dbschema/LostUniqueConstraintTest.java
@@ -10,25 +10,20 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class LostUniqueConstraintTest {
-    private static final String VIEW_FILE = "src/test/resources/lostUniqueConstraint/lost_lens.json";
+    private static final String LENS_FILE = "src/test/resources/lostUniqueConstraint/lost_lens.json";
     private static final String DBMETADATA_FILE = "src/test/resources/lostUniqueConstraint/lost.db-extract.json";
 
-    private final ImmutableSet<Lens> viewDefinitions = LensParsingTest.loadLensesH2(VIEW_FILE, DBMETADATA_FILE);
+    private final ImmutableSet<Lens> lensDefinitions = LensParsingTest.loadLensesH2(LENS_FILE, DBMETADATA_FILE);
 
     public LostUniqueConstraintTest() throws Exception {
     }
 
     @Test
     public void testPersonUniqueConstraint() {
-        ImmutableSet<String> constraints = viewDefinitions.stream()
-                .map(RelationDefinition::getUniqueConstraints)
-                .flatMap(Collection::stream)
-                .map(UniqueConstraint::getAttributes)
-                .flatMap(Collection::stream)
-                .map(v -> v.getID().getName())
-                .collect(ImmutableCollectors.toSet());
-
-        assertEquals(ImmutableSet.of("id", "id2"), constraints);
+        var first = lensDefinitions.stream().findFirst().get();
+        var second = lensDefinitions.stream().skip(1).findFirst().get();
+        assertEquals(1, first.getUniqueConstraints().size());
+        assertEquals(1, second.getUniqueConstraints().size());
     }
 
 }
