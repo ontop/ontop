1766caf0c6d850a8703dccad84233c0e5b10928a
DRovara <damianrovara@gmail.com>
2023-03-29 16:33:12 +0200

    Fixed an issue with dremio, where VALUES clauses with integer constants caused the query to fail

--- db/rdb/src/main/java/it/unibz/inf/ontop/generation/serializer/impl/DremioSelectFromWhereSerializer.java
+++ db/rdb/src/main/java/it/unibz/inf/ontop/generation/serializer/impl/DremioSelectFromWhereSerializer.java
@@ -1,15 +1,22 @@
 package it.unibz.inf.ontop.generation.serializer.impl;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import it.unibz.inf.ontop.dbschema.DBParameters;
 import it.unibz.inf.ontop.dbschema.QualifiedAttributeID;
+import it.unibz.inf.ontop.dbschema.QuotedID;
+import it.unibz.inf.ontop.dbschema.RelationID;
+import it.unibz.inf.ontop.generation.algebra.SQLValuesExpression;
 import it.unibz.inf.ontop.generation.algebra.SelectFromWhereWithModifiers;
 import it.unibz.inf.ontop.generation.serializer.SelectFromWhereSerializer;
 import it.unibz.inf.ontop.model.term.*;
 import it.unibz.inf.ontop.model.term.functionsymbol.db.impl.NullIgnoringDBGroupConcatFunctionSymbol;
 import it.unibz.inf.ontop.model.type.DBTermType;
+
+import java.util.stream.Collectors;
 
 @Singleton
 public class DremioSelectFromWhereSerializer extends DefaultSelectFromWhereSerializer implements SelectFromWhereSerializer {
@@ -36,6 +43,39 @@ public class DremioSelectFromWhereSerializer extends DefaultSelectFromWhereSeria
                     @Override
                     protected String serializeLimitOffset(long limit, long offset, boolean noSortCondition) {
                         return String.format("LIMIT %d OFFSET %d", limit, offset);
+                    }
+
+                    private String serializeValuesEntry(Constant constant, ImmutableMap<Variable, QualifiedAttributeID> childColumnIDs) {
+                        String serialization = sqlTermSerializer.serialize(constant, childColumnIDs);
+                        if(constant instanceof DBConstant && ((DBConstant) constant).getType().getCategory() == DBTermType.Category.INTEGER) {
+                            return String.format("CAST(%s as INTEGER)", serialization);
+                        }
+                        return serialization;
+                    }
+
+                    //Due to a limitation of dremio, we need to cast integer constants in VALUES terms to integers.
+                    @Override
+                    public QuerySerialization visit(SQLValuesExpression sqlValuesExpression) {
+                        ImmutableList<Variable> orderedVariables = sqlValuesExpression.getOrderedVariables();
+                        ImmutableMap<Variable, QuotedID> variableAliases = createVariableAliases(ImmutableSet.copyOf(orderedVariables));
+                        // Leaf node
+                        ImmutableMap<Variable, QualifiedAttributeID> childColumnIDs = ImmutableMap.of();
+
+                        String tuplesSerialized = sqlValuesExpression.getValues().stream()
+                                .map(tuple -> tuple.stream()
+                                        .map(constant -> serializeValuesEntry(constant, childColumnIDs))
+                                        .collect(Collectors.joining(",", " (", ")")))
+                                .collect(Collectors.joining(","));
+                        RelationID alias = generateFreshViewAlias();
+                        String internalColumnNames = orderedVariables.stream()
+                                .map(variableAliases::get)
+                                .map(QuotedID::toString)
+                                .collect(Collectors.joining(",", " (", ")"));
+                        String sql = "(VALUES " + tuplesSerialized + ") AS " + alias + internalColumnNames;
+
+                        ImmutableMap<Variable, QualifiedAttributeID> columnIDs = attachRelationAlias(alias, variableAliases);
+
+                        return new QuerySerializationImpl(sql, columnIDs);
                     }
 
                     //                    @Override
