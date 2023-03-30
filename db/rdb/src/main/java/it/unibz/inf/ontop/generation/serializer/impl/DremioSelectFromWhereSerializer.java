5d5594244a8b7e6b8abb1dae32adbd5bc77685b2
DRovara <damianrovara@gmail.com>
2023-03-30 09:08:45 +0200

    Cleaned up code for DremioSelectFromWhereSerializer to minimize copied code

--- db/rdb/src/main/java/it/unibz/inf/ontop/generation/serializer/impl/DremioSelectFromWhereSerializer.java
+++ db/rdb/src/main/java/it/unibz/inf/ontop/generation/serializer/impl/DremioSelectFromWhereSerializer.java
@@ -45,37 +45,14 @@ public class DremioSelectFromWhereSerializer extends DefaultSelectFromWhereSeria
                         return String.format("LIMIT %d OFFSET %d", limit, offset);
                     }
 
-                    private String serializeValuesEntry(Constant constant, ImmutableMap<Variable, QualifiedAttributeID> childColumnIDs) {
+                    //Due to a limitation of dremio, we need to cast integer constants in VALUES terms to integers, as they would be types as int64 otherwise.
+                    @Override
+                    protected String serializeValuesEntry(Constant constant, ImmutableMap<Variable, QualifiedAttributeID> childColumnIDs) {
                         String serialization = sqlTermSerializer.serialize(constant, childColumnIDs);
                         if(constant instanceof DBConstant && ((DBConstant) constant).getType().getCategory() == DBTermType.Category.INTEGER) {
                             return String.format("CAST(%s as INTEGER)", serialization);
                         }
                         return serialization;
-                    }
-
-                    //Due to a limitation of dremio, we need to cast integer constants in VALUES terms to integers.
-                    @Override
-                    public QuerySerialization visit(SQLValuesExpression sqlValuesExpression) {
-                        ImmutableList<Variable> orderedVariables = sqlValuesExpression.getOrderedVariables();
-                        ImmutableMap<Variable, QuotedID> variableAliases = createVariableAliases(ImmutableSet.copyOf(orderedVariables));
-                        // Leaf node
-                        ImmutableMap<Variable, QualifiedAttributeID> childColumnIDs = ImmutableMap.of();
-
-                        String tuplesSerialized = sqlValuesExpression.getValues().stream()
-                                .map(tuple -> tuple.stream()
-                                        .map(constant -> serializeValuesEntry(constant, childColumnIDs))
-                                        .collect(Collectors.joining(",", " (", ")")))
-                                .collect(Collectors.joining(","));
-                        RelationID alias = generateFreshViewAlias();
-                        String internalColumnNames = orderedVariables.stream()
-                                .map(variableAliases::get)
-                                .map(QuotedID::toString)
-                                .collect(Collectors.joining(",", " (", ")"));
-                        String sql = "(VALUES " + tuplesSerialized + ") AS " + alias + internalColumnNames;
-
-                        ImmutableMap<Variable, QualifiedAttributeID> columnIDs = attachRelationAlias(alias, variableAliases);
-
-                        return new QuerySerializationImpl(sql, columnIDs);
                     }
 
                     //                    @Override
