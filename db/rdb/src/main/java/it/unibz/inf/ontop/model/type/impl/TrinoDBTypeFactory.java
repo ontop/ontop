1ea1a6c45bff557c58cfd78ddf0ab9c8af7612d4
DRovara <damianrovara@gmail.com>
2023-04-03 11:59:43 +0200

    Implemented FlattenLens for Athena

--- db/rdb/src/main/java/it/unibz/inf/ontop/model/type/impl/TrinoDBTypeFactory.java
+++ db/rdb/src/main/java/it/unibz/inf/ontop/model/type/impl/TrinoDBTypeFactory.java
@@ -15,7 +15,6 @@ import java.util.regex.Pattern;
 
 import static it.unibz.inf.ontop.model.type.DBTermType.Category.DECIMAL;
 import static it.unibz.inf.ontop.model.type.impl.NonStringNonNumberNonBooleanNonDatetimeDBTermType.StrictEqSupport.NOTHING;
-import static it.unibz.inf.ontop.model.type.impl.NonStringNonNumberNonBooleanNonDatetimeDBTermType.StrictEqSupport.SAME_TYPE_NO_CONSTANT;
 
 public class TrinoDBTypeFactory extends DefaultSQLDBTypeFactory {
     protected static final String INT2_STR = "INT2";
@@ -38,25 +37,31 @@ public class TrinoDBTypeFactory extends DefaultSQLDBTypeFactory {
 
     @AssistedInject
     protected TrinoDBTypeFactory(@Assisted TermType rootTermType, @Assisted TypeFactory typeFactory) {
-        super(createTrinoTypeMap(rootTermType, typeFactory), createTrinoCodeMap(), createGenericAbstractTypeMap(rootTermType, typeFactory));
+        super(createTrinoTypeMap(rootTermType, typeFactory), createTrinoCodeMap(), createGenericAbstractTypeMap(rootTermType, typeFactory, "()"));
     }
 
-    private static ImmutableList<GenericDBTermType> createGenericAbstractTypeMap(TermType rootTermType, TypeFactory typeFactory) {
+    protected TrinoDBTypeFactory(TermType rootTermType, TypeFactory typeFactory, ImmutableList<GenericDBTermType> genericAbstractTypeMap) {
+        super(createTrinoTypeMap(rootTermType, typeFactory), createTrinoCodeMap(), genericAbstractTypeMap);
+    }
+
+    protected static ImmutableList<GenericDBTermType> createGenericAbstractTypeMap(TermType rootTermType, TypeFactory typeFactory, String brackets) {
         TermTypeAncestry rootAncestry = rootTermType.getAncestry();
+        final char openingBracket = brackets.charAt(0);
+        final char closingBracket = brackets.charAt(1);
 
         GenericDBTermType abstractArrayType = new ArrayDBTermType(ARRAY_STR, rootAncestry, s -> {
             if(s.equals("ARRAY"))
                 return Optional.of(typeFactory.getDBTypeFactory().getDBStringType());
-            if(!s.startsWith("ARRAY(") || !s.endsWith(")")) {
+            if(!s.startsWith("ARRAY" + openingBracket) || !s.endsWith(closingBracket + "")) {
                 return Optional.empty();
             }
             String contents = s.substring(6, s.length() - 1);
 
             int depth = 0;
             for(int i = 0; i < contents.length(); i++) {
-                if(contents.charAt(i) == '(')
+                if(contents.charAt(i) == openingBracket)
                     depth += 1;
-                else if(contents.charAt(i) == ')')
+                else if(contents.charAt(i) == closingBracket)
                     depth -= 1;
                 else if(contents.charAt(i) == ',' && depth == 0)
                     return Optional.empty();
