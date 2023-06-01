0d4ba6a7df1976ba484a6b94153fe2078e6c27b3
DRovara <damianrovara@gmail.com>
2023-06-01 09:41:11 +0200

    Possibly fixed some failing test cases

--- db/rdb/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/db/impl/H2SQLDBFunctionSymbolFactory.java
+++ db/rdb/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/db/impl/H2SQLDBFunctionSymbolFactory.java
@@ -343,4 +343,16 @@ public class H2SQLDBFunctionSymbolFactory extends AbstractSQLDBFunctionSymbolFac
                         "END",
                 term);
     }
+
+    @Override
+    protected String serializeDateTrunc(ImmutableList<? extends ImmutableTerm> terms,
+                                        Function<ImmutableTerm, String> termConverter, TermFactory termFactory) {
+        String template = String.format(" WHEN %s LIKE '%%s' THEN DATETRUNC(%%s, %s)", termConverter.apply(terms.get(1)), termConverter.apply(terms.get(0)));
+        ImmutableList<String> possibleParts = ImmutableList.of("millennium", "century", "decade", "year", "quarter", "month", "day", "week", "hour", "minute", "second", "millisecond", "microsecond");
+        StringBuilder serializationBuilder = new StringBuilder("CASE");
+        possibleParts.stream()
+                .forEach(part -> serializationBuilder.append(String.format(template, part, part)));
+        serializationBuilder.append(" ELSE NULL END");
+        return serializationBuilder.toString();
+    }
 }
