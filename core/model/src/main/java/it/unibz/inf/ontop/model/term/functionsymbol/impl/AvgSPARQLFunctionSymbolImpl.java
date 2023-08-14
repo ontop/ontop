2f119f89213baef85942e870d827e0a7d6643813
DRovara <damianrovara@gmail.com>
2023-08-14 17:43:26 +0200

    Removed unneeded imports

--- core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/AvgSPARQLFunctionSymbolImpl.java
+++ core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/AvgSPARQLFunctionSymbolImpl.java
@@ -1,26 +1,7 @@
 package it.unibz.inf.ontop.model.term.functionsymbol.impl;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Maps;
-import it.unibz.inf.ontop.iq.node.VariableNullability;
-import it.unibz.inf.ontop.iq.request.DefinitionPushDownRequest;
-import it.unibz.inf.ontop.model.term.*;
-import it.unibz.inf.ontop.model.term.functionsymbol.InequalityLabel;
-import it.unibz.inf.ontop.model.type.ConcreteNumericRDFDatatype;
-import it.unibz.inf.ontop.model.type.DBTermType;
 import it.unibz.inf.ontop.model.type.RDFTermType;
-import it.unibz.inf.ontop.model.type.TypeFactory;
 import it.unibz.inf.ontop.model.vocabulary.SPARQL;
-import it.unibz.inf.ontop.model.vocabulary.XSD;
-import it.unibz.inf.ontop.substitution.Substitution;
-import it.unibz.inf.ontop.utils.ImmutableCollectors;
-import it.unibz.inf.ontop.utils.VariableGenerator;
-
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 
 public class AvgSPARQLFunctionSymbolImpl extends AbstractStatisticalSPARQLAggregationFunctionSymbolImpl {
