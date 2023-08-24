b769258c40dfe532a1dbbb1ee166f72952115a01
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2023-08-24 18:21:47 +0200

    OntopEndpointWithAccessControlInMappingTest enriched.

--- client/cli/src/test/java/it/unibz/inf/ontop/cli/OntopEndpointWithAccessControlInMappingTest.java
+++ client/cli/src/test/java/it/unibz/inf/ontop/cli/OntopEndpointWithAccessControlInMappingTest.java
@@ -85,6 +85,59 @@ public class OntopEndpointWithAccessControlInMappingTest {
     }
 
     @Test
+    public void testAsReaderAndLibrarian1() {
+        String sparqlEndpoint = "http://localhost:" + PORT + "/sparql";
+        SPARQLRepository repo = new SPARQLRepository(sparqlEndpoint);
+        repo.setAdditionalHttpHeaders(
+                ImmutableMap.of(
+                        "x-user", "joe",
+                        "x-roles", "librarian,reader"
+                        ));
+        repo.init();
+
+        String queryString = "PREFIX : <http://meraka/moss/exampleBooks.owl#>\n" +
+                "SELECT ?x ?edition\n" +
+                "WHERE { ?x a :Book; :hasEdition ?z.\n" +
+                "\t\t ?z a :SpecialEdition; :editionNumber ?edition\n" +
+                "}";
+
+        try (RepositoryConnection conn = repo.getConnection()) {
+            TupleQuery tupleQuery = conn.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
+            try (TupleQueryResult result = tupleQuery.evaluate()) {
+                long count = result.stream().count();
+                assertEquals(7, count);
+            }
+        }
+    }
+
+    @Test
+    public void testAsReaderAndLibrarian2() {
+        String sparqlEndpoint = "http://localhost:" + PORT + "/sparql";
+        SPARQLRepository repo = new SPARQLRepository(sparqlEndpoint);
+        repo.setAdditionalHttpHeaders(
+                ImmutableMap.of(
+                        "x-user", "joe",
+                        "x-roles", "reader",
+                        "x-groups", "librarian"
+                ));
+        repo.init();
+
+        String queryString = "PREFIX : <http://meraka/moss/exampleBooks.owl#>\n" +
+                "SELECT ?x ?edition\n" +
+                "WHERE { ?x a :Book; :hasEdition ?z.\n" +
+                "\t\t ?z a :SpecialEdition; :editionNumber ?edition\n" +
+                "}";
+
+        try (RepositoryConnection conn = repo.getConnection()) {
+            TupleQuery tupleQuery = conn.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
+            try (TupleQueryResult result = tupleQuery.evaluate()) {
+                long count = result.stream().count();
+                assertEquals(7, count);
+            }
+        }
+    }
+
+    @Test
     public void testAsAnonymous() {
         String sparqlEndpoint = "http://localhost:" + PORT + "/sparql";
         SPARQLRepository repo = new SPARQLRepository(sparqlEndpoint);
@@ -101,6 +154,50 @@ public class OntopEndpointWithAccessControlInMappingTest {
             try (TupleQueryResult result = tupleQuery.evaluate()) {
                 long count = result.stream().count();
                 assertEquals(0, count);
+            }
+        }
+    }
+
+    @Test
+    public void testAsRoger1() {
+        String sparqlEndpoint = "http://localhost:" + PORT + "/sparql";
+        SPARQLRepository repo = new SPARQLRepository(sparqlEndpoint);
+        repo.setAdditionalHttpHeaders(ImmutableMap.of("x-user", "roger"));
+        repo.init();
+
+        try (RepositoryConnection conn = repo.getConnection()) {
+            String queryString = "PREFIX : <http://meraka/moss/exampleBooks.owl#>\n" +
+                    "SELECT ?x ?edition\n" +
+                    "WHERE { ?x a :Book; :hasEdition ?z.\n" +
+                    "\t\t ?z a :SpecialEdition; :editionNumber ?edition\n" +
+                    "}";
+
+            TupleQuery tupleQuery = conn.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
+            try (TupleQueryResult result = tupleQuery.evaluate()) {
+                long count = result.stream().count();
+                assertEquals(7, count);
+            }
+        }
+    }
+
+    @Test
+    public void testAsRoger2() {
+        String sparqlEndpoint = "http://localhost:" + PORT + "/sparql";
+        SPARQLRepository repo = new SPARQLRepository(sparqlEndpoint);
+        repo.setAdditionalHttpHeaders(ImmutableMap.of("x-user", "roger", "x-roles", "reader,fan"));
+        repo.init();
+
+        try (RepositoryConnection conn = repo.getConnection()) {
+            String queryString = "PREFIX : <http://meraka/moss/exampleBooks.owl#>\n" +
+                    "SELECT ?x ?edition\n" +
+                    "WHERE { ?x a :Book; :hasEdition ?z.\n" +
+                    "\t\t ?z a :SpecialEdition; :editionNumber ?edition\n" +
+                    "}";
+
+            TupleQuery tupleQuery = conn.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
+            try (TupleQueryResult result = tupleQuery.evaluate()) {
+                long count = result.stream().count();
+                assertEquals(7, count);
             }
         }
     }
