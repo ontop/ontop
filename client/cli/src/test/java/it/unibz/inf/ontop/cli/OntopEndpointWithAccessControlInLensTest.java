619750af8a92aadd61e1e31c1f464cfae8828cec
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2023-08-24 18:36:02 +0200

    Test added for access-control in the lenses.

--- client/cli/src/test/java/it/unibz/inf/ontop/cli/OntopEndpointWithAccessControlInLensTest.java
+++ client/cli/src/test/java/it/unibz/inf/ontop/cli/OntopEndpointWithAccessControlInLensTest.java
@@ -5,25 +5,26 @@ import org.junit.ClassRule;
 import org.junit.rules.ExternalResource;
 
 
-public class OntopEndpointWithAccessControlInMappingTest extends AbstractOntopEndpointWithAccessControlTest {
+public class OntopEndpointWithAccessControlInLensTest extends AbstractOntopEndpointWithAccessControlTest {
 
     @ClassRule
     public static ExternalResource h2Connection = new H2ExternalResourceForBookExample();
-    private static final String PORT = "29889";
+    private static final String PORT = "29890";
     private static final String DBURL = "jdbc:h2:tcp://localhost:19123/./src/test/resources/h2/books.h2;ACCESS_MODE_DATA=r";
     private static final String DBUSER = "sa";
     private static final String DBPASSWORD = "test";
 
-    public OntopEndpointWithAccessControlInMappingTest() {
+    public OntopEndpointWithAccessControlInLensTest() {
         super(PORT);
     }
 
 
     @BeforeClass
     public static void setupEndpoint() {
-        Ontop.main("endpoint", "-m", "src/test/resources/books/exampleBooks-access-control-in-mapping.obda",
+        Ontop.main("endpoint", "-m", "src/test/resources/books/exampleBooks-access-control-in-lens.obda",
                 "-p", "src/test/resources/books/exampleBooks-access-control.properties",
                 "-t", "src/test/resources/books/exampleBooks.owl",
+                "-l", "src/test/resources/books/exampleBooks-access-control-lenses.json",
                 "--db-url=" + DBURL,
                 "--db-user=" + DBUSER,
                 "--db-password=" + DBPASSWORD,
