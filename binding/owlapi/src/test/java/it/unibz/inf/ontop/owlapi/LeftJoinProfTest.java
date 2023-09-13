09b0d9910cbf6e78b63522bf6221663ef2dddc36
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2023-09-13 08:48:22 +0200

    Test with currently unsupported query added.

--- binding/owlapi/src/test/java/it/unibz/inf/ontop/owlapi/LeftJoinProfTest.java
+++ binding/owlapi/src/test/java/it/unibz/inf/ontop/owlapi/LeftJoinProfTest.java
@@ -1285,6 +1285,25 @@ public class LeftJoinProfTest extends AbstractOWLAPITest {
         checkReturnedValues(querySelect, "v", expectedValues);
     }
 
+    @Test
+    public void testOrderByDistinct() throws Exception {
+
+        String query =  "PREFIX : <http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#>\n" +
+                "\n" +
+                "SELECT DISTINCT ?v \n" +
+                "WHERE {\n" +
+                "   ?v :teaches ?c .\n" +
+                "   ?v :lastName ?l .\n" +
+                "}" +
+                "ORDER BY ?l";
+
+        String [] expectedValues = {
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/3>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/8>",
+                "<http://www.semanticweb.org/user/ontologies/2016/8/untitled-ontology-84#professor/1>"};
+        String sql = checkReturnedValuesAndReturnSql(query, "v", Arrays.asList(expectedValues));
+    }
+
     private static boolean containsMoreThanOneOccurrence(String query, String pattern) {
         int firstOccurrenceIndex = query.indexOf(pattern);
         if (firstOccurrenceIndex >= 0) {
