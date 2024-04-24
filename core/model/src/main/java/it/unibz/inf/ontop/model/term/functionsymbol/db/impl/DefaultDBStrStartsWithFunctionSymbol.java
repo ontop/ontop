acda6d5a96f15742a767f5b9b621d72df1cd5845
ManuelaCorte <manu.cortepause@gmail.com>
2024-04-24 11:08:02 +0200

    Optimizations for strstartswith function

--- core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/db/impl/DefaultDBStrStartsWithFunctionSymbol.java
+++ core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/db/impl/DefaultDBStrStartsWithFunctionSymbol.java
@@ -1,9 +1,9 @@
 package it.unibz.inf.ontop.model.term.functionsymbol.db.impl;
 
 import com.google.common.collect.ImmutableList;
-import it.unibz.inf.ontop.model.term.ImmutableExpression;
-import it.unibz.inf.ontop.model.term.ImmutableTerm;
-import it.unibz.inf.ontop.model.term.TermFactory;
+import it.unibz.inf.ontop.iq.node.VariableNullability;
+import it.unibz.inf.ontop.model.term.*;
+import it.unibz.inf.ontop.model.term.functionsymbol.db.IRIStringTemplateFunctionSymbol;
 import it.unibz.inf.ontop.model.type.DBTermType;
 
 import java.util.function.Function;
@@ -32,7 +32,6 @@ public class DefaultDBStrStartsWithFunctionSymbol extends DBBooleanFunctionSymbo
     public String getNativeDBString(ImmutableList<? extends ImmutableTerm> terms,
                                     Function<ImmutableTerm, String> termConverter, TermFactory termFactory) {
         ImmutableTerm secondTerm = terms.get(1);
-
         // TODO: use a non-strict equality
         return termConverter.apply(
                 termFactory.getStrictEquality(
@@ -41,6 +40,33 @@ public class DefaultDBStrStartsWithFunctionSymbol extends DBBooleanFunctionSymbo
                                 termFactory.getDBIntegerConstant(1),
                                 termFactory.getDBCharLength(secondTerm)),
                         secondTerm));
+    }
+
+    @Override
+    protected ImmutableTerm buildTermAfterEvaluation(ImmutableList<ImmutableTerm> newTerms, TermFactory termFactory, VariableNullability variableNullability) {
+        if (newTerms.get(1) instanceof DBConstant) {
+            DBConstant pattern = (DBConstant) newTerms.get(1);
+
+            if (newTerms.get(0) instanceof DBConstant) {
+                DBConstant value = (DBConstant) newTerms.get(0);
+                return value.getValue().startsWith(pattern.getValue())
+                        ? termFactory.getTrueOrNullFunctionalTerm(ImmutableList.of(termFactory.getDBIsNotNull(value)))
+                        : termFactory.getFalseOrNullFunctionalTerm(ImmutableList.of(termFactory.getDBIsNull(value)));
+            }
+
+            if (newTerms.get(0) instanceof ImmutableFunctionalTerm) {
+                ImmutableFunctionalTerm value = (ImmutableFunctionalTerm) newTerms.get(0);
+                if (value.getFunctionSymbol() instanceof IRIStringTemplateFunctionSymbol) {
+                    IRIStringTemplateFunctionSymbol iriTemplate = (IRIStringTemplateFunctionSymbol) value.getFunctionSymbol();
+                    return iriTemplate.getTemplate().startsWith(pattern.getValue())
+                            ? termFactory.getTrueOrNullFunctionalTerm(ImmutableList.of(termFactory.getDBIsNotNull(value)))
+                            : termFactory.getFalseOrNullFunctionalTerm(ImmutableList.of(termFactory.getDBIsNull(value)));
+                }
+
+            }
+        }
+
+        return super.buildTermAfterEvaluation(newTerms, termFactory, variableNullability);
     }
 
     @Override
