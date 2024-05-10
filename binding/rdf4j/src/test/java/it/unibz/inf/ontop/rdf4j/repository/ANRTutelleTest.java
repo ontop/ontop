04cbf92b1d61ae284c43ba4bc93b3423db14e975
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2024-05-10 17:33:42 +0200

    4 tests added to ANRTutelleTest. 2 not passed.

--- binding/rdf4j/src/test/java/it/unibz/inf/ontop/rdf4j/repository/ANRTutelleTest.java
+++ binding/rdf4j/src/test/java/it/unibz/inf/ontop/rdf4j/repository/ANRTutelleTest.java
@@ -24,8 +24,9 @@ public class ANRTutelleTest extends AbstractRDF4JTest {
     public static void after() throws SQLException {
         release();
     }
+
     @Test
-    public void testQuery() {
+    public void testLJ1() {
         var query = "prefix ex: <http://example.org/>\n" +
                 "\n" +
                 "select * where {\n" +
@@ -36,6 +37,66 @@ public class ANRTutelleTest extends AbstractRDF4JTest {
                 "}";
         int count = runQueryAndCount(query);
         assertEquals(2, count);
+
+        var sql = reformulateIntoNativeQuery(query).toLowerCase();
+        assertFalse("The left-join should have been optimized out:\n " + sql,sql.contains("left"));
+    }
+
+    @Test
+    public void testLJ2() {
+        var query = "prefix ex: <http://example.org/>\n" +
+                "\n" +
+                "select * where {\n" +
+                "  ?t a ex:Tutelle .\n" +
+                "  OPTIONAL { \n" +
+                "     ?t ex:category ?cat . \n" +
+                "    # OPTIONAL { \n" +
+                "     ?cat ex:name ?name \n" +
+                "    #}\n" +
+                "  }\n" +
+                "}";
+        int count = runQueryAndCount(query);
+        assertEquals(1, count);
+
+        var sql = reformulateIntoNativeQuery(query).toLowerCase();
+        assertFalse("The left-join should have been optimized out:\n " + sql,sql.contains("left"));
+    }
+
+    @Test
+    public void testLJ3() {
+        var query = "prefix ex: <http://example.org/>\n" +
+                "\n" +
+                "select * where {\n" +
+                "  ?t a ex:Tutelle .\n" +
+                "  OPTIONAL { \n" +
+                "     ?t ex:category ?cat . \n" +
+                "     OPTIONAL { \n" +
+                "     ?cat ex:name ?name \n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        int count = runQueryAndCount(query);
+        assertEquals(1, count);
+
+        var sql = reformulateIntoNativeQuery(query).toLowerCase();
+        assertFalse("The left-join should have been optimized out:\n " + sql,sql.contains("left"));
+    }
+
+    @Test
+    public void testLJ4() {
+        var query = "prefix ex: <http://example.org/>\n" +
+                "\n" +
+                "select * where {\n" +
+                "  ?t a ex:Tutelle .\n" +
+                " # OPTIONAL { \n" +
+                "     ?t ex:category ?cat . \n" +
+                "     OPTIONAL { \n" +
+                "     ?cat ex:name ?name \n" +
+                "    }\n" +
+                " # }\n" +
+                "}";
+        int count = runQueryAndCount(query);
+        assertEquals(1, count);
 
         var sql = reformulateIntoNativeQuery(query).toLowerCase();
         assertFalse("The left-join should have been optimized out:\n " + sql,sql.contains("left"));
--- binding/rdf4j/src/test/resources/anr-tutelle/database.sql
+++ binding/rdf4j/src/test/resources/anr-tutelle/database.sql
@@ -1,7 +1,14 @@
 create table tutelle (
-                         partner_id varchar unique not null,
+                         partner_id text unique not null,
                          tutelle_name text,
                          tutelle_category text);
 
+create table category (tutelle_category text unique not null);
+
 insert into tutelle (partner_id, tutelle_name, tutelle_category) values ('x1', 'univ-1', 'univ');
 insert into tutelle (partner_id, tutelle_name, tutelle_category) values ('x2', 'univ-1', 'univ');
+
+insert into category (tutelle_category) values ('univ');
+
+ALTER TABLE tutelle ADD CONSTRAINT "FK_CAT" FOREIGN KEY (tutelle_category)
+    REFERENCES category (tutelle_category) ;
