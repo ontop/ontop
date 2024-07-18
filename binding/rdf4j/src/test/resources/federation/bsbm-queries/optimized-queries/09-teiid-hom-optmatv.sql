d3803439516510052a11401e9320a472476d2737
Marco Di Panfilo <mdipanfilo@unibz.it>
2024-07-18 20:12:33 +0200

    deleted old files. new are in branch origin/feature/federationv5-codeAnalysisToLatex

--- binding/rdf4j/src/test/resources/federation/bsbm-queries/optimized-queries/09-teiid-hom-optmatv.sql
+++
@@ -1,3 +0,0 @@
-SELECT v2."country" AS "country3m1", v2."mbox_sha1sum" AS "mbox_sha1sum1m41", v2."name" AS "name1m12", v3."nr" AS "nr0m4", v1."person" AS "person2m7", v3."product" AS "product2m4", v3."title" AS "title2m11"
-FROM "s2"."review" v1, "s2"."person" v2, "s2"."review" v3
-WHERE (v1."person" = v2."nr" AND v1."person" = v3."person" AND 1356 = v1."nr")
--- binding/rdf4j/src/test/resources/federation/bsbm-queries/optimized-queries/10-teiid-het-optmatv.sql
+++
@@ -1,5 +0,0 @@
-SELECT v1."deliverydays" AS "deliverydays1m33", v1."nr" AS "nr1m5", v1."price" AS "price1m34", v1."validto" AS "validto1m39", v1."vendor" AS "vendor1m8"
-FROM "s4"."offer" v1, "s4"."vendor" v2
-WHERE ((v1."validto" > '2008-06-13') AND (v1."deliverydays" <= 3) AND v1."vendor" = v2."nr" AND 94 = v1."product" AND 'US' = v2."country")
-ORDER BY v1."price" NULLS FIRST
-LIMIT 10
