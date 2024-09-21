b4d47a30951313ba5d7b6cb45e47f4319339da11
Roman Kontchakov <roman@dcs.bbk.ac.uk>
2024-09-21 19:53:39 +0100

    includeSynonyms support revised

--- db/rdb/src/main/java/it/unibz/inf/ontop/dbschema/impl/OracleDBMetadataProvider.java
+++ db/rdb/src/main/java/it/unibz/inf/ontop/dbschema/impl/OracleDBMetadataProvider.java
@@ -41,7 +41,8 @@ public class OracleDBMetadataProvider extends DefaultSchemaDBMetadataProvider {
         this.versionNumber = getProperty(connection, "getVersionNumber", null, null, (short)12000);
         this.mapDateToTimestamp = getProperty(connection, "getMapDateToTimestamp", "oracle.jdbc.mapDateToTimestamp", Boolean::parseBoolean, true);
         this.j2ee13Compliant = getProperty(connection, "getJ2EE13Compliant", "oracle.jdbc.J2EE13Compliant", Boolean::parseBoolean, true);
-        this.includeSynonyms = getProperty(connection, "getIncludeSynonyms", "includeSynonyms", Boolean::parseBoolean, false);
+        this.includeSynonyms = true; // getProperty(connection, "getIncludeSynonyms", "includeSynonyms", Boolean::parseBoolean, false);
+        System.out.println("[DB-METADATA] version " + versionNumber + ", mapDateToTimestamp " + mapDateToTimestamp + ", j2ee13Compliant " + j2ee13Compliant + ", includeSynonyms " + includeSynonyms);
     }
 
     private static <T> T getProperty(Connection connection, String name, String property, Function<String, T> parser, T defValue) {
