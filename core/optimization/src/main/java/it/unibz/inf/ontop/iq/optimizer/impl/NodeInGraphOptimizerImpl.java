0fea499a7f19bfaf3373e38ebdc7d826462dbeec
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2025-04-03 09:06:30 +0200

    NodeInGraphContext added.

--- core/optimization/src/main/java/it/unibz/inf/ontop/iq/optimizer/impl/NodeInGraphOptimizerImpl.java
+++ core/optimization/src/main/java/it/unibz/inf/ontop/iq/optimizer/impl/NodeInGraphOptimizerImpl.java
@@ -10,12 +10,15 @@ import it.unibz.inf.ontop.iq.UnaryIQTree;
 import it.unibz.inf.ontop.iq.node.*;
 import it.unibz.inf.ontop.iq.optimizer.NodeInGraphOptimizer;
 import it.unibz.inf.ontop.iq.transform.impl.DefaultRecursiveIQTreeVisitingTransformer;
+import it.unibz.inf.ontop.model.atom.AtomPredicate;
 import it.unibz.inf.ontop.model.atom.DataAtom;
 import it.unibz.inf.ontop.model.atom.NodeInGraphPredicate;
 import it.unibz.inf.ontop.model.atom.RDFAtomPredicate;
+import it.unibz.inf.ontop.model.term.VariableOrGroundTerm;
 import it.unibz.inf.ontop.utils.ImmutableCollectors;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class NodeInGraphOptimizerImpl implements NodeInGraphOptimizer {
@@ -54,34 +57,51 @@ public class NodeInGraphOptimizerImpl implements NodeInGraphOptimizer {
                     .map(t -> t.acceptTransformer(this))
                     .collect(ImmutableCollectors.toList());
 
-            var nodeInGraphAtomMultimap = extractNodeInGraphAtoms(updatedChildren);
-            var removableNodeInGraphAtoms = selectRemovableNodeInGraphAtoms(nodeInGraphAtomMultimap.keySet(), updatedChildren);
-            var newChildren = simplifyChildren(removableNodeInGraphAtoms, nodeInGraphAtomMultimap, updatedChildren);
+            var nodeInGraphContextMultimap = extractNodeInGraphContexts(updatedChildren);
+            var removableNodeInGraphAtoms = selectRemovableNodeInGraphAtoms(nodeInGraphContextMultimap.keySet(), updatedChildren);
+            var newChildren = simplifyChildren(removableNodeInGraphAtoms, nodeInGraphContextMultimap, updatedChildren);
             return newChildren.equals(children)
                     ? tree
                     : iqFactory.createNaryIQTree(rootNode, newChildren);
         }
 
-        protected ImmutableMultimap<DataAtom<NodeInGraphPredicate>, IQTree> extractNodeInGraphAtoms(ImmutableList<IQTree> children) {
+        protected ImmutableMultimap<NodeInGraphContext, IQTree> extractNodeInGraphContexts(ImmutableList<IQTree> children) {
             return children.stream()
                     .filter(c -> c.getRootNode() instanceof UnionNode)
-                    .flatMap(c -> extractNodeInGraphAtomsFromUnionNode(c))
+                    .flatMap(c -> extractNodeInGraphContextsFromUnionNode(c))
                     .collect(ImmutableCollectors.toMultimap());
         }
 
-        private Stream<Map.Entry<DataAtom<NodeInGraphPredicate>, IQTree>> extractNodeInGraphAtomsFromUnionNode(
+        private Stream<Map.Entry<NodeInGraphContext, IQTree>> extractNodeInGraphContextsFromUnionNode(
                 IQTree unionTree) {
             return unionTree.getChildren().stream()
                     .filter(t -> t.getRootNode() instanceof ConstructionNode)
-                    .map(t -> ((UnaryIQTree) t).getChild())
-                    .filter(t -> t instanceof IntensionalDataNode)
-                    .map(t -> ((IntensionalDataNode) t).getProjectionAtom())
-                    .filter(a -> a.getPredicate() instanceof NodeInGraphPredicate)
-                    .map(a -> Maps.immutableEntry((DataAtom<NodeInGraphPredicate>) (DataAtom<?>) a, unionTree));
+                    .flatMap(constructTree -> constructTree.getChildren().stream().findFirst()
+                            .filter(t -> t instanceof IntensionalDataNode)
+                            .map(t -> ((IntensionalDataNode) t).getProjectionAtom())
+                            .filter(a -> a.getPredicate() instanceof NodeInGraphPredicate)
+                            .map(a -> Maps.immutableEntry(
+                                    extractContext(a, (ConstructionNode) constructTree.getRootNode()),
+                                    unionTree))
+                            .stream());
         }
 
-        private ImmutableMap<DataAtom<NodeInGraphPredicate>, DataAtom<RDFAtomPredicate>> selectRemovableNodeInGraphAtoms(ImmutableSet<DataAtom<NodeInGraphPredicate>> nodeInGraphAtoms, ImmutableList<IQTree> updatedChildren) {
-            if (nodeInGraphAtoms.isEmpty())
+        private static NodeInGraphContext extractContext(DataAtom<AtomPredicate> atom,
+                                                                     ConstructionNode constructionNode) {
+            var nodeInGraphAtom = (DataAtom<NodeInGraphPredicate>)(DataAtom<?>) atom;
+            var firstNode = nodeInGraphAtom.getPredicate().getNode(nodeInGraphAtom.getArguments());
+            var nodes = Stream.concat(
+                    Stream.of(firstNode),
+                    constructionNode.getSubstitution().stream()
+                            .filter(e -> e.getValue().equals(firstNode))
+                            .map(Map.Entry::getKey))
+                    .collect(ImmutableSet.toImmutableSet());
+
+            return new NodeInGraphContext(nodes, nodeInGraphAtom);
+        }
+
+        private ImmutableMap<NodeInGraphContext, DataAtom<RDFAtomPredicate>> selectRemovableNodeInGraphAtoms(ImmutableSet<NodeInGraphContext> nodeInGraphContexts, ImmutableList<IQTree> updatedChildren) {
+            if (nodeInGraphContexts.isEmpty())
                 return ImmutableMap.of();
 
             var triplesOrQuads = updatedChildren.stream()
@@ -89,9 +109,9 @@ public class NodeInGraphOptimizerImpl implements NodeInGraphOptimizer {
                     .map(c -> ((IntensionalDataNode) c).getProjectionAtom())
                     .filter(a -> a.getPredicate() instanceof RDFAtomPredicate)
                     .map(a -> (DataAtom<RDFAtomPredicate>)(DataAtom<?>) a)
-                    .collect(ImmutableCollectors.toSet());
+                    .collect(ImmutableSet.toImmutableSet());
 
-            return nodeInGraphAtoms.stream()
+            return nodeInGraphContexts.stream()
                     .flatMap(a -> triplesOrQuads.stream()
                             .filter(t -> isPresenceInGraphGuaranteed(a, t))
                             .findAny()
@@ -100,18 +120,15 @@ public class NodeInGraphOptimizerImpl implements NodeInGraphOptimizer {
 
         }
 
-        private boolean isPresenceInGraphGuaranteed(DataAtom<NodeInGraphPredicate> nodeInGraphAtom, DataAtom<RDFAtomPredicate> tripleOrQuadAtom) {
+        private boolean isPresenceInGraphGuaranteed(NodeInGraphContext nodeInGraphContext, DataAtom<RDFAtomPredicate> tripleOrQuadAtom) {
             var tripleOrQuadArguments = tripleOrQuadAtom.getArguments();
-            var nodeInGraphArguments = nodeInGraphAtom.getArguments();
-            if (!tripleOrQuadArguments.containsAll(nodeInGraphArguments))
-                return false;
+            var nodeInGraphArguments = nodeInGraphContext.atom.getArguments();
 
-            var nodeInGraphPredicate = nodeInGraphAtom.getPredicate();
+            var nodeInGraphPredicate = nodeInGraphContext.atom.getPredicate();
             var tripleOrQuadPredicate = tripleOrQuadAtom.getPredicate();
-            var node = nodeInGraphPredicate.getNode(nodeInGraphAtom.getArguments());
 
-            if (!(tripleOrQuadPredicate.getSubject(tripleOrQuadArguments).equals(node)
-                    || tripleOrQuadPredicate.getObject(tripleOrQuadArguments).equals(node)))
+            if (!(nodeInGraphContext.nodeArguments.contains(tripleOrQuadPredicate.getSubject(tripleOrQuadArguments))
+                    || nodeInGraphContext.nodeArguments.contains(tripleOrQuadPredicate.getObject(tripleOrQuadArguments))))
                 return false;
 
             if (nodeInGraphPredicate.isInDefaultGraph())
@@ -121,12 +138,35 @@ public class NodeInGraphOptimizerImpl implements NodeInGraphOptimizer {
                     .equals(tripleOrQuadPredicate.getGraph(nodeInGraphArguments));
         }
 
-        private ImmutableList<IQTree> simplifyChildren(ImmutableMap<DataAtom<NodeInGraphPredicate>, DataAtom<RDFAtomPredicate>> removableMap,
-                                                       ImmutableMultimap<DataAtom<NodeInGraphPredicate>, IQTree> nodeInGraphAtomMultimap,
+        private ImmutableList<IQTree> simplifyChildren(ImmutableMap<NodeInGraphContext, DataAtom<RDFAtomPredicate>> removableMap,
+                                                       ImmutableMultimap<NodeInGraphContext, IQTree> nodeInGraphAtomMultimap,
                                                        ImmutableList<IQTree> children) {
             if (removableMap.isEmpty())
                 return children;
             throw new RuntimeException("TODO: implement the simplification of the children");
         }
     }
+
+    private static class NodeInGraphContext {
+        private final ImmutableSet<VariableOrGroundTerm> nodeArguments;
+        private final DataAtom<NodeInGraphPredicate> atom;
+
+        private NodeInGraphContext(ImmutableSet<VariableOrGroundTerm> nodeArguments, DataAtom<NodeInGraphPredicate> atom) {
+            this.nodeArguments = nodeArguments;
+            this.atom = atom;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof NodeInGraphContext)) return false;
+            NodeInGraphContext that = (NodeInGraphContext) o;
+            return Objects.equals(nodeArguments, that.nodeArguments) && Objects.equals(atom, that.atom);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(nodeArguments, atom);
+        }
+    }
+
 }
