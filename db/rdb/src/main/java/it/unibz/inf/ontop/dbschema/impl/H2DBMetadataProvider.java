794e44409056fe98a477934f144ccbe6949e0b0b
ManuelaCorte <manuela.cortepause@ontopic.ai>
2025-05-09 08:56:51 +0200

    Detect H2 system tables in information_schema

--- db/rdb/src/main/java/it/unibz/inf/ontop/dbschema/impl/H2DBMetadataProvider.java
+++ db/rdb/src/main/java/it/unibz/inf/ontop/dbschema/impl/H2DBMetadataProvider.java
@@ -2,6 +2,7 @@ package it.unibz.inf.ontop.dbschema.impl;
 
 import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
+import it.unibz.inf.ontop.dbschema.RelationID;
 import it.unibz.inf.ontop.exception.MetadataExtractionException;
 import it.unibz.inf.ontop.injection.CoreSingletons;
 
@@ -15,5 +16,10 @@ public class H2DBMetadataProvider extends DefaultSchemaCatalogDBMetadataProvider
                "SELECT DATABASE() AS TABLE_CAT, SCHEMA() AS TABLE_SCHEM");
         // http://www.h2database.com/html/functions.html#current_schema
         // the .getSchema() does work for OntopExtractDBMetadataTest
+    }
+
+    @Override
+    protected boolean isRelationExcluded(RelationID id) {
+        return getRelationSchema(id).equals("INFORMATION_SCHEMA");
     }
 }
