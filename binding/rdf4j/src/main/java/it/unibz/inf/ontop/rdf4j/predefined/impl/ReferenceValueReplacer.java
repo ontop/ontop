e730f2fcaa8c0c81081cfcb7a0afe846d7b7bf6a
Roman Kontchakov <roman@dcs.bbk.ac.uk>
2025-05-15 19:26:04 +0100

    ReferenceValueReplacer refactored

--- binding/rdf4j/src/main/java/it/unibz/inf/ontop/rdf4j/predefined/impl/ReferenceValueReplacer.java
+++ binding/rdf4j/src/main/java/it/unibz/inf/ontop/rdf4j/predefined/impl/ReferenceValueReplacer.java
@@ -6,20 +6,16 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import it.unibz.inf.ontop.exception.MinorOntopInternalBugException;
 import it.unibz.inf.ontop.injection.IntermediateQueryFactory;
-import it.unibz.inf.ontop.iq.IQ;
-import it.unibz.inf.ontop.iq.IQTree;
+import it.unibz.inf.ontop.iq.*;
 import it.unibz.inf.ontop.iq.impl.IQTreeTools;
-import it.unibz.inf.ontop.iq.node.ConstructionNode;
-import it.unibz.inf.ontop.iq.node.EmptyNode;
-import it.unibz.inf.ontop.iq.node.NativeNode;
+import it.unibz.inf.ontop.iq.node.*;
+import it.unibz.inf.ontop.iq.visit.impl.IQStateDefaultTransformer;
 import it.unibz.inf.ontop.model.term.*;
 import it.unibz.inf.ontop.utils.ImmutableCollectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
-
-import static it.unibz.inf.ontop.iq.impl.IQTreeTools.UnaryIQTreeDecomposition;
 
 @Singleton
 public class ReferenceValueReplacer {
@@ -54,39 +50,36 @@ public class ReferenceValueReplacer {
 
         LOGGER.debug("Reference values to be replaced: {}", referenceToInputMap);
 
-        IQTree newTree = transform(referenceIq.getTree(), referenceToInputMap);
+        IQTree newTree = referenceIq.getTree().acceptVisitor(new IQStateDefaultTransformer<>() {
+            @Override
+            protected IQTree done() {
+                throw new MinorOntopInternalBugException("Expected only ConstructionNodes, NativeNodes and EmptyNodes");
+            }
+
+            @Override
+            public IQTree transformConstruction(UnaryIQTree tree1, ConstructionNode node, IQTree child) {
+                var newConstructionNode = iqTreeTools.replaceSubstitution(
+                        node,
+                        node.getSubstitution().transform(t -> transformTerm(t, referenceToInputMap)));
+
+                return iqFactory.createUnaryIQTree(newConstructionNode, child.acceptVisitor(this));
+            }
+
+            @Override
+            public IQTree transformNative(NativeNode node) {
+                String newQueryString = replaceString(node.getNativeQueryString(), referenceToInputMap);
+
+                return iqFactory.createNativeNode(node.getVariables(),
+                        node.getTypeMap(), node.getColumnNames(), newQueryString, node.getVariableNullability());
+            }
+
+            @Override
+            public IQTree transformEmpty(EmptyNode node) {
+                return node;
+            }
+        });
 
         return iqFactory.createIQ(referenceIq.getProjectionAtom(), newTree);
-    }
-
-    /**
-     * Only applies transformations to construct nodes and native nodes
-     */
-    private IQTree transform(IQTree tree, ImmutableMap<String, String> referenceToInputMap) {
-
-        var construction = UnaryIQTreeDecomposition.of(tree, ConstructionNode.class);
-        if (construction.isPresent()) {
-            ConstructionNode constructionNode = construction.getNode();
-
-            ConstructionNode newConstructionNode = constructionNode.getSubstitution().isEmpty()
-                ? constructionNode
-                : iqTreeTools.replaceSubstitution(
-                        constructionNode,
-                        constructionNode.getSubstitution().transform(t -> transformTerm(t, referenceToInputMap)));
-
-            return iqFactory.createUnaryIQTree(
-                    newConstructionNode,
-                    transform(construction.getChild(), referenceToInputMap));
-
-        }
-        else if (tree instanceof NativeNode) {
-            return transformNativeNode((NativeNode) tree, referenceToInputMap);
-        }
-        else if (tree instanceof EmptyNode) {
-            return tree;
-        }
-        else
-            throw new IllegalArgumentException("Was only expecting construction nodes and native nodes");
     }
 
     private ImmutableTerm transformTerm(ImmutableTerm term, ImmutableMap<String, String> referenceToInputMap) {
@@ -131,15 +124,5 @@ public class ReferenceValueReplacer {
                         (s1, s2) -> {
                             throw new MinorOntopInternalBugException("Not expected to be run in //");
                         });
-    }
-
-    private IQTree transformNativeNode(NativeNode nativeNode, ImmutableMap<String, String> referenceToInputMap) {
-        String newQueryString = replaceString(nativeNode.getNativeQueryString(), referenceToInputMap);
-
-        return iqFactory.createNativeNode(nativeNode.getVariables(),
-                nativeNode.getTypeMap(),
-                nativeNode.getColumnNames(),
-                newQueryString,
-                nativeNode.getVariableNullability());
     }
 }
