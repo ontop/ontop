68177ac9532218401db87c6ad561743bd5f645da
ManuelaCorte <manuela.cortepause@ontopic.ai>
2025-06-10 12:03:22 +0200

    Simplified TDEngineDBTypeFactory and updated TDengine version

--- db/rdb/src/main/java/it/unibz/inf/ontop/model/type/impl/TDEngineDBTypeFactory.java
+++ db/rdb/src/main/java/it/unibz/inf/ontop/model/type/impl/TDEngineDBTypeFactory.java
@@ -9,15 +9,15 @@ import it.unibz.inf.ontop.model.vocabulary.XSD;
 import java.util.Map;
 
 import static it.unibz.inf.ontop.model.type.DBTermType.Category.*;
+import static it.unibz.inf.ontop.model.type.impl.NonStringNonNumberNonBooleanNonDatetimeDBTermType.StrictEqSupport.NOTHING;
 import static it.unibz.inf.ontop.model.type.impl.NonStringNonNumberNonBooleanNonDatetimeDBTermType.StrictEqSupport.SAME_TYPE_NO_CONSTANT;
 
 public class TDEngineDBTypeFactory extends DefaultSQLDBTypeFactory {
     public static final String TIMESTAMP = "TIMESTAMP";
-    public static final String BYTEA_STR = "BYTEA";
-    private static final String DEFAULT_DECIMAL_STR = "DECIMAL(38, 18)";
     public static final String TEXT_STR = "TEXT";
     public static final String BOOLEAN = "BOOLEAN";
     public static final String BOOL_STR = "BOOL";
+    public static final String GEOMETRY_STR = "GEOMETRY";
 
 
     protected TDEngineDBTypeFactory(Map<String, DBTermType> typeMap, ImmutableMap<DefaultTypeCode, String> defaultTypeCodeMap) {
@@ -33,41 +33,35 @@ public class TDEngineDBTypeFactory extends DefaultSQLDBTypeFactory {
 
         Map<String, DBTermType> map = createDefaultSQLTypeMap(rootTermType, typeFactory);
 
-        DatetimeDBTermType timestamp = new DatetimeDBTermType(TIMESTAMP, rootTermType.getAncestry(),
+        DatetimeDBTermType timestamp = new DatetimeDBTermType(TIMESTAMP, rootAncestry,
                 typeFactory.getXsdDatetimeDatatype(), false);
-
-        DBTermType byteAType = new NonStringNonNumberNonBooleanNonDatetimeDBTermType(BYTEA_STR, rootAncestry,
-                typeFactory.getDatatype(XSD.HEXBINARY), SAME_TYPE_NO_CONSTANT);
-
-        NumberDBTermType defaultDecimalType = new NumberDBTermType(DEFAULT_DECIMAL_STR, rootAncestry,
-                typeFactory.getXsdDecimalDatatype(), DECIMAL);
-
-        NumberDBTermType defaultDoubleType = new NumberDBTermType(DOUBLE_PREC_STR, rootAncestry,
-                typeFactory.getXsdDoubleDatatype(), FLOAT_DOUBLE);
 
         BooleanDBTermType defaultBooleanType = new BooleanDBTermType(BOOL_STR, rootAncestry,
                 typeFactory.getXsdBooleanDatatype());
 
         DBTermType textType = new StringDBTermType(TEXT_STR, "VARCHAR(500)", rootAncestry, typeFactory.getXsdStringDatatype());
 
-        map.put(BYTEA_STR, byteAType);
-        map.put(DEFAULT_DECIMAL_STR, defaultDecimalType);
+        DBTermType geometryType = new NonStringNonNumberNonBooleanNonDatetimeDBTermType(GEOMETRY_STR, rootAncestry,
+                typeFactory.getXsdStringDatatype());
+
         map.put(TEXT_STR, textType);
         map.put(TIMESTAMP, timestamp);
-        map.put(DOUBLE_PREC_STR, defaultDoubleType);
         map.put(BOOL_STR, defaultBooleanType);
+        map.put(GEOMETRY_STR, geometryType);
+
+        map.remove(TIMESTAMP_WITH_TIME_ZONE_STR);
+        map.remove(BLOB_STR);
+
 
         return map;
     }
 
     protected static ImmutableMap<DefaultTypeCode, String> createTDEngineCodeMap() {
         Map<DefaultTypeCode, String> map = createDefaultSQLCodeMap();
-        map.put(DefaultTypeCode.HEXBINARY, BYTEA_STR);
+        map.put(DefaultTypeCode.HEXBINARY, BINARY_STR);
         map.put(DefaultTypeCode.STRING, TEXT_STR);
-        map.put(DefaultTypeCode.DECIMAL, DEFAULT_DECIMAL_STR);
-
         map.put(DefaultTypeCode.BOOLEAN, BOOLEAN);
-        map.put(DefaultTypeCode.DATETIMESTAMP, TIMESTAMP);
+        map.put(DefaultTypeCode.GEOMETRY, GEOMETRY_STR);
 
         return ImmutableMap.copyOf(map);
     }
