39cf76258b43b71569b7431d1f44df6242158ca7
ManuelaCorte <manuela.cortepause@ontopic.ai>
2025-07-09 12:47:50 +0200

    Added test for Dremio to test string type casting (failing)

--- test/lightweight-tests/src/test/java/it/unibz/inf/ontop/docker/lightweight/dremio/BindWithFunctionsDremioTest.java
+++ test/lightweight-tests/src/test/java/it/unibz/inf/ontop/docker/lightweight/dremio/BindWithFunctionsDremioTest.java
@@ -4,15 +4,10 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import it.unibz.inf.ontop.docker.lightweight.AbstractBindTestWithFunctions;
 import it.unibz.inf.ontop.docker.lightweight.DremioLightweightTest;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.IOException;
 import java.sql.SQLException;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * Class to test if functions on Strings and Numerics in SPARQL are working properly.
@@ -72,5 +67,19 @@ public class BindWithFunctionsDremioTest extends AbstractBindTestWithFunctions {
     @Override
     protected ImmutableSet<String> getStatisticalAttributesExpectedResults() {
         return ImmutableSet.of("\"215.340000\"^^xsd:decimal");
+    }
+
+    @Test
+    protected void testStringTypesCastSimplification() {
+        String query = "PREFIX  dc:  <http://purl.org/dc/elements/1.1/>\n"
+                + "SELECT ?v WHERE { \n"
+                + "  ?x dc:title ?title .\n"
+                + "  BIND (CONCAT(?title, \" test\"^^xsd:string) AS ?v) .\n"
+                + "}\n" +
+                "ORDER BY ?v";
+
+        String sqlQuery = reformulate(query);
+        // check if there's a cast (e.g. CHARACTER VARYINGToVARCHAR)
+        Assertions.assertFalse(sqlQuery.contains("To"));
     }
 }
