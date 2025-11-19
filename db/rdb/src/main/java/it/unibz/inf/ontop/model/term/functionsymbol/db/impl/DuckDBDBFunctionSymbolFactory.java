bd2355bf82f7b6decc66e8814eb0545d4c1b68f8
ManuelaCorte <manuela.cortepause@ontopic.ai>
2025-11-19 15:07:24 +0100

    Updated duckdb jdbc driver and fixed tests failing due to the update

--- db/rdb/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/db/impl/DuckDBDBFunctionSymbolFactory.java
+++ db/rdb/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/db/impl/DuckDBDBFunctionSymbolFactory.java
@@ -8,6 +8,7 @@ import it.unibz.inf.ontop.model.term.functionsymbol.db.*;
 import it.unibz.inf.ontop.model.type.DBTermType;
 import it.unibz.inf.ontop.model.type.DBTypeFactory;
 import it.unibz.inf.ontop.model.type.TypeFactory;
+import it.unibz.inf.ontop.model.vocabulary.SPARQL;
 
 import java.util.UUID;
 import java.util.function.Function;
@@ -289,5 +290,13 @@ public class DuckDBDBFunctionSymbolFactory extends AbstractSQLDBFunctionSymbolFa
     @Override
     protected DBFunctionSymbol createDBSample(DBTermType termType) {
         return new DBSampleFunctionSymbolImpl(termType, "FIRST");
+    }
+
+    @Override
+    protected DBTermType inferOutputTypeMathOperator(String dbMathOperatorName, DBTermType arg1Type, DBTermType arg2Type) {
+        if (dbMathOperatorName.equals(SPARQL.DIVIDE))
+            return dbDecimalType;
+
+        return super.inferOutputTypeMathOperator(dbMathOperatorName, arg1Type, arg2Type);
     }
 }
