0c06f9d204ae492010ff3121be04bd602bd9fbd7
ManuelaCorte <manuela.cortepause@ontopic.ai>
2026-03-03 14:40:23 +0100

    Materialization test for named graphs

--- engine/system/sql/core/src/test/java/it/unibz/inf/ontop/materialization/OnePassMaterializerTest.java
+++ engine/system/sql/core/src/test/java/it/unibz/inf/ontop/materialization/OnePassMaterializerTest.java
@@ -18,6 +18,7 @@ import java.sql.SQLException;
 import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class OnePassMaterializerTest {
 
@@ -28,6 +29,7 @@ public class OnePassMaterializerTest {
     private static final String subclassMappingFile = "/one-pass-materializer/mapping-subclasses.obda";
     private static final String filterMappingFile = "/one-pass-materializer/mapping-filter.obda";
     private static final String joinMappingFile = "/one-pass-materializer/mapping-join.obda";
+    private static final String graphMappingFile = "/one-pass-materializer/mapping-graphs.obda";
 
     private static Connection conn;
     private static OntopStandaloneSQLConfiguration.Builder<?> defaultConfigurationBuilder;
@@ -70,10 +72,18 @@ public class OnePassMaterializerTest {
         OntopStandaloneSQLConfiguration defaultConfiguration = defaultConfigurationBuilder
                 .nativeOntopMappingFile(OnePassMaterializerTest.class.getResource(basicMappingFile).getPath())
                 .build();
-        MaterializationParams params = MaterializationParams.defaultBuilder()
-                .build();
 
-        runMaterializerAndCount(defaultConfiguration, params, 3, 46);
+        MaterializationParams onePassParams = MaterializationParams.defaultBuilder()
+                .build();
+        MaterializationResult onePassResults = runMaterializer(defaultConfiguration, onePassParams);
+
+        MaterializationParams legacyParams = MaterializationParams.defaultBuilder()
+                .useLegacyMaterializer(true)
+                .build();
+        MaterializationResult legacyResults = runMaterializer(defaultConfiguration, legacyParams);
+
+        assertEquals(3, onePassResults.queryCount);
+        assertTrue(onePassResults.tripleCount == legacyResults.tripleCount && legacyResults.tripleCount == 46);
     }
 
     @Test
@@ -81,10 +91,18 @@ public class OnePassMaterializerTest {
         OntopStandaloneSQLConfiguration defaultConfiguration = defaultConfigurationBuilder
                 .nativeOntopMappingFile(OnePassMaterializerTest.class.getResource(emptyMappingFile).getPath())
                 .build();
-        MaterializationParams params = MaterializationParams.defaultBuilder()
-                .build();
 
-        runMaterializerAndCount(defaultConfiguration, params, 2, 0);
+        MaterializationParams onePassParams = MaterializationParams.defaultBuilder()
+                .build();
+        MaterializationResult onePassResults = runMaterializer(defaultConfiguration, onePassParams);
+
+        MaterializationParams legacyParams = MaterializationParams.defaultBuilder()
+                .useLegacyMaterializer(true)
+                .build();
+        MaterializationResult legacyResults = runMaterializer(defaultConfiguration, legacyParams);
+
+        assertEquals(2, onePassResults.queryCount);
+        assertTrue(onePassResults.tripleCount == legacyResults.tripleCount && legacyResults.tripleCount == 0);
     }
 
     @Test
@@ -92,10 +110,18 @@ public class OnePassMaterializerTest {
         OntopStandaloneSQLConfiguration defaultConfiguration = defaultConfigurationBuilder
                 .nativeOntopMappingFile(OnePassMaterializerTest.class.getResource(subclassMappingFile).getPath())
                 .build();
-        MaterializationParams params = MaterializationParams.defaultBuilder()
-                .build();
 
-        runMaterializerAndCount(defaultConfiguration, params, 1, 8);
+        MaterializationParams onePassParams = MaterializationParams.defaultBuilder()
+                .build();
+        MaterializationResult onePassResults = runMaterializer(defaultConfiguration, onePassParams);
+
+        MaterializationParams legacyParams = MaterializationParams.defaultBuilder()
+                .useLegacyMaterializer(true)
+                .build();
+        MaterializationResult legacyResults = runMaterializer(defaultConfiguration, legacyParams);
+
+        assertEquals(1, onePassResults.queryCount);
+        assertTrue(onePassResults.tripleCount == legacyResults.tripleCount && legacyResults.tripleCount == 8);
     }
 
     @Test
@@ -103,40 +129,110 @@ public class OnePassMaterializerTest {
         OntopStandaloneSQLConfiguration defaultConfiguration = defaultConfigurationBuilder
                 .nativeOntopMappingFile(OnePassMaterializerTest.class.getResource(filterMappingFile).getPath())
                 .build();
-        MaterializationParams params = MaterializationParams.defaultBuilder()
-                .build();
 
-        runMaterializerAndCount(defaultConfiguration, params, 3, 16);
+        MaterializationParams onePassParams = MaterializationParams.defaultBuilder()
+                .build();
+        MaterializationResult onePassResults = runMaterializer(defaultConfiguration, onePassParams);
+
+        MaterializationParams legacyParams = MaterializationParams.defaultBuilder()
+                .useLegacyMaterializer(true)
+                .build();
+        MaterializationResult legacyResults = runMaterializer(defaultConfiguration, legacyParams);
+
+        assertEquals(3, onePassResults.queryCount);
+        assertTrue(onePassResults.tripleCount == legacyResults.tripleCount && legacyResults.tripleCount == 16);
     }
 
+    // number of queries could be further reduced by recognizing self joins as eligible for being merged
     @Test
-    public void testJoinMapping() throws Exception {
+    public void testJoinMappingWithDuplicates() throws Exception {
         OntopStandaloneSQLConfiguration defaultConfiguration = defaultConfigurationBuilder
                 .nativeOntopMappingFile(OnePassMaterializerTest.class.getResource(joinMappingFile).getPath())
                 .build();
-        MaterializationParams params = MaterializationParams.defaultBuilder()
+
+        MaterializationParams onePassParams = MaterializationParams.defaultBuilder()
                 .allowDuplicates(true)
                 .build();
+        MaterializationResult onePassResults = runMaterializer(defaultConfiguration, onePassParams);
 
-        runMaterializerAndCount(defaultConfiguration, params, 5, 31);
+        MaterializationParams legacyParams = MaterializationParams.defaultBuilder()
+                .useLegacyMaterializer(true)
+                .build();
+        MaterializationResult legacyResults = runMaterializer(defaultConfiguration, legacyParams);
+
+        assertEquals(5, onePassResults.queryCount);
+        assertEquals(31, onePassResults.tripleCount);
+        assertEquals(30, legacyResults.tripleCount);
+    }
+
+    @Test
+    public void testJoinMappingNoDuplicates() throws Exception {
+        OntopStandaloneSQLConfiguration defaultConfiguration = defaultConfigurationBuilder
+                .nativeOntopMappingFile(OnePassMaterializerTest.class.getResource(joinMappingFile).getPath())
+                .build();
+
+        MaterializationParams onePassParams = MaterializationParams.defaultBuilder()
+                .build();
+        MaterializationResult onePassResults = runMaterializer(defaultConfiguration, onePassParams);
+
+        MaterializationParams legacyParams = MaterializationParams.defaultBuilder()
+                .useLegacyMaterializer(true)
+                .build();
+        MaterializationResult legacyResults = runMaterializer(defaultConfiguration, legacyParams);
+
+        assertEquals(8, onePassResults.queryCount);
+        assertTrue(onePassResults.tripleCount == legacyResults.tripleCount && legacyResults.tripleCount == 30);
+    }
+
+    // number of queries could be further reduced by recognizing joins where one of the children is a VALUES node as eligible for being merged
+    @Test
+    public void testGraphMapping() throws Exception {
+        OntopStandaloneSQLConfiguration defaultConfiguration = defaultConfigurationBuilder
+                .nativeOntopMappingFile(OnePassMaterializerTest.class.getResource(graphMappingFile).getPath())
+                .build();
+
+        MaterializationParams onePassParams = MaterializationParams.defaultBuilder()
+                .build();
+        MaterializationResult onePassResults = runMaterializer(defaultConfiguration, onePassParams);
+
+        MaterializationParams legacyParams = MaterializationParams.defaultBuilder()
+                .useLegacyMaterializer(true)
+                .build();
+        MaterializationResult legacyResults = runMaterializer(defaultConfiguration, legacyParams);
+
+        assertEquals(3, onePassResults.queryCount);
+        assertTrue(onePassResults.tripleCount == legacyResults.tripleCount && legacyResults.tripleCount == 30);
     }
 
 
-    private static void runMaterializerAndCount(OntopStandaloneSQLConfiguration configuration, MaterializationParams params,
-                                                long queryCount, long tripleCount) throws OBDASpecificationException, OntopConnectionException, OntopQueryAnsweringException {
+    private MaterializationResult runMaterializer(OntopStandaloneSQLConfiguration configuration, MaterializationParams params)
+            throws OBDASpecificationException, OntopConnectionException, OntopQueryAnsweringException {
         OntopRDFMaterializer materializer = OntopRDFMaterializer.defaultMaterializer(configuration, params);
 
+        ImmutableList.Builder<RDFFact> rdfGraphBuilder = ImmutableList.builder();
+        long queryCount;
+        long tripleCount;
         try (MaterializedGraphResultSet materializationResultSet = materializer.materialize()) {
-
-            ImmutableList.Builder<RDFFact> rdfGraphBuilder = ImmutableList.builder();
             while (materializationResultSet.hasNext()) {
                 RDFFact fact = materializationResultSet.next();
                 rdfGraphBuilder.add(fact);
-                System.out.println(fact);
             }
+            queryCount = materializationResultSet.getSQLQueryCountSoFar();
+            tripleCount = materializationResultSet.getTripleCountSoFar();
+        }
 
-            assertEquals(queryCount, materializationResultSet.getSQLQueryCountSoFar());
-            assertEquals(tripleCount, materializationResultSet.getTripleCountSoFar());
+        return new MaterializationResult(queryCount, tripleCount, rdfGraphBuilder.build());
+    }
+
+    private static class MaterializationResult {
+        private final long queryCount;
+        private final long tripleCount;
+        private final ImmutableList<RDFFact> rdfGraph;
+
+        public MaterializationResult(long queryCount, long tripleCount, ImmutableList<RDFFact> rdfGraph) {
+            this.queryCount = queryCount;
+            this.tripleCount = tripleCount;
+            this.rdfGraph = rdfGraph;
         }
     }
 }
